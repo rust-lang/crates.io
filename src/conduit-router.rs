f60ef4b95814df37fb83e0fcb08c7d50bdae0778
Yehuda Katz <wycats@gmail.com>
2014-06-26 22:31:35 -0700

    Router is now a conduit handler

--- src/conduit-router.rs
+++ src/conduit-router.rs
@@ -5,8 +5,10 @@ extern crate router = "route_recognizer";
 extern crate conduit;
 
 use std::collections::HashMap;
+use std::any::Any;
+
 use router::{Router, Match};
-use conduit::{Method, Handler};
+use conduit::{Method, Handler, Request, Response};
 
 pub struct RouteBuilder<E> {
     routers: HashMap<Method, Router<Box<Handler<E>>>>
@@ -76,51 +78,107 @@ impl<E> RouteBuilder<E> {
     }
 }
 
+impl<E> conduit::Handler<E> for RouteBuilder<E> {
+    fn call(&self, request: &mut Request) -> Result<Response, E> {
+        let m = {
+            let method = request.method();
+            let path = request.path();
+
+            self.recognize(&method, path).unwrap()
+        };
+
+        {
+            let extensions = request.mut_extensions();
+            extensions.insert("router.params", box m.params.clone() as Box<Any>);
+        }
+
+        (*m.handler).call(request)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     extern crate semver;
     use std::io::net::ip::IpAddr;
     use std::collections::HashMap;
     use std::io::MemReader;
+    use std::any::{Any, AnyRefExt};
     use super::*;
-    use conduit;
-    use conduit::{Method, Scheme, Host, Headers, Extensions};
 
-    struct RequestSentinel;
+    use router::Params;
+    use conduit;
+    use conduit::{Handler, Method, Scheme, Host, Headers, Extensions};
+
+    struct RequestSentinel {
+        method: Method,
+        path: String,
+        extensions: HashMap<&'static str, Box<Any>>
+    }
+
+    impl RequestSentinel {
+        fn new(method: Method, path: &'static str) -> RequestSentinel {
+            RequestSentinel {
+                path: path.to_str(),
+                extensions: HashMap::new(),
+                method: method
+            }
+        }
+    }
 
     impl conduit::Request for RequestSentinel {
         fn http_version(&self) -> semver::Version { unimplemented!() }
         fn conduit_version(&self) -> semver::Version { unimplemented!() }
-        fn method(&self) -> Method { unimplemented!() }
+        fn method(&self) -> Method { self.method }
         fn scheme(&self) -> Scheme { unimplemented!() }
         fn host<'a>(&'a self) -> Host<'a> { unimplemented!() }
         fn virtual_root<'a>(&'a self) -> Option<&'a str> { unimplemented!() }
-        fn path<'a>(&'a self) -> &'a str { unimplemented!() }
+        fn path<'a>(&'a self) -> &'a str {
+            self.path.as_slice()
+        }
         fn query_string<'a>(&'a self) -> Option<&'a str> { unimplemented!() }
         fn remote_ip(&self) -> IpAddr { unimplemented!() }
         fn content_length(&self) -> Option<uint> { unimplemented!() }
         fn headers<'a>(&'a self) -> &'a Headers { unimplemented!() }
         fn body<'a>(&'a mut self) -> &'a mut Reader { unimplemented!() }
-        fn extensions<'a>(&'a self) -> &'a Extensions { unimplemented!() }
-        fn mut_extensions<'a>(&'a mut self) -> &'a mut Extensions { unimplemented!() }
+        fn extensions<'a>(&'a self) -> &'a Extensions {
+            &self.extensions
+        }
+        fn mut_extensions<'a>(&'a mut self) -> &'a mut Extensions {
+            &mut self.extensions
+        }
     }
 
     #[test]
-    fn basic_test() {
+    fn as_conduit_handler() {
         let mut router = RouteBuilder::new();
+        router.post("/posts/:id", handler1);
         router.get("/posts/:id", handler1);
 
-        let m = router.recognize(&conduit::Get, "/posts/1").unwrap();
-        let res = (*m.handler).call(&mut RequestSentinel).unwrap();
+        let mut req = RequestSentinel::new(conduit::Get, "/posts/1");
+        let mut res = router.call(&mut req).unwrap();
 
         assert_eq!(res.status, (200, "OK"));
+        assert_eq!(res.body.read_to_str().unwrap(), "1, Get".to_str());
+
+        let mut req = RequestSentinel::new(conduit::Post, "/posts/10");
+        let mut res = router.call(&mut req).unwrap();
+
+        assert_eq!(res.status, (200, "OK"));
+        assert_eq!(res.body.read_to_str().unwrap(), "10, Post".to_str());
     }
 
-    fn handler1(_: &mut conduit::Request) -> Result<conduit::Response, ()> {
+    fn handler1(req: &mut conduit::Request) -> Result<conduit::Response, ()> {
+        let params = req.extensions().find(&"router.params")
+            .and_then(|a| a.as_ref::<Params>()).expect("Missing params");
+
+        let mut res = vec!();
+        res.push(params["id"]);
+        res.push(format!("{}", req.method()));
+
         Ok(conduit::Response {
             status: (200, "OK"),
             headers: HashMap::new(),
-            body: box MemReader::new(vec!())
+            body: box MemReader::new(res.connect(", ").into_bytes())
         })
     }
 }
