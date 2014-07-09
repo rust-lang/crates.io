cffcbce48d360851bca8f65382bb89c1c3c80a62
Alex Crichton <alex@alexcrichton.com>
2014-07-09 08:39:20 -0700

    Add some simple error handling...

--- app/controllers/package/edit.js
+++ app/controllers/package/edit.js
@@ -1,10 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.ObjectController.extend({
+  needs: ['application'],
   actions: {
     updatePackage: function() {
-      this.get('model').save();
-      this.transitionTo('package', this.get('model'));
+      var self = this;
+      this.get('model').save().then(function(pkg) {
+        self.transitionToRoute('package', pkg);
+      }).catch(function(reason) {
+        self.get('model').rollback();
+        var msg;
+        if (reason.status === 403) {
+          msg = "A login is required to perform that action";
+        } else {
+          msg = "An unknown error occurred";
+        }
+        self.controllerFor('application').setFlashError(msg);
+        self.transitionToRoute('index');
+      });
     }
   }
 });
