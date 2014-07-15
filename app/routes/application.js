c61d3db72c290e72168a317ba702685f124b9315
Alex Crichton <alex@alexcrichton.com>
2014-07-15 07:36:43 -0700

    Use ic-ajax for catch()

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,13 +1,14 @@
 import Ember from 'ember';
+import ajax from 'ic-ajax';
 
 export default Ember.Route.extend({
   beforeModel: function() {
     var self = this;
     if (this.session.get('isLoggedIn') &&
         this.session.get('currentUser') === null) {
-      return Ember.$.getJSON('/me').then(function(d) {
+      return ajax('/me').then(function(d) {
         self.session.set('currentUser', d.user);
-      }).fail(function() {
+      }).catch(function() {
         self.session.logoutUser();
       });
     }
