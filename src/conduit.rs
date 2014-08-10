4630b441946407bcf98069f0a5677ec9f059f714
Alex Crichton <alex@alexcrichton.com>
2014-08-09 22:15:31 -0700

    Update to master

--- src/conduit.rs
+++ src/conduit.rs
@@ -114,11 +114,11 @@ pub struct Response {
 
 /// A Handler takes a request and returns a response or an error.
 /// By default, a bare function implements `Handler`.
-pub trait Handler : Share + Send {
+pub trait Handler : Sync + Send {
     fn call(&self, request: &mut Request) -> Result<Response, Box<Show>>;
 }
 
-impl<T: Send + Share + Show> Handler for fn(&mut Request) -> Result<Response, T> {
+impl<T: Send + Sync + Show> Handler for fn(&mut Request) -> Result<Response, T> {
     fn call(&self, request: &mut Request) -> Result<Response, Box<Show>> {
         { (*self)(request) }.map_err(|e| box e as Box<Show>)
     }
