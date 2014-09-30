8e95671a6eeac7e6d867637cdb4765629db6db58
Alex Crichton <alex@alexcrichton.com>
2014-09-30 11:17:58 -0700

    Depend on moment.js for time utilities

--- Brocfile.js
+++ Brocfile.js
@@ -17,4 +17,6 @@ var app = new EmberApp();
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 
+app.import('vendor/moment/moment.js');
+
 module.exports = app.toTree();
