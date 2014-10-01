3c8afc6223fb7d1400aed1248d286b20c7a2d9dd
Alex Crichton <alex@alexcrichton.com>
2014-10-01 10:43:43 -0700

    Add some graphs

--- app/controllers/crate.js
+++ app/controllers/crate.js
@@ -37,7 +37,36 @@ export default Ember.ObjectController.extend({
         },
 
         renderChart: function(downloads) {
-            // TODO: actually render a chart
+            var dates = {};
+            for (var i = 0; i < 90; i++) {
+                var now = moment().subtract(i, 'days');
+                dates[now.format('MMM D')] = {date: now, cnt: 0};
+            }
+            downloads.forEach(function(d) {
+                var key = moment(d.get('date')).utc().format('MMM D');
+                if (dates[key]) {
+                    dates[key].cnt += d.get('downloads');
+                }
+            });
+
+            var data = [['Date', this.get('currentVersion').get('num')]];
+            for (var date in dates) {
+                data.push([dates[date].date.toDate(), dates[date].cnt]);
+            }
+
+            data = google.visualization.arrayToDataTable(data);
+            var el = document.getElementById('graph-data');
+            var chart = new google.visualization.LineChart(el);
+            chart.draw(data, {
+                chartArea: {'width': '80%', 'height': '80%'},
+                hAxis: {
+                    minorGridlines: { count: 8 },
+                },
+                vAxis: {
+                    minorGridlines: { count: 5 },
+                    viewWindow: { min: 0, },
+                },
+            });
         },
     },
 });
