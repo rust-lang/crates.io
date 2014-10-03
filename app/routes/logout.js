8687c73a018a135b55afe83f42b86ffcb2dfc1a6
Alex Crichton <alex@alexcrichton.com>
2014-10-02 21:05:29 -0700

    Don't redirect on login

--- app/routes/logout.js
+++ app/routes/logout.js
@@ -1,11 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  activate: function() {
-    var self = this;
-    Ember.$.getJSON('/logout', function() {
-      self.session.logoutUser();
-      self.transitionTo('index');
-    });
-  }
+    activate: function() {
+        var self = this;
+        Ember.$.getJSON('/logout', function() {
+            self.session.logoutUser();
+            self.transitionTo('index');
+        });
+    }
 });
