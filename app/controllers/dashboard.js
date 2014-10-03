5230b4769a98d6f125b2c68469873ef07802bd99
Alex Crichton <alex@alexcrichton.com>
2014-10-03 15:06:57 -0700

    Implement latest updates on dashboard

--- app/controllers/dashboard.js
+++ app/controllers/dashboard.js
@@ -1,12 +1,17 @@
 import Ember from 'ember';
+import ajax from 'ic-ajax';
 
 var TO_SHOW = 5;
 
 export default Ember.ObjectController.extend({
     fetchingCrates: true,
     fetchingFollowing: true,
+    fetchingFeed: true,
+    loadingMore: false,
+    hasMore: false,
     myCrates: [],
     myFollowing: [],
+    myFeed: [],
 
     visibleCrates: function() {
         return this.get('myCrates').slice(0, TO_SHOW);
@@ -23,4 +28,29 @@ export default Ember.ObjectController.extend({
     hasMoreFollowing: function() {
         return this.get('myFollowing').length > TO_SHOW;
     }.property('myFollowing'),
+
+    actions: {
+        loadMore: function() {
+            var self = this;
+            this.set('loadingMore', true);
+            var page = (this.get('myFeed').length / 10) + 1;
+            ajax('/me/updates?page=' + page).then(function(data) {
+                // Wow, I sure wish I knew why none of this works like it should!
+                //
+                // TODO: fix this so we don't have to push everything in and
+                //       then re-find the things
+                self.store.pushMany('crate', data.crates);
+                var versions = self.store.pushMany('version', data.versions);
+                versions.forEach(function(v) {
+                    self.store.find('crate', v._data.crate_id).then(function(c) {
+                        v.set('crate', c);
+                    });
+                });
+                self.get('myFeed').pushObjects(versions);
+                self.set('hasMore', data.meta.more);
+            }).finally(function() {
+                self.set('loadingMore', false);
+            });
+        },
+    },
 });
--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -7,23 +7,24 @@ export default Ember.Route.extend(AuthenticatedRoute, {
         var self = this;
         controller.set('fetchingCrates', true);
         controller.set('fetchingFollowing', true);
+        controller.set('fetchingFeed', true);
         ajax('/crates?user_id=' + model.get('id')).then(function(data) {
-            data.crates = Ember.A(data.crates);
-            data.crates.forEach(function(crate, i, arr) {
-                arr[i] = self.store.push('crate', crate);
-            });
-            controller.set('myCrates', data.crates);
+            controller.set('myCrates',
+                           self.store.pushMany('crate', data.crates));
         }).finally(function() {
             controller.set('fetchingCrates', false);
         });
+
         ajax('/crates?following=1').then(function(data) {
-            Ember.A(data.crates).forEach(function(crate, i, arr) {
-                arr[i] = self.store.push('crate', crate);
-            });
-            controller.set('myFollowing', data.crates);
+            controller.set('myFollowing',
+                           self.store.pushMany('crate', data.crates));
         }).finally(function() {
             controller.set('fetchingFollowing', false);
         });
+
+        if (controller.get('myFeed').length == 0) {
+            controller.send('loadMore');
+        }
     },
 
     model: function() {
