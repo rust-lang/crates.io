735a9c08e36be7d2f0ca21038c2b26191f7ec68b
Alex Crichton <alex@alexcrichton.com>
2014-10-07 08:35:53 -0700

    Update to rust master

--- examples/simple-file-server.rs
+++ examples/simple-file-server.rs
@@ -1,6 +1,3 @@
-extern crate green;
-extern crate rustuv;
-
 extern crate civet;
 extern crate conduit;
 extern crate "conduit-static" as conduit_static;
@@ -13,23 +10,6 @@ use conduit_static::Static;
 fn main() {
     let handler = Static::new(os::getcwd());
     let _a = Server::start(Config { port: 8888, threads: 50 }, handler);
-
-    wait_for_sigint();
-}
-
-fn wait_for_sigint() {
-    use green::{SchedPool, PoolConfig, GreenTaskBuilder};
-    use std::io::signal::{Listener, Interrupt};
-    use std::task::TaskBuilder;
-
-    let mut config = PoolConfig::new();
-    config.event_loop_factory = rustuv::event_loop;
-
-    let mut pool = SchedPool::new(config);
-    TaskBuilder::new().green(&mut pool).spawn(proc() {
-        let mut l = Listener::new();
-        l.register(Interrupt).unwrap();
-        l.rx.recv();
-    });
-    pool.shutdown();
+    let (_tx, rx) = channel::<()>();
+    rx.recv();
 }
