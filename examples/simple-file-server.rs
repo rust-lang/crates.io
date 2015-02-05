854ba4051cb8ffc44893ef7224e18833d527f805
Alex Crichton <alex@alexcrichton.com>
2015-02-05 08:32:34 -0800

    Update to rust master

--- examples/simple-file-server.rs
+++ examples/simple-file-server.rs
@@ -1,17 +1,17 @@
-#![feature(os)]
+#![feature(env)]
 
 extern crate civet;
 extern crate conduit;
 extern crate "conduit-static" as conduit_static;
 
-use std::os;
+use std::env;
 use std::sync::mpsc::channel;
 
 use civet::{Config, Server};
 use conduit_static::Static;
 
 fn main() {
-    let handler = Static::new(os::getcwd().unwrap());
+    let handler = Static::new(env::current_dir().unwrap());
     let _a = Server::start(Config { port: 8888, threads: 50 }, handler);
     let (_tx, rx) = channel::<()>();
     rx.recv().unwrap();
