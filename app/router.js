2080ed20cec760cc670e6d5740af7576f2de6526
Alex Crichton <alex@alexcrichton.com>
2015-02-10 22:41:58 -0800

    Actually use the analytics mixing

--- app/router.js
+++ app/router.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 import config from './config/environment';
 import googlePageview from 'cargo/mixins/google-pageview';
 
-var Router = Ember.Router.extend({
+var Router = Ember.Router.extend(googlePageview, {
   location: config.locationType
 });
 
