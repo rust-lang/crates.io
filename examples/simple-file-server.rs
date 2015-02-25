9fbebc6eb42acf51f1b18c956d17d043822ac7aa
Alex Crichton <alex@alexcrichton.com>
2015-02-24 16:55:51 -0800

    Bump to 0.7.0

--- examples/simple-file-server.rs
+++ examples/simple-file-server.rs
@@ -1,4 +1,4 @@
-#![feature(env)]
+#![feature(env, old_path)]
 
 extern crate civet;
 extern crate conduit;
@@ -11,7 +11,7 @@ use civet::{Config, Server};
 use conduit_static::Static;
 
 fn main() {
-    let handler = Static::new(env::current_dir().unwrap());
+    let handler = Static::new(env::current_dir().unwrap().as_str().unwrap());
     let _a = Server::start(Config { port: 8888, threads: 50 }, handler);
     let (_tx, rx) = channel::<()>();
     rx.recv().unwrap();
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
-#![feature(old_io, old_path, core)]
+#![feature(io, path, core, fs)]
 #![cfg_attr(test, deny(warnings))]
+#![cfg_attr(test, feature(old_io, old_path))]
 
 extern crate conduit;
 extern crate "conduit-mime-types" as mime;
@@ -7,20 +8,23 @@ extern crate time;
 
 use std::collections::HashMap;
 use std::error::Error;
-use std::old_io::FileType;
-use std::old_io::fs::File;
-use std::old_io::util::NullReader;
+use std::io;
+use std::io::prelude::*;
+use std::path::{PathBuf, AsPath};
+use std::fs::File;
 use conduit::{Request, Response, Handler};
 
+#[cfg(test)] mod tempdir;
+
 pub struct Static {
-    path: Path,
+    path: PathBuf,
     types: mime::Types
 }
 
 impl Static {
-    pub fn new(path: Path) -> Static {
+    pub fn new<P: AsPath + ?Sized>(path: &P) -> Static {
         Static {
-            path: path,
+            path: path.as_path().to_path_buf(),
             types: mime::Types::new()
                 .ok().expect("Couldn't load mime-types")
         }
@@ -30,31 +34,28 @@ impl Static {
 impl Handler for Static {
     fn call(&self, request: &mut Request) -> Result<Response, Box<Error+Send>> {
         let request_path = &request.path()[1..];
+        if request_path.contains("..") { return Ok(not_found()) }
+
         let path = self.path.join(request_path);
-
-        if !self.path.is_ancestor_of(&path) {
-            return Ok(not_found())
-        }
-
         let mime = self.types.mime_for_path(&path);
         let file = match File::open(&path) {
             Ok(f) => f,
             Err(..) => return Ok(not_found()),
         };
-        let stat = try!(file.stat().map_err(|e| Box::new(e) as Box<Error+Send>));
-        match stat.kind {
-            FileType::Directory => return Ok(not_found()),
-            _ => {}
+        let data = try!(file.metadata().map_err(|e| Box::new(e) as Box<Error+Send>));
+        if data.is_dir() {
+            return Ok(not_found())
         }
         let ts = time::Timespec {
-            sec: (stat.modified as i64) / 1000,
-            nsec: (((stat.modified as u32) % 1000) as i32) * 1000000
+            sec: (data.modified() as i64) / 1000,
+            nsec: (((data.modified() as u32) % 1000) as i32) * 1000000
         };
         let tm = time::at(ts).to_utc();
 
         let mut headers = HashMap::new();
         headers.insert("Content-Type".to_string(), vec![mime.to_string()]);
-        headers.insert("Content-Length".to_string(), vec![stat.size.to_string()]);
+        headers.insert("Content-Length".to_string(),
+                       vec![data.len().to_string()]);
         headers.insert("Last-Modified".to_string(),
                        vec![tm.strftime("%a, %d %b %Y %T GMT").unwrap()
                               .to_string()]);
@@ -71,7 +72,7 @@ fn not_found() -> Response {
     Response {
         status: (404, "Not Found"),
         headers: HashMap::new(),
-        body: Box::new(NullReader),
+        body: Box::new(io::empty()),
     }
 }
 
@@ -79,7 +80,9 @@ fn not_found() -> Response {
 mod tests {
     extern crate "conduit-test" as test;
 
-    use std::old_io::{fs, File, TempDir, USER_RWX};
+    use std::fs::{self, File};
+    use std::io::prelude::*;
+    use tempdir::TempDir;
 
     use conduit::{Handler, Method};
     use Static;
@@ -89,11 +92,13 @@ mod tests {
         let td = TempDir::new("conduit-static").unwrap();
         let root = td.path();
         let handler = Static::new(root.clone());
-        File::create(&root.join("Cargo.toml")).write_all(b"[package]").unwrap();
+        File::create(&root.join("Cargo.toml")).unwrap()
+             .write_all(b"[package]").unwrap();
         let mut req = test::MockRequest::new(Method::Get, "/Cargo.toml");
         let mut res = handler.call(&mut req).ok().expect("No response");
-        let body = res.body.read_to_string().ok().expect("No body");
-        assert_eq!(body.as_slice(), "[package]");
+        let mut body = String::new();
+        res.body.read_to_string(&mut body).unwrap();
+        assert_eq!(body, "[package]");
         assert_eq!(res.headers.get("Content-Type"),
                    Some(&vec!("text/plain".to_string())));
         assert_eq!(res.headers.get("Content-Length"),
@@ -104,7 +109,7 @@ mod tests {
     fn test_mime_types() {
         let td = TempDir::new("conduit-static").unwrap();
         let root = td.path();
-        fs::mkdir(&root.join("src"), USER_RWX).unwrap();
+        fs::create_dir(&root.join("src")).unwrap();
         File::create(&root.join("src/fixture.css")).unwrap();
 
         let handler = Static::new(root.clone());
@@ -132,7 +137,7 @@ mod tests {
         let td = TempDir::new("conduit-static").unwrap();
         let root = td.path();
 
-        fs::mkdir(&root.join("foo"), USER_RWX).unwrap();
+        fs::create_dir(&root.join("foo")).unwrap();
 
         let handler = Static::new(root.clone());
         let mut req = test::MockRequest::new(Method::Get, "/foo");
