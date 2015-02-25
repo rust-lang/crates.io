af6f4b5cfa46bfbec8af368190e436d0dd611a08
Alex Crichton <alex@alexcrichton.com>
2015-02-24 16:28:29 -0800

    Bump to 0.7.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 #![feature(core)]
-#![cfg_attr(test, feature(old_io))]
+#![cfg_attr(test, feature(io, net))]
 
 extern crate conduit;
 
@@ -20,30 +20,30 @@ pub trait Middleware: Send + Sync + 'static {
 }
 
 pub trait AroundMiddleware: Handler {
-    fn with_handler(&mut self, handler: Box<Handler + Send + Sync>);
+    fn with_handler(&mut self, handler: Box<Handler>);
 }
 
 pub struct MiddlewareBuilder {
-    middlewares: Vec<Box<Middleware + Send + Sync>>,
-    handler: Option<Box<Handler + Send + Sync>>
+    middlewares: Vec<Box<Middleware>>,
+    handler: Option<Box<Handler>>
 }
 
 impl MiddlewareBuilder {
     pub fn new<H: Handler>(handler: H) -> MiddlewareBuilder {
         MiddlewareBuilder {
             middlewares: vec!(),
-            handler: Some(Box::new(handler) as Box<Handler + Send + Sync>)
+            handler: Some(Box::new(handler) as Box<Handler>)
         }
     }
 
     pub fn add<M: Middleware>(&mut self, middleware: M) {
-        self.middlewares.push(Box::new(middleware) as Box<Middleware + Send + Sync>);
+        self.middlewares.push(Box::new(middleware) as Box<Middleware>);
     }
 
     pub fn around<M: AroundMiddleware>(&mut self, mut middleware: M) {
         let handler = self.handler.take().unwrap();
         middleware.with_handler(handler);
-        self.handler = Some(Box::new(middleware) as Box<Handler + Send + Sync>);
+        self.handler = Some(Box::new(middleware) as Box<Handler>);
     }
 }
 
@@ -92,8 +92,9 @@ mod tests {
 
     use std::collections::HashMap;
     use std::error::Error;
-    use std::old_io::net::ip::IpAddr;
-    use std::old_io::{self, MemReader};
+    use std::io::{self, Cursor};
+    use std::io::prelude::*;
+    use std::net::IpAddr;
 
     use conduit;
     use conduit::{Request, Response, Host, Headers, Method, Scheme, Extensions};
@@ -129,7 +130,7 @@ mod tests {
         fn remote_ip(&self) -> IpAddr { unimplemented!() }
         fn content_length(&self) -> Option<u64> { unimplemented!() }
         fn headers<'a>(&'a self) -> &'a Headers { unimplemented!() }
-        fn body<'a>(&'a mut self) -> &'a mut Reader { unimplemented!() }
+        fn body<'a>(&'a mut self) -> &'a mut Read { unimplemented!() }
         fn extensions<'a>(&'a self) -> &'a Extensions {
             &self.extensions
         }
@@ -158,7 +159,7 @@ mod tests {
                 Ok(Response {
                     status: (500, "Internal Server Error"),
                     headers: HashMap::new(),
-                    body: Box::new(MemReader::new(e.into_bytes()))
+                    body: Box::new(Cursor::new(e.into_bytes()))
                 })
             })
         }
@@ -168,7 +169,7 @@ mod tests {
 
     impl Middleware for ProducesError {
         fn before(&self, _: &mut Request) -> Result<(), Box<Error+Send>> {
-            Err(Box::new(old_io::standard_error(old_io::OtherIoError)) as Box<Error+Send>)
+            Err(Box::new(io::Error::new(io::ErrorKind::Other, "", None)))
         }
     }
 
@@ -181,13 +182,13 @@ mod tests {
             Ok(Response {
                 status: (200, "OK"),
                 headers: HashMap::new(),
-                body: Box::new(MemReader::new(vec!()))
+                body: Box::new(Cursor::new(vec!()))
             })
         }
     }
 
     struct MyAroundMiddleware {
-        handler: Option<Box<Handler + Send + Sync>>
+        handler: Option<Box<Handler>>
     }
 
     impl MyAroundMiddleware {
@@ -199,7 +200,7 @@ mod tests {
     impl Middleware for MyAroundMiddleware {}
 
     impl AroundMiddleware for MyAroundMiddleware {
-        fn with_handler(&mut self, handler: Box<Handler + Send + Sync>) {
+        fn with_handler(&mut self, handler: Box<Handler>) {
             self.handler = Some(handler)
         }
     }
@@ -219,24 +220,20 @@ mod tests {
         Response {
             status: (200, "OK"),
             headers: HashMap::new(),
-            body: Box::new(MemReader::new(string.into_bytes()))
+            body: Box::new(Cursor::new(string.into_bytes()))
         }
     }
 
-    fn handler(req: &mut Request) -> Result<Response, old_io::IoError> {
+    fn handler(req: &mut Request) -> io::Result<Response> {
         let hello = get_extension::<String>(req);
         Ok(response(hello.clone()))
     }
 
-    fn error_handler(_: &mut Request) -> Result<Response, old_io::IoError> {
-        Err(old_io::IoError {
-            kind: old_io::OtherIoError,
-            desc: "Error in handler",
-            detail: None,
-        })
+    fn error_handler(_: &mut Request) -> io::Result<Response> {
+        Err(io::Error::new(io::ErrorKind::Other, "Error in handler", None))
     }
 
-    fn middle_handler(req: &mut Request) -> Result<Response, old_io::IoError> {
+    fn middle_handler(req: &mut Request) -> io::Result<Response> {
         let hello = get_extension::<String>(req);
         let middle = get_extension::<String>(req);
 
@@ -251,7 +248,9 @@ mod tests {
         let mut req = RequestSentinel::new(Method::Get, "/");
         let mut res = builder.call(&mut req).ok().expect("No response");
 
-        assert_eq!(res.body.read_to_string().ok().expect("No body"), "hello".to_string());
+        let mut s = String::new();
+        res.body.read_to_string(&mut s).unwrap();
+        assert_eq!(s, "hello".to_string());
     }
 
     #[test]
@@ -277,7 +276,9 @@ mod tests {
         let mut res = builder.call(&mut req).ok().expect("Error not handled");
 
         assert_eq!(res.status, (500, "Internal Server Error"));
-        assert_eq!(res.body.read_to_string().ok().expect("No body"), "Error in handler".to_string());
+        let mut s = String::new();
+        res.body.read_to_string(&mut s).unwrap();
+        assert_eq!(s, "Error in handler".to_string());
     }
 
     #[test]
@@ -289,6 +290,8 @@ mod tests {
         let mut req = RequestSentinel::new(Method::Get, "/");
         let mut res = builder.call(&mut req).ok().expect("No response");
 
-        assert_eq!(res.body.read_to_string().ok().expect("No body"), "hello hello".to_string());
+        let mut s = String::new();
+        res.body.read_to_string(&mut s).unwrap();
+        assert_eq!(s, "hello hello".to_string());
     }
 }
