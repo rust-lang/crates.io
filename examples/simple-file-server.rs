61dffd995f67b9cfe68fdf8e27d73ed1a0f72ddb
Alex Crichton <alex@alexcrichton.com>
2015-02-26 10:50:35 -0800

    Update to rust master

--- examples/simple-file-server.rs
+++ examples/simple-file-server.rs
@@ -11,7 +11,7 @@ use civet::{Config, Server};
 use conduit_static::Static;
 
 fn main() {
-    let handler = Static::new(env::current_dir().unwrap().as_str().unwrap());
+    let handler = Static::new(&env::current_dir().unwrap());
     let _a = Server::start(Config { port: 8888, threads: 50 }, handler);
     let (_tx, rx) = channel::<()>();
     rx.recv().unwrap();
