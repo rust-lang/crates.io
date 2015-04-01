a7211a80a82fbd7365d3891fa0227d612ab31431
Gabor Nagy <mail@aigeruth.hu>
2015-04-01 13:09:58 +0200

    Add an example

---
+++ example/src/main.rs
@@ -0,0 +1,39 @@
+extern crate civet;
+extern crate conduit;
+extern crate conduit_router;
+
+use std::collections::HashMap;
+use std::io::{self, Cursor};
+use std::sync::mpsc::channel;
+
+use civet::{Config, response, Server};
+use conduit::{Request, Response};
+use conduit_router::{RouteBuilder, RequestParams};
+
+
+fn name(req: &mut Request) -> io::Result<Response> {
+    let name = req.params().find("name").unwrap();
+    let bytes = format!("Hello {}!", name).into_bytes();
+
+    Ok(response(200, HashMap::new(), Cursor::new(bytes)))
+}
+
+
+fn hello(_req: &mut Request) -> io::Result<Response> {
+    let hello = "Hello world!".to_string().into_bytes();
+    Ok(response(200, HashMap::new(), Cursor::new(hello)))
+}
+
+
+fn main() {
+    let mut router = RouteBuilder::new();
+
+    router.get("/", hello);
+    router.get("/:name", name);
+
+    let _server = Server::start(Config { port: 8888, threads: 1 }, router);
+
+    // Preventing process exit.
+    let (_tx, rx) = channel::<()>();
+    rx.recv().unwrap();
+}
