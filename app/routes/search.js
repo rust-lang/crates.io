5996b8a4534bc2dc663683be06dafc957ec7fc11
Matt Brubeck <mbrubeck@limpet.net>
2015-05-21 15:54:57 -0700

    Give the search results page a title

--- app/routes/search.js
+++ app/routes/search.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    title: Ember.computed.reads("controller.name"),
+
     queryParams: {
         q: { refreshModel: true },
         page: { refreshModel: true },
