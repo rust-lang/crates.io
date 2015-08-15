e62e56e45905f355649ba150e63a9f1da237041b
Alexis Beingessner <a.beingessner@gmail.com>
2015-08-14 17:39:13 -0700

    cleanup http

--- src/http.rs
+++ src/http.rs
@@ -2,6 +2,7 @@ use curl;
 use oauth2::*;
 use app::App;
 
+/// Does all the nonsense for sending a GET to Github
 pub fn github(app: &App, url: &str, auth: &Token)
     -> Result<curl::http::Response, curl::ErrCode> {
     println!("HTTP: {}", url);
@@ -13,6 +14,8 @@ pub fn github(app: &App, url: &str, auth: &Token)
      .exec()
 }
 
+/// Gets a token with the given string as the access token, but all
+/// other info null'd out. Generally, just to be fed to the `github` fn.
 pub fn token(token: String) -> Token {
     Token {
         access_token: token,
