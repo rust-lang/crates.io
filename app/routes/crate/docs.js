c07c455506768b8f799607841b396ea77d66036b
Dirk Gadsden <dirk@esherido.com>
2015-11-03 23:14:31 -0800

    Show flash message before redirecting to crate's documentation

--- app/routes/crate/docs.js
+++ app/routes/crate/docs.js
@@ -2,23 +2,28 @@ import Ember from 'ember';
 import Crate from 'cargo/models/crate';
 
 export default Ember.Route.extend({
-		afterModel: function(data) {
+    setupController: function(controller, data) {
         var crate;
 
-				if (data instanceof Crate) {
-						crate = data;
-				} else {
-						crate = data.crate;
-				}
-
-        var documentation = crate.get('documentation');
-
-        if (documentation) {
-            window.location = documentation;
+        if (data instanceof Crate) {
+            crate = data;
         } else {
-            // Redirect to the crate's main page if no documentation
-            // URL is found.
-            this.transitionTo('crate', crate);
+            crate = data.crate;
         }
-		}
+
+        var documentation = crate.get('documentation'),
+            self          = this;
+
+        setTimeout(function() {
+            if (documentation) {
+                window.location = documentation;
+            } else {
+                // Redirect to the crate's main page if no documentation
+                // URL is found.
+                this.transitionTo('crate', crate);
+            }
+        }, 2500);
+
+        this._super(controller, crate);
+    },
 });
