fccf81de8a39abe8751848570d35dfb513ca01b6
Tobias Bieniek <tobias.bieniek@gmx.de>
2015-11-04 12:04:24 +0100

    templates/crates: Replace deprecated {{view 'select'}} with emberx-select
    see http://emberjs.com/deprecations/v1.x/#toc_ember-select

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,4 +1,6 @@
 import resolver from './helpers/resolver';
+import registerSelectHelper from './helpers/register-select-helper';
+registerSelectHelper();
 import {
   setResolver
 } from 'ember-qunit';
