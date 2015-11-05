a8a16b9aad03d2b96327016b8b9ef3e8240fbdcd
Tobias Bieniek <tobias.bieniek@gmx.de>
2015-11-05 11:07:47 +0100

    routes/github-login: Add documentation

--- app/routes/github-login.js
+++ app/routes/github-login.js
@@ -1,6 +1,19 @@
 import Ember from 'ember';
 import ajax from 'ic-ajax';
 
+/**
+ * Calling this route will query the `/authorize_url` API endpoint
+ * and redirect to the received URL to initiate the OAuth flow.
+ *
+ * Example URL:
+ * https://github.com/login/oauth/authorize?client_id=...&state=...&scope=read%3Aorg
+ *
+ * Once the user has allowed the OAuth flow access the page will redirect him
+ * to the `/github_authorize` route of this application.
+ *
+ * @see https://developer.github.com/v3/oauth/#redirect-users-to-request-github-access
+ * @see `/github_authorize` route
+ */
 export default Ember.Route.extend({
   beforeModel: function() {
     return ajax('/authorize_url').then(function(url) {
