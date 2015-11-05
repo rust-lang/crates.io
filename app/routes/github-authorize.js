df3b9182f75266d8da1a4dc671472d4e2a0a7712
Tobias Bieniek <tobias.bieniek@gmx.de>
2015-11-05 11:18:49 +0100

    routes/github-authorize: Add documentation

--- app/routes/github-authorize.js
+++ app/routes/github-authorize.js
@@ -1,6 +1,19 @@
 import Ember from 'ember';
 import ajax from 'ic-ajax';
 
+/**
+ * This route will be called from the GitHub OAuth flow once the user has
+ * accepted or rejected the data access permissions. It will forward the
+ * temporary `code` received from the GitHub API to our own API server which
+ * will exchange it for an access token.
+ *
+ * After the exchange the API will return an API token and the user information.
+ * The result will be stored and the popup window closed. The `/login` route
+ * will then continue to evaluate the response.
+ *
+ * @see https://developer.github.com/v3/oauth/#github-redirects-back-to-your-site
+ * @see `/login` route
+ */
 export default Ember.Route.extend({
     beforeModel: function(transition) {
         return ajax('/authorize', {data: transition.queryParams}).then(function(d) {
