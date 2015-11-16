4a7292495125c1d3a034d209ae1491f0b7a69354
Tobias Bieniek <tobias.bieniek@gmx.de>
2015-11-16 10:02:44 +0100

    Ember: Add "crate" serializer
    This serializer will filter `versions: null` and remove it from the hash because in our case `null` means unknown instead of empty.

---
+++ app/serializers/crate.js
@@ -0,0 +1,13 @@
+import DS from 'ember-data';
+
+export default DS.RESTSerializer.extend({
+    isNewSerializerAPI: true,
+
+    extractRelationships(modelClass, resourceHash) {
+        if (resourceHash.versions == null) {
+            delete resourceHash.versions;
+        }
+
+        return this._super(modelClass, resourceHash);
+    }
+});
