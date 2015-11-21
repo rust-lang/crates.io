943a6face1f8e6e17a326b1e305516a91f7f3a9c
Tobias Bieniek <tobias.bieniek@gmx.de>
2015-11-21 20:14:06 +0100

    routes/crate/reverse-dep: Simplify route code
    Just use modelFor() 😉

--- app/routes/crate/reverse-dependencies.js
+++ app/routes/crate/reverse-dependencies.js
@@ -1,44 +1,19 @@
 import Ember from 'ember';
-import Crate from 'cargo/models/crate';
 
 export default Ember.Route.extend({
     queryParams: {
         page: { refreshModel: true },
     },
 
-    crate: null,
-    reverse_dependencies: null,
-    params: null,
-
-    model(params, transition) {
-        this.set('params', params);
-        return this._super(params, transition);
-    },
-
-    afterModel(data) {
-        var crate;
-        if (data instanceof Crate) {
-            crate = data;
-        } else {
-            crate = data.crate;
-        }
-
-        var params = this.get('params');
+    model(params) {
         params.reverse = true;
-        params.crate = crate;
+        params.crate = this.modelFor('crate').crate;
 
-        return this.store.query('dependency', params).then((deps) => {
-            var controller = this.controllerFor('crate/reverse_dependencies');
-            if (controller) {
-                controller.set('model', deps);
-            }
-            this.set('reverse_dependencies', deps);
-            this.set('crate', crate);
-        });
+        return this.store.query('dependency', params);
     },
 
-    setupController(controller) {
-        this._super(controller, this.get('reverse_dependencies'));
-        controller.set('crate', this.get('crate'));
-    }
+    setupController(controller, model) {
+        controller.set('crate', this.modelFor('crate').crate);
+        this._super(controller, model);
+    },
 });
