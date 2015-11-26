4cb3e6b360a120af18071cac8be8cb01431b2366
Sean Griffin <sean@seantheprogrammer.com>
2015-11-26 10:56:11 -0700

    Rely on the database to verify data integrity of versions
    This one is particularly interesting, as `updated_at` would never
    actually change

--- src/bin/migrate.rs
+++ src/bin/migrate.rs
@@ -540,6 +540,10 @@ fn migrations() -> Vec<Migration> {
             CREATE TRIGGER trigger_crates_set_updated_at BEFORE UPDATE
             ON crates
             FOR EACH ROW EXECUTE PROCEDURE set_updated_at();
+
+            CREATE TRIGGER trigger_versions_set_updated_at BEFORE UPDATE
+            ON versions
+            FOR EACH ROW EXECUTE PROCEDURE set_updated_at();
             "));
             Ok(())
 
@@ -571,6 +575,7 @@ fn migrations() -> Vec<Migration> {
 
             DROP TRIGGER trigger_crate_owners_set_updated_at ON crate_owners;
             DROP TRIGGER trigger_crates_set_updated_at ON crates;
+            DROP TRIGGER trigger_versions_set_updated_at ON versions;
 
             DROP FUNCTION set_updated_at();
             "));
