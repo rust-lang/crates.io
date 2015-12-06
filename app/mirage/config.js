d7f7f077e2ddfcd36a1b6d1613964a945a3eca84
Tobias Bieniek <tobias.bieniek@gmx.de>
2015-12-06 21:45:48 +0100

    Ember: Add "search" acceptance test

--- app/mirage/config.js
+++ app/mirage/config.js
@@ -1,5 +1,17 @@
 import summaryFixture from '../mirage/fixtures/summary';
+import searchFixture from '../mirage/fixtures/search';
 
 export default function() {
     this.get('/summary', () => summaryFixture);
+    this.get('/api/v1/crates', (db, request) => {
+        if (request.queryParams.q) {
+            const page = parseInt(request.queryParams.page);
+            const perPage = parseInt(request.queryParams.per_page);
+            const start = (page - 1) * perPage;
+            return {
+                crates: searchFixture.crates.slice(start, start + perPage),
+                meta: searchFixture.meta,
+            };
+        }
+    });
 }
---
+++ tests/acceptance/search-test.js
@@ -0,0 +1,42 @@
+import { test } from 'qunit';
+import moduleForAcceptance from 'cargo/tests/helpers/module-for-acceptance';
+
+moduleForAcceptance('Acceptance | search');
+
+test('searching for "rust"', function(assert) {
+    visit('/');
+    fillIn('input.search', 'rust');
+    andThen(function() {
+        findWithAssert('form.search').submit();
+    });
+    wait();
+
+    andThen(function() {
+        assert.equal(currentURL(), '/search?q=rust');
+        assert.equal(document.title, 'Search Results for \'rust\' - Cargo');
+
+        findWithAssert('a[href="/search?page=2&q=rust"]');
+        assert.notOk(find('a[href="/search?page=3&q=rust"]')[0]);
+
+        hasText(assert, '#crates-heading', 'Search Results for \'rust\'');
+        hasText(assert, '#results', 'Displaying 1-10 of 18 total results');
+
+        hasText(assert, '#crates .row:first .desc .info', 'rust_mixin 0.0.1');
+        hasText(assert, '#crates .row:first .desc .summary', 'Yo dawg, use Rust to generate Rust, right in your Rust. (See `external_mixin` to use scripting languages.)');
+        hasText(assert, '#crates .row:first .downloads', '477');
+    });
+    click('a[href="/search?page=2&q=rust"]');
+
+    andThen(function() {
+        assert.equal(currentURL(), '/search?page=2&q=rust');
+        assert.equal(document.title, 'Search Results for \'rust\' - Cargo');
+
+        findWithAssert('a[href="/search?q=rust"]');
+        assert.notOk(find('a[href="/search?page=3&q=rust"]')[0]);
+
+        hasText(assert, '#crates-heading', 'Search Results for \'rust\'');
+        hasText(assert, '#results', 'Displaying 11-18 of 18 total results');
+
+        hasText(assert, '#crates .row:first .desc .info', 'rusted_cypher 0.7.1');
+    });
+});
