1cd09af94e186d89bcc2e416d67a5f039cc92836
Tobias Bieniek <tobias.bieniek@gmx.de>
2015-12-06 20:38:27 +0100

    Ember/tests: Extract hasText test helper

--- tests/acceptance/front-page-test.js
+++ tests/acceptance/front-page-test.js
@@ -14,22 +14,19 @@ test('visiting /', function(assert) {
         findWithAssert('a[href="/crates"]');
         findWithAssert('a[href="/login"]');
 
-        const $downloads = findWithAssert('.downloads .num');
-        assert.equal($downloads.text().trim(), '13,534,453');
-
-        const $crates = findWithAssert('.crates .num');
-        assert.equal($crates.text().trim(), '3,430');
+        hasText(assert, '.downloads .num', '13,534,453');
+        hasText(assert, '.crates .num', '3,430');
 
         const $newCrate = findWithAssert('#new-crates ul > li:first a');
-        assert.equal($newCrate.text().trim(), 'mkstemp (0.2.0)');
+        hasText(assert, $newCrate, 'mkstemp (0.2.0)');
         assert.equal($newCrate.attr('href').trim(), '/crates/mkstemp');
 
         const $mostDownloaded = findWithAssert('#most-downloaded ul > li:first a');
-        assert.equal($mostDownloaded.text().trim(), 'libc (0.2.2)');
+        hasText(assert, $mostDownloaded, 'libc (0.2.2)');
         assert.equal($mostDownloaded.attr('href').trim(), '/crates/libc');
 
         const $justUpdated = findWithAssert('#just-updated ul > li:first a');
-        assert.equal($justUpdated.text().trim(), 'nanomsg (0.4.2)');
+        hasText(assert, $justUpdated, 'nanomsg (0.4.2)');
         assert.equal($justUpdated.attr('href').trim(), '/crates/nanomsg');
     });
 });
