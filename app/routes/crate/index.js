7b21e8e98b906105b61f1ac80bef1cf049ee9722
Ryan Williams <github@ryanwillia.ms>
2016-01-18 08:30:53 +1300

    Fix trailing slash redirect breaking browser back button

--- app/routes/crate/index.js
+++ app/routes/crate/index.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     redirect() {
-        this.transitionTo('crate.version', '');
+        this.replaceWith('crate.version', '');
     }
 });
