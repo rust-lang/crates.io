9c10e18280abe95b2dc25e418fe082ffb315a1b1
Alex Burka <aburka@seas.upenn.edu>
2016-01-31 22:54:09 -0500

    Revert "Fix trailing slash redirect breaking browser back button"
    This reverts commit 7b21e8e98b906105b61f1ac80bef1cf049ee9722 (#251)
    because it caused #252 while fixing #236.

--- app/routes/crate/index.js
+++ app/routes/crate/index.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     redirect() {
-        this.replaceWith('crate.version', '');
+        this.transitionTo('crate.version', '');
     }
 });
