ce546960a487ee36450b15481e2c1ad044f29dcd
Jake Goldsborough <rjgoldsborough@gmail.com>
2016-08-29 18:30:01 -0700

    making ember return an error but not redirect if crate not found

--- app/routes/crate.js
+++ app/routes/crate.js
@@ -4,9 +4,8 @@ export default Ember.Route.extend({
     model(params) {
         return this.store.find('crate', params.crate_id).catch(e => {
             if (e.errors.any(e => e.detail === 'Not Found')) {
-                this.controllerFor('application').set('nextFlashError', `Crate '${params.crate_id}' does not exist`);
-                return this.replaceWith('index');
+                throw new Error(`${params.crate_id} not found`);
             }
         });
-    },
+    }
 });
