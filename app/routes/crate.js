78f2227e11c2518bcd5562e9e7d336e72c51267d
Jake Goldsborough <rjgoldsborough@gmail.com>
2016-08-30 11:20:08 -0700

    making app use flash error and adding error substate template to ember

--- app/routes/crate.js
+++ app/routes/crate.js
@@ -4,7 +4,7 @@ export default Ember.Route.extend({
     model(params) {
         return this.store.find('crate', params.crate_id).catch(e => {
             if (e.errors.any(e => e.detail === 'Not Found')) {
-                throw new Error(`${params.crate_id} not found`);
+                return this.controllerFor('application').set('flashError', `Crate '${params.crate_id}' does not exist`);
             }
         });
     }
