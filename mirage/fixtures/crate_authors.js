7da845b86e5a203c906c699e6596bf7e610d0e3d
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2017-02-11 17:59:21 -0500

    Add an ember acceptance test for a crate's page

---
+++ mirage/fixtures/crate_authors.js
@@ -0,0 +1,16 @@
+// jscs:disable validateQuoteMarks
+export default {
+    "meta": {
+        "names": [
+            "Daniel Fagnan <someone@example.com>",
+            "Jason E. Aten",
+            "David C. Bishop",
+            "Dennis Lawler",
+            "Zachary Tong",
+            "Dan Burkert",
+            "Benoît Labaere <someoneelse@example.com>",
+            "Chip Collier"
+        ]
+    },
+    "users": []
+};
---
+++ tests/acceptance/crate-test.js
@@ -0,0 +1,14 @@
+import { test } from 'qunit';
+import moduleForAcceptance from 'cargo/tests/helpers/module-for-acceptance';
+
+moduleForAcceptance('Acceptance | crate page');
+
+test('visiting a crate page from the front page', function(assert) {
+    visit('/');
+    click('#just-updated ul > li:first a');
+
+    andThen(function() {
+        assert.equal(currentURL(), '/crates/nanomsg');
+        assert.equal(document.title, 'nanomsg - Cargo: packages for Rust');
+    });
+});
