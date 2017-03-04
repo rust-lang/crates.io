99afd2f34a92e62e96276ecdc2b5ceace2a9d79c
Gabriel Smith <ga29smith@gmail.com>
2017-03-04 14:44:40 -0500

    Disable "Version does not exist" error if the version is null

--- app/routes/crate/version.js
+++ app/routes/crate/version.js
@@ -29,7 +29,7 @@ export default Ember.Route.extend({
         return crate.get('versions')
             .then(versions => {
                 const version = versions.find(version => version.get('num') === params.version_num);
-                if (!version) {
+                if (params.version_num && !version) {
                     this.controllerFor('application').set('nextFlashError',
                         `Version '${params.version_num}' of crate '${crate.get('name')}' does not exist`);
                 }
