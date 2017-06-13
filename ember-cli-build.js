9365b08da0db2ae3931f481b42b904ac176753fc
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-06-13 23:41:10 +0200

    Remove obsolete "moment" bower dependencies
    "ember-cli-moment-shim" takes care of that for us automatically

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -20,7 +20,6 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
-  app.import('bower_components/moment/moment.js');
   app.import('bower_components/normalize-css/normalize.css');
   app.import('bower_components/clipboard/dist/clipboard.js');
   return app.toTree();
