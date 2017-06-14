d5f0bdbe50fd6c1de59da2e06dddb753f8705135
Nicholas Bailey <nicholas.j.bailey91@gmail.com>
2017-06-14 02:02:16 -0400

    Stopped EncodableCrate from returning a license, and instead made EncodableVersion return it.

--- src/krate.rs
+++ src/krate.rs
@@ -103,7 +103,6 @@ pub struct EncodableCrate {
     pub description: Option<String>,
     pub homepage: Option<String>,
     pub documentation: Option<String>,
-    pub license: Option<String>,
     pub repository: Option<String>,
     pub links: CrateLinks,
     pub exact_match: bool,
@@ -527,7 +526,6 @@ impl Crate {
             homepage: homepage,
             exact_match: exact_match,
             description: description,
-            license: license,
             repository: repository,
             links: CrateLinks {
                 version_downloads: format!("/api/v1/crates/{}/downloads", name),
