e14815f8a1d646f8b85558bc61c0e4caa55781c3
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-06-17 11:03:21 +0200

    Fix ESLint issues in Node.js files

--- config/environment.js
+++ config/environment.js
@@ -1,51 +1,53 @@
-/* jshint node: true */
+'use strict';
+
+/* eslint-env node */
 
 module.exports = function(environment) {
-  var ENV = {
-    modulePrefix: 'cargo',
-    environment: environment,
-    rootURL: '/',
-    locationType: 'router-scroll',
-    historySupportMiddleware: true,
-    EmberENV: {
-      FEATURES: {
-        // Here you can enable experimental features on an ember canary build
-        // e.g. 'with-controller': true
-      },
-      EXTEND_PROTOTYPES: {
-        // Prevent Ember Data from overriding Date.parse.
-        Date: false
-      }
-    },
+    var ENV = {
+        modulePrefix: 'cargo',
+        environment,
+        rootURL: '/',
+        locationType: 'router-scroll',
+        historySupportMiddleware: true,
+        EmberENV: {
+            FEATURES: {
+                // Here you can enable experimental features on an ember canary build
+                // e.g. 'with-controller': true
+            },
+            EXTEND_PROTOTYPES: {
+                // Prevent Ember Data from overriding Date.parse.
+                Date: false
+            }
+        },
 
-    APP: {
-      // Here you can pass flags/options to your application instance
-      // when it is created
+        APP: {
+            // Here you can pass flags/options to your application instance
+            // when it is created
+        }
+    };
+
+    if (environment === 'development') {
+        // ENV.APP.LOG_RESOLVER = true;
+        // ENV.APP.LOG_ACTIVE_GENERATION = true;
+        // ENV.APP.LOG_TRANSITIONS = true;
+        // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
+        // ENV.APP.LOG_VIEW_LOOKUPS = true;
     }
-  };
 
-  if (environment === 'development') {
-    // ENV.APP.LOG_RESOLVER = true;
-    // ENV.APP.LOG_ACTIVE_GENERATION = true;
-    // ENV.APP.LOG_TRANSITIONS = true;
-    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    // ENV.APP.LOG_VIEW_LOOKUPS = true;
-  }
+    if (environment === 'test') {
+        // Testem prefers this...
+        ENV.locationType = 'none';
 
-  if (environment === 'test') {
-    // Testem prefers this...
-    ENV.locationType = 'none';
+        // keep test console output quieter
+        ENV.APP.LOG_ACTIVE_GENERATION = false;
+        ENV.APP.LOG_VIEW_LOOKUPS = false;
 
-    // keep test console output quieter
-    ENV.APP.LOG_ACTIVE_GENERATION = false;
-    ENV.APP.LOG_VIEW_LOOKUPS = false;
+        ENV.APP.rootElement = '#ember-testing';
+    }
 
-    ENV.APP.rootElement = '#ember-testing';
-  }
+    if (environment === 'production') {
+        // here be dragons
+    }
 
-  if (environment === 'production') {
-
-  }
-
-  return ENV;
+    return ENV;
 };
