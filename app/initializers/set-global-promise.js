0f38b6e601315d4c01adcc50f965cc081718c1d0
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-06-17 11:15:18 +0200

    Add initializer setting window.Promise to RSVP.Promise

---
+++ app/initializers/set-global-promise.js
@@ -0,0 +1,9 @@
+import RSVP from 'rsvp';
+
+export function initialize() {
+    // async/await is using window.Promise by default and we want async/await to
+    // use RSVP instead which is properly integrated with Ember's runloop
+    window.Promise = RSVP.Promise;
+}
+
+export default { initialize };
