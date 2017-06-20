fe02763a1ddd1d20764355a9edaae09bc8c0cb76
Natalie Boehm <nmb56@pitt.edu>
2017-06-20 13:43:45 -0400

    added PaginationMixin so that number of results displays correctly

--- app/controllers/team.js
+++ app/controllers/team.js
@@ -1,8 +1,9 @@
 import Ember from 'ember';
+import PaginationMixin from '../mixins/pagination';
 
 const { computed } = Ember;
 
-export default Ember.Controller.extend({
+export default Ember.Controller.extend(PaginationMixin, {
     queryParams: ['page', 'per_page', 'sort'],
     page: '1',
     per_page: 10,
