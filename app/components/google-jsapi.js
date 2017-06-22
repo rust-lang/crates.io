ceaa0e65f509cabc22c90519ca01cc6c7c6191e9
Ricardo Mendes <rokusu@gmail.com>
2017-06-22 01:25:11 +0100

    Move google jsapi initializer to component
    This is in preparation for FastBoot. `didInsertElement` is not run when
    rendering in FastBoot, so it’s fine to access `Ember.$`.

---
+++ app/components/google-jsapi.js
@@ -0,0 +1,15 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+    didInsertElement() {
+        Ember.$.getScript('https://www.google.com/jsapi', function() {
+            window.google.load('visualization', '1.0', {
+                packages: ['corechart'],
+                callback() {
+                    window.googleChartsLoaded = true;
+                    Ember.$(document).trigger('googleChartsLoaded');
+                }
+            });
+        });
+    }
+});
