9234f8aeb3fb139fc54c55628a3e153f3682b51e
Sivakumar Kailasam <sivakumar.ur.friend@gmail.com>
2017-06-26 10:56:31 +0530

    Fix broken tests

--- app/app.js
+++ app/app.js
@@ -3,20 +3,12 @@ import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
 
-let App;
-
-Ember.MODEL_FACTORY_INJECTIONS = true;
-
-App = Ember.Application.extend({
+let App = Ember.Application.extend({
     modulePrefix: config.modulePrefix,
     podModulePrefix: config.podModulePrefix,
     Resolver
 });
 
 loadInitializers(App, config.modulePrefix);
-
-Ember.$.ajaxSetup({
-    cache: false
-});
 
 export default App;
