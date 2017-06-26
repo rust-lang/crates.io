e421eb8c2b3f8018325ab3befa0eca3053818665
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-06-26 17:59:58 +0200

    TemplateLint: Disable some rules for now

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -4,4 +4,10 @@
 
 module.exports = {
     extends: 'recommended',
+
+    rules: {
+        'block-indentation': false,
+        'img-alt-attributes': false,
+        'triple-curlies': false,
+    },
 };
