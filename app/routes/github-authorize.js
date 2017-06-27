3b7aa803cefa510b9d4a33ca5550e9b3ccb9003e
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2017-06-27 15:25:28 -0700

    Revert "Replace ic-ajax & other ajax calls with ember-fetch"
    This reverts commit dbc1c9a08b3d9f79fab8e4bef27692aeb38f5a42.

--- app/routes/github-authorize.js
+++ app/routes/github-authorize.js
@@ -1,5 +1,5 @@
 import Ember from 'ember';
-import ajax from 'ember-fetch/ajax';
+import ajax from 'ic-ajax';
 
 /**
  * This route will be called from the GitHub OAuth flow once the user has
--- app/routes/github-login.js
+++ app/routes/github-login.js
@@ -1,5 +1,5 @@
 import Ember from 'ember';
-import ajax from 'ember-fetch/ajax';
+import ajax from 'ic-ajax';
 
 /**
  * Calling this route will query the `/authorize_url` API endpoint
