0c81f8cf2ddcd3810781c2dd07f25efddd3c17ce
Sivakumar Kailasam <sivakumar.ur.friend@gmail.com>
2017-06-28 16:48:59 +0530

    Env based fastboot host whitelisting

--- config/environment.js
+++ config/environment.js
@@ -26,7 +26,7 @@ module.exports = function(environment) {
         },
 
         fastboot: {
-            hostWhitelist: ['crates.io', /^localhost:\d+$/]
+            hostWhitelist: ['crates.io', /^[\w\-]+\.herokuapp\.com$/]
         }
     };
 
@@ -36,6 +36,7 @@ module.exports = function(environment) {
         // ENV.APP.LOG_TRANSITIONS = true;
         // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
         // ENV.APP.LOG_VIEW_LOOKUPS = true;
+        ENV.fastboot.hostWhitelist = ['crates.io', /^[\w\-]+\.herokuapp\.com$/, /^localhost:\d+$/];
     }
 
     if (environment === 'test') {
