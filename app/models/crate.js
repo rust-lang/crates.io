776ed77d394bc3f96465730cad596bbbbeb635d1
Natalie Boehm <nmb56@pitt.edu>
2017-07-10 16:44:38 -0400

    addition of dropdown selection for ability to sort by recent downloads

--- app/models/crate.js
+++ app/models/crate.js
@@ -4,6 +4,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
     name: DS.attr('string'),
     downloads: DS.attr('number'),
+    recent_downloads: DS.attr('number'),
     created_at: DS.attr('date'),
     updated_at: DS.attr('date'),
     max_version: DS.attr('string'),
