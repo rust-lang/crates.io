642a095f87b0b0ae961432b5d9e9f85d280d31ab
Natalie Boehm <nmb56@pitt.edu>
2017-07-11 12:32:20 -0400

    all-time and in past 90 days downloads displayed in list, currently both totals are listed but has ability to show just one according to sort

--- app/components/crate-row.js
+++ app/components/crate-row.js
@@ -1,5 +1,31 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 
 export default Component.extend({
-    classNames: ['crate', 'row']
+    classNames: ['crate', 'row'],
+    sort: null,
+
+    isDownloads: computed('sort', function() {
+        if (this.get('sort') === 'All-Time Downloads') {
+            return true;
+        } else {
+            return false;
+        }
+    }),
+
+    isRecentDownloads: computed('sort', function() {
+        if (this.get('sort') === 'Recent Downloads') {
+            return true;
+        } else {
+            return false;
+        }
+    }),
+
+    isAlpha: computed('sort', function() {
+        if (this.get('sort') === 'Alphabetical') {
+            return true;
+        } else {
+            return false;
+        }
+    })
 });
