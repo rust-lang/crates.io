1f9c68a93d4008c198a0b99804aaef41f73cdbc8
Natalie Boehm <nmb56@pitt.edu>
2017-07-12 11:15:56 -0400

    Remove unused code for showing only either recent downloads or all time downloads in results

--- app/components/crate-row.js
+++ app/components/crate-row.js
@@ -1,31 +1,5 @@
 import Component from '@ember/component';
-import { computed } from '@ember/object';
 
 export default Component.extend({
-    classNames: ['crate', 'row'],
-    sort: null,
-
-    isDownloads: computed('sort', function() {
-        if (this.get('sort') === 'All-Time Downloads') {
-            return true;
-        } else {
-            return false;
-        }
-    }),
-
-    isRecentDownloads: computed('sort', function() {
-        if (this.get('sort') === 'Recent Downloads') {
-            return true;
-        } else {
-            return false;
-        }
-    }),
-
-    isAlpha: computed('sort', function() {
-        if (this.get('sort') === 'Alphabetical') {
-            return true;
-        } else {
-            return false;
-        }
-    })
+    classNames: ['crate', 'row']
 });
