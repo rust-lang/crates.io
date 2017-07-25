dfb21b87f957498ca4caba08fffb6b85316a4273
Natalie Boehm <nmb56@pitt.edu>
2017-07-25 15:04:35 -0400

    really allow too many arguments on krate::encodable

--- src/krate.rs
+++ src/krate.rs
@@ -509,7 +509,7 @@ impl Crate {
         )
     }
 
-    #[allow(too_many_arguments)]
+    #[cfg_attr(feature = "cargo-clippy", allow(too_many_arguments))]
     pub fn encodable(
         self,
         max_version: semver::Version,
