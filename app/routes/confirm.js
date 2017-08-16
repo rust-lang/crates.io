bd4686789c1ac55002c99a53e6589a9f388619f2
Natalie Boehm <nmb56@pitt.edu>
2017-08-16 12:05:40 -0400

    fixes bug where user model would not reload between confirming email and navigating to account settings page.
    Uses GET request to `/me` to get the latest version of the model upon returning successfully from the PUT request to `/confirm`.

--- app/routes/confirm.js
+++ app/routes/confirm.js
@@ -7,7 +7,23 @@ export default Ember.Route.extend({
 
     model(params) {
         return this.get('ajax').raw(`/api/v1/confirm/${params.email_token}`, { method: 'PUT', data: {}})
-            .then(({response}) => {})
+            .then(({response}) => {
+                /*  We need this block to reload the user model from the database,
+                    without which if we haven't submitted another GET /me after
+                    clicking the link and before checking their account info page,
+                    the user will still see that their email has not yet been
+                    validated and could potentially be confused, resend the email,
+                    and set up a situation where their email has been verified but
+                    they have an unverified token sitting in the DB.
+
+                    Suggestions of a more ideomatic way to fix/test this are welcome!
+                */
+                if (this.session.get('isLoggedIn')) {
+                    this.get('ajax').request('/me').then((response) => {
+                        this.session.set('currentUser', this.store.push(this.store.normalize('user', response.user)));
+                    })
+                }
+            })
             .catch((error) => {
                 if (error.payload) {
                     this.get('flashMessages').queue(`Error in email confirmation: ${error.payload.errors[0].detail}`);
