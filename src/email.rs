8256def0d93331b3cfc24feeb50941528153481d
Natalie Boehm <nmb56@pitt.edu>
2017-09-13 14:46:46 -0400

    Remove 'not found' when getting mailgun env vars, change env var struct to be an Option for dealing with development versus production modes, change build_email to return a CargoResult<Email> for better error handling

--- src/email.rs
+++ src/email.rs
@@ -16,56 +16,72 @@ pub struct MailgunConfigVars {
     pub smtp_server: String,
 }
 
-fn init_config_vars() -> MailgunConfigVars {
+pub fn init_config_vars() -> Option<MailgunConfigVars> {
     dotenv().ok();
 
-    let mailgun_config = MailgunConfigVars {
-        smtp_login: env::var("MAILGUN_SMTP_LOGIN").unwrap_or_else(|_| String::from("Not Found")),
-        smtp_password: env::var("MAILGUN_SMTP_PASSWORD").unwrap_or_else(|_| {
-            String::from("Not Found")
-        }),
-        smtp_server: env::var("MAILGUN_SMTP_SERVER").unwrap_or_else(|_| String::from("Not Found")),
+    let mailgun_config = match (
+        env::var("MAILGUN_SMTP_LOGIN"),
+        env::var("MAILGUN_SMTP_PASSWORD"),
+        env::var("MAILGUN_SMTP_SERVER"),
+    ) {
+        (Ok(login), Ok(password), Ok(server)) => {
+            Some(MailgunConfigVars {
+                smtp_login: login,
+                smtp_password: password,
+                smtp_server: server,
+            })
+        }
+        _ => None,
     };
 
     mailgun_config
 }
 
-fn build_email(recipient: &str, subject: &str, body: &str, smtp_login: &str) -> Email {
+pub fn build_email(
+    recipient: &str,
+    subject: &str,
+    body: &str,
+    mailgun_config: &Option<MailgunConfigVars>,
+) -> CargoResult<Email> {
+    let sender = mailgun_config
+        .as_ref()
+        .map(|s| s.smtp_login.as_str())
+        .unwrap_or("Development Mode");
+
     let email = EmailBuilder::new()
         .to(recipient)
-        .from(smtp_login)
+        .from(sender)
         .subject(subject)
         .body(body)
-        .build()
-        .expect("Failed to build confirm email message");
+        .build()?;
 
-    email
+    Ok(email)
 }
 
 pub fn send_email(recipient: &str, subject: &str, body: &str) -> CargoResult<()> {
     let mailgun_config = init_config_vars();
-    let email = build_email(recipient, subject, body, &mailgun_config.smtp_login);
+    let email = build_email(recipient, subject, body, &mailgun_config)?;
 
-    if mailgun_config.smtp_login == "Not Found" && mailgun_config.smtp_password == "Not Found" &&
-        mailgun_config.smtp_server == "Not Found"
-    {
-        let mut sender = FileEmailTransport::new(Path::new("/tmp"));
-        let result = sender.send(email.clone());
-        result.map_err(
-            |_| bad_request("Email file could not be generated"),
-        )?;
-    } else {
-        let mut transport = SmtpTransportBuilder::new(
-            (mailgun_config.smtp_server.as_str(), SUBMISSION_PORT),
-        ).expect("Failed to create message transport")
-            .credentials(&mailgun_config.smtp_login, &mailgun_config.smtp_password)
-            .security_level(SecurityLevel::AlwaysEncrypt)
-            .smtp_utf8(true)
-            .authentication_mechanism(Mechanism::Plain)
-            .build();
+    match mailgun_config {
+        Some(mailgun_config) => {
+            let mut transport =
+                SmtpTransportBuilder::new((mailgun_config.smtp_server.as_str(), SUBMISSION_PORT))?
+                    .credentials(&mailgun_config.smtp_login, &mailgun_config.smtp_password)
+                    .security_level(SecurityLevel::AlwaysEncrypt)
+                    .smtp_utf8(true)
+                    .authentication_mechanism(Mechanism::Plain)
+                    .build();
 
-        let result = transport.send(email.clone());
-        result.map_err(|_| bad_request("Error in sending email"))?;
+            let result = transport.send(email.clone());
+            result.map_err(|_| bad_request("Error in sending email"))?;
+        }
+        None => {
+            let mut sender = FileEmailTransport::new(Path::new("/tmp"));
+            let result = sender.send(email.clone());
+            result.map_err(
+                |_| bad_request("Email file could not be generated"),
+            )?;
+        }
     }
 
     Ok(())
