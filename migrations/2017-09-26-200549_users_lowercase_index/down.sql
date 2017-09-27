27f1e819d63c6da0248dfa3a6a1c10b2f944d99f
Richard Petrie <rap1011@ksu.edu>
2017-09-26 19:27:34 -0500

    Might as well make gh_login a unique key if we're going to be enforcing unique lower. Github also enforces case insensitive usernames, so current auth should be fine. \n\nAdditionally we drop the index_users_gh_login as postgres automatically indexes unique keys.

--- migrations/2017-09-26-200549_users_lowercase_index/down.sql
+++ migrations/2017-09-26-200549_users_lowercase_index/down.sql
@@ -1,1 +1,3 @@
-DROP INDEX IF EXISTS lower_gh_login;
+DROP INDEX lower_gh_login;
+ALTER TABLE users DROP CONSTRAINT unique_gh_login;
+CREATE INDEX index_users_gh_login ON users (gh_login);
--- migrations/2017-09-26-200549_users_lowercase_index/up.sql
+++ migrations/2017-09-26-200549_users_lowercase_index/up.sql
@@ -1,1 +1,3 @@
-CREATE UNIQUE INDEX lower_gh_login ON users (LOWER(gh_login));
+DROP INDEX IF EXISTS index_users_gh_login;
+ALTER TABLE users ADD CONSTRAINT unique_gh_login UNIQUE(gh_login);
+CREATE UNIQUE INDEX lower_gh_login ON users (lower(gh_login));
