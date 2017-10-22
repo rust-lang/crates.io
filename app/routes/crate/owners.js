fc9234fce69e54df8e16b007607dfdf67d1eb671
iain8 <iain@fastmail.com>
2017-10-22 16:53:31 -0400

    remove redundant route

--- app/routes/crate/owners.js
+++
@@ -1,8 +0,0 @@
-import Route from '@ember/routing/route';
-
-export default Route.extend({
-    // model() {
-    //     this.controllerFor(this.routeName)
-    //         .set('crate', this.modelFor('crate'));
-    // }
-});
