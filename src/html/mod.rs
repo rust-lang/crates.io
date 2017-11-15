992bfa16a98445ff9f3b00f2622419e6e5816d00
Sean Linsley <code@seanlinsley.com>
2017-11-14 19:16:24 -0600

    embed SVGs

--- src/html/mod.rs
+++ src/html/mod.rs
@@ -2,6 +2,7 @@
 
 use std::fs::File;
 use std::io::Read;
+use std::collections::HashMap;
 
 use conduit::{Request, Response};
 
@@ -27,6 +28,7 @@ pub fn index(req: &mut Request) -> CargoResult<Response> {
 
     let mut handlebars = Handlebars::new();
     handlebars.register_helper("format_num", Box::new(format_num));
+    handlebars.register_helper("embed_svg", Box::new(embed_svg));
     handlebars.register_template_string("categories", include_str!("_categories.hbs"))?;
     handlebars.register_template_string("crates", include_str!("_crates.hbs"))?;
     handlebars.register_template_string("keywords", include_str!("_keywords.hbs"))?;
@@ -34,7 +36,6 @@ pub fn index(req: &mut Request) -> CargoResult<Response> {
     let mut json = json!(&krate::metadata::summary_json(&*req.db_conn()?).unwrap());
     json["current_user"] = json!({"id": 1, "name": "Sean Linsley"});
     // TODO: flash message
-    // TODO: SVG embed
     // TODO: global layout, that other pages can render into
     // TODO: user image in header
 
@@ -47,5 +48,23 @@ fn format_num(h: &Helper, _: &Handlebars, rc: &mut RenderContext) -> Result<(), 
     let param = h.param(0).expect("no argument passed").value();
     let rendered = format!("{}", param);
     try!(rc.writer.write(rendered.into_bytes().as_ref()));
+    Ok(())
+}
+
+fn embed_svg(h: &Helper, _: &Handlebars, rc: &mut RenderContext) -> Result<(), RenderError> {
+    let param = h.param(0).expect("no argument passed").value().to_string().replace("\"", "");
+
+    let mut svgs = HashMap::new();
+    svgs.insert("crate".to_string(),           include_str!("svgs/crate.svg").to_string());
+    svgs.insert("button-download".to_string(), include_str!("svgs/button-download.svg").to_string());
+    svgs.insert("download".to_string(),        include_str!("svgs/download.svg").to_string());
+    svgs.insert("flag".to_string(),            include_str!("svgs/flag.svg").to_string());
+    svgs.insert("lock".to_string(),            include_str!("svgs/lock.svg").to_string());
+    svgs.insert("right-arrow".to_string(),     include_str!("svgs/right-arrow.svg").to_string());
+
+    let svg = &svgs[&param];
+
+    try!(rc.writer.write(svg.as_ref()));
+
     Ok(())
 }
