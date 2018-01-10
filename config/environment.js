1ff0a2370e318aad88b3d0f83dd05a8d26dae033
Sorin Davidoi <sorin.davidoi@gmail.com>
2018-01-10 10:52:21 +0100

    feat(config/environment): Disable accessibility audit in during development
    This will prevent the violations from being highlighted on the page,
    which is annoying since the color-contrast rule is currently disabled
    during tests and thus a lot of elements would be highlighted.
    https://github.com/ember-a11y/ember-a11y-testing#development-usage

--- config/environment.js
+++ config/environment.js
@@ -39,6 +39,11 @@ module.exports = function(environment) {
         // ENV.APP.LOG_TRANSITIONS = true;
         // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
         // ENV.APP.LOG_VIEW_LOOKUPS = true;
+        ENV['ember-a11y-testing'] = {
+            componentOptions: {
+                turnAuditOff: true,
+            }
+        };
     }
 
     if (environment === 'test') {
