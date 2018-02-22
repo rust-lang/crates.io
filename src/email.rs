dca11691c752da029fff492fe2921d8a57a1e948
Justin Geibel <jtgeibel@gmail.com>
2018-02-22 02:27:45 -0500

    Upgrade `lettre` and address API changes
    SmtpTransport::simple_builder() automatically validates the TLS
    certificate against the provided domain name.

--- src/email.rs
+++ src/email.rs
@@ -2,13 +2,14 @@ use std::env;
 use std::path::Path;
 
 use dotenv::dotenv;
-use lettre::email::{Email, EmailBuilder};
-use lettre::transport::file::FileEmailTransport;
-use lettre::transport::smtp::authentication::Mechanism;
-use lettre::transport::smtp::SUBMISSION_PORT;
-use lettre::transport::smtp::{SecurityLevel, SmtpTransportBuilder};
-use lettre::transport::EmailTransport;
 use util::{bad_request, CargoResult};
+
+use lettre::file::FileEmailTransport;
+use lettre::EmailTransport;
+use lettre::smtp::SmtpTransport;
+use lettre::smtp::authentication::{Credentials, Mechanism};
+
+use lettre_email::{Email, EmailBuilder};
 
 #[derive(Debug)]
 pub struct MailgunConfigVars {
@@ -77,20 +78,21 @@ fn send_email(recipient: &str, subject: &str, body: &str) -> CargoResult<()> {
 
     match mailgun_config {
         Some(mailgun_config) => {
-            let mut transport =
-                SmtpTransportBuilder::new((mailgun_config.smtp_server.as_str(), SUBMISSION_PORT))?
-                    .credentials(&mailgun_config.smtp_login, &mailgun_config.smtp_password)
-                    .security_level(SecurityLevel::AlwaysEncrypt)
-                    .smtp_utf8(true)
-                    .authentication_mechanism(Mechanism::Plain)
-                    .build();
+            let mut transport = SmtpTransport::simple_builder(mailgun_config.smtp_server)?
+                .credentials(Credentials::new(
+                    mailgun_config.smtp_login,
+                    mailgun_config.smtp_password,
+                ))
+                .smtp_utf8(true)
+                .authentication_mechanism(Mechanism::Plain)
+                .build();
 
-            let result = transport.send(email.clone());
+            let result = transport.send(&email);
             result.map_err(|_| bad_request("Error in sending email"))?;
         }
         None => {
             let mut sender = FileEmailTransport::new(Path::new("/tmp"));
-            let result = sender.send(email.clone());
+            let result = sender.send(&email);
             result.map_err(|_| bad_request("Email file could not be generated"))?;
         }
     }
