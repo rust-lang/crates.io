508b40f07dc93be5a0b3eef1bc5ffb89170a2244
Jarek Radosz <jradosz@gmail.com>
2018-03-24 04:11:58 +0100

    Don’t render the test app outside #ember-testing.
    Co-authored-by: t-sauer <t-sauer@outlook.de>

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,9 +1,10 @@
 import Application from '../app';
+import config from '../config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
 import loadEmberExam from 'ember-exam/test-support/load';
 
-setApplication(Application.create({ autoboot: false }));
+setApplication(Application.create(config.APP));
 
 loadEmberExam();
 start();
