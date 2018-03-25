a5ddc52c4065785de01122bc1d56aaa99336b86b
Jarek Radosz <jradosz@gmail.com>
2018-03-25 16:05:48 +0200

    Update tests to use the new API (RFC232/RFC268).
    Tests transformed using ember-qunit-codemod.

--- tests/acceptance/categories-test.js
+++ tests/acceptance/categories-test.js
@@ -1,47 +1,49 @@
-import { test } from 'qunit';
+import { module, test } from 'qunit';
+import { setupApplicationTest } from 'ember-qunit';
 import { visit } from 'ember-native-dom-helpers';
 import a11yAudit from 'ember-a11y-testing/test-support/audit';
-import moduleForAcceptance from 'cargo/tests/helpers/module-for-acceptance';
 import axeConfig from '../axe-config';
 
-moduleForAcceptance('Acceptance | categories');
+module('Acceptance | categories', function(hooks) {
+    setupApplicationTest(hooks);
 
-test('is accessible', async function(assert) {
-    assert.expect(0);
+    test('is accessible', async function(assert) {
+        assert.expect(0);
 
-    server.create('category', { category: 'API bindings', crates_cnt: 0 });
-    server.create('category', { category: 'Algorithms', crates_cnt: 1 });
-    server.create('category', { category: 'Asynchronous', crates_cnt: 3910 });
+        server.create('category', { category: 'API bindings', crates_cnt: 0 });
+        server.create('category', { category: 'Algorithms', crates_cnt: 1 });
+        server.create('category', { category: 'Asynchronous', crates_cnt: 3910 });
 
-    await visit('/categories');
-    await a11yAudit(axeConfig);
-});
+        await visit('/categories');
+        await a11yAudit(axeConfig);
+    });
 
-test('category/:category_id is accessible', async function(assert) {
-    assert.expect(0);
+    test('category/:category_id is accessible', async function(assert) {
+        assert.expect(0);
 
-    server.create('category', { category: 'Algorithms', crates_cnt: 1 });
+        server.create('category', { category: 'Algorithms', crates_cnt: 1 });
 
-    await visit('/categories/algorithms');
-    await a11yAudit(axeConfig);
-});
+        await visit('/categories/algorithms');
+        await a11yAudit(axeConfig);
+    });
 
-test('listing categories', async function(assert) {
-    server.create('category', { category: 'API bindings', crates_cnt: 0 });
-    server.create('category', { category: 'Algorithms', crates_cnt: 1 });
-    server.create('category', { category: 'Asynchronous', crates_cnt: 3910 });
+    test('listing categories', async function(assert) {
+        server.create('category', { category: 'API bindings', crates_cnt: 0 });
+        server.create('category', { category: 'Algorithms', crates_cnt: 1 });
+        server.create('category', { category: 'Asynchronous', crates_cnt: 3910 });
 
-    await visit('/categories');
+        await visit('/categories');
 
-    assert.dom('[data-test-category="api-bindings"] [data-test-crate-count]').hasText('0 crates');
-    assert.dom('[data-test-category="algorithms"] [data-test-crate-count]').hasText('1 crate');
-    assert.dom('[data-test-category="asynchronous"] [data-test-crate-count]').hasText('3,910 crates');
-});
+        assert.dom('[data-test-category="api-bindings"] [data-test-crate-count]').hasText('0 crates');
+        assert.dom('[data-test-category="algorithms"] [data-test-crate-count]').hasText('1 crate');
+        assert.dom('[data-test-category="asynchronous"] [data-test-crate-count]').hasText('3,910 crates');
+    });
 
-test('category/:category_id index default sort is recent-downloads', async function(assert) {
-    server.create('category', { category: 'Algorithms', crates_cnt: 1 });
+    test('category/:category_id index default sort is recent-downloads', async function(assert) {
+        server.create('category', { category: 'Algorithms', crates_cnt: 1 });
 
-    await visit('/categories/algorithms');
+        await visit('/categories/algorithms');
 
-    assert.dom('[data-test-category-sort] [data-test-current-order]').hasText('Recent Downloads');
+        assert.dom('[data-test-category-sort] [data-test-current-order]').hasText('Recent Downloads');
+    });
 });
--- tests/acceptance/crate-test.js
+++ tests/acceptance/crate-test.js
@@ -1,266 +1,268 @@
 import Service from '@ember/service';
-import { test } from 'qunit';
+import { module, test } from 'qunit';
+import { setupApplicationTest } from 'ember-qunit';
 import { click, visit, currentURL, currentRouteName, fillIn } from 'ember-native-dom-helpers';
 import a11yAudit from 'ember-a11y-testing/test-support/audit';
-import moduleForAcceptance from 'cargo/tests/helpers/module-for-acceptance';
 import axeConfig from '../axe-config';
 
-moduleForAcceptance('Acceptance | crate page');
+module('Acceptance | crate page', function(hooks) {
+    setupApplicationTest(hooks);
 
-test('is accessible', async function(assert) {
-    assert.expect(0);
+    test('is accessible', async function(assert) {
+        assert.expect(0);
 
-    server.create('crate', 'withVersion', { id: 'nanomsg' });
+        server.create('crate', 'withVersion', { id: 'nanomsg' });
 
-    await visit('/');
-    await a11yAudit(axeConfig);
-});
+        await visit('/');
+        await a11yAudit(axeConfig);
+    });
 
-test('/crates/:crate is accessible', async function(assert) {
-    assert.expect(0);
+    test('/crates/:crate is accessible', async function(assert) {
+        assert.expect(0);
 
-    server.create('crate', { id: 'nanomsg', max_version: '0.6.1' });
-    server.create('version', { crate: 'nanomsg', num: '0.6.0' });
-    server.create('version', { crate: 'nanomsg', num: '0.6.1' });
+        server.create('crate', { id: 'nanomsg', max_version: '0.6.1' });
+        server.create('version', { crate: 'nanomsg', num: '0.6.0' });
+        server.create('version', { crate: 'nanomsg', num: '0.6.1' });
 
-    await visit('/crates/nanomsg');
-    await a11yAudit(axeConfig);
-});
+        await visit('/crates/nanomsg');
+        await a11yAudit(axeConfig);
+    });
 
-test('/crates/:crate/:version is accessible', async function(assert) {
-    assert.expect(0);
+    test('/crates/:crate/:version is accessible', async function(assert) {
+        assert.expect(0);
 
-    server.create('crate', { id: 'nanomsg', max_version: '0.6.1' });
-    server.create('version', { crate: 'nanomsg', num: '0.6.0' });
-    server.create('version', { crate: 'nanomsg', num: '0.6.1' });
+        server.create('crate', { id: 'nanomsg', max_version: '0.6.1' });
+        server.create('version', { crate: 'nanomsg', num: '0.6.0' });
+        server.create('version', { crate: 'nanomsg', num: '0.6.1' });
 
-    await visit('/crates/nanomsg/0.6.0');
-    await a11yAudit(axeConfig);
-});
+        await visit('/crates/nanomsg/0.6.0');
+        await a11yAudit(axeConfig);
+    });
 
-test('/crates/:crate/owners is accessible', async function(assert) {
-    assert.expect(0);
+    test('/crates/:crate/owners is accessible', async function(assert) {
+        assert.expect(0);
 
-    server.loadFixtures();
+        server.loadFixtures();
 
-    await visit('/crates/nanomsg/owners');
-    await a11yAudit(axeConfig);
-});
+        await visit('/crates/nanomsg/owners');
+        await a11yAudit(axeConfig);
+    });
 
-test('visiting a crate page from the front page', async function(assert) {
-    server.create('crate', 'withVersion', { id: 'nanomsg' });
+    test('visiting a crate page from the front page', async function(assert) {
+        server.create('crate', 'withVersion', { id: 'nanomsg' });
 
-    await visit('/');
-    await click('[data-test-just-updated] [data-test-crate-link="0"]');
+        await visit('/');
+        await click('[data-test-just-updated] [data-test-crate-link="0"]');
 
-    assert.equal(currentURL(), '/crates/nanomsg');
-    assert.equal(document.title, 'nanomsg - Cargo: packages for Rust');
-});
+        assert.equal(currentURL(), '/crates/nanomsg');
+        assert.equal(document.title, 'nanomsg - Cargo: packages for Rust');
+    });
 
-test('visiting /crates/nanomsg', async function(assert) {
-    server.create('crate', { id: 'nanomsg', max_version: '0.6.1' });
-    server.create('version', { crate: 'nanomsg', num: '0.6.0' });
-    server.create('version', { crate: 'nanomsg', num: '0.6.1' });
+    test('visiting /crates/nanomsg', async function(assert) {
+        server.create('crate', { id: 'nanomsg', max_version: '0.6.1' });
+        server.create('version', { crate: 'nanomsg', num: '0.6.0' });
+        server.create('version', { crate: 'nanomsg', num: '0.6.1' });
 
-    await visit('/crates/nanomsg');
+        await visit('/crates/nanomsg');
 
-    assert.equal(currentURL(), '/crates/nanomsg');
-    assert.equal(currentRouteName(), 'crate.index');
-    assert.equal(document.title, 'nanomsg - Cargo: packages for Rust');
+        assert.equal(currentURL(), '/crates/nanomsg');
+        assert.equal(currentRouteName(), 'crate.index');
+        assert.equal(document.title, 'nanomsg - Cargo: packages for Rust');
 
-    assert.dom('[data-test-heading] [data-test-crate-name]').hasText('nanomsg');
-    assert.dom('[data-test-heading] [data-test-crate-version]').hasText('0.6.1');
-});
+        assert.dom('[data-test-heading] [data-test-crate-name]').hasText('nanomsg');
+        assert.dom('[data-test-heading] [data-test-crate-version]').hasText('0.6.1');
+    });
 
-test('visiting /crates/nanomsg/', async function(assert) {
-    server.create('crate', { id: 'nanomsg', max_version: '0.6.1' });
-    server.create('version', { crate: 'nanomsg', num: '0.6.0' });
-    server.create('version', { crate: 'nanomsg', num: '0.6.1' });
+    test('visiting /crates/nanomsg/', async function(assert) {
+        server.create('crate', { id: 'nanomsg', max_version: '0.6.1' });
+        server.create('version', { crate: 'nanomsg', num: '0.6.0' });
+        server.create('version', { crate: 'nanomsg', num: '0.6.1' });
 
-    await visit('/crates/nanomsg/');
+        await visit('/crates/nanomsg/');
 
-    assert.equal(currentURL(), '/crates/nanomsg/');
-    assert.equal(currentRouteName(), 'crate.index');
-    assert.equal(document.title, 'nanomsg - Cargo: packages for Rust');
+        assert.equal(currentURL(), '/crates/nanomsg/');
+        assert.equal(currentRouteName(), 'crate.index');
+        assert.equal(document.title, 'nanomsg - Cargo: packages for Rust');
 
-    assert.dom('[data-test-heading] [data-test-crate-name]').hasText('nanomsg');
-    assert.dom('[data-test-heading] [data-test-crate-version]').hasText('0.6.1');
-});
+        assert.dom('[data-test-heading] [data-test-crate-name]').hasText('nanomsg');
+        assert.dom('[data-test-heading] [data-test-crate-version]').hasText('0.6.1');
+    });
 
-test('visiting /crates/nanomsg/0.6.0', async function(assert) {
-    server.create('crate', { id: 'nanomsg', max_version: '0.6.1' });
-    server.create('version', { crate: 'nanomsg', num: '0.6.0' });
-    server.create('version', { crate: 'nanomsg', num: '0.6.1' });
+    test('visiting /crates/nanomsg/0.6.0', async function(assert) {
+        server.create('crate', { id: 'nanomsg', max_version: '0.6.1' });
+        server.create('version', { crate: 'nanomsg', num: '0.6.0' });
+        server.create('version', { crate: 'nanomsg', num: '0.6.1' });
 
-    await visit('/crates/nanomsg/0.6.0');
+        await visit('/crates/nanomsg/0.6.0');
 
-    assert.equal(currentURL(), '/crates/nanomsg/0.6.0');
-    assert.equal(currentRouteName(), 'crate.version');
-    assert.equal(document.title, 'nanomsg - Cargo: packages for Rust');
+        assert.equal(currentURL(), '/crates/nanomsg/0.6.0');
+        assert.equal(currentRouteName(), 'crate.version');
+        assert.equal(document.title, 'nanomsg - Cargo: packages for Rust');
 
-    assert.dom('[data-test-heading] [data-test-crate-name]').hasText('nanomsg');
-    assert.dom('[data-test-heading] [data-test-crate-version]').hasText('0.6.0');
-});
+        assert.dom('[data-test-heading] [data-test-crate-name]').hasText('nanomsg');
+        assert.dom('[data-test-heading] [data-test-crate-version]').hasText('0.6.0');
+    });
 
-test('navigating to the all versions page', async function(assert) {
-    server.loadFixtures();
+    test('navigating to the all versions page', async function(assert) {
+        server.loadFixtures();
 
-    await visit('/crates/nanomsg');
-    await click('[data-test-all-versions-link]');
+        await visit('/crates/nanomsg');
+        await click('[data-test-all-versions-link]');
 
-    assert.dom('.info').hasText(/All 13\s+versions of nanomsg since\s+December \d+, 2014/);
-});
+        assert.dom('.info').hasText(/All 13\s+versions of nanomsg since\s+December \d+, 2014/);
+    });
 
-test('navigating to the reverse dependencies page', async function(assert) {
-    server.loadFixtures();
+    test('navigating to the reverse dependencies page', async function(assert) {
+        server.loadFixtures();
 
-    await visit('/crates/nanomsg');
-    await click('[data-test-reverse-deps-link]');
+        await visit('/crates/nanomsg');
+        await click('[data-test-reverse-deps-link]');
 
-    assert.equal(currentURL(), '/crates/nanomsg/reverse_dependencies');
-    assert.dom('a[href="/crates/unicorn-rpc"]').hasText('unicorn-rpc');
-});
+        assert.equal(currentURL(), '/crates/nanomsg/reverse_dependencies');
+        assert.dom('a[href="/crates/unicorn-rpc"]').hasText('unicorn-rpc');
+    });
 
-test('navigating to a user page', async function(assert) {
-    server.loadFixtures();
+    test('navigating to a user page', async function(assert) {
+        server.loadFixtures();
 
-    await visit('/crates/nanomsg');
-    await click('[data-test-owners] [data-test-user-link="blabaere"]');
+        await visit('/crates/nanomsg');
+        await click('[data-test-owners] [data-test-user-link="blabaere"]');
 
-    assert.equal(currentURL(), '/users/blabaere');
-    assert.dom('[data-test-heading] [data-test-username]').hasText('blabaere');
-});
+        assert.equal(currentURL(), '/users/blabaere');
+        assert.dom('[data-test-heading] [data-test-username]').hasText('blabaere');
+    });
 
-test('navigating to a team page', async function(assert) {
-    server.loadFixtures();
+    test('navigating to a team page', async function(assert) {
+        server.loadFixtures();
 
-    await visit('/crates/nanomsg');
-    await click('[data-test-owners] [data-test-team-link="github:org:thehydroimpulse"]');
+        await visit('/crates/nanomsg');
+        await click('[data-test-owners] [data-test-team-link="github:org:thehydroimpulse"]');
 
-    assert.equal(currentURL(), '/teams/github:org:thehydroimpulse');
-    assert.dom('[data-test-heading] [data-test-team-name]').hasText('thehydroimpulseteam');
-});
+        assert.equal(currentURL(), '/teams/github:org:thehydroimpulse');
+        assert.dom('[data-test-heading] [data-test-team-name]').hasText('thehydroimpulseteam');
+    });
 
-test('crates having user-owners', async function(assert) {
-    server.loadFixtures();
+    test('crates having user-owners', async function(assert) {
+        server.loadFixtures();
 
-    await visit('/crates/nanomsg');
+        await visit('/crates/nanomsg');
 
-    assert.dom('[data-test-owners] [data-test-team-link="github:org:thehydroimpulse"] img')
-        .hasAttribute('src', 'https://avatars.githubusercontent.com/u/565790?v=3&s=64');
+        assert.dom('[data-test-owners] [data-test-team-link="github:org:thehydroimpulse"] img')
+            .hasAttribute('src', 'https://avatars.githubusercontent.com/u/565790?v=3&s=64');
 
-    assert.dom('[data-test-owners] li').exists({ count: 4 });
-});
+        assert.dom('[data-test-owners] li').exists({ count: 4 });
+    });
 
-test('crates having team-owners', async function(assert) {
-    server.loadFixtures();
+    test('crates having team-owners', async function(assert) {
+        server.loadFixtures();
 
-    await visit('/crates/nanomsg');
+        await visit('/crates/nanomsg');
 
-    assert.dom('[data-test-owners] [data-test-team-link="github:org:thehydroimpulse"]').exists();
-    assert.dom('[data-test-owners] li').exists({ count: 4 });
-});
+        assert.dom('[data-test-owners] [data-test-team-link="github:org:thehydroimpulse"]').exists();
+        assert.dom('[data-test-owners] li').exists({ count: 4 });
+    });
 
-test('crates license is supplied by version', async function(assert) {
-    server.loadFixtures();
+    test('crates license is supplied by version', async function(assert) {
+        server.loadFixtures();
 
-    await visit('/crates/nanomsg');
-    assert.dom('[data-test-license]').hasText('Apache-2.0');
+        await visit('/crates/nanomsg');
+        assert.dom('[data-test-license]').hasText('Apache-2.0');
 
-    await click('[data-test-version-link="0.5.0"]');
-    assert.dom('[data-test-license]').hasText('MIT/Apache-2.0');
-});
+        await click('[data-test-version-link="0.5.0"]');
+        assert.dom('[data-test-license]').hasText('MIT/Apache-2.0');
+    });
 
-test('navigating to the owners page when not logged in', async function(assert) {
-    server.loadFixtures();
+    test('navigating to the owners page when not logged in', async function(assert) {
+        server.loadFixtures();
 
-    await visit('/crates/nanomsg');
+        await visit('/crates/nanomsg');
 
-    assert.dom('#crate-owners p a').doesNotExist();
-});
+        assert.dom('#crate-owners p a').doesNotExist();
+    });
 
-test('navigating to the owners page when not an owner', async function(assert) {
-    server.loadFixtures();
+    test('navigating to the owners page when not an owner', async function(assert) {
+        server.loadFixtures();
 
-    this.application.__deprecatedInstance__.register('service:session', Service.extend({
-        currentUser: {
-            login: 'iain8'
-        },
-        loadUser() {},
-    }));
+        this.owner.register('service:session', Service.extend({
+            currentUser: {
+                login: 'iain8'
+            },
+            loadUser() {},
+        }));
 
-    await visit('/crates/nanomsg');
+        await visit('/crates/nanomsg');
 
-    assert.dom('#crate-owners p a').doesNotExist();
-});
+        assert.dom('#crate-owners p a').doesNotExist();
+    });
 
-test('navigating to the owners page', async function(assert) {
-    server.loadFixtures();
+    test('navigating to the owners page', async function(assert) {
+        server.loadFixtures();
 
-    this.application.__deprecatedInstance__.register('service:session', Service.extend({
-        currentUser: {
-            login: 'thehydroimpulse'
-        },
-        loadUser() {},
-    }));
+        this.owner.register('service:session', Service.extend({
+            currentUser: {
+                login: 'thehydroimpulse'
+            },
+            loadUser() {},
+        }));
 
-    await visit('/crates/nanomsg');
-    await click('#crate-owners p a');
+        await visit('/crates/nanomsg');
+        await click('#crate-owners p a');
 
-    assert.dom('#crates-heading h1').hasText('Manage Crate Owners');
-});
+        assert.dom('#crates-heading h1').hasText('Manage Crate Owners');
+    });
 
-test('listing crate owners', async function(assert) {
-    server.loadFixtures();
+    test('listing crate owners', async function(assert) {
+        server.loadFixtures();
 
-    await visit('/crates/nanomsg/owners');
+        await visit('/crates/nanomsg/owners');
 
-    assert.dom('.owners .row').exists({ count: 2 });
-    assert.dom('a[href="/users/thehydroimpulse"]').exists();
-    assert.dom('a[href="/users/blabaere"]').exists();
-});
+        assert.dom('.owners .row').exists({ count: 2 });
+        assert.dom('a[href="/users/thehydroimpulse"]').exists();
+        assert.dom('a[href="/users/blabaere"]').exists();
+    });
 
-test('attempting to add owner without username', async function(assert) {
-    server.loadFixtures();
+    test('attempting to add owner without username', async function(assert) {
+        server.loadFixtures();
 
-    await visit('/crates/nanomsg/owners');
-    await click('#add-owner');
+        await visit('/crates/nanomsg/owners');
+        await click('#add-owner');
 
-    assert.dom('.error').exists();
-    assert.dom('.error').hasText('Please enter a username');
-    assert.dom('.owners .row').exists({ count: 2 });
-});
+        assert.dom('.error').exists();
+        assert.dom('.error').hasText('Please enter a username');
+        assert.dom('.owners .row').exists({ count: 2 });
+    });
 
-test('attempting to add non-existent owner', async function(assert) {
-    server.loadFixtures();
+    test('attempting to add non-existent owner', async function(assert) {
+        server.loadFixtures();
 
-    await visit('/crates/nanomsg/owners');
-    await fillIn('input[name="username"]', 'spookyghostboo');
-    await click('#add-owner');
+        await visit('/crates/nanomsg/owners');
+        await fillIn('input[name="username"]', 'spookyghostboo');
+        await click('#add-owner');
 
-    assert.dom('.error').exists();
-    assert.dom('.error').hasText('Error sending invite');
-    assert.dom('.owners .row').exists({ count: 2 });
-});
+        assert.dom('.error').exists();
+        assert.dom('.error').hasText('Error sending invite');
+        assert.dom('.owners .row').exists({ count: 2 });
+    });
 
-test('add a new owner', async function(assert) {
-    server.loadFixtures();
+    test('add a new owner', async function(assert) {
+        server.loadFixtures();
 
-    await visit('/crates/nanomsg/owners');
-    await fillIn('input[name="username"]', 'iain8');
-    await click('#add-owner');
+        await visit('/crates/nanomsg/owners');
+        await fillIn('input[name="username"]', 'iain8');
+        await click('#add-owner');
 
-    assert.dom('.invited').exists();
-    assert.dom('.invited').hasText('An invite has been sent to iain8');
-    assert.dom('.owners .row').exists({ count: 2 });
-});
+        assert.dom('.invited').exists();
+        assert.dom('.invited').hasText('An invite has been sent to iain8');
+        assert.dom('.owners .row').exists({ count: 2 });
+    });
 
-test('remove a crate owner', async function(assert) {
-    server.loadFixtures();
+    test('remove a crate owner', async function(assert) {
+        server.loadFixtures();
 
-    await visit('/crates/nanomsg/owners');
-    await click('.owners .row:first-child .remove-owner');
+        await visit('/crates/nanomsg/owners');
+        await click('.owners .row:first-child .remove-owner');
 
-    assert.dom('.removed').exists();
-    assert.dom('.owners .row').exists({ count: 1 });
+        assert.dom('.removed').exists();
+        assert.dom('.owners .row').exists({ count: 1 });
+    });
 });
