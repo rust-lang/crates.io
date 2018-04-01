7e9f2a8e2a1eaaadde175b3839e487af369db3a8
Jarek Radosz <jradosz@gmail.com>
2018-04-01 19:19:42 +0200

    Freeze the time in tests.
    Fixes remaining issues in Percy.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -49,5 +49,11 @@ module.exports = function(defaults) {
     // modules that you would like to import into your application
     // please specify an object with the list of modules as keys
     // along with the exports of each module as its value.
+    app.import('node_modules/timekeeper/lib/timekeeper.js', {
+        using: [
+            { transformation: 'cjs', as: 'timekeeper' },
+        ],
+    });
+
     return app.toTree();
 };
--- tests/helpers/setup-mirage.js
+++ tests/helpers/setup-mirage.js
@@ -1,5 +1,6 @@
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 import { faker } from 'ember-cli-mirage';
+import timekeeper from 'timekeeper';
 
 export default function(hooks) {
     setupMirage(hooks);
@@ -7,5 +8,10 @@ export default function(hooks) {
     // To have deterministic visual tests, the seed has to be constant
     hooks.beforeEach(function() {
         faker.seed(12345);
+        timekeeper.freeze(new Date('11/20/2017 12:00'));
+    });
+
+    hooks.afterEach(function() {
+        timekeeper.reset();
     });
 }
