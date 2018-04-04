6f83ebfedea7592a2923cfeadb06c0b17681609a
Jarek Radosz <jradosz@gmail.com>
2018-04-04 17:43:30 +0200

    Drop unnecessary `data` initialization.
    It is set in `afterModel` before being accessed by `setupController`.

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -5,8 +5,6 @@ import RSVP from 'rsvp';
 import AuthenticatedRoute from '../mixins/authenticated-route';
 
 export default Route.extend(AuthenticatedRoute, {
-    data: {},
-
     setupController(controller) {
         this._super(...arguments);
 
