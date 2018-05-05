f816e37dda9773addfea526503f49a9a4299a291
Justin Geibel <jtgeibel@gmail.com>
2018-05-05 18:54:36 -0400

    Implement basic conduit::Request support for a struct
    The current implementation doesn't handle a request body at all.

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,25 @@
 extern crate conduit;
+extern crate http;
 extern crate hyper;
+extern crate semver;
 
+use std::io::{Cursor, Read};
 use std::net::SocketAddr;
 
 use hyper::rt::Future;
 use hyper::service::service_fn_ok;
-use hyper::{Body, Request, Response, Server};
+use hyper::{Body, Method, Request, Response, Server, Version};
 
 #[derive(Debug)]
-struct Headers<'a>(&'a hyper::HeaderMap);
+struct Parts(http::request::Parts);
 
-impl<'a> conduit::Headers for Headers<'a> {
+impl conduit::Headers for Parts {
     /// Find all values associated with a header, or None.
     ///
     /// If the value of a header is not valid UTF-8, that value
     /// is replaced with the emtpy string.
     fn find(&self, key: &str) -> Option<Vec<&str>> {
-        let values = self.0
+        let values = self.0.headers
             .get_all(key)
             .iter()
             .map(|v| v.to_str().unwrap_or(""))
@@ -30,7 +33,7 @@ impl<'a> conduit::Headers for Headers<'a> {
     }
 
     fn has(&self, key: &str) -> bool {
-        self.0.contains_key(key)
+        self.0.headers.contains_key(key)
     }
 
     /// Returns a representation of all headers
@@ -38,7 +41,7 @@ impl<'a> conduit::Headers for Headers<'a> {
     /// There is currently a bug where keys with mutliple values will be duplicated.
     /// See: https://github.com/hyperium/http/issues/199
     fn all(&self) -> Vec<(&str, Vec<&str>)> {
-        let keys = self.0.keys();
+        let keys = self.0.headers.keys();
         let mut all = Vec::new();
         for key in keys {
             let key = key.as_str();
@@ -50,6 +53,102 @@ impl<'a> conduit::Headers for Headers<'a> {
     }
 }
 
+struct ConduitRequest {
+    parts: Parts,
+    body: Cursor<Vec<u8>>,
+    extensions: conduit::Extensions,
+}
+
+impl conduit::Request for ConduitRequest {
+    fn http_version(&self) -> semver::Version {
+        match self.parts.0.version {
+            Version::HTTP_09 => version(0, 9),
+            Version::HTTP_10 => version(1, 0),
+            Version::HTTP_11 => version(1, 1),
+            Version::HTTP_2 => version(2, 0),
+        }
+    }
+
+    fn conduit_version(&self) -> semver::Version {
+        version(0, 1)
+    }
+
+    fn method(&self) -> conduit::Method {
+        match self.parts.0.method {
+            Method::CONNECT => conduit::Method::Connect,
+            Method::DELETE => conduit::Method::Delete,
+            Method::GET => conduit::Method::Get,
+            Method::HEAD => conduit::Method::Head,
+            Method::OPTIONS => conduit::Method::Options,
+            Method::PATCH => conduit::Method::Patch,
+            Method::POST => conduit::Method::Post,
+            Method::PUT => conduit::Method::Put,
+            Method::TRACE => conduit::Method::Trace,
+            _ => conduit::Method::Other(self.parts.0.method.to_string()),
+        }
+    }
+
+    fn scheme(&self) -> conduit::Scheme {
+        match self.parts.0.uri.scheme_part() {
+            Some(s) if s.as_str() == "https" => conduit::Scheme::Https,
+            _ => conduit::Scheme::Http,
+        }
+    }
+
+    fn headers(&self) -> &conduit::Headers {
+        &self.parts
+    }
+
+    fn content_length(&self) -> Option<u64> {
+        Some(self.body.get_ref().len() as u64)
+    }
+
+    /// Always returns an address of 0.0.0.0:0
+    fn remote_addr(&self) -> SocketAddr {
+        ([0, 0, 0, 0], 0).into()
+    }
+
+    fn virtual_root(&self) -> Option<&str> {
+        None
+    }
+
+    fn path(&self) -> &str {
+        &self.parts.0.uri.path()
+    }
+
+    fn extensions(&self) -> &conduit::Extensions {
+        &self.extensions
+    }
+
+    fn mut_extensions(&mut self) -> &mut conduit::Extensions {
+        &mut self.extensions
+    }
+
+    fn host(&self) -> conduit::Host {
+        // FIXME: Ensure the hyper server always provides a host so that unwrap is okay
+        conduit::Host::Name(&self.parts.0.uri.host().unwrap())
+    }
+
+    fn query_string(&self) -> Option<&str> {
+        self.parts.0.uri.query()
+    }
+
+    fn body(&mut self) -> &mut Read {
+        self.body.set_position(0);
+        &mut self.body
+    }
+}
+
+impl ConduitRequest {
+    fn new(parts: Parts, body: Vec<u8>) -> ConduitRequest {
+        ConduitRequest {
+            parts,
+            body: Cursor::new(body),
+            extensions: conduit::Extensions::new(),
+        }
+    }
+}
+
 pub fn run(addr: SocketAddr) {
     let new_svc = || service_fn_ok(handler);
 
@@ -58,12 +157,24 @@ pub fn run(addr: SocketAddr) {
 }
 
 fn handler(request: Request<Body>) -> Response<Body> {
-    use conduit::Headers;
-    let headers = Headers(request.headers());
-    println!("{:?}", request.headers().keys_len());
+    use conduit::Request;
+
+    // FIXME: buffer body until it is received in full
+    let (parts, _body) = request.into_parts();
+    let request = ConduitRequest::new(Parts(parts), Vec::new());
     Response::new(Body::from(format!(
         "all: {:?}\nfind A: {:?}\n",
-        headers.all(),
-        headers.find("A")
+        request.headers().all(),
+        request.headers().find("A")
     )))
+}
+
+fn version(major: u64, minor: u64) -> semver::Version {
+    semver::Version {
+        major,
+        minor,
+        patch: 0,
+        pre: vec![],
+        build: vec![],
+    }
 }
