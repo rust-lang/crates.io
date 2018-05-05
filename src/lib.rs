7adc3936caa179126c382096cf163800c8571028
Justin Geibel <jtgeibel@gmail.com>
2018-05-04 20:42:46 -0400

    "Hello, world!" implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,20 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+extern crate hyper;
+
+use std::net::SocketAddr;
+
+use hyper::rt::Future;
+use hyper::service::service_fn_ok;
+use hyper::{Body, Response, Server};
+
+static TEXT: &str = "Hello, World!";
+
+pub struct ConduitServer;
+
+impl ConduitServer {
+    pub fn run(addr: SocketAddr) {
+        let new_svc = || service_fn_ok(|_req| Response::new(Body::from(TEXT)));
+
+        let server = Server::bind(&addr).serve(new_svc);
+        hyper::rt::run(server.map_err(|_| ()));
     }
 }
