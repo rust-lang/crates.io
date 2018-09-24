57036ba4db59966fd83042b9fbdff259ab92793a
Sean Griffin <sean@seantheprogrammer.com>
2018-09-24 17:10:11 -0600

    Replace curl with reqwest in on_call

--- src/on_call.rs
+++ src/on_call.rs
@@ -1,7 +1,5 @@
-use curl::easy::*;
-use serde_json;
+use reqwest::{self, header, StatusCode as Status};
 use std::env;
-use std::io::prelude::*;
 
 use util::*;
 
@@ -31,47 +29,27 @@ impl Event {
         let api_token = env::var("PAGERDUTY_API_TOKEN")?;
         let service_key = env::var("PAGERDUTY_INTEGRATION_KEY")?;
 
-        let mut headers = List::new();
-        headers.append("Accept: application/vnd.pagerduty+json;version=2")?;
-        headers.append(&format!("Authorization: Token token={}", api_token))?;
-        headers.append("Content-Type: application/json")?;
+        let mut response = reqwest::Client::new()
+            .post("https://events.pagerduty.com/generic/2010-04-15/create_event.json")
+            .header(header::ACCEPT, "application/vnd.pagerduty+json;version=2")
+            .header(header::AUTHORIZATION, format!("Token token={}", api_token))
+            .json(&FullEvent {
+                service_key,
+                event: self,
+            })
+            .send()?;
 
-        let mut handle = Easy::new();
-        handle.url("https://events.pagerduty.com/generic/2010-04-15/create_event.json")?;
-        handle.post(true)?;
-        handle.http_headers(headers)?;
-
-        let full_event = FullEvent {
-            service_key,
-            event: self,
-        };
-        let json_body = serde_json::to_string(&full_event)?;
-        let mut bytes_to_write = json_body.as_bytes();
-        let mut data = Vec::new();
-
-        {
-            let mut handle = handle.transfer();
-            handle
-                .read_function(|bytes| bytes_to_write.read(bytes).map_err(|_| ReadError::Abort))?;
-            handle.write_function(|buf| {
-                data.extend_from_slice(buf);
-                Ok(buf.len())
-            })?;
-            handle.perform()?;
-        }
-
-        match handle.response_code()? {
-            200 => Ok(()),
-            400 => {
-                let error = serde_json::from_slice::<InvalidEvent>(&data)?;
+        match response.status() {
+            s if s.is_success() => Ok(()),
+            Status::BAD_REQUEST => {
+                let error = response.json::<InvalidEvent>()?;
                 Err(internal(&format_args!("pagerduty error: {:?}", error)))
             }
-            403 => Err(internal("rate limited by pagerduty")),
+            Status::FORBIDDEN => Err(internal("rate limited by pagerduty")),
             n => {
-                let resp = String::from_utf8_lossy(&data);
                 Err(internal(&format_args!(
-                    "Got a non 200 response code from pagerduty: {} with {}",
-                    n, resp
+                    "Got a non 200 response code from pagerduty: {} with {:?}",
+                    n, response
                 )))
             }
         }
