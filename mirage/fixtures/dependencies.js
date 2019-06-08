7bf133716c8b7366fea9402c856d638b09f4a89d
critical-path <37241479+critical-path@users.noreply.github.com>
2019-06-07 22:42:17 -0400

    Update dependencies.js

--- mirage/fixtures/dependencies.js
+++ mirage/fixtures/dependencies.js
@@ -37,11 +37,11 @@ export default [
         crate_id: 'mock-build-dependency',
         default_features: true,
         features: '',
-        id: 111111,
+        id: 11111,
         kind: 'build',
         optional: false,
         req: '0.1.0',
         target: null,
-        version_id: 111111,
-    },
+        version_id: 11111,
+    }
 ];
