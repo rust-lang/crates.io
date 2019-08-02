cff7481e29b1ff55de3561c19fb2486f2320f9c7
Kazuyoshi Kato <kato.kazuyoshi@gmail.com>
2019-08-01 21:32:38 -0700

    Write /tmp/app-initialized from Fastboot-side

--- fastboot.js
+++ fastboot.js
@@ -1,10 +1,35 @@
+/* eslint-disable no-console */
+
 'use strict';
 
+const fs = require('fs');
 const FastBootAppServer = require('fastboot-app-server');
+
+// because fastboot-app-server uses cluster, but it might change in future
+const cluster = require('cluster');
+
+function writeAppInitializedWhenReady() {
+    let timeout;
+
+    timeout = setInterval(function() {
+        console.log('waiting backend');
+        if (fs.existsSync('/tmp/backend-initialized')) {
+            console.log('backend is up. let heroku know the app is ready');
+            fs.writeFileSync('/tmp/app-initialized', 'hello');
+            clearInterval(timeout);
+        } else {
+            console.log('backend is still not up');
+        }
+    }, 1000);
+}
 
 let server = new FastBootAppServer({
     distPath: 'dist',
     port: 9000,
 });
+
+if (!cluster.isWorker) {
+    writeAppInitializedWhenReady();
+}
 
 server.start();
