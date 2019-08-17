4bfd40c7a01f6eeb9e7afe7217284c866b3079ad
Sven Marnach <sven@marnach.net>
2019-08-17 21:55:44 +0200

    Refactor enqueue helper in the enqueue-job binary.

--- src/bin/enqueue-job.rs
+++ src/bin/enqueue-job.rs
@@ -1,18 +1,23 @@
 use cargo_registry::util::{human, CargoError, CargoResult};
 use cargo_registry::{db, tasks};
+use diesel::PgConnection;
 use std::env::args;
-use swirl::Job;
 
 fn main() -> CargoResult<()> {
+    let conn = db::connect_now()?;
     match &*args().nth(1).unwrap_or_default() {
-        "update_downloads" => enqueue(tasks::update_downloads()),
-        "dump_db" => enqueue(tasks::dump_db()),
+        "update_downloads" => tasks::update_downloads().enqueue(&conn),
+        "dump_db" => tasks::dump_db().enqueue(&conn),
         other => Err(human(&format!("Unrecognized job type `{}`", other))),
     }
 }
 
-fn enqueue<J: Job>(job: J) -> CargoResult<()> {
-    let conn = db::connect_now()?;
-    job.enqueue(&conn)
-        .map_err(|e| CargoError::from_std_error(e))
+/// Helper to map the `PerformError` returned by `swirl::Job::enqueue()` to a
+/// `CargoError`. Can be removed once `map_err()` isn't needed any more.
+trait Enqueue: swirl::Job {
+    fn enqueue(self, conn: &PgConnection) -> CargoResult<()> {
+        <Self as swirl::Job>::enqueue(self, conn).map_err(|e| CargoError::from_std_error(e))
+    }
 }
+
+impl<J: swirl::Job> Enqueue for J {}
