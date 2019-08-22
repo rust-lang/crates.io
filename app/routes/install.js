fac77662a110909cd783b46d51b3f0ce5f698c12
Kazuyoshi Kato <kato.kazuyoshi@gmail.com>
2019-08-21 23:41:19 -0700

    Make /install work under FastBoot

--- app/routes/install.js
+++ app/routes/install.js
@@ -1,7 +1,20 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default Route.extend({
+  fastboot: service(),
+
   redirect() {
-    window.location = 'https://doc.rust-lang.org/cargo/getting-started/installation.html';
+    this._redirectTo('https://doc.rust-lang.org/cargo/getting-started/installation.html');
+  },
+
+  _redirectTo(url) {
+    if (this.fastboot.isFastBoot) {
+      let headers = this.fastboot.response.headers;
+      headers.set('location', url);
+      this.set('fastboot.response.statusCode', 301);
+    } else {
+      window.location = url;
+    }
   },
 });
