02562c2e3cf6f92454240ab24db37a064f0977d5
Ricardo Mendes <rokusu@gmail.com>
2019-09-21 12:58:28 +0100

    Make crate-readme component scroll to URL fragment if specified
    Fixes #1847

--- app/components/crate-readme.js
+++ app/components/crate-readme.js
@@ -7,5 +7,13 @@ export default Component.extend({
     this.$('pre > code').each(function() {
       window.Prism.highlightElement(this);
     });
+    this.scrollToFragment();
+  },
+
+  scrollToFragment() {
+    if (location.hash) {
+      let anchor_id = location.hash.substr(1);
+      document.getElementById(anchor_id).scrollIntoView();
+    }
   },
 });
