90f7533a3d9649a899b41f8ab7227c2367653742
Hanif Bin Ariffin <hanif.ariffin.4326@gmail.com>
2019-09-29 17:15:30 -0400

    Minute change

--- app/routes/crate/version.js
+++ app/routes/crate/version.js
@@ -11,7 +11,7 @@ export default Route.extend({
 
   flashMessages: service(),
 
-  refreshAfterLogin: observer('session.isLoggedIn', function () {
+  refreshAfterLogin: observer('session.isLoggedIn', function() {
     this.refresh();
   }),
 
