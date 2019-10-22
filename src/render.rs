c569e94d0f11a6769520abd073a3db7ecfffd78a
Andre Zanellato <andre.zanellato@gmail.com>
2019-10-22 20:48:48 -0300

    Use the default tag_attribute's as well

--- src/render.rs
+++ src/render.rs
@@ -23,11 +23,8 @@ impl<'a> MarkdownRenderer<'a> {
     /// relative links in the input document.  See that function for more detail.
     fn new(base_url: Option<&'a str>) -> MarkdownRenderer<'a> {
         let tag_attributes = hashmap(&[
-            ("a", hashset(&["href", "id", "target"])),
-            ("img", hashset(&["width", "height", "src", "alt", "align"])),
+            ("a", hashset(&["id", "target"])),
             ("input", hashset(&["checked", "disabled", "type"])),
-            ("th", hashset(&["rowspan", "colspan"])),
-            ("td", hashset(&["rowspan", "colspan"])),
         ]);
         let allowed_classes = hashmap(&[(
             "code",
@@ -54,7 +51,8 @@ impl<'a> MarkdownRenderer<'a> {
         html_sanitizer
             .add_tags(&["input"])
             .link_rel(Some("nofollow noopener noreferrer"))
-            .tag_attributes(tag_attributes)
+            .add_tag_attributes("a", &["id", "target"])
+            .add_tag_attributes("input", &["checked", "disabled", "type"])
             .allowed_classes(allowed_classes)
             .url_relative(sanitize_url)
             .id_prefix(Some("user-content-"));
