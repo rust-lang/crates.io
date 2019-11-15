2bdf7aedc7b943bb2cff8c164e9742552457d9b7
Kazuyoshi Kato <kato.kazuyoshi@gmail.com>
2019-11-14 23:07:13 -0800

    "wait -n" needs bash

--- script/start-web.sh
+++ script/start-web.sh
@@ -1,7 +1,7 @@
-#! /bin/sh
+#! /bin/bash
 set -ue
 
-if [ "${USE_FASTBOOT:-0}" = 1 ]; then
+if [[ "${USE_FASTBOOT:-0}" = 1 ]]; then
     export USE_FASTBOOT=1
     node --optimize_for_size --max_old_space_size=200 fastboot.js &
     bin/start-nginx ./target/release/server &
