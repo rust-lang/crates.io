dd5eeb6091298f2ce1257c6b69fc951e5670dd34
Hyungsuk Kang <hskang9@gmail.com>
2019-11-24 08:41:15 +0900

    Enable differentiation between - and _
    referred in #2775

--- migrations/20150319224700_dumped_migration_93/up.sql
+++ migrations/20150319224700_dumped_migration_93/up.sql
@@ -1,4 +1,1 @@
-CREATE FUNCTION canon_crate_name(text) RETURNS text AS $$
-                    SELECT replace(lower($1), '-', '_')
-                $$ LANGUAGE SQL
-            ;
+CREATE FUNCTION canon_crate_name(text) RETURNS text;
