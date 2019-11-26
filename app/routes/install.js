952e8ba5729a83bd5d672ad6abb4813a9b66b67f
Kazuyoshi Kato <kato.kazuyoshi@gmail.com>
2019-11-25 20:55:36 -0800

    Extract the redirection logic to a new service
    The service can be used from other routes that modify window.location.

--- app/routes/install.js
+++ app/routes/install.js
@@ -2,19 +2,9 @@ import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 
 export default Route.extend({
-  fastboot: service(),
+  redirector: service(),
 
   redirect() {
-    this._redirectTo('https://doc.rust-lang.org/cargo/getting-started/installation.html');
-  },
-
-  _redirectTo(url) {
-    if (this.fastboot.isFastBoot) {
-      let headers = this.fastboot.response.headers;
-      headers.set('location', url);
-      this.set('fastboot.response.statusCode', 301);
-    } else {
-      window.location = url;
-    }
+    this.redirector.redirectTo('https://doc.rust-lang.org/cargo/getting-started/installation.html');
   },
 });
