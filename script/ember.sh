8e02c1e81295f3a7badb65dcc47abcfbab053d11
Justin Geibel <jtgeibel@gmail.com>
2019-12-05 20:08:48 -0500

    Fix logic inversion in `script/ember.sh`

--- script/ember.sh
+++ script/ember.sh
@@ -4,9 +4,9 @@ set -ue
 export FASTBOOT_DISABLED
 
 if [ -z "${USE_FASTBOOT-}" ]; then
-    unset FASTBOOT_DISABLED
-else
     FASTBOOT_DISABLED=1
+else
+    unset FASTBOOT_DISABLED
 fi
 
 ember "$@"
