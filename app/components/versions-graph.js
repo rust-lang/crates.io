c200569ba9d9571226822651bc370d080a5b4fb8
Simon Walker <s.r.walker101@googlemail.com>
2019-12-18 00:35:37 +0000

    Implements recent downloads graph by using new endpoint
    We call `fetch` to get the recent downloads. As `fetch` is async, we
    must put most of the work in the async `.then` method.

--- app/components/versions-graph.js
+++ app/components/versions-graph.js
@@ -1,4 +1,5 @@
 import semver from 'semver';
+import fetch from 'fetch';
 import Component from '@ember/component';
 
 // Component to plot downloadeds split by semver minor version.
@@ -21,68 +22,80 @@ export default Component.extend({
   didRender() {
     this._super(...arguments);
 
-    let data = this.data;
+    let data = this.model.versions;
 
     // Early exit if the google plotting libraries do not exist
     let show = data && window.google && window.googleChartsLoaded;
     this.element.style.display = show ? '' : 'none';
     if (!show) {
-      console.error('cannot show graph due to missing google dependencies');
       return;
     }
 
-    // Build up the list of unique major.minor versions of this crate, and total
-    // up the number of downloads for each semver version
-    let downloadsPerVersion = new Map();
-    data.forEach(v => {
-      let major = semver.major(v.num);
-      let minor = semver.minor(v.num);
-      let downloads = v.downloads;
+    // Fetch the recent downloads from the API
+    fetch(`/api/v1/crates/${this.model.name}/recent_downloads`)
+      .then(async r => {
+        if (!r.ok) {
+          console.error('error fetching recent downloads from API');
+          return;
+        }
 
-      // XXX ugly hack to get semver to parse the version correctly later on.
-      // We want to do a semver-aware sort, but the `semver.lt` function only
-      // understands version triples, not doubles.
-      let key = `${major}.${minor}.0`;
-      if (downloadsPerVersion.has(key)) {
-        let old = downloadsPerVersion.get(key);
-        downloadsPerVersion.set(key, old + downloads);
-      } else {
-        downloadsPerVersion.set(key, downloads);
-      }
-    });
+        // TODO: error handling
+        const data = await r.json();
 
-    // Build up the plotting data
-    let plotData = [
-      // Headings and the nature of additional parameters
-      ['Version', 'Downloads', { role: 'style' }, { role: 'annotation' }],
-    ];
+        // Build up the list of unique major.minor versions of this crate, and total
+        // up the number of downloads for each semver version
+        let downloadsPerVersion = new Map();
+        data.downloads.forEach(v => {
+          let major = semver.major(v.version);
+          let minor = semver.minor(v.version);
+          let downloads = v.downloads;
 
-    // Update plotData with rows in the correct format for google visualization library
-    for (let [key, value] of sortIncreasingBySemver(downloadsPerVersion)) {
-      plotData.push([key, value, '#62865f', value]);
-    }
+          // XXX ugly hack to get semver to parse the version correctly later on.
+          // We want to do a semver-aware sort, but the `semver.lt` function only
+          // understands version triples, not doubles.
+          let key = `${major}.${minor}.0`;
+          if (downloadsPerVersion.has(key)) {
+            let old = downloadsPerVersion.get(key);
+            downloadsPerVersion.set(key, old + downloads);
+          } else {
+            downloadsPerVersion.set(key, downloads);
+          }
+        });
 
-    let myData = window.google.visualization.arrayToDataTable(plotData);
+        // Build up the plotting data
+        let plotData = [
+          // Headings and the nature of additional parameters
+          ['Version', 'Downloads', { role: 'style' }, { role: 'annotation' }],
+        ];
 
-    // Plot options
-    let options = {
-      chart: {
-        title: 'Downloads',
-      },
-      chartArea: { left: 85, width: '77%', height: '80%' },
-      hAxis: {
-        minorGridlines: { count: 8 },
-      },
-      vAxis: {
-        minorGridlines: { count: 5 },
-        viewWindow: { min: 0 },
-      },
-      legend: { position: 'none' },
-    };
+        // Update plotData with rows in the correct format for google visualization library
+        for (let [key, value] of sortIncreasingBySemver(downloadsPerVersion)) {
+          plotData.push([key, value, '#62865f', value]);
+        }
 
-    // Draw the plot into the current element
-    let chart = new window.google.visualization.BarChart(this.element);
-    chart.draw(myData, options);
+        let myData = window.google.visualization.arrayToDataTable(plotData);
+
+        // Plot options
+        let options = {
+          chart: {
+            title: 'Downloads',
+          },
+          chartArea: { left: 85, width: '77%', height: '80%' },
+          hAxis: {
+            minorGridlines: { count: 8 },
+          },
+          vAxis: {
+            minorGridlines: { count: 5 },
+            viewWindow: { min: 0 },
+          },
+          legend: { position: 'none' },
+        };
+
+        // Draw the plot into the current element
+        let chart = new window.google.visualization.BarChart(this.element);
+        chart.draw(myData, options);
+      })
+      .catch(e => console.error(`Error fetching data from API: ${e}`));
   },
 });
 
