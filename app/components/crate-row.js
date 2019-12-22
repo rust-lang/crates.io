7fbec76ab8ec427e9002e0290dd2dfe61e1d83c2
Tobias Bieniek <tobias.bieniek@gmail.com>
2019-12-22 11:06:34 +0100

    crate-row: Move `data-test-crate-row` property into the template

--- app/components/crate-row.js
+++ app/components/crate-row.js
@@ -6,6 +6,4 @@ export default Component.extend({
   crateTomlText: computed('crate.name', 'max_version', function() {
     return `${this.get('crate.name')} = "${this.get('crate.max_version')}"`;
   }),
-
-  'data-test-crate-row': true,
 });
