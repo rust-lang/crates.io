e6403a904c1b3499ac1cd98318b7c35738cfb69f
Denis Savchuk <denis.savchuk@vineti.com>
2019-12-26 18:30:53 +0400

    fix(component): Move test to right folder

--- tests/integration/components/flesh-message-test.js
+++ tests/integration/components/flesh-message-test.js
@@ -3,7 +3,7 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-module('Integration | Component | flash-message', function(hooks) {
+module('Integration | Component | flesh-message', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
