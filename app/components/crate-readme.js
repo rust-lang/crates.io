5d24bc2951ccc5c29b701a76ffd643aa9cb1d6ae
Tobias Bieniek <tobias.bieniek@gmail.com>
2019-12-26 18:42:47 +0100

    CrateReadme: Use `highlight-syntax` modifier

--- app/components/crate-readme.js
+++ app/components/crate-readme.js
@@ -1,13 +1,7 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  tagName: '',
+
   rendered: '',
-
-  didRender() {
-    this._super(...arguments);
-
-    this.element.querySelectorAll('pre > code').forEach(function(node) {
-      window.Prism.highlightElement(node);
-    });
-  },
 });
