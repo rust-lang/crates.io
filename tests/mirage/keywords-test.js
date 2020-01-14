b9128f32dc414831a6a24ce4113fd0f070a3d4a0
Tobias Bieniek <tobias.bieniek@gmail.com>
2020-01-14 21:41:19 +0100

    Add tests for `GET /api/v1/keywords/:id` mirage request handler

--- tests/mirage/keywords-test.js
+++ tests/mirage/keywords-test.js
@@ -82,4 +82,30 @@ module('Mirage | Keywords', function(hooks) {
       assert.equal(responsePayload.meta.total, 25);
     });
   });
+
+  module('GET /api/v1/keywords/:id', function() {
+    test('returns 404 for unknown keywords', async function(assert) {
+      let response = await fetch('/api/v1/keywords/foo');
+      assert.equal(response.status, 404);
+
+      let responsePayload = await response.json();
+      assert.deepEqual(responsePayload, { errors: [{ detail: 'Not Found' }] });
+    });
+
+    test('returns a keyword object for known keywords', async function(assert) {
+      this.server.create('keyword', { keyword: 'cli' });
+
+      let response = await fetch('/api/v1/keywords/cli');
+      assert.equal(response.status, 200);
+
+      let responsePayload = await response.json();
+      assert.deepEqual(responsePayload, {
+        keyword: {
+          id: 'cli',
+          crates_cnt: 0,
+          keyword: 'cli',
+        },
+      });
+    });
+  });
 });
