0f11af4c609365a3c13dfb591f150cf09539e682
Tobias Bieniek <tobias.bieniek@gmail.com>
2020-01-15 14:20:25 +0100

    Add tests for `GET /api/v1/crates/:id/downloads` mirage request handler

--- tests/mirage/crates-test.js
+++ tests/mirage/crates-test.js
@@ -868,4 +868,68 @@ module('Mirage | Keywords', function(hooks) {
       assert.equal(responsePayload.meta.total, 25);
     });
   });
+
+  module('GET /api/v1/crates/:id/downloads', function() {
+    test('returns 404 for unknown crates', async function(assert) {
+      let response = await fetch('/api/v1/crates/foo/downloads');
+      assert.equal(response.status, 404);
+
+      let responsePayload = await response.json();
+      assert.deepEqual(responsePayload, { errors: [{ detail: 'Not Found' }] });
+    });
+
+    test('empty case', async function(assert) {
+      this.server.create('crate', { name: 'rand' });
+
+      let response = await fetch('/api/v1/crates/rand/downloads');
+      assert.equal(response.status, 200);
+
+      let responsePayload = await response.json();
+      assert.deepEqual(responsePayload, {
+        version_downloads: [],
+        meta: {
+          extra_downloads: [],
+        },
+      });
+    });
+
+    test('returns a list of version downloads belonging to the specified crate version', async function(assert) {
+      this.server.create('crate', { name: 'rand' });
+      let versions = this.server.createList('version', 2, { crate: 'rand' });
+      this.server.create('version-download', { version: versions[0].id, date: '2020-01-13' });
+      this.server.create('version-download', { version: versions[1].id, date: '2020-01-14' });
+      this.server.create('version-download', { version: versions[1].id, date: '2020-01-15' });
+
+      let response = await fetch('/api/v1/crates/rand/downloads');
+      assert.equal(response.status, 200);
+
+      // TODO Remove the `id` properties from the response
+      let responsePayload = await response.json();
+      assert.deepEqual(responsePayload, {
+        version_downloads: [
+          {
+            id: '1',
+            date: '2020-01-13',
+            downloads: 9380,
+            version: '1',
+          },
+          {
+            id: '2',
+            date: '2020-01-14',
+            downloads: 16415,
+            version: '2',
+          },
+          {
+            id: '3',
+            date: '2020-01-15',
+            downloads: 23450,
+            version: '2',
+          },
+        ],
+        meta: {
+          extra_downloads: [],
+        },
+      });
+    });
+  });
 });
