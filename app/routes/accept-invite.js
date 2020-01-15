b12465e783d9fc3b4d99e2f87ab924d5b60fabc4
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2020-01-15 09:46:01 -0500

    Remove extra slash in URL

--- app/routes/accept-invite.js
+++ app/routes/accept-invite.js
@@ -4,7 +4,7 @@ import ajax from 'ember-fetch/ajax';
 export default Route.extend({
   async model(params) {
     try {
-      await ajax(`/api/v1//me/crate_owner_invitations/accept/${params.token}`, { method: 'PUT', body: '{}' });
+      await ajax(`/api/v1/me/crate_owner_invitations/accept/${params.token}`, { method: 'PUT', body: '{}' });
       this.set('response', { accepted: true });
       return { response: this.get('response') };
     } catch (error) {
