a911fece41534c38621bd3db076ee682dc4d869b
Tobias Bieniek <tobias.bieniek@gmail.com>
2020-02-02 17:27:08 +0100

    Remove obsolete `Promise` global override
    This is no longer needed in modern Ember.js projects

--- app/initializers/set-global-promise.js
+++
@@ -1,9 +0,0 @@
-import RSVP from 'rsvp';
-
-export function initialize() {
-  // async/await is using window.Promise by default and we want async/await to
-  // use RSVP instead which is properly integrated with Ember's runloop
-  window.Promise = RSVP.Promise;
-}
-
-export default { initialize };
