a64fc2a7d7c4c1f6736c9cf72920c089744142ef
denis_savchuk@Deniss-MacBook-Pro.local (RSA) <denis_savchuk@Deniss-MacBook-Pro.local (RSA)>
2020-02-04 14:07:58 -0800

    fix: Return flash message component to default state

--- app/components/flash-message.js
+++ app/components/flash-message.js
@@ -1,18 +1,5 @@
 import Component from '@ember/component';
-import { computed } from '@ember/object';
-import { readOnly } from '@ember/object/computed';
-import { inject as service } from '@ember/service';
 
 export default Component.extend({
-  flashMessages: service(),
-  message: readOnly('flashMessages.message'),
-  options: readOnly('flashMessages.options'),
-  type: computed('flashMessages.options', function() {
-    return this.get('flashMessages.options.type') || 'warning';
-  }),
-  classes: computed('message', 'type', function() {
-    return `${this.message ? 'show' : ''} ${this.type}`;
-  }),
-
   tagName: '',
 });
