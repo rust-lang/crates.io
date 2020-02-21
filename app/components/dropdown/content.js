668ba225b7376757bacde616dd64e3fae8e3afe6
Tobias Bieniek <tobias.bieniek@gmail.com>
2020-02-21 11:45:05 +0100

    Dropdown::Content: Remove `click()` handler
    All of our dropdowns contain only links, so `this.close()` will actually never be called.

--- app/components/dropdown/content.js
+++ app/components/dropdown/content.js
@@ -3,18 +3,4 @@ import Component from '@ember/component';
 export default Component.extend({
   classNames: ['rl-dropdown'],
   classNameBindings: ['isExpanded:open'],
-
-  click(event) {
-    let closeOnChildClick = 'a:link';
-    let $target = event.target;
-    let $c = this.element;
-
-    if ($target === $c) {
-      return;
-    }
-
-    if ($target.closest(closeOnChildClick, $c).length) {
-      this.close();
-    }
-  },
 });
