6054af96e813e334653c5a112abf13bdd1a4c1bc
Tobias Bieniek <tobias.bieniek@gmail.com>
2020-04-01 22:27:10 +0200

    routes/user: Convert `model()` to async/await

--- app/routes/user.js
+++ app/routes/user.js
@@ -1,6 +1,5 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
-import RSVP from 'rsvp';
 
 export default Route.extend({
   flashMessages: service(),
@@ -10,23 +9,21 @@ export default Route.extend({
     sort: { refreshModel: true },
   },
 
-  model(params) {
+  async model(params) {
     const { user_id } = params;
-    return this.store.queryRecord('user', { user_id }).then(
-      user => {
-        params.user_id = user.get('id');
-        params.include_yanked = 'n';
-        return RSVP.hash({
-          crates: this.store.query('crate', params),
-          user,
-        });
-      },
-      e => {
-        if (e.errors.some(e => e.detail === 'Not Found')) {
-          this.flashMessages.queue(`User '${params.user_id}' does not exist`);
-          return this.replaceWith('index');
-        }
-      },
-    );
+    try {
+      let user = await this.store.queryRecord('user', { user_id });
+
+      params.user_id = user.get('id');
+      params.include_yanked = 'n';
+      let crates = await this.store.query('crate', params);
+
+      return { crates, user };
+    } catch (e) {
+      if (e.errors.some(e => e.detail === 'Not Found')) {
+        this.flashMessages.queue(`User '${params.user_id}' does not exist`);
+        return this.replaceWith('index');
+      }
+    }
   },
 });
