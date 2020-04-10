28f5e8daa6bc9afaf7d4bc7c4c79f58f96c9d442
Tobias Bieniek <tobias.bieniek@gmail.com>
2020-04-10 22:52:29 +0200

    CrateRow: Inline computed `crateTomlText` property

--- app/components/crate-row.js
+++ app/components/crate-row.js
@@ -1,9 +1,5 @@
 import Component from '@ember/component';
-import { computed } from '@ember/object';
 
 export default Component.extend({
   tagName: '',
-  crateTomlText: computed('crate.name', 'max_version', function () {
-    return `${this.get('crate.name')} = "${this.get('crate.max_version')}"`;
-  }),
 });
