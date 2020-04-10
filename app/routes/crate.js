159a906ac2985be5d430edcd3aa1df474a65bc32
Tobias Bieniek <tobias.bieniek@gmail.com>
2020-04-10 19:08:09 +0200

    crate: Convert `model()` hook to async/await

--- app/routes/crate.js
+++ app/routes/crate.js
@@ -4,12 +4,14 @@ import { inject as service } from '@ember/service';
 export default Route.extend({
   flashMessages: service(),
 
-  model(params) {
-    return this.store.find('crate', params.crate_id).catch(e => {
+  async model(params) {
+    try {
+      return await this.store.find('crate', params.crate_id);
+    } catch (e) {
       if (e.errors.some(e => e.detail === 'Not Found')) {
         this.flashMessages.show(`Crate '${params.crate_id}' does not exist`);
       }
-    });
+    }
   },
 
   afterModel(model) {
