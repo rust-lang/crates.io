c0c9dcdab58fca915bc697f3278467cc42c9716d
Tobias Bieniek <tobias.bieniek@gmail.com>
2020-04-13 14:51:11 +0200

    dashboard: Merge `model()` and `afterModel()` hook

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -5,12 +5,12 @@ import RSVP from 'rsvp';
 import AuthenticatedRoute from '../mixins/authenticated-route';
 
 export default Route.extend(AuthenticatedRoute, {
-  setupController(controller) {
+  setupController(controller, model) {
     this._super(...arguments);
 
-    controller.set('myCrates', this.get('data.myCrates'));
-    controller.set('myFollowing', this.get('data.myFollowing'));
-    controller.set('myStats', this.get('data.myStats'));
+    controller.set('myCrates', model.myCrates);
+    controller.set('myFollowing', model.myFollowing);
+    controller.set('myStats', model.myStats);
 
     if (!controller.loadingMore) {
       controller.set('myFeed', A());
@@ -18,21 +18,13 @@ export default Route.extend(AuthenticatedRoute, {
     }
   },
 
-  model() {
-    return this.get('session.currentUser');
-  },
+  async model() {
+    let user = this.session.currentUser;
 
-  async afterModel(user) {
-    let myCrates = this.store.query('crate', {
-      user_id: user.get('id'),
-    });
-
-    let myFollowing = this.store.query('crate', {
-      following: 1,
-    });
-
+    let myCrates = this.store.query('crate', { user_id: user.get('id') });
+    let myFollowing = this.store.query('crate', { following: 1 });
     let myStats = user.stats();
 
-    this.set('data', await RSVP.hash({ myCrates, myFollowing, myStats }));
+    return await RSVP.hash({ myCrates, myFollowing, myStats });
   },
 });
