72268b88d974cc32e6877537515fec69ad7e31aa
Tobias Bieniek <tobias.bieniek@gmail.com>
2020-04-28 20:05:39 +0200

    keyword: Use async/await for `model()`

--- app/routes/keyword.js
+++ app/routes/keyword.js
@@ -4,12 +4,14 @@ import { inject as service } from '@ember/service';
 export default Route.extend({
   flashMessages: service(),
 
-  model({ keyword_id }) {
-    return this.store.find('keyword', keyword_id).catch(e => {
+  async model({ keyword_id }) {
+    try {
+      return await this.store.find('keyword', keyword_id);
+    } catch (e) {
       if (e.errors.some(e => e.detail === 'Not Found')) {
         this.flashMessages.queue(`Keyword '${keyword_id}' does not exist`);
         return this.replaceWith('index');
       }
-    });
+    }
   },
 });
