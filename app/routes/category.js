1cf5e0f67c84a4eb8ca1d9ca92191fc72babe9f8
Tobias Bieniek <tobias.bieniek@gmail.com>
2020-04-28 20:05:33 +0200

    category: Use async/await for `model()`

--- app/routes/category.js
+++ app/routes/category.js
@@ -4,12 +4,14 @@ import { inject as service } from '@ember/service';
 export default Route.extend({
   flashMessages: service(),
 
-  model(params) {
-    return this.store.find('category', params.category_id).catch(e => {
+  async model(params) {
+    try {
+      return await this.store.find('category', params.category_id);
+    } catch (e) {
       if (e.errors.some(e => e.detail === 'Not Found')) {
         this.flashMessages.queue(`Category '${params.category_id}' does not exist`);
         return this.replaceWith('index');
       }
-    });
+    }
   },
 });
