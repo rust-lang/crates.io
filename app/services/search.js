441aca5d49f7d60167d8f836aaf3692d3880f644
Tobias Bieniek <tobias.bieniek@gmail.com>
2020-04-30 11:33:18 +0200

    services/search: Simplify `inputValue` property
    There does not seem to be a need for this to be a computed property

--- app/services/search.js
+++ app/services/search.js
@@ -1,8 +1,6 @@
-import { oneWay } from '@ember/object/computed';
 import Service from '@ember/service';
 
 export default class SearchService extends Service {
   q = null;
-
-  @oneWay('q') inputValue;
+  inputValue = null;
 }
