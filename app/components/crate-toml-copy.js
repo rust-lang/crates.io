334b959750dd83878b9898094e0a77aff505379f
Tobias Bieniek <tobias@bieniek.cloud>
2020-06-17 21:47:31 +0200

    CrateTomlCopy: Remove redundant `showNotification` property

--- app/components/crate-toml-copy.js
+++ app/components/crate-toml-copy.js
@@ -7,13 +7,10 @@ import { rawTimeout, task } from 'ember-concurrency';
 
 export default class CrateTomlCopy extends Component {
   @tracked showSuccess = false;
-  @tracked showNotification = false;
 
   @(task(function* (isSuccess) {
     this.showSuccess = isSuccess;
-    this.showNotification = true;
     yield rawTimeout(2000);
-    this.showNotification = false;
   }).restartable())
   showNotificationTask;
 
