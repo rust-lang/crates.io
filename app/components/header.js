c732290a2e56294df4a84009c0f1bae82e85bc87
Tobias Bieniek <tobias@bieniek.cloud>
2020-06-17 19:57:24 +0200

    Header: Convert to ES6 class

--- app/components/header.js
+++ app/components/header.js
@@ -1,23 +1,23 @@
 import Component from '@ember/component';
+import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 
-export default Component.extend({
-  header: service(),
-  router: service(),
-  session: service(),
+export default class Header extends Component {
+  @service header;
+  @service router;
+  @service session;
 
-  tagName: '',
+  tagName = '';
 
-  actions: {
-    search(event) {
-      event.preventDefault();
+  @action
+  search(event) {
+    event.preventDefault();
 
-      this.router.transitionTo('search', {
-        queryParams: {
-          q: this.header.searchValue,
-          page: 1,
-        },
-      });
-    },
-  },
-});
+    this.router.transitionTo('search', {
+      queryParams: {
+        q: this.header.searchValue,
+        page: 1,
+      },
+    });
+  }
+}
