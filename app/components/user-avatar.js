a852f5dd2cf5b3968d9fe7bcab5ab66603308d70
Tobias Bieniek <tobias@bieniek.cloud>
2020-06-17 20:29:54 +0200

    UserAvatar: Remove obsolete `@computed` decorators

--- app/components/user-avatar.js
+++ app/components/user-avatar.js
@@ -1,9 +1,7 @@
-import { computed } from '@ember/object';
 import { readOnly } from '@ember/object/computed';
 import Component from '@glimmer/component';
 
 export default class UserAvatar extends Component {
-  @computed('args.size')
   get width() {
     if (this.args.size === 'medium') {
       return 85;
@@ -16,12 +14,10 @@ export default class UserAvatar extends Component {
 
   @readOnly('width') height;
 
-  @computed('args.user')
   get alt() {
     return `${this.args.user.name} (${this.args.user.login})`;
   }
 
-  @computed('width', 'args.user')
   get src() {
     return `${this.args.user.avatar}&s=${this.width * 2}`;
   }
