e48b2eaf2de29832f3d92a345ea70419bf5c5759
Tobias Bieniek <tobias@bieniek.cloud>
2020-06-17 20:28:07 +0200

    UserAvatar: Convert to glimmer component

--- app/components/user-avatar.js
+++ app/components/user-avatar.js
@@ -1,15 +1,13 @@
-import Component from '@ember/component';
 import { computed } from '@ember/object';
 import { readOnly } from '@ember/object/computed';
+import Component from '@glimmer/component';
 
 export default class UserAvatar extends Component {
-  tagName = '';
-
-  @computed('size')
+  @computed('args.size')
   get width() {
-    if (this.size === 'medium') {
+    if (this.args.size === 'medium') {
       return 85;
-    } else if (this.size === 'medium-small') {
+    } else if (this.args.size === 'medium-small') {
       return 32;
     } else {
       return 22; // small
@@ -18,13 +16,13 @@ export default class UserAvatar extends Component {
 
   @readOnly('width') height;
 
-  @computed('user')
+  @computed('args.user')
   get alt() {
-    return `${this.user.name} (${this.user.login})`;
+    return `${this.args.user.name} (${this.args.user.login})`;
   }
 
-  @computed('size', 'user')
+  @computed('width', 'args.user')
   get src() {
-    return `${this.user.avatar}&s=${this.width * 2}`;
+    return `${this.args.user.avatar}&s=${this.width * 2}`;
   }
 }
