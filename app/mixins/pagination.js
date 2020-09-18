4d397235c2d811d57a1a25cbb1b502d47303921e
Tobias Bieniek <tobias@bieniek.cloud>
2020-09-18 09:48:28 +0200

    mixins/pagination: Extract `pagination` computed property macro

--- app/mixins/pagination.js
+++ app/mixins/pagination.js
@@ -6,71 +6,71 @@ const VIEWABLE_PAGES = 9;
 
 // eslint-disable-next-line ember/no-new-mixins
 export default Mixin.create({
-  // wire up these ember-style variables to the expected query parameters
-  itemsPerPage: readOnly('per_page'),
-  selectedPage: readOnly('page'),
+  pagination: pagination(),
 
-  currentPage: computed('selectedPage', function () {
-    return parseInt(this.selectedPage, 10) || 1;
-  }),
+  currentPage: readOnly('pagination.currentPage'),
+  currentPageStart: readOnly('pagination.currentPageStart'),
+  currentPageEnd: readOnly('pagination.currentPageEnd'),
 
-  currentPageStart: computed('currentPage', 'itemsPerPage', 'totalItems', function () {
-    if (this.totalItems === 0) {
-      return 0;
-    }
-    return (this.currentPage - 1) * this.itemsPerPage + 1;
-  }),
+  availablePages: readOnly('pagination.availablePages'),
 
-  currentPageEnd: computed('currentPage', 'itemsPerPage', 'totalItems', function () {
-    return Math.min(this.currentPage * this.itemsPerPage, this.totalItems);
-  }),
+  nextPage: readOnly('pagination.nextPage'),
+  prevPage: readOnly('pagination.prevPage'),
 
-  availablePages: computed('totalItems', 'itemsPerPage', function () {
-    return Math.ceil(this.totalItems / this.itemsPerPage || 1);
-  }),
-
-  nextPage: computed('currentPage', 'availablePages', function () {
-    let nextPage = this.currentPage + 1;
-    let availablePages = this.availablePages;
-    if (nextPage <= availablePages) {
-      return nextPage;
-    } else {
-      return this.currentPage;
-    }
-  }),
-
-  prevPage: computed('currentPage', function () {
-    let prevPage = this.currentPage - 1;
-    if (prevPage > 0) {
-      return prevPage;
-    } else {
-      return this.currentPage;
-    }
-  }),
-
-  // Gives page numbers to the surrounding 9 pages.
-  pages: computed('currentPage', 'availablePages', function () {
-    let pages = [];
-    let currentPage = this.currentPage;
-    let availablePages = this.availablePages;
-    let lowerBound = 0;
-    let upperBound = 0;
-
-    // Always show the same number of pages even if we're
-    // at the beginning or at the end of the list.
-    if (availablePages - currentPage < Math.ceil(VIEWABLE_PAGES / 2)) {
-      lowerBound = Math.max(0, availablePages - VIEWABLE_PAGES);
-      upperBound = availablePages;
-    } else if (currentPage <= Math.ceil(VIEWABLE_PAGES / 2)) {
-      lowerBound = 0;
-      upperBound = Math.min(availablePages, VIEWABLE_PAGES);
-    } else {
-      lowerBound = currentPage - Math.ceil(VIEWABLE_PAGES / 2);
-      upperBound = currentPage + Math.floor(VIEWABLE_PAGES / 2);
-    }
-    for (let i = lowerBound; i < upperBound; i++) {
-      pages.push(i + 1);
-    }
-    return pages;
-  }),
+  pages: readOnly('pagination.pages'),
 });
+
+function pagination() {
+  return computed('page', 'per_page', 'totalItems', function () {
+    let { page, per_page: perPage, totalItems } = this;
+    return _pagination(page, perPage, totalItems);
+  });
+}
+
+function _pagination(page, perPage, totalItems) {
+  let currentPage = parseInt(page, 10) || 1;
+
+  let currentPageStart = totalItems === 0 ? 0 : (currentPage - 1) * perPage + 1;
+  let currentPageEnd = Math.min(currentPage * perPage, totalItems);
+
+  let availablePages = Math.ceil(totalItems / perPage || 1);
+
+  let nextPage = currentPage + 1;
+  if (nextPage > availablePages) {
+    nextPage = currentPage;
+  }
+
+  let prevPage = currentPage - 1;
+  if (prevPage <= 0) {
+    prevPage = currentPage;
+  }
+
+  // Always show the same number of pages even if we're
+  // at the beginning or at the end of the list.
+  let lowerBound, upperBound;
+  if (availablePages - currentPage < Math.ceil(VIEWABLE_PAGES / 2)) {
+    lowerBound = Math.max(0, availablePages - VIEWABLE_PAGES);
+    upperBound = availablePages;
+  } else if (currentPage <= Math.ceil(VIEWABLE_PAGES / 2)) {
+    lowerBound = 0;
+    upperBound = Math.min(availablePages, VIEWABLE_PAGES);
+  } else {
+    lowerBound = currentPage - Math.ceil(VIEWABLE_PAGES / 2);
+    upperBound = currentPage + Math.floor(VIEWABLE_PAGES / 2);
+  }
+
+  let pages = [];
+  for (let i = lowerBound; i < upperBound; i++) {
+    pages.push(i + 1);
+  }
+
+  return {
+    currentPage,
+    currentPageStart,
+    currentPageEnd,
+    availablePages,
+    nextPage,
+    prevPage,
+    pages,
+  };
+}
