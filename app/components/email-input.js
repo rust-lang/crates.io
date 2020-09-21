2ddca280fc7001959730a6371f9dff2770ab8c35
Tobias Bieniek <tobias@bieniek.cloud>
2020-09-21 22:36:00 +0200

    EmailInput: Convert to ES6 class

--- app/components/email-input.js
+++ app/components/email-input.js
@@ -1,38 +1,44 @@
 import Component from '@ember/component';
-import { computed } from '@ember/object';
+import { action, computed } from '@ember/object';
 import { empty } from '@ember/object/computed';
 
 import { task } from 'ember-concurrency';
 
 import ajax from '../utils/ajax';
 
-export default Component.extend({
-  tagName: '',
-  type: '',
-  value: '',
-  isEditing: false,
-  user: null,
-  disableSave: empty('user.email'),
-  notValidEmail: false,
-  prevEmail: '',
+export default class EmailInput extends Component {
+  tagName = '';
 
-  emailIsNull: computed('user.email', function () {
+  type = '';
+  value = '';
+  isEditing = false;
+  user = null;
+
+  @empty('user.email') disableSave;
+
+  notValidEmail = false;
+  prevEmail = '';
+
+  @computed('user.email')
+  get emailIsNull() {
     let email = this.get('user.email');
     return email == null;
-  }),
+  }
 
-  emailNotVerified: computed('user.{email,email_verified}', function () {
+  @computed('user.{email,email_verified}')
+  get emailNotVerified() {
     let email = this.get('user.email');
     let verified = this.get('user.email_verified');
 
     return email != null && !verified;
-  }),
+  }
 
-  isError: false,
-  emailError: '',
-  disableResend: false,
+  isError = false;
+  emailError = '';
+  disableResend = false;
 
-  resendButtonText: computed('disableResend', 'user.email_verification_sent', function () {
+  @computed('disableResend', 'user.email_verification_sent')
+  get resendButtonText() {
     if (this.disableResend) {
       return 'Sent!';
     } else if (this.get('user.email_verification_sent')) {
@@ -40,9 +46,9 @@ export default Component.extend({
     } else {
       return 'Send verification email';
     }
-  }),
+  }
 
-  resendEmailTask: task(function* () {
+  @task(function* () {
     let user = this.user;
 
     try {
@@ -57,62 +63,64 @@ export default Component.extend({
         this.set('emailError', 'Unknown error in resending message');
       }
     }
-  }),
+  })
+  resendEmailTask;
 
-  actions: {
-    editEmail() {
-      let email = this.value;
-      let isEmailNull = function (email) {
-        return email == null;
-      };
+  @action
+  editEmail() {
+    let email = this.value;
+    let isEmailNull = function (email) {
+      return email == null;
+    };
 
-      this.set('emailIsNull', isEmailNull(email));
-      this.set('isEditing', true);
-      this.set('prevEmail', this.value);
-    },
+    this.set('emailIsNull', isEmailNull(email));
+    this.set('isEditing', true);
+    this.set('prevEmail', this.value);
+  }
 
-    saveEmail() {
-      let userEmail = this.value;
-      let user = this.user;
+  @action
+  saveEmail() {
+    let userEmail = this.value;
+    let user = this.user;
 
-      let emailIsProperFormat = function (userEmail) {
-        let regExp = /^\S+@\S+\.\S+$/;
-        return regExp.test(userEmail);
-      };
+    let emailIsProperFormat = function (userEmail) {
+      let regExp = /^\S+@\S+\.\S+$/;
+      return regExp.test(userEmail);
+    };
 
-      if (!emailIsProperFormat(userEmail)) {
-        this.set('notValidEmail', true);
-        return;
-      }
+    if (!emailIsProperFormat(userEmail)) {
+      this.set('notValidEmail', true);
+      return;
+    }
 
-      user.set('email', userEmail);
-      user
-        .save()
-        .then(() => {
-          this.set('serverError', null);
-          this.set('user.email_verification_sent', true);
-          this.set('user.email_verified', false);
-        })
-        .catch(err => {
-          let msg;
-          if (err.errors && err.errors[0] && err.errors[0].detail) {
-            msg = `An error occurred while saving this email, ${err.errors[0].detail}`;
-          } else {
-            msg = 'An unknown error occurred while saving this email.';
-          }
-          this.set('serverError', msg);
-          this.set('isError', true);
-          this.set('emailError', `Error in saving email: ${msg}`);
-        });
+    user.set('email', userEmail);
+    user
+      .save()
+      .then(() => {
+        this.set('serverError', null);
+        this.set('user.email_verification_sent', true);
+        this.set('user.email_verified', false);
+      })
+      .catch(err => {
+        let msg;
+        if (err.errors && err.errors[0] && err.errors[0].detail) {
+          msg = `An error occurred while saving this email, ${err.errors[0].detail}`;
+        } else {
+          msg = 'An unknown error occurred while saving this email.';
+        }
+        this.set('serverError', msg);
+        this.set('isError', true);
+        this.set('emailError', `Error in saving email: ${msg}`);
+      });
 
-      this.set('isEditing', false);
-      this.set('notValidEmail', false);
-      this.set('disableResend', false);
-    },
+    this.set('isEditing', false);
+    this.set('notValidEmail', false);
+    this.set('disableResend', false);
+  }
 
-    cancelEdit() {
-      this.set('isEditing', false);
-      this.set('value', this.prevEmail);
-    },
-  },
-});
+  @action
+  cancelEdit() {
+    this.set('isEditing', false);
+    this.set('value', this.prevEmail);
+  }
+}
