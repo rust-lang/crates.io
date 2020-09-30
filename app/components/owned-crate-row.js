154dcef562b5012b401fad2abea91a9caf8524a4
Tobias Bieniek <tobias@bieniek.cloud>
2020-09-30 22:32:59 +0200

    OwnedCrateRow: Move `input` element into the `label`
    This removes the need for the `controlId` which makes the JS implementation simpler

--- app/components/owned-crate-row.js
+++ app/components/owned-crate-row.js
@@ -1,10 +1,7 @@
 import { action } from '@ember/object';
-import { guidFor } from '@ember/object/internals';
 import Component from '@glimmer/component';
 
 export default class OwnedCrateRow extends Component {
-  controlId = `${guidFor(this)}-checkbox`;
-
   @action setEmailNotifications(event) {
     let { checked } = event.target;
     this.args.ownedCrate.set('email_notifications', checked);
