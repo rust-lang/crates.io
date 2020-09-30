8fd745053c0d9accdc08ac301a09836e2f3081e9
Tobias Bieniek <tobias@bieniek.cloud>
2020-09-30 22:28:55 +0200

    OwnedCrateRow: Convert to Glimmer component

--- app/components/owned-crate-row.js
+++ app/components/owned-crate-row.js
@@ -1,14 +1,12 @@
-import Component from '@ember/component';
 import { action } from '@ember/object';
 import { guidFor } from '@ember/object/internals';
+import Component from '@glimmer/component';
 
 export default class OwnedCrateRow extends Component {
-  tagName = '';
-
   controlId = `${guidFor(this)}-checkbox`;
 
   @action setEmailNotifications(event) {
     let { checked } = event.target;
-    this.ownedCrate.set('email_notifications', checked);
+    this.args.ownedCrate.set('email_notifications', checked);
   }
 }
