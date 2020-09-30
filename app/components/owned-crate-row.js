1525e2e1c4e52f964da7234e3472abad85abbfe0
Tobias Bieniek <tobias@bieniek.cloud>
2020-09-30 22:17:40 +0200

    OwnedCrateRow: Inline `name` alias

--- app/components/owned-crate-row.js
+++ app/components/owned-crate-row.js
@@ -1,11 +1,8 @@
 import Component from '@ember/component';
 import { action, computed } from '@ember/object';
-import { alias } from '@ember/object/computed';
 
 export default class OwnedCrateRow extends Component {
   tagName = '';
-
-  @alias('ownedCrate.name') name;
 
   @computed('ownedCrate.id')
   get controlId() {
