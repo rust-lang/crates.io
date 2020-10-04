7bdb9f5c38e99a006b6dadb5bcf40a4cb8b4f570
Tobias Bieniek <tobias@bieniek.cloud>
2020-10-05 00:11:03 +0200

    utils/ajax: Throw an `Error` instance instead of a random object

--- app/utils/ajax.js
+++ app/utils/ajax.js
@@ -5,5 +5,17 @@ export default async function ajax(input, init) {
   if (response.ok) {
     return await response.json();
   }
-  throw response;
+  throw new HttpError(input, init, response);
+}
+
+export class HttpError extends Error {
+  constructor(url, init, response) {
+    let method = init.method ?? 'GET';
+    let message = `${method} ${url} failed with: ${response.status} ${response.statusText}`;
+    super(message);
+    this.name = 'HttpError';
+    this.method = method;
+    this.url = url;
+    this.response = response;
+  }
 }
