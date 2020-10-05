43baa414389f41fe983500fd2d61f453387e8bac
Tobias Bieniek <tobias@bieniek.cloud>
2020-10-05 10:01:32 +0200

    catch-all: Fix broken input field

--- app/controllers/catch-all.js
+++ app/controllers/catch-all.js
@@ -1,7 +1,10 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default class CatchAllController extends Controller {
+  @service header;
+
   @action search(query) {
     return this.transitionToRoute('search', { queryParams: { q: query } });
   }
