f2a3bbd8dc18dff35921e9d89a352ae3f4a837c9
Tobias Bieniek <tobias@bieniek.cloud>
2020-10-08 01:02:12 +0200

    adapters: Convert to native classes

--- app/adapters/crate-owner-invite.js
+++ app/adapters/crate-owner-invite.js
@@ -1,8 +1,9 @@
 import ApplicationAdapter from './application';
 
-export default ApplicationAdapter.extend({
-  namespace: 'api/v1/me',
+export default class CrateOwnerInviteAdapter extends ApplicationAdapter {
+  namespace = 'api/v1/me';
+
   pathForType() {
     return 'crate_owner_invitations';
-  },
-});
+  }
+}
--- app/adapters/user.js
+++ app/adapters/user.js
@@ -1,8 +1,8 @@
 import ApplicationAdapter from './application';
 
-export default ApplicationAdapter.extend({
+export default class UserAdapter extends ApplicationAdapter {
   queryRecord(store, type, query) {
     let url = this.urlForFindRecord(query.user_id, 'user');
     return this.ajax(url, 'GET');
-  },
-});
+  }
+}
