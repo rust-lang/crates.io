22b8ea178e8dd50f6efe054955cd0f338c846353
Tobias Bieniek <tobias@bieniek.cloud>
2020-10-18 23:06:25 +0200

    services/google-charts: Use `addEventListener()` instead of property assignment

--- app/services/google-charts.js
+++ app/services/google-charts.js
@@ -25,10 +25,10 @@ async function loadScript(src) {
   await new Promise((resolve, reject) => {
     const script = document.createElement('script');
     script.src = src;
-    script.onload = resolve;
-    script.onerror = event => {
+    script.addEventListener('load', resolve);
+    script.addEventListener('error', event => {
       reject(new ExternalScriptError(event.target.src));
-    };
+    });
     document.body.appendChild(script);
   });
 }
