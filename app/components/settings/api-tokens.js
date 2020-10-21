675abb1be15a1ef7e64f894e349dcbf38267247d
Tobias Bieniek <tobias@bieniek.cloud>
2020-10-22 00:56:22 +0200

    Inline `ApiTokenRow` component

--- app/components/settings/api-tokens.js
+++ app/components/settings/api-tokens.js
@@ -3,8 +3,11 @@ import { notEmpty, filterBy, sort } from '@ember/object/computed';
 import { inject as service } from '@ember/service';
 import Component from '@glimmer/component';
 
+import { task } from 'ember-concurrency';
+
 export default class ApiTokens extends Component {
   @service store;
+  @service notifications;
 
   tokenSort = ['created_at:desc'];
   @sort('args.tokens', 'tokenSort') sortedTokens;
@@ -16,4 +19,32 @@ export default class ApiTokens extends Component {
       created_at: new Date(Date.now() + 2000),
     });
   }
+
+  @task(function* (token) {
+    try {
+      yield token.save();
+    } catch (error) {
+      let msg =
+        error.errors && error.errors[0] && error.errors[0].detail
+          ? `An error occurred while saving this token, ${error.errors[0].detail}`
+          : 'An unknown error occurred while saving this token';
+
+      this.notifications.error(msg);
+    }
+  })
+  saveTokenTask;
+
+  @task(function* (token) {
+    try {
+      yield token.destroyRecord();
+    } catch (error) {
+      let msg =
+        error.errors && error.errors[0] && error.errors[0].detail
+          ? `An error occurred while revoking this token, ${error.errors[0].detail}`
+          : 'An unknown error occurred while revoking this token';
+
+      this.notifications.error(msg);
+    }
+  })
+  revokeTokenTask;
 }
