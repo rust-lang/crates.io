58119327eff80048395325784ad8599f96e0f67f
Tobias Bieniek <tobias@bieniek.cloud>
2020-10-29 13:55:31 +0100

    models/version-download: Add type comments

--- app/models/version-download.js
+++ app/models/version-download.js
@@ -1,7 +1,9 @@
 import Model, { belongsTo, attr } from '@ember-data/model';
 
 export default class VersionDownload extends Model {
+  /** @type number */
   @attr downloads;
+  /** @type string */
   @attr date;
 
   @belongsTo('version', { async: false }) version;
