d4bba5ea3d13e2f5869d7002fa479c20814264c9
Tobias Bieniek <tobias@bieniek.cloud>
2020-10-29 13:55:21 +0100

    models/version-download: Keep `date` attribute a string
    JS has no `DateWithoutTime` class, and using `Date` makes it unnecessarily complicated here, so we'll just keep it as a string and do the conversion only when needed.

--- app/models/version-download.js
+++ app/models/version-download.js
@@ -2,7 +2,7 @@ import Model, { belongsTo, attr } from '@ember-data/model';
 
 export default class VersionDownload extends Model {
   @attr downloads;
-  @attr('date') date;
+  @attr date;
 
   @belongsTo('version', { async: false }) version;
 }
