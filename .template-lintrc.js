2634313cfd0111c6f87ea3359953af4e9868c9bb
Tobias Bieniek <tobias@bieniek.cloud>
2020-11-04 15:42:13 +0100

    template-lint: Enable `a11y` preset

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -1,5 +1,14 @@
 'use strict';
 
 module.exports = {
-  extends: 'octane',
+  extends: ['octane', 'a11y'],
+
+  pending: [
+    { moduleId: 'app/components/email-input', only: ['require-input-label'] },
+    { moduleId: 'app/components/header', only: ['no-duplicate-landmark-elements'] },
+    // see https://github.com/ember-template-lint/ember-template-lint/issues/1604
+    { moduleId: 'app/components/pagination', only: ['no-invalid-link-title'] },
+    { moduleId: 'app/templates/catch-all', only: ['require-input-label'] },
+    { moduleId: 'app/components/settings/api-tokens', only: ['require-input-label'] },
+  ],
 };
