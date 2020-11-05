82edd838065598c6e10c3d6150a25b14015c3397
Tobias Bieniek <tobias@bieniek.cloud>
2020-11-05 10:55:51 +0100

    Implement `crates-admin` binary

--- script/ci/prune-cache.sh
+++ script/ci/prune-cache.sh
@@ -12,7 +12,7 @@ du -hs target/debug
 
 crate_name="cargo-registry"
 test_name="all"
-bin_names="background-worker delete-crate delete-version enqueue-job monitor populate render-readmes server test-pagerduty transfer-crates verify-token"
+bin_names="background-worker crates-admin delete-crate delete-version enqueue-job monitor populate render-readmes server test-pagerduty transfer-crates verify-token"
 
 normalized_crate_name=${crate_name//-/_}
 rm -vf target/debug/$normalized_crate_name-*
