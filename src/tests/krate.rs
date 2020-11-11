a244c3cb767cd8c8f2066b877062199650b8ed67
Tobias Bieniek <tobias@bieniek.cloud>
2020-11-11 23:05:31 +0100

    tests/krate: Flip `assert_eq!()` arguments
    The "expectation" part is usually the second argument, not the first.

--- src/tests/krate.rs
+++ src/tests/krate.rs
@@ -2491,12 +2491,12 @@ fn pagination_links_included_if_applicable() {
     let page3 = anon.search("page=3&per_page=1");
     let page4 = anon.search("page=4&per_page=1");
 
-    assert_eq!(Some("?per_page=1&page=2".to_string()), page1.meta.next_page);
-    assert_eq!(None, page1.meta.prev_page);
-    assert_eq!(Some("?page=3&per_page=1".to_string()), page2.meta.next_page);
-    assert_eq!(Some("?page=1&per_page=1".to_string()), page2.meta.prev_page);
-    assert_eq!(None, page4.meta.next_page);
-    assert_eq!(Some("?page=2&per_page=1".to_string()), page3.meta.prev_page);
+    assert_eq!(page1.meta.next_page, Some("?per_page=1&page=2".to_string()));
+    assert_eq!(page1.meta.prev_page, None);
+    assert_eq!(page2.meta.next_page, Some("?page=3&per_page=1".to_string()));
+    assert_eq!(page2.meta.prev_page, Some("?page=1&per_page=1".to_string()));
+    assert_eq!(page4.meta.next_page, None);
+    assert_eq!(page3.meta.prev_page, Some("?page=2&per_page=1".to_string()));
 }
 
 #[test]
