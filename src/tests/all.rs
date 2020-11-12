5d9ffc72b94e65566ec0dbbf569313e205643f9a
Justin Geibel <jtgeibel@gmail.com>
2020-11-11 20:01:33 -0500

    Apply clippy suggestions for comparison_to_empty

--- src/tests/all.rs
+++ src/tests/all.rs
@@ -167,7 +167,7 @@ fn build_app(
 #[track_caller]
 fn env(var: &str) -> String {
     match dotenv::var(var) {
-        Ok(ref s) if s == "" => panic!("environment variable `{}` must not be empty", var),
+        Ok(ref s) if s.is_empty() => panic!("environment variable `{}` must not be empty", var),
         Ok(s) => s,
         _ => panic!(
             "environment variable `{}` must be defined and valid unicode",
