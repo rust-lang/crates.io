572b77acfa02d4dee52c0564d8fbe33184c1f65d
Tobias Bieniek <tobias@bieniek.cloud>
2020-12-16 20:05:36 +0100

    qunit-dom: Add missing `setup()` call

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,8 +1,13 @@
 import { setApplication } from '@ember/test-helpers';
 import start from 'ember-exam/test-support/start';
+import * as QUnit from 'qunit';
+
+import { setup } from 'qunit-dom';
 
 import Application from '../app';
 import config from '../config/environment';
+
+setup(QUnit.assert);
 
 setApplication(Application.create(config.APP));
 
