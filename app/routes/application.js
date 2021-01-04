3775157c690a85653ebcdfb3263155a98bbccb2d
Tobias Bieniek <tobias@bieniek.cloud>
2021-01-04 14:34:37 +0100

    Revert "Load `Intl` polyfills, if needed"
    This reverts commit cfeff4d073f4a1857a05b8c3f584f8a7f1ef4eb0.
    
    This is no longer needed, now that we use `toLocaleString()` instead

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,17 +2,11 @@ import { action } from '@ember/object';
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 
-import { shouldPolyfill as shouldPolyfillGetCanonicalLocales } from '@formatjs/intl-getcanonicallocales/should-polyfill';
-import { shouldPolyfill as shouldPolyfillLocale } from '@formatjs/intl-locale/should-polyfill';
-import { shouldPolyfill as shouldPolyfillNumberFormat } from '@formatjs/intl-numberformat/should-polyfill';
-import { shouldPolyfill as shouldPolyfillPluralRules } from '@formatjs/intl-pluralrules/should-polyfill';
-
 export default class ApplicationRoute extends Route {
-  @service notifications;
   @service progress;
   @service session;
 
-  async beforeModel() {
+  beforeModel() {
     // trigger the task, but don't wait for the result here
     //
     // we don't need a `catch()` block here because network
@@ -21,35 +15,6 @@ export default class ApplicationRoute extends Route {
     //
     // eslint-disable-next-line ember-concurrency/no-perform-without-catch
     this.session.loadUserTask.perform();
-
-    // load `Intl` polyfills if necessary
-    let polyfillImports = [];
-    if (shouldPolyfillGetCanonicalLocales()) {
-      console.debug('Loading Intl.getCanonicalLocales() polyfill…');
-      polyfillImports.push(import('@formatjs/intl-getcanonicallocales/polyfill'));
-    }
-    if (shouldPolyfillLocale()) {
-      console.debug('Loading Intl.Locale polyfill…');
-      polyfillImports.push(import('@formatjs/intl-locale/polyfill'));
-    }
-    if (shouldPolyfillPluralRules()) {
-      console.debug('Loading Intl.PluralRules polyfill…');
-      polyfillImports.push(import('@formatjs/intl-pluralrules/polyfill'));
-      polyfillImports.push(import('@formatjs/intl-pluralrules/locale-data/en'));
-    }
-    if (shouldPolyfillNumberFormat()) {
-      console.debug('Loading Intl.NumberFormat polyfill…');
-      polyfillImports.push(import('@formatjs/intl-numberformat/polyfill'));
-      polyfillImports.push(import('@formatjs/intl-numberformat/locale-data/en'));
-    }
-
-    try {
-      await Promise.all(polyfillImports);
-    } catch {
-      let message =
-        'We tried to load some polyfill code for your browser, but network issues caused the request to fail. If you notice any issues please try to reload the page.';
-      this.notifications.warning(message);
-    }
   }
 
   @action loading(transition) {
