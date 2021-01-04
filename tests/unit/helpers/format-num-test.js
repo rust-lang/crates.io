4328db9ce2269b0d1a19276364b432aa7428bac8
Tobias Bieniek <tobias@bieniek.cloud>
2021-01-04 14:28:51 +0100

    tests/helpers/format-num: Convert to rendering test
    This removes the tight coupling on the implementation details of the helper and allows us to refactor it.

--- tests/unit/helpers/format-num-test.js
+++ tests/unit/helpers/format-num-test.js
@@ -1,20 +1,31 @@
+import { render } from '@ember/test-helpers';
+import { setupRenderingTest } from 'ember-qunit';
 import { module, test } from 'qunit';
 
+import { hbs } from 'ember-cli-htmlbars';
 import window from 'ember-window-mock';
 import { setupWindowMock } from 'ember-window-mock/test-support';
 
-import { formatNum } from '../../../helpers/format-num';
-
 module('Unit | Helper | format-num', function (hooks) {
+  setupRenderingTest(hooks);
   setupWindowMock(hooks);
 
-  test('it works', function (assert) {
+  test('it works', async function (assert) {
     window.navigator = { language: 'en' };
 
-    assert.equal(formatNum(42), '42');
-    assert.equal(formatNum(0), '0');
-    assert.equal(formatNum(0.2), '0.2');
-    assert.equal(formatNum(1000), '1,000');
-    assert.equal(formatNum(1000000), '1,000,000');
+    await render(hbs`{{format-num 42}}`);
+    assert.dom().hasText('42');
+
+    await render(hbs`{{format-num 0}}`);
+    assert.dom().hasText('0');
+
+    await render(hbs`{{format-num 0.2}}`);
+    assert.dom().hasText('0.2');
+
+    await render(hbs`{{format-num 1000}}`);
+    assert.dom().hasText('1,000');
+
+    await render(hbs`{{format-num 1000000}}`);
+    assert.dom().hasText('1,000,000');
   });
 });
