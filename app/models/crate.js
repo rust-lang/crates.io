9816e49c8e727ba3750e5a3ac8bfa7235cded1ee
Tobias Bieniek <tobias@bieniek.cloud>
2021-01-12 22:45:30 +0100

    models/crate: Add `defaultVersion` property

--- app/models/crate.js
+++ app/models/crate.js
@@ -31,6 +31,21 @@ export default class Crate extends Model {
   @hasMany('categories', { async: true }) categories;
   @hasMany('dependency', { async: true }) reverse_dependencies;
 
+  /**
+   * This is the default version that will be shown when visiting the crate
+   * details page. Note that this can be `undefined` if all versions of the crate
+   * have been yanked.
+   * @return {string}
+   */
+  get defaultVersion() {
+    if (this.max_stable_version) {
+      return this.max_stable_version;
+    }
+    if (this.max_version && this.max_version !== '0.0.0') {
+      return this.max_version;
+    }
+  }
+
   follow = memberAction({ type: 'PUT', path: 'follow' });
   unfollow = memberAction({ type: 'DELETE', path: 'follow' });
 
