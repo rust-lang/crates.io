86949fe1ce7c614fab02214dd3f9fbc05b673ff5
Tobias Bieniek <tobias@bieniek.cloud>
2021-01-13 19:10:37 +0100

    CrateRow: Display `defaultVersion` instead of `max_version`
    ... and hide the version if all versions are yanked

--- tests/components/crate-row-test.js
+++ tests/components/crate-row-test.js
@@ -10,7 +10,7 @@ module('Component | CrateRow', function (hooks) {
   setupRenderingTest(hooks);
   setupMirage(hooks);
 
-  test('shows crate name and highest version', async function (assert) {
+  test('shows crate name and highest stable version', async function (assert) {
     let crate = this.server.create('crate', { name: 'foo' });
     this.server.create('version', { crate, num: '1.0.0' });
     this.server.create('version', { crate, num: '1.2.3', yanked: true });
@@ -22,11 +22,26 @@ module('Component | CrateRow', function (hooks) {
 
     await render(hbs`<CrateRow @crate={{this.crate}} />`);
     assert.dom('[data-test-crate-link]').hasText('foo');
-    assert.dom('[data-test-version]').hasText('v2.0.0-beta.1');
+    assert.dom('[data-test-version]').hasText('v1.1.2');
     assert.dom('[data-test-copy-toml-button]').exists();
   });
 
-  test('shows crate name and `0.0.0` version if all versions are yanked', async function (assert) {
+  test('shows crate name and highest version, if there is no stable version available', async function (assert) {
+    let crate = this.server.create('crate', { name: 'foo' });
+    this.server.create('version', { crate, num: '1.0.0-beta.1' });
+    this.server.create('version', { crate, num: '1.0.0-beta.3' });
+    this.server.create('version', { crate, num: '1.0.0-beta.2' });
+
+    let store = this.owner.lookup('service:store');
+    this.crate = await store.findRecord('crate', crate.name);
+
+    await render(hbs`<CrateRow @crate={{this.crate}} />`);
+    assert.dom('[data-test-crate-link]').hasText('foo');
+    assert.dom('[data-test-version]').hasText('v1.0.0-beta.3');
+    assert.dom('[data-test-copy-toml-button]').exists();
+  });
+
+  test('shows crate name and no version if all versions are yanked', async function (assert) {
     let crate = this.server.create('crate', { name: 'foo' });
     this.server.create('version', { crate, num: '1.0.0', yanked: true });
     this.server.create('version', { crate, num: '1.2.3', yanked: true });
@@ -36,7 +51,7 @@ module('Component | CrateRow', function (hooks) {
 
     await render(hbs`<CrateRow @crate={{this.crate}} />`);
     assert.dom('[data-test-crate-link]').hasText('foo');
-    assert.dom('[data-test-version]').hasText('v0.0.0');
-    assert.dom('[data-test-copy-toml-button]').exists();
+    assert.dom('[data-test-version]').doesNotExist();
+    assert.dom('[data-test-copy-toml-button]').doesNotExist();
   });
 });
