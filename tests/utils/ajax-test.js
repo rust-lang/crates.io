8b2d149751adeff9ac6a16ef52034668508dbfcb
Tobias Bieniek <tobias@bieniek.cloud>
2021-01-28 17:38:12 +0100

    utils/ajax: Add error identification properties

--- tests/utils/ajax-test.js
+++ tests/utils/ajax-test.js
@@ -25,7 +25,7 @@ module('ajax()', function (hooks) {
     assert.deepEqual(response, { foo: 'bar' });
   });
 
-  test('throws an `HttpError` for non-2xx responses', async function (assert) {
+  test('throws an `HttpError` for 5xx responses', async function (assert) {
     this.server.get('/foo', { foo: 42 }, 500);
 
     await assert.rejects(ajax('/foo'), function (error) {
@@ -37,11 +37,46 @@ module('ajax()', function (hooks) {
       assert.equal(error.method, 'GET');
       assert.equal(error.url, '/foo');
       assert.ok(error.cause);
+      assert.true(error.isHttpError);
+      assert.true(error.isServerError);
+      assert.false(error.isClientError);
+      assert.false(error.isJsonError);
+      assert.false(error.isNetworkError);
 
       let { cause } = error;
       assert.ok(cause instanceof HttpError);
       assert.equal(cause.name, 'HttpError');
       assert.equal(cause.message, 'GET /foo failed with: 500 Internal Server Error');
+      assert.equal(cause.method, 'GET');
+      assert.equal(cause.url, '/foo');
+      assert.ok(cause.response);
+      assert.equal(cause.response.url, '/foo');
+      return true;
+    });
+  });
+
+  test('throws an `HttpError` for 4xx responses', async function (assert) {
+    this.server.get('/foo', { foo: 42 }, 404);
+
+    await assert.rejects(ajax('/foo'), function (error) {
+      let expectedMessage = 'GET /foo failed\n\ncaused by: HttpError: GET /foo failed with: 404 Not Found';
+
+      assert.ok(error instanceof AjaxError);
+      assert.equal(error.name, 'AjaxError');
+      assert.ok(error.message.startsWith(expectedMessage), error.message);
+      assert.equal(error.method, 'GET');
+      assert.equal(error.url, '/foo');
+      assert.ok(error.cause);
+      assert.true(error.isHttpError);
+      assert.false(error.isServerError);
+      assert.true(error.isClientError);
+      assert.false(error.isJsonError);
+      assert.false(error.isNetworkError);
+
+      let { cause } = error;
+      assert.ok(cause instanceof HttpError);
+      assert.equal(cause.name, 'HttpError');
+      assert.equal(cause.message, 'GET /foo failed with: 404 Not Found');
       assert.equal(cause.method, 'GET');
       assert.equal(cause.url, '/foo');
       assert.ok(cause.response);
@@ -62,6 +97,11 @@ module('ajax()', function (hooks) {
       assert.equal(error.method, 'GET');
       assert.equal(error.url, '/foo');
       assert.ok(error.cause);
+      assert.false(error.isHttpError);
+      assert.false(error.isServerError);
+      assert.false(error.isClientError);
+      assert.true(error.isJsonError);
+      assert.false(error.isNetworkError);
 
       let { cause } = error;
       assert.ok(!(cause instanceof HttpError));
@@ -85,6 +125,11 @@ module('ajax()', function (hooks) {
       assert.equal(error.method, 'GET');
       assert.equal(error.url, '/foo');
       assert.ok(error.cause);
+      assert.false(error.isHttpError);
+      assert.false(error.isServerError);
+      assert.false(error.isClientError);
+      assert.false(error.isJsonError);
+      assert.true(error.isNetworkError);
 
       let { cause } = error;
       assert.ok(!(cause instanceof HttpError));
