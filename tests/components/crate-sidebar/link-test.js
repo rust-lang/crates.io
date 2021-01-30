dd28b3ae7a058ae5e1b2cdac3d7501bf0cb86f78
Tobias Bieniek <tobias@bieniek.cloud>
2021-01-30 03:02:19 +0100

    CrateSidebar: Extract `Link` subcomponent

---
+++ tests/components/crate-sidebar/link-test.js
@@ -0,0 +1,19 @@
+import { render } from '@ember/test-helpers';
+import { module, test } from 'qunit';
+
+import { hbs } from 'ember-cli-htmlbars';
+
+import { setupRenderingTest } from 'cargo/tests/helpers';
+
+module('Component | CrateSidebar::Link', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('renders title and link', async function (assert) {
+    await render(hbs`<CrateSidebar::Link @title="Homepage" @url="https://www.rust-lang.org" />`);
+    assert.dom('[data-test-title]').hasText('Homepage');
+    assert
+      .dom('[data-test-link]')
+      .hasAttribute('href', 'https://www.rust-lang.org')
+      .hasText('https://www.rust-lang.org');
+  });
+});
