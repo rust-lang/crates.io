b9507a9bdc026d360e1ceef9e1abd61a9b56962b
Tobias Bieniek <tobias@bieniek.cloud>
2021-02-07 14:19:13 +0100

    CrateHeader: Allow `@version` to be undefined

--- app/components/crate-header.js
+++ app/components/crate-header.js
@@ -3,4 +3,8 @@ import Component from '@glimmer/component';
 
 export default class CrateHeader extends Component {
   @service session;
+
+  get documentationLink() {
+    return this.args.version?.documentationLink ?? this.args.crate.documentation;
+  }
 }
