accbec0080106b4d21f23806386886a96e332759
Tobias Bieniek <tobias@bieniek.cloud>
2021-02-09 00:26:27 +0100

    VersionList::Row: Improve release track indicator tooltip

--- app/components/version-list/row.js
+++ app/components/version-list/row.js
@@ -5,6 +5,29 @@ import Component from '@glimmer/component';
 export default class VersionRow extends Component {
   @service session;
 
+  get releaseTrackTitle() {
+    let { version } = this.args;
+    if (version.yanked) {
+      return 'This version was yanked';
+    }
+
+    let { releaseTrack } = version;
+
+    let modifiers = [];
+    if (version.isPrerelease) {
+      modifiers.push('prerelease');
+    }
+    if (version.isHighestOfReleaseTrack) {
+      modifiers.push('latest');
+    }
+
+    let title = `Release Track: ${releaseTrack}`;
+    if (modifiers.length !== 0) {
+      title += ` (${modifiers.join(', ')})`;
+    }
+    return title;
+  }
+
   @computed('args.version.crate.owner_user', 'session.currentUser.id')
   get isOwner() {
     return this.args.version.crate?.owner_user?.findBy('id', this.session.currentUser?.id);
