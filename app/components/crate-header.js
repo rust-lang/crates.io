912921ad632990a111912f881349941fbfbaa00b
Tobias Bieniek <tobias@bieniek.cloud>
2021-02-25 16:03:09 +0100

    Add navigation tabs to the crate details page

--- app/components/crate-header.js
+++ app/components/crate-header.js
@@ -2,5 +2,6 @@ import { inject as service } from '@ember/service';
 import Component from '@glimmer/component';
 
 export default class CrateHeader extends Component {
+  @service router;
   @service session;
 }
