b082f009954d8f8d12c2fc2bec8805adadfad2ab
Pietro Albini <pietro@pietroalbini.org>
2021-03-18 17:33:38 +0100

    downloads_counter: add tests for persist_* methods

--- src/downloads_counter.rs
+++ src/downloads_counter.rs
@@ -162,7 +162,7 @@ impl DownloadsCounter {
     }
 }
 
-#[derive(Debug, Default, Copy, Clone)]
+#[derive(Debug, Default, Copy, Clone, Eq, PartialEq)]
 pub struct PersistStats {
     shard: Option<usize>,
     counted_downloads: usize,
@@ -192,5 +192,190 @@ impl PersistStats {
             self.counted_downloads,
             self.pending_downloads,
         );
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::models::{Crate, NewCrate, NewUser, NewVersion, User};
+    use diesel::PgConnection;
+    use semver::Version;
+
+    #[test]
+    fn test_increment_and_persist_all() {
+        let counter = DownloadsCounter::new();
+        let conn = crate::db::test_conn();
+        let mut state = State::new(&conn);
+
+        let v1 = state.new_version(&conn);
+        let v2 = state.new_version(&conn);
+        let v3 = state.new_version(&conn);
+
+        // Add 15 downloads between v1 and v2, and no downloads for v3.
+        for _ in 0..10 {
+            counter.increment(v1);
+        }
+        for _ in 0..5 {
+            counter.increment(v2);
+        }
+        assert_eq!(15, counter.pending_count.load(Ordering::SeqCst));
+
+        // Persist everything to the database
+        let stats = counter
+            .persist_all_shards_with_conn(&conn)
+            .expect("failed to persist all shards");
+
+        // Ensure the stats are accurate
+        assert_eq!(
+            stats,
+            PersistStats {
+                shard: None,
+                counted_downloads: 15,
+                counted_versions: 2,
+                pending_downloads: 0,
+            }
+        );
+
+        // Ensure the download counts in the database are what we expect.
+        state.assert_downloads_count(&conn, v1, 10);
+        state.assert_downloads_count(&conn, v2, 5);
+        state.assert_downloads_count(&conn, v3, 0);
+    }
+
+    #[test]
+    fn test_increment_and_persist_shard() {
+        let counter = DownloadsCounter::new();
+        let conn = crate::db::test_conn();
+        let mut state = State::new(&conn);
+
+        let v1 = state.new_version(&conn);
+        let v1_shard = counter.inner.determine_map(&v1);
+
+        // For this test to work we need the two versions to be stored in different shards.
+        let mut v2 = state.new_version(&conn);
+        while counter.inner.determine_map(&v2) == v1_shard {
+            v2 = state.new_version(&conn);
+        }
+        let v2_shard = counter.inner.determine_map(&v2);
+
+        // Add 15 downloads between v1 and v2.
+        for _ in 0..10 {
+            counter.increment(v1);
+        }
+        for _ in 0..5 {
+            counter.increment(v2);
+        }
+        assert_eq!(15, counter.pending_count.load(Ordering::SeqCst));
+
+        // Persist one shard at the time and ensure the stats returned for each shard are expected.
+        let mut pending = 15;
+        for shard in 0..counter.shards_count() {
+            let stats = counter
+                .persist_next_shard_with_conn(&conn)
+                .expect("failed to persist shard");
+
+            if shard == v1_shard {
+                pending -= 10;
+                assert_eq!(
+                    stats,
+                    PersistStats {
+                        shard: Some(shard),
+                        counted_downloads: 10,
+                        counted_versions: 1,
+                        pending_downloads: pending,
+                    }
+                );
+                state.assert_downloads_count(&conn, v1, 10);
+            } else if shard == v2_shard {
+                pending -= 5;
+                assert_eq!(
+                    stats,
+                    PersistStats {
+                        shard: Some(shard),
+                        counted_downloads: 5,
+                        counted_versions: 1,
+                        pending_downloads: pending,
+                    }
+                );
+                state.assert_downloads_count(&conn, v2, 5);
+            } else {
+                assert_eq!(
+                    stats,
+                    PersistStats {
+                        shard: Some(shard),
+                        counted_downloads: 0,
+                        counted_versions: 0,
+                        pending_downloads: pending,
+                    }
+                );
+            };
+        }
+        assert_eq!(pending, 0);
+
+        // Finally ensure that the download counts in the database are what we expect.
+        state.assert_downloads_count(&conn, v1, 10);
+        state.assert_downloads_count(&conn, v2, 5);
+    }
+
+    struct State {
+        user: User,
+        krate: Crate,
+        next_version: u32,
+    }
+
+    impl State {
+        fn new(conn: &PgConnection) -> Self {
+            let user = NewUser {
+                gh_id: 0,
+                gh_login: "ghost",
+                ..NewUser::default()
+            }
+            .create_or_update(None, conn)
+            .expect("failed to create user");
+
+            let krate = NewCrate {
+                name: "foo",
+                ..NewCrate::default()
+            }
+            .create_or_update(conn, user.id, None)
+            .expect("failed to create crate");
+
+            Self {
+                user,
+                krate,
+                next_version: 1,
+            }
+        }
+
+        fn new_version(&mut self, conn: &PgConnection) -> i32 {
+            let version = NewVersion::new(
+                self.krate.id,
+                &Version::parse(&format!("{}.0.0", self.next_version)).unwrap(),
+                &HashMap::new(),
+                None,
+                None,
+                0,
+                self.user.id,
+            )
+            .expect("failed to create version")
+            .save(conn, &[], "ghost@example.com")
+            .expect("failed to save version");
+
+            self.next_version += 1;
+            version.id
+        }
+
+        fn assert_downloads_count(&self, conn: &PgConnection, version: i32, expected: i64) {
+            use crate::schema::version_downloads::dsl::*;
+            use diesel::dsl::*;
+
+            let actual: Option<i64> = version_downloads
+                .select(sum(downloads))
+                .filter(version_id.eq(version))
+                .first(conn)
+                .unwrap();
+            assert_eq!(actual.unwrap_or(0), expected);
+        }
     }
 }
