3e95f76e6c1b9e9b1c2be859f6383095b0762d0f
Tobias Bieniek <tobias@bieniek.cloud>
2021-04-06 22:30:13 +0200

    routes/accept-invite: Show specific error messages if available
    If the API responds with a specific error message we show that error instead of the generic one.

--- app/routes/accept-invite.js
+++ app/routes/accept-invite.js
@@ -6,9 +6,11 @@ export default class AcceptInviteRoute extends Route {
   async model(params) {
     try {
       await ajax(`/api/v1/me/crate_owner_invitations/accept/${params.token}`, { method: 'PUT', body: '{}' });
-      return { response: { accepted: true } };
-    } catch {
-      return { response: { accepted: false } };
+      return { ok: true };
+    } catch (error) {
+      let json = await error.json?.();
+      let errorText = json?.errors?.[0]?.detail;
+      return { ok: false, errorText };
     }
   }
 }
