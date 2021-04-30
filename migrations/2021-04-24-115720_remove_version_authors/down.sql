70c8e2a75dc933aea8829eb0d3f1f5272b824b55
二手掉包工程师 <rustin.liu@gmail.com>
2021-04-30 17:19:49 +0800

    Update migrations/2021-04-24-115720_remove_version_authors/down.sql
    Co-authored-by: Pietro Albini <pietro@pietroalbini.org>

--- migrations/2021-04-24-115720_remove_version_authors/down.sql
+++ migrations/2021-04-24-115720_remove_version_authors/down.sql
@@ -1,6 +1,6 @@
 CREATE TABLE version_authors(
     id SERIAL  NOT NULL CONSTRAINT version_authors_pkey PRIMARY KEY,
-    version_id INTEGER NOT NULL CONSTRAINT fk_version_authors_version_id REFERENCES versions ON DELETE CASCADED,
+    version_id INTEGER NOT NULL CONSTRAINT fk_version_authors_version_id REFERENCES versions ON DELETE CASCADE,
     name VARCHAR NOT NULL
 );
 
