a37b94abb59d843df5af90b5076123333a4aa7a3
Tobias Bieniek <tobias@bieniek.cloud>
2021-06-21 21:29:07 +0200

    mirage/team: Add `org` hidden property
    This can be used to create consistent teams more easily

--- mirage/factories/team.js
+++ mirage/factories/team.js
@@ -4,13 +4,14 @@ const ORGS = ['rust-lang', 'emberjs', 'rust-random', 'georust', 'actix'];
 
 export default Factory.extend({
   name: i => `team-${i + 1}`,
+  org: i => ORGS[i % ORGS.length],
 
-  login(i) {
-    return `github:${ORGS[i % ORGS.length]}:${this.name}`;
+  login() {
+    return `github:${this.org}:${this.name}`;
   },
 
-  url(i) {
-    return `https://github.com/${ORGS[i % ORGS.length]}`;
+  url() {
+    return `https://github.com/${this.org}`;
   },
 
   avatar: 'https://avatars1.githubusercontent.com/u/14631425?v=4',
