dbd00f843e319a720963bc5894a20b070f8492d0
Tobias Bieniek <tobias@bieniek.cloud>
2021-06-21 21:01:01 +0200

    models/team: Convert computed properties to plain getters
    `@attr` is auto-tracked these days, so the getters should recompute fine when things change

--- app/models/team.js
+++ app/models/team.js
@@ -1,5 +1,4 @@
 import Model, { attr } from '@ember-data/model';
-import { computed } from '@ember/object';
 
 export default class Team extends Model {
   @attr email;
@@ -10,15 +9,13 @@ export default class Team extends Model {
   @attr url;
   @attr kind;
 
-  @computed('login', function () {
+  get org_name() {
     let login = this.login;
     let login_split = login.split(':');
     return login_split[1];
-  })
-  org_name;
+  }
 
-  @computed('name', 'org_name', function () {
+  get display_name() {
     return `${this.org_name}/${this.name}`;
-  })
-  display_name;
+  }
 }
