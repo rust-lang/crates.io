ac9dba740bdd84def5d66f384da0061edc4381f5
Tobias Bieniek <tobias@bieniek.cloud>
2021-08-18 21:27:32 +0200

    Fix "Edge for Android" compatibility

--- config/targets.js
+++ config/targets.js
@@ -9,5 +9,11 @@ module.exports = {
     'last 1 Edge version',
     'last 1 UCAndroid version',
     'last 1 years',
+    // Edge for Android is currently (2021-08-18) using an outdated Chromium version (v77),
+    // which is not recognized by `browserslist` and `caniuse`. This can be removed once
+    // Edge for Android has promoted their "Edge for Android Beta" app to production, which
+    // is using a more up-to-date Chromium version.
+    // (see https://github.com/rust-lang/crates.io/issues/3838)
+    'Chrome 77',
   ],
 };
