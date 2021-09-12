b571353d1eb5f450b445f3b9577f0559cd5b60b1
Tobias Bieniek <tobias@bieniek.cloud>
2021-09-13 01:16:31 +0200

    sentry: Add documentation

--- src/sentry.rs
+++ src/sentry.rs
@@ -1,6 +1,14 @@
 use sentry::{ClientInitGuard, ClientOptions, IntoDsn};
 use std::borrow::Cow;
 
+/// Initializes the Sentry SDK from the environment variables.
+///
+/// If `SENTRY_DSN_API` is not set then Sentry will not be initialized,
+/// otherwise it is required to be a valid DSN string. `SENTRY_ENV_API` must
+/// be set if a DSN is provided.
+///
+/// `HEROKU_SLUG_COMMIT`, if present, will be used as the `release` property
+/// on all events.
 #[must_use]
 pub fn init() -> Option<ClientInitGuard> {
     dotenv::var("SENTRY_DSN_API")
