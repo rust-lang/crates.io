e33473c72debc1aa879c8d5e7efad4b6aa4db88e
Tobias Bieniek <tobias@bieniek.cloud>
2021-10-20 22:37:36 +0200

    crate.version: Use `macro-decorators` instead of computed properties

--- app/controllers/crate/version.js
+++ app/controllers/crate/version.js
@@ -1,14 +1,12 @@
 import Controller from '@ember/controller';
-import { computed } from '@ember/object';
-import { alias } from '@ember/object/computed';
 import { inject as service } from '@ember/service';
 
 import { task } from 'ember-concurrency';
+import { alias } from 'macro-decorators';
 
 export default class CrateVersionController extends Controller {
   @service session;
 
-  @computed('requestedVersion', 'currentVersion', 'crate')
   get downloadsContext() {
     return this.requestedVersion ? this.currentVersion : this.crate;
   }
@@ -18,7 +16,6 @@ export default class CrateVersionController extends Controller {
   @alias('model.requestedVersion') requestedVersion;
   @alias('model.version') currentVersion;
 
-  @computed('crate.owner_user', 'session.currentUser.id')
   get isOwner() {
     return this.crate.owner_user.findBy('id', this.session.currentUser?.id);
   }
