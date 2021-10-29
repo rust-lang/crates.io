fac7733a2c03a1f9bd35dc9246bbd7eca3fda9f2
Tobias Bieniek <tobias@bieniek.cloud>
2021-10-29 17:12:24 +0200

    worker: Add module doc comment

--- src/worker.rs
+++ src/worker.rs
@@ -1,3 +1,8 @@
+//! The `worker` module contains all the tasks that can be queued up for the
+//! background worker process to work on. This includes recurring tasks like
+//! the daily database maintenance, but also operations like rendering READMEs
+//! and uploading them to S3.
+
 mod daily_db_maintenance;
 pub mod dump_db;
 mod update_downloads;
