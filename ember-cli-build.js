1c58cb3ad49e414dd83279a9f814882f1d4683cd
Tobias Bieniek <tobias@bieniek.cloud>
2021-12-07 01:09:56 +0100

    embroider: Enable `staticAddonTestSupportTrees` option

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -47,6 +47,8 @@ module.exports = function (defaults) {
   if (USE_EMBROIDER) {
     const { Webpack } = require('@embroider/webpack');
     return require('@embroider/compat').compatBuild(app, Webpack, {
+      staticAddonTestSupportTrees: true,
+
       // see https://github.com/embroider-build/embroider/issues/823
       compatAdapters: new Map([['ember-get-config', null]]),
     });
