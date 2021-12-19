f1ee935994bef689281df937b0882abbdde8aac7
Tobias Bieniek <tobias@bieniek.cloud>
2021-12-19 20:40:09 +0100

    Add `Development Mode` test
    This can be used during development of pages that are only available if authenticated.

---
+++ tests/acceptance/dev-mode-test.js
@@ -0,0 +1,25 @@
+import { module, test } from 'qunit';
+
+import { setupApplicationTest } from 'cargo/tests/helpers';
+
+import { visit } from '../helpers/visit-ignoring-abort';
+
+let s = new URLSearchParams(window.location.search);
+if (s.has('devmode')) {
+  /**
+   * This module will only be available if the "Development Mode" option of QUnit is chosen
+   * @link http://localhost:4200/tests/?notrycatch&devmode&filter=Development%20Mode
+   */
+  module('Development Mode', function (hooks) {
+    setupApplicationTest(hooks);
+
+    test('authenticated', async function () {
+      let user = this.server.create('user');
+      this.authenticateAs(user);
+
+      await visit('/');
+      // eslint-disable-next-line ember/no-pause-test
+      await this.pauseTest();
+    });
+  });
+}
