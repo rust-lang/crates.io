6800250364fdab081ba54228845fa54aec627e1f
Tobias Bieniek <tobias@bieniek.cloud>
2021-12-19 20:39:07 +0100

    Adjust application test background color
    This ensures that the checker board pattern is used in rendering tests, but application tests use the correct background from the `body` element.

--- tests/helpers/index.js
+++ tests/helpers/index.js
@@ -10,4 +10,18 @@ export function setupApplicationTest(hooks, options) {
   upstreamSetupApplicationTest(hooks, options);
   setupMirage(hooks);
   setupSentryMock(hooks);
+  setupAppTestBodyClass(hooks);
+}
+
+function setupAppTestBodyClass(hooks) {
+  const ID = 'app-test';
+
+  hooks.beforeEach(function () {
+    this.bodyClass = this.owner.lookup('service:body-class');
+    this.bodyClass.register(ID, ['app-test']);
+  });
+
+  hooks.afterEach(function () {
+    this.bodyClass.deregister(ID);
+  });
 }
