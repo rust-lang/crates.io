13b08faa79bffd3ea45aeb071a47acab5be4e205
Tobias Bieniek <tobias@bieniek.cloud>
2021-12-21 19:58:37 +0100

    Adjust `/me` redirections to `/settings/tokens`

--- app/routes/me/index.js
+++ app/routes/me/index.js
@@ -5,6 +5,8 @@ export default class MeIndexRoute extends Route {
   @service router;
 
   redirect() {
-    this.router.replaceWith('settings');
+    // `cargo login` is showing links to https://crates.io/me to access the API tokens,
+    // so we need to keep this route and redirect the user to the API tokens settings page.
+    this.router.replaceWith('settings.tokens');
   }
 }
