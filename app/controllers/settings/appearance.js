03d5999eaa6f5bf8c352b261c14e8c1311348093
Tobias Bieniek <tobias@bieniek.cloud>
2021-12-26 18:40:01 +0100

    settings/appearance: Replace toggle button with radio buttons

--- app/controllers/settings/appearance.js
+++ app/controllers/settings/appearance.js
@@ -1,6 +1,15 @@
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 
 export default class AppearanceSettingsController extends Controller {
   @service design;
+
+  get theme() {
+    return this.design.useNewDesign ? 'new-design' : 'classic';
+  }
+
+  @action setTheme(theme) {
+    this.design.setNewDesign(theme === 'new-design');
+  }
 }
