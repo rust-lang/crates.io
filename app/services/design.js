dee6f98735026f23dc11f73c8fb320db4da592c4
Tobias Bieniek <tobias@bieniek.cloud>
2021-12-26 18:52:40 +0100

    services/design: Remove obsolete `toggle()` method

--- app/services/design.js
+++ app/services/design.js
@@ -1,8 +1,5 @@
-import { action } from '@ember/object';
 import Service, { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
-
-import window from 'ember-window-mock';
 
 import config from '../config/environment';
 import * as localStorage from '../utils/local-storage';
@@ -12,16 +9,6 @@ export default class DesignService extends Service {
 
   @tracked useNewDesign = !this.fastboot.isFastBoot && localStorage.getItem('use-new-design') === 'true';
   @tracked showToggleButton = config.environment === 'development' || config.environment === 'test';
-
-  constructor() {
-    super(...arguments);
-    window.toggleDesign = () => this.toggle();
-  }
-
-  @action
-  toggle() {
-    this.setNewDesign(!this.useNewDesign);
-  }
 
   setNewDesign(value) {
     this.useNewDesign = value;
