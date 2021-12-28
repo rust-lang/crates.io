be7d7d117a07033e21bfd1fbac175a538669ed68
Tobias Bieniek <tobias@bieniek.cloud>
2021-12-29 00:03:09 +0100

    Implement `processSearchQuery()` function

---
+++ app/utils/search.js
@@ -0,0 +1,34 @@
+const KEYWORDS_PREFIX = 'keywords:';
+
+/**
+ * Process a search query string and extract filters like `keywords:`.
+ *
+ * @param {string} query
+ * @return {{ q: string, keyword?: string, all_keywords?: string }}
+ */
+export function processSearchQuery(query) {
+  let tokens = query.trim().split(/\s+/);
+
+  let queries = [];
+  let keywords = [];
+  for (let token of tokens) {
+    if (token.startsWith(KEYWORDS_PREFIX)) {
+      keywords = token
+        .slice(KEYWORDS_PREFIX.length)
+        .split(',')
+        .map(it => it.trim())
+        .filter(Boolean);
+    } else {
+      queries.push(token);
+    }
+  }
+
+  let result = { q: queries.join(' ') };
+  if (keywords.length === 1) {
+    result.keyword = keywords[0];
+  } else if (keywords.length !== 0) {
+    result.all_keywords = keywords.join(' ');
+  }
+
+  return result;
+}
---
+++ tests/utils/search-test.js
@@ -0,0 +1,22 @@
+import { module, test } from 'qunit';
+
+import { processSearchQuery } from '../../utils/search';
+
+module('processSearchQuery()', function () {
+  const TESTS = [
+    ['foo', { q: 'foo' }],
+    ['  foo    bar     ', { q: 'foo bar' }],
+    ['foo keywords:bar', { q: 'foo', keyword: 'bar' }],
+    ['foo keywords:', { q: 'foo' }],
+    ['keywords:bar foo', { q: 'foo', keyword: 'bar' }],
+    ['foo \t   keywords:bar    baz', { q: 'foo baz', keyword: 'bar' }],
+    ['foo keywords:bar,baz', { q: 'foo', all_keywords: 'bar baz' }],
+    ['foo keywords:bar keywords:baz', { q: 'foo', keyword: 'baz' }],
+  ];
+
+  for (let [input, expectation] of TESTS) {
+    test(input, function (assert) {
+      assert.deepEqual(processSearchQuery(input), expectation);
+    });
+  }
+});
