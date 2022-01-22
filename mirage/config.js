17708300e1cf6d6dc5028240674646a6b8e96f4f
Tobias Bieniek <tobias@bieniek.cloud>
2022-01-22 16:43:05 +0100

    mirage: Migrate to new `makeServer()` API

--- mirage/config.js
+++ mirage/config.js
@@ -1,3 +1,5 @@
+import { createServer } from 'miragejs';
+
 import * as Categories from './route-handlers/categories';
 import * as Crates from './route-handlers/crates';
 import * as DocsRS from './route-handlers/docs-rs';
@@ -10,19 +12,25 @@ import * as Summary from './route-handlers/summary';
 import * as Teams from './route-handlers/teams';
 import * as Users from './route-handlers/users';
 
-export default function () {
-  Categories.register(this);
-  Crates.register(this);
-  DocsRS.register(this);
-  Invites.register(this);
-  Keywords.register(this);
-  Metadata.register(this);
-  Me.register(this);
-  Session.register(this);
-  Summary.register(this);
-  Teams.register(this);
-  Users.register(this);
+export default function makeServer(config) {
+  return createServer({
+    ...config,
 
-  // Used by ember-cli-code-coverage
-  this.passthrough('/write-coverage');
+    routes() {
+      Categories.register(this);
+      Crates.register(this);
+      DocsRS.register(this);
+      Invites.register(this);
+      Keywords.register(this);
+      Metadata.register(this);
+      Me.register(this);
+      Session.register(this);
+      Summary.register(this);
+      Teams.register(this);
+      Users.register(this);
+
+      // Used by ember-cli-code-coverage
+      this.passthrough('/write-coverage');
+    },
+  });
 }
