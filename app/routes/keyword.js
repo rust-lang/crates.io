8f62c4c51a4289b2160adba0ccea17b736a46efe
Tobias Bieniek <tobias@bieniek.cloud>
2022-01-23 01:00:51 +0100

    Merge `keyword.index` into parent route

--- app/routes/keyword.js
+++ app/routes/keyword.js
@@ -1,7 +1,24 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
-export default class KeywordRoute extends Route {
-  model({ keyword_id }) {
-    return keyword_id;
+export default class KeywordIndexRoute extends Route {
+  @service router;
+  @service store;
+
+  queryParams = {
+    page: { refreshModel: true },
+    sort: { refreshModel: true },
+  };
+
+  async model(params, transition) {
+    let { keyword_id: keyword, page, sort } = params;
+
+    try {
+      let crates = await this.store.query('crate', { keyword, page, sort });
+      return { keyword, crates };
+    } catch (error) {
+      let title = `${keyword}: Failed to load crates`;
+      this.router.replaceWith('catch-all', { transition, error, title, tryAgain: true });
+    }
   }
 }
