66982141ca7d9addacdf65794f09a5ce7331a14a
Tobias Bieniek <tobias@bieniek.cloud>
2022-04-06 19:11:17 +0200

    highlight.js: Fix CSS import

--- app/modifiers/highlight-syntax.js
+++ app/modifiers/highlight-syntax.js
@@ -1,6 +1,5 @@
 import { modifier } from 'ember-modifier';
 import hljs from 'highlight.js/lib/core';
-import 'highlight.js/styles/github.css';
 import bash from 'highlight.js/lib/languages/bash';
 import c from 'highlight.js/lib/languages/c';
 import cpp from 'highlight.js/lib/languages/cpp';
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -46,6 +46,7 @@ module.exports = function (defaults) {
   });
 
   app.import('node_modules/normalize.css/normalize.css', { prepend: true });
+  app.import('node_modules/highlight.js/styles/github.css');
   app.import('vendor/qunit.css', { type: 'test' });
 
   const { Webpack } = require('@embroider/webpack');
