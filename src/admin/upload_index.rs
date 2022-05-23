7a915287a95b8e01e6c83155d1ad599713adb551
Tobias Bieniek <tobias@bieniek.cloud>
2022-05-23 20:50:30 +0200

    admin/upload_index: Use `indicatif` for progress reporting

--- src/admin/upload_index.rs
+++ src/admin/upload_index.rs
@@ -1,7 +1,6 @@
-use std::time::{Duration, Instant};
-
 use crate::admin::dialoguer;
 use cargo_registry_index::{Repository, RepositoryConfig};
+use indicatif::{ProgressBar, ProgressIterator, ProgressStyle};
 use reqwest::blocking::Client;
 
 use crate::config;
@@ -33,23 +32,18 @@ pub fn run(opts: Opts) -> anyhow::Result<()> {
         return Ok(());
     }
 
-    let mut progress_update_time = Instant::now();
-    for (i, file) in files.iter().enumerate() {
+    let pb = ProgressBar::new(files.len() as u64);
+    pb.set_style(ProgressStyle::default_bar().template("{bar:60} ({pos}/{len}, ETA {eta})"));
+
+    for file in files.iter().progress_with(pb) {
         let crate_name = file.file_name().unwrap().to_str().unwrap();
         let path = repo.index_file(crate_name);
         if !path.exists() {
-            println!("skipping file `{}`", crate_name);
             continue;
         }
+
         let contents = std::fs::read_to_string(&path)?;
         uploader.upload_index(&client, crate_name, contents)?;
-
-        // Print a progress update every 10 seconds.
-        let now = Instant::now();
-        if now - progress_update_time > Duration::from_secs(10) {
-            progress_update_time = now;
-            println!("uploading {}/{}", i, files.len());
-        }
     }
 
     println!(
