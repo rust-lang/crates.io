9c2430a2e8740ceeb6819e24ec719ede9ddd7562
Billy Chan <ccw.billy.123@gmail.com>
2022-07-28 17:51:20 +0800

    Fix lints

--- app/controllers/crate/version.js
+++ app/controllers/crate/version.js
@@ -1,7 +1,7 @@
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
-import { action } from '@ember/object';
 
 import { task } from 'ember-concurrency';
 import { alias } from 'macro-decorators';
