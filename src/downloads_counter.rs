7314e5b2eac5b944123c1ec012d1a21f439cca8e
Arlo Siemsen <arsiem@microsoft.com>
2022-08-18 13:22:59 -0500

    Use BTreeMap for features to ensure stable sorting

--- src/downloads_counter.rs
+++ src/downloads_counter.rs
@@ -247,7 +247,7 @@ mod tests {
     use crate::models::{Crate, NewCrate, NewUser, NewVersion, User};
     use diesel::PgConnection;
     use semver::Version;
-    use std::collections::HashMap;
+    use std::collections::BTreeMap;
 
     #[test]
     fn test_increment_and_persist_all() {
@@ -452,7 +452,7 @@ mod tests {
             let version = NewVersion::new(
                 self.krate.id,
                 &Version::parse(&format!("{}.0.0", self.next_version)).unwrap(),
-                &HashMap::new(),
+                &BTreeMap::new(),
                 None,
                 None,
                 0,
