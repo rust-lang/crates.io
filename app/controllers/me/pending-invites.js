11fafa7cd58ce5d2754e7200e3283a42b0e3372c
Esteban Borai <estebanborai@gmail.com>
2022-09-18 21:14:23 -0300

    fix: remove unused `queryParams`

--- app/controllers/me/pending-invites.js
+++ app/controllers/me/pending-invites.js
@@ -5,8 +5,6 @@ import { reads } from 'macro-decorators';
 import { pagination } from '../../utils/seek-pagination';
 
 export default class PendingInvitesController extends Controller {
-  queryParams = ['page', 'per_page', 'sort'];
-
   @reads('model.meta.next_page') nextPage;
   @pagination() pagination;
 }
