d2a3fa7f0f28a83f43c505bf30c76a51a2110aa4
Tobias Bieniek <tobias@bieniek.cloud>
2022-11-16 19:21:30 +0100

    cloudfront: Retry requests on server or network error (#5479)

--- src/worker/cloudfront.rs
+++ src/worker/cloudfront.rs
@@ -6,6 +6,8 @@ use aws_sigv4::{
     SigningParams,
 };
 use reqwest::blocking::Client;
+use retry::delay::{jitter, Exponential};
+use retry::OperationResult;
 
 #[derive(Clone)]
 pub struct CloudFront {
@@ -35,9 +37,16 @@ impl CloudFront {
             "https://cloudfront.amazonaws.com/2020-05-31/distribution/{}/invalidation",
             self.distribution_id
         );
-        let now = chrono::offset::Utc::now().timestamp_micros();
-        let body = format!(
-            r#"
+
+        let backoff = Exponential::from_millis(1000).map(jitter).take(2);
+        retry::retry_with_index(backoff, |attempt| {
+            if attempt > 1 {
+                debug!(?attempt, "Retrying CloudFront invalidation…")
+            }
+
+            let now = chrono::offset::Utc::now().timestamp_micros();
+            let body = format!(
+                r#"
 <?xml version="1.0" encoding="UTF-8"?>
 <InvalidationBatch xmlns="http://cloudfront.amazonaws.com/doc/2020-05-31/">
     <CallerReference>{now}</CallerReference>
@@ -49,30 +58,61 @@ impl CloudFront {
     </Paths>
 </InvalidationBatch>
 "#
-        );
+            );
 
-        let request = http::Request::post(&url).body(&body)?;
-        let request = SignableRequest::from(&request);
-        let params = SigningParams::builder()
-            .access_key(&self.access_key)
-            .secret_key(&self.secret_key)
-            .region("us-east-1") // cloudfront is a regionless service, use the default region for signing.
-            .service_name("cloudfront")
-            .settings(SigningSettings::default())
-            .time(SystemTime::now())
-            .build()
-            .unwrap(); // all required fields are set
-        let (mut signature_headers, _) = http_request::sign(request, &params).unwrap().into_parts();
-        let response = client
-            .post(url)
-            .headers(signature_headers.take_headers().unwrap_or_default())
-            .body(body)
-            .send()?;
-        if let Some(err) = response.error_for_status_ref().err() {
-            let headers = format!("{:?}", response.headers());
-            let body = response.text()?;
-            return Err(err).context(format!("{headers}; body: {body}"));
-        }
-        Ok(())
+            let request = match http::Request::post(&url)
+                .body(&body)
+                .context("Failed to construct HTTP request")
+            {
+                Ok(request) => request,
+                Err(error) => return OperationResult::Err(error),
+            };
+
+            let request = SignableRequest::from(&request);
+            let params = SigningParams::builder()
+                .access_key(&self.access_key)
+                .secret_key(&self.secret_key)
+                .region("us-east-1") // cloudfront is a regionless service, use the default region for signing.
+                .service_name("cloudfront")
+                .settings(SigningSettings::default())
+                .time(SystemTime::now())
+                .build()
+                .unwrap(); // all required fields are set
+
+            let (mut signature_headers, _) =
+                http_request::sign(request, &params).unwrap().into_parts();
+
+            let response = match client
+                .post(&url)
+                .headers(signature_headers.take_headers().unwrap_or_default())
+                .body(body)
+                .send()
+                .context("Failed to send invalidation request")
+            {
+                Ok(response) => response,
+                Err(error) => return OperationResult::Retry(error),
+            };
+
+            let status = response.status();
+
+            let result = match response.error_for_status_ref() {
+                Ok(_) => Ok(()),
+                Err(error) => {
+                    let headers = format!("{:?}", response.headers());
+                    let body = response
+                        .text()
+                        .unwrap_or_else(|_| "Failed to decode response payload".to_string());
+
+                    Err(error).context(format!("{headers}; body: {body}"))
+                }
+            };
+
+            match result {
+                Ok(_) => OperationResult::Ok(()),
+                Err(error) if status.is_server_error() => OperationResult::Retry(error),
+                Err(error) => OperationResult::Err(error),
+            }
+        })
+        .map_err(|error| error.error)
     }
 }
