f90bf435222a85395dec67c8b51cad9e24626082
Tobias Bieniek <tobias@bieniek.cloud>
2022-11-24 15:40:40 +0100

    ApiToken: Extract `insert_with_scopes()` function

--- src/models/token.rs
+++ src/models/token.rs
@@ -36,6 +36,16 @@ pub struct ApiToken {
 impl ApiToken {
     /// Generates a new named API token for a user
     pub fn insert(conn: &PgConnection, user_id: i32, name: &str) -> AppResult<CreatedApiToken> {
+        Self::insert_with_scopes(conn, user_id, name, None, None)
+    }
+
+    pub fn insert_with_scopes(
+        conn: &PgConnection,
+        user_id: i32,
+        name: &str,
+        crate_scopes: Option<Vec<scopes::CrateScope>>,
+        endpoint_scopes: Option<Vec<scopes::EndpointScope>>,
+    ) -> AppResult<CreatedApiToken> {
         let token = SecureToken::generate(SecureTokenKind::Api);
 
         let model: ApiToken = diesel::insert_into(api_tokens::table)
@@ -43,6 +53,8 @@ impl ApiToken {
                 api_tokens::user_id.eq(user_id),
                 api_tokens::name.eq(name),
                 api_tokens::token.eq(&*token),
+                api_tokens::crate_scopes.eq(crate_scopes),
+                api_tokens::endpoint_scopes.eq(endpoint_scopes),
             ))
             .get_result(conn)?;
 
