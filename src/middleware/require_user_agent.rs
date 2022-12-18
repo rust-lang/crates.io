6bd96427a36785df389ffddd8d7568121e3448fc
Tobias Bieniek <tobias@bieniek.cloud>
2022-12-18 18:40:39 +0100

    middleware/require_user_agent: Move `cdn_user_agent` field into server `config` (#5688)

--- src/middleware/require_user_agent.rs
+++ src/middleware/require_user_agent.rs
@@ -8,28 +8,27 @@
 //! 0.17 (released alongside rustc 1.17).
 
 use super::prelude::*;
-use std::env;
+use crate::middleware::app::RequestApp;
 
 use crate::util::request_header;
 
 #[derive(Default)]
 pub struct RequireUserAgent {
-    cdn_user_agent: String,
     handler: Option<Box<dyn Handler>>,
 }
 
 impl AroundMiddleware for RequireUserAgent {
     fn with_handler(&mut self, handler: Box<dyn Handler>) {
-        self.cdn_user_agent =
-            env::var("WEB_CDN_USER_AGENT").unwrap_or_else(|_| "Amazon CloudFront".into());
         self.handler = Some(handler);
     }
 }
 
 impl Handler for RequireUserAgent {
     fn call(&self, req: &mut dyn RequestExt) -> AfterResult {
+        let cdn_user_agent = &req.app().config.cdn_user_agent;
+
         let agent = request_header(req, header::USER_AGENT);
-        let has_user_agent = !agent.is_empty() && agent != self.cdn_user_agent;
+        let has_user_agent = !agent.is_empty() && agent != cdn_user_agent;
         let is_download = req.path().ends_with("download");
         if !has_user_agent && !is_download {
             add_custom_metadata(req, "cause", "no user agent");
