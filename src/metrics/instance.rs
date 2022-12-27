65348642b105a97e632ac5e7bd34f1731a323d19
Tobias Bieniek <tobias@bieniek.cloud>
2022-12-27 18:45:04 +0100

    metrics/instance: Narrow `gather()` return type

--- src/metrics/instance.rs
+++ src/metrics/instance.rs
@@ -17,7 +17,6 @@
 //! As a rule of thumb, if the metric requires a database query to be updated it's probably a
 //! service-level metric, and you should add it to `src/metrics/service.rs` instead.
 
-use crate::util::errors::AppResult;
 use crate::{app::App, db::DieselPool};
 use prometheus::{
     proto::MetricFamily, Histogram, HistogramVec, IntCounter, IntCounterVec, IntGauge, IntGaugeVec,
@@ -64,7 +63,7 @@ metrics! {
 }
 
 impl InstanceMetrics {
-    pub fn gather(&self, app: &App) -> AppResult<Vec<MetricFamily>> {
+    pub fn gather(&self, app: &App) -> prometheus::Result<Vec<MetricFamily>> {
         // Database pool stats
         self.refresh_pool_stats("primary", &app.primary_database)?;
         if let Some(follower) = &app.read_only_replica_database {
