3190fc6d409865a9b1f9595f0b178fed3b56f511
Tobias Bieniek <tobias@bieniek.cloud>
2023-01-02 18:51:14 +0100

    util/request_helpers: Extract `HeaderMapExt` trait

--- src/util/request_helpers.rs
+++ src/util/request_helpers.rs
@@ -1,5 +1,6 @@
 use conduit::RequestExt;
 use http::header::AsHeaderName;
+use http::HeaderMap;
 
 /// Returns the value of the request header, or an empty slice if it is not
 /// present.
@@ -11,8 +12,23 @@ pub fn request_header<K>(req: &dyn RequestExt, key: K) -> &str
 where
     K: AsHeaderName,
 {
-    req.headers()
-        .get(key)
-        .map(|value| value.to_str().unwrap_or_default())
-        .unwrap_or_default()
+    req.headers().get_str_or_default(key)
+}
+
+pub trait HeaderMapExt {
+    /// Returns the value of the request header, or an empty slice if it is not
+    /// present.
+    ///
+    /// If a header appears multiple times, this will return only one of them.
+    ///
+    /// If the header value is invalid utf8, an empty slice will be returned.
+    fn get_str_or_default<K: AsHeaderName>(&self, key: K) -> &str;
+}
+
+impl HeaderMapExt for HeaderMap {
+    fn get_str_or_default<K: AsHeaderName>(&self, key: K) -> &str {
+        self.get(key)
+            .map(|value| value.to_str().unwrap_or_default())
+            .unwrap_or_default()
+    }
 }
