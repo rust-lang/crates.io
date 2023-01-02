4434d5a117b028c2de1699099e9d41cf198db8a3
Tobias Bieniek <tobias@bieniek.cloud>
2023-01-02 18:53:02 +0100

    util/request_helpers: Inline `request_header()` function

--- src/util/request_helpers.rs
+++ src/util/request_helpers.rs
@@ -1,19 +1,5 @@
-use conduit::RequestExt;
 use http::header::AsHeaderName;
 use http::HeaderMap;
-
-/// Returns the value of the request header, or an empty slice if it is not
-/// present.
-///
-/// If a header appears multiple times, this will return only one of them.
-///
-/// If the header value is invalid utf8, an empty slice will be returned.
-pub fn request_header<K>(req: &dyn RequestExt, key: K) -> &str
-where
-    K: AsHeaderName,
-{
-    req.headers().get_str_or_default(key)
-}
 
 pub trait HeaderMapExt {
     /// Returns the value of the request header, or an empty slice if it is not
