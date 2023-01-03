04a9964776ddf4c26a66b05ee2a54b2fa9556590
Tobias Bieniek <tobias@bieniek.cloud>
2023-01-03 20:23:05 +0100

    middleware/app: Add `axum` equivalent

--- src/middleware/app.rs
+++ src/middleware/app.rs
@@ -1,4 +1,8 @@
 use super::prelude::*;
+use axum::extract::State;
+use axum::middleware::Next;
+use axum::response::Response;
+use http::Request;
 
 use crate::app::AppState;
 
@@ -18,6 +22,17 @@ impl Middleware for AppMiddleware {
         req.mut_extensions().insert(self.app.clone());
         Ok(())
     }
+}
+
+/// `axum` middleware that injects the `AppState` instance into the `Request` extensions.
+pub async fn add_app_state_extension<B>(
+    State(app_state): State<AppState>,
+    mut request: Request<B>,
+    next: Next<B>,
+) -> Response {
+    request.extensions_mut().insert(app_state);
+
+    next.run(request).await
 }
 
 /// Adds an `app()` method to the `Request` type returning the global `App` instance
