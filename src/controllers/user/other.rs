c008bd640f6cab5e40d1bdaeb0a219e153893531
Tobias Bieniek <tobias@bieniek.cloud>
2023-01-05 20:08:58 +0100

    GET /users/:user_id/stats: Convert to async fn

--- src/controllers/user/other.rs
+++ src/controllers/user/other.rs
@@ -23,22 +23,25 @@ pub async fn show(req: ConduitRequest) -> AppResult<Json<Value>> {
 }
 
 /// Handles the `GET /users/:user_id/stats` route.
-pub fn stats(req: ConduitRequest) -> AppResult<Json<Value>> {
-    use diesel::dsl::sum;
+pub async fn stats(req: ConduitRequest) -> AppResult<Json<Value>> {
+    conduit_compat(move || {
+        use diesel::dsl::sum;
 
-    let user_id = req
-        .param("user_id")
-        .unwrap()
-        .parse::<i32>()
-        .map_err(|err| err.chain(bad_request("invalid user_id")))?;
-    let conn = req.app().db_read_prefer_primary()?;
+        let user_id = req
+            .param("user_id")
+            .unwrap()
+            .parse::<i32>()
+            .map_err(|err| err.chain(bad_request("invalid user_id")))?;
+        let conn = req.app().db_read_prefer_primary()?;
 
-    let data: i64 = CrateOwner::by_owner_kind(OwnerKind::User)
-        .inner_join(crates::table)
-        .filter(crate_owners::owner_id.eq(user_id))
-        .select(sum(crates::downloads))
-        .first::<Option<i64>>(&*conn)?
-        .unwrap_or(0);
+        let data: i64 = CrateOwner::by_owner_kind(OwnerKind::User)
+            .inner_join(crates::table)
+            .filter(crate_owners::owner_id.eq(user_id))
+            .select(sum(crates::downloads))
+            .first::<Option<i64>>(&*conn)?
+            .unwrap_or(0);
 
-    Ok(Json(json!({ "total_downloads": data })))
+        Ok(Json(json!({ "total_downloads": data })))
+    })
+    .await
 }
