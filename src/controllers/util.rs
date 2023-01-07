ad644a9d28c1d0b262bb45da30b3a1e53813e301
Tobias Bieniek <tobias@bieniek.cloud>
2023-01-07 17:02:09 +0100

    controllers/util: Implement `RequestPartsExt` trait
    This allows us to implement things independent from passing in `Parts` or a full `Request`

--- src/controllers/util.rs
+++ src/controllers/util.rs
@@ -1,6 +1,7 @@
 use super::prelude::*;
 use crate::util::errors::{forbidden, internal, AppError, AppResult};
-use http::Request;
+use http::request::Parts;
+use http::{Extensions, HeaderMap, HeaderValue, Method, Request, Uri, Version};
 
 /// The Origin header (https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Origin)
 /// is sent with CORS requests and POST requests, and indicates where the request comes from.
@@ -22,4 +23,76 @@ pub fn verify_origin<B>(req: &Request<B>) -> AppResult<()> {
         return Err(internal(&error_message).chain(forbidden()));
     }
     Ok(())
+}
+
+pub trait RequestPartsExt {
+    fn method(&self) -> &Method;
+    fn uri(&self) -> &Uri;
+    fn version(&self) -> Version;
+    fn headers(&self) -> &HeaderMap<HeaderValue>;
+    fn extensions(&self) -> &Extensions;
+    fn extensions_mut(&mut self) -> &mut Extensions;
+}
+
+impl RequestPartsExt for Parts {
+    fn method(&self) -> &Method {
+        &self.method
+    }
+    fn uri(&self) -> &Uri {
+        &self.uri
+    }
+    fn version(&self) -> Version {
+        self.version
+    }
+    fn headers(&self) -> &HeaderMap<HeaderValue> {
+        &self.headers
+    }
+    fn extensions(&self) -> &Extensions {
+        &self.extensions
+    }
+    fn extensions_mut(&mut self) -> &mut Extensions {
+        &mut self.extensions
+    }
+}
+
+impl<B> RequestPartsExt for Request<B> {
+    fn method(&self) -> &Method {
+        self.method()
+    }
+    fn uri(&self) -> &Uri {
+        self.uri()
+    }
+    fn version(&self) -> Version {
+        self.version()
+    }
+    fn headers(&self) -> &HeaderMap<HeaderValue> {
+        self.headers()
+    }
+    fn extensions(&self) -> &Extensions {
+        self.extensions()
+    }
+    fn extensions_mut(&mut self) -> &mut Extensions {
+        self.extensions_mut()
+    }
+}
+
+impl RequestPartsExt for ConduitRequest {
+    fn method(&self) -> &Method {
+        self.0.method()
+    }
+    fn uri(&self) -> &Uri {
+        self.0.uri()
+    }
+    fn version(&self) -> Version {
+        self.0.version()
+    }
+    fn headers(&self) -> &HeaderMap<HeaderValue> {
+        self.0.headers()
+    }
+    fn extensions(&self) -> &Extensions {
+        self.0.extensions()
+    }
+    fn extensions_mut(&mut self) -> &mut Extensions {
+        self.0.extensions_mut()
+    }
 }
