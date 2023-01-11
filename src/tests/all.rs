69d566c6063788e61d5a08c328f309238d237896
Justin Geibel <jtgeibel@gmail.com>
2023-01-10 21:47:56 -0500

    Revert "Merge pull request #5909 from Turbo87/revert-bg-worker-changes"
    This reverts commit 838ac1d8d8062508d4ef62e052656f094ed29ccf, reversing
    changes made to a732c1f83b1b3a7f84f373ff4699743506679d54.
    
    This reintroduces changes that were originally landed in #5837 and
    reverted in #5909.

--- src/tests/all.rs
+++ src/tests/all.rs
@@ -162,16 +162,11 @@ fn new_category<'a>(category: &'a str, slug: &'a str, description: &'a str) -> N
 // This reflects the configuration of our test environment. In the production application, this
 // does not hold true.
 #[test]
-fn multiple_live_references_to_the_same_connection_can_be_checked_out() {
-    use std::ptr;
-
+#[should_panic]
+fn recursive_get_of_db_conn_in_tests_will_panic() {
     let (app, _) = TestApp::init().empty();
     let app = app.as_inner();
 
-    let conn1 = app.primary_database.get().unwrap();
-    let conn2 = app.primary_database.get().unwrap();
-    let conn1_ref: &PgConnection = &conn1;
-    let conn2_ref: &PgConnection = &conn2;
-
-    assert!(ptr::eq(conn1_ref, conn2_ref));
+    let _conn1 = app.primary_database.get().unwrap();
+    let _conn2 = app.primary_database.get().unwrap();
 }
