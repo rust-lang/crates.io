b735b594a76d7f34f4d991c1411db81d0af0dbc8
Tobias Bieniek <tobias@bieniek.cloud>
2023-01-13 18:08:40 +0100

    conduit-axum: Remove unused `box_error()` fn

--- conduit-axum/src/conduit.rs
+++ conduit-axum/src/conduit.rs
@@ -3,18 +3,3 @@ use std::error::Error;
 pub use http::{header, Extensions, HeaderMap, Method, Request, Response, StatusCode, Uri};
 
 pub type BoxError = Box<dyn Error + Send>;
-
-/// A helper to convert a concrete error type into a `Box<dyn Error + Send>`
-///
-/// # Example
-///
-/// ```
-/// # use std::error::Error;
-/// # use axum::body::Bytes;
-/// # use conduit_axum::{box_error, Response};
-/// # let _: Result<Response<Bytes>, Box<dyn Error + Send>> =
-/// Response::builder().body(Bytes::new()).map_err(box_error);
-/// ```
-pub fn box_error<E: Error + Send + 'static>(error: E) -> BoxError {
-    Box::new(error)
-}
