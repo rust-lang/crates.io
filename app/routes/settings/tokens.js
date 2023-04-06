6733cf4c86d3553318eb2ffee9d80a2ca63b3924
Tobias Bieniek <tobias@bieniek.cloud>
2023-04-06 14:34:06 +0200

    settings/tokens: Use `TrackedArray` instead of prototype extensions and `macro-decorators`

--- app/routes/settings/tokens.js
+++ app/routes/settings/tokens.js
@@ -1,5 +1,7 @@
 import { inject as service } from '@ember/service';
 
+import { TrackedArray } from 'tracked-built-ins';
+
 import AuthenticatedRoute from '../-authenticated-route';
 
 export default class TokenSettingsRoute extends AuthenticatedRoute {
@@ -7,6 +9,6 @@ export default class TokenSettingsRoute extends AuthenticatedRoute {
 
   async model() {
     let apiTokens = await this.store.findAll('api-token');
-    return apiTokens.toArray();
+    return TrackedArray.from(apiTokens.slice());
   }
 }
