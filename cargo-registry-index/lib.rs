edcc0acfbdb63cded213cedc9bc469f92bd320ee
Tobias Bieniek <tobias@bieniek.cloud>
2023-04-20 11:14:17 +0200

    index/Crate: Implement `write_crates()` fn

--- cargo-registry-index/lib.rs
+++ cargo-registry-index/lib.rs
@@ -156,6 +156,13 @@ impl Crate {
     }
 }
 
+pub fn write_crates<W: Write>(crates: &[Crate], mut writer: W) -> anyhow::Result<()> {
+    for krate in crates {
+        krate.write_to(&mut writer)?;
+    }
+    Ok(())
+}
+
 #[derive(Serialize, Deserialize, Debug, PartialEq, Eq)]
 pub struct Dependency {
     pub name: String,
@@ -625,6 +632,34 @@ mod tests {
         let mut buffer = Vec::new();
         assert_ok!(krate.write_to(&mut buffer));
         assert_ok_eq!(String::from_utf8(buffer), "\
+            {\"name\":\"foo\",\"vers\":\"1.2.3\",\"deps\":[],\"cksum\":\"0123456789asbcdef\",\"features\":{},\"yanked\":null}\n\
+        ");
+    }
+
+    #[test]
+    fn test_write_crates() {
+        let versions = vec!["0.1.0", "1.0.0-beta.1", "1.0.0", "1.2.3"];
+        let crates = versions
+            .into_iter()
+            .map(|vers| Crate {
+                name: "foo".to_string(),
+                vers: vers.to_string(),
+                deps: vec![],
+                cksum: "0123456789asbcdef".to_string(),
+                features: Default::default(),
+                features2: None,
+                yanked: None,
+                links: None,
+                v: None,
+            })
+            .collect::<Vec<_>>();
+
+        let mut buffer = Vec::new();
+        assert_ok!(write_crates(&crates, &mut buffer));
+        assert_ok_eq!(String::from_utf8(buffer), "\
+            {\"name\":\"foo\",\"vers\":\"0.1.0\",\"deps\":[],\"cksum\":\"0123456789asbcdef\",\"features\":{},\"yanked\":null}\n\
+            {\"name\":\"foo\",\"vers\":\"1.0.0-beta.1\",\"deps\":[],\"cksum\":\"0123456789asbcdef\",\"features\":{},\"yanked\":null}\n\
+            {\"name\":\"foo\",\"vers\":\"1.0.0\",\"deps\":[],\"cksum\":\"0123456789asbcdef\",\"features\":{},\"yanked\":null}\n\
             {\"name\":\"foo\",\"vers\":\"1.2.3\",\"deps\":[],\"cksum\":\"0123456789asbcdef\",\"features\":{},\"yanked\":null}\n\
         ");
     }
