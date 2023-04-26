f14995ac22ba89767d75a6c7dab9d8390f78bb19
Tobias Bieniek <tobias@bieniek.cloud>
2023-04-26 14:02:05 +0200

    VersionList::Row: Display `rust-version` data

--- tests/acceptance/versions-test.js
+++ tests/acceptance/versions-test.js
@@ -1,6 +1,8 @@
 import { click, currentURL, findAll, visit } from '@ember/test-helpers';
 import { module, test } from 'qunit';
 
+import percySnapshot from '@percy/ember';
+
 import { setupApplicationTest } from 'cargo/tests/helpers';
 
 module('Acceptance | crate versions page', function (hooks) {
@@ -10,7 +12,7 @@ module('Acceptance | crate versions page', function (hooks) {
     let crate = this.server.create('crate', { name: 'nanomsg' });
     this.server.create('version', { crate, num: '0.1.0', created_at: '2017-01-01' });
     this.server.create('version', { crate, num: '0.2.0', created_at: '2018-01-01' });
-    this.server.create('version', { crate, num: '0.3.0', created_at: '2019-01-01' });
+    this.server.create('version', { crate, num: '0.3.0', created_at: '2019-01-01', rust_version: '1.69' });
     this.server.create('version', { crate, num: '0.2.1', created_at: '2020-01-01' });
 
     await visit('/crates/nanomsg/versions');
@@ -18,6 +20,8 @@ module('Acceptance | crate versions page', function (hooks) {
 
     let versions = findAll('[data-test-version]').map(it => it.dataset.testVersion);
     assert.deepEqual(versions, ['0.2.1', '0.3.0', '0.2.0', '0.1.0']);
+
+    await percySnapshot(assert);
 
     await click('[data-test-current-order]');
     await click('[data-test-semver-sort] a');
