5c38e56f24b15e607a28d977dc2333ce500d262f
Tobias Bieniek <tobias@bieniek.cloud>
2023-07-09 11:29:02 +0200

    storage: Extract `DEFAULT_REGION` constant

--- src/storage.rs
+++ src/storage.rs
@@ -3,8 +3,10 @@ use anyhow::Context;
 use object_store::aws::AmazonS3Builder;
 use object_store::ObjectStore;
 
+const DEFAULT_REGION: &str = "us-west-1";
+
 pub fn from_environment() -> Box<dyn ObjectStore> {
-    let region = dotenvy::var("S3_REGION").unwrap_or("us-west-1".to_string());
+    let region = dotenvy::var("S3_REGION").unwrap_or(DEFAULT_REGION.to_string());
     let bucket = env("S3_BUCKET");
     let access_key = env("AWS_ACCESS_KEY");
     let secret_key = env("AWS_SECRET_KEY");
