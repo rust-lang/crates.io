bc4a06db8b5e1e2b343eca7740c094411d15209e
Tobias Bieniek <tobias@bieniek.cloud>
2023-07-20 09:25:29 +0200

    TarballBuilder: Extract `build_with_compression()` fn

--- crates_io_tarball/src/builder.rs
+++ crates_io_tarball/src/builder.rs
@@ -1,4 +1,5 @@
 use flate2::read::GzEncoder;
+use flate2::Compression;
 use std::io::Read;
 
 pub struct TarballBuilder {
@@ -32,10 +33,14 @@ impl TarballBuilder {
     }
 
     pub fn build(self) -> Vec<u8> {
+        self.build_with_compression(Compression::default())
+    }
+
+    pub fn build_with_compression(self, compression: Compression) -> Vec<u8> {
         let tarball_bytes = self.build_unzipped();
 
         let mut gzip_bytes = vec![];
-        GzEncoder::new(tarball_bytes.as_slice(), Default::default())
+        GzEncoder::new(tarball_bytes.as_slice(), compression)
             .read_to_end(&mut gzip_bytes)
             .unwrap();
 
