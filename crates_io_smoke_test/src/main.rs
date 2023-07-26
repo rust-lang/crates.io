206ac89e2397c86c0458bc415c3facaa9c47e25f
Tobias Bieniek <tobias@bieniek.cloud>
2023-07-26 15:02:20 +0200

    smoke_test: Add `tracing` setup

--- crates_io_smoke_test/src/main.rs
+++ crates_io_smoke_test/src/main.rs
@@ -1,3 +1,25 @@
+#[macro_use]
+extern crate tracing;
+
+use tracing_subscriber::filter::LevelFilter;
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
+use tracing_subscriber::{EnvFilter, Layer};
+
 fn main() {
-    println!("Hello world!");
+    init_tracing();
+
+    info!("Hello world!");
+}
+
+fn init_tracing() {
+    let env_filter = EnvFilter::builder()
+        .with_default_directive(LevelFilter::INFO.into())
+        .from_env_lossy();
+
+    let log_layer = tracing_subscriber::fmt::layer()
+        .compact()
+        .with_filter(env_filter);
+
+    tracing_subscriber::registry().with(log_layer).init();
 }
