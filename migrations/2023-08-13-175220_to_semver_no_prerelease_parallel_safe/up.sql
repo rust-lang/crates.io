78c1c4414772a764a6be665fe5f6839264a2404b
Adam Harvey <adam@adamharvey.name>
2023-08-21 16:42:33 -0700

    db: fix return type of `to_semver_no_prerelease()`

--- migrations/2023-08-13-175220_to_semver_no_prerelease_parallel_safe/up.sql
+++ migrations/2023-08-13-175220_to_semver_no_prerelease_parallel_safe/up.sql
@@ -1,10 +1,10 @@
 DROP FUNCTION to_semver_no_prerelease(text);
 
-CREATE FUNCTION to_semver_no_prerelease(text) RETURNS semver_triple IMMUTABLE PARALLEL SAFE AS $$
+CREATE FUNCTION to_semver_no_prerelease(text) RETURNS public.semver_triple IMMUTABLE PARALLEL SAFE AS $$
   SELECT (
     split_part($1, '.', 1)::numeric,
     split_part($1, '.', 2)::numeric,
     split_part(split_part($1, '+', 1), '.', 3)::numeric
-  )::semver_triple
+  )::public.semver_triple
   WHERE strpos($1, '-') = 0
-  $$ LANGUAGE SQL SET search_path = public;
+  $$ LANGUAGE SQL;
