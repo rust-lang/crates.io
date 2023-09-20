43864453a6dd01259156a9bec7ebc66919968b34
hi-rustin <rustin.liu@gmail.com>
2023-09-20 19:53:39 +0800

    Split up yank ratelimit test
    The combination of `sleep()` with a slow database was making this test a bit flaky. This change should fix it.

--- src/tests/krate/yanking.rs
+++ src/tests/krate/yanking.rs
@@ -1,7 +1,10 @@
 use crate::builders::PublishBuilder;
 use crate::routes::crates::versions::yank_unyank::YankRequestHelper;
 use crate::util::{RequestHelper, TestApp};
+use chrono::Utc;
 use crates_io::rate_limiter::LimitedAction;
+use crates_io::schema::publish_limit_buckets;
+use diesel::{ExpressionMethods, RunQueryDsl};
 use std::time::Duration;
 
 #[test]
@@ -62,46 +65,74 @@ fn yank_works_as_intended() {
     assert!(!json.version.yanked);
 }
 
-#[test]
-fn test_yank_rate_limiting() {
-    #[track_caller]
-    fn check_yanked(app: &TestApp, is_yanked: bool) {
-        let crates = app.crates_from_index_head("yankable");
-        assert_eq!(crates.len(), 1);
-        assert_some_eq!(crates[0].yanked, is_yanked);
-    }
+#[track_caller]
+fn check_yanked(app: &TestApp, is_yanked: bool) {
+    let crates = app.crates_from_index_head("yankable");
+    assert_eq!(crates.len(), 1);
+    assert_some_eq!(crates[0].yanked, is_yanked);
+}
 
+#[test]
+fn yank_ratelimit_hit() {
     let (app, _, _, token) = TestApp::full()
         .with_rate_limit(LimitedAction::YankUnyank, Duration::from_millis(500), 1)
         .with_token();
+
+    // Set up the database so it'll think we've massively rate-limited ourselves.
+    app.db(|conn| {
+        // Ratelimit bucket should next refill in about a year
+        let far_future = Utc::now().naive_utc() + Duration::from_secs(60 * 60 * 24 * 365);
+        diesel::insert_into(publish_limit_buckets::table)
+            .values((
+                publish_limit_buckets::user_id.eq(token.as_model().user_id),
+                publish_limit_buckets::action.eq(LimitedAction::YankUnyank),
+                publish_limit_buckets::tokens.eq(0),
+                publish_limit_buckets::last_refill.eq(far_future),
+            ))
+            .execute(conn)
+            .expect("Failed to set fake ratelimit")
+    });
 
     // Upload a new crate
     let crate_to_publish = PublishBuilder::new("yankable", "1.0.0");
     token.publish_crate(crate_to_publish).good();
     check_yanked(&app, false);
 
-    // Yank the crate
-    token.yank("yankable", "1.0.0").good();
-    check_yanked(&app, true);
-
-    // Try unyanking the crate, will get rate limited
-    token
-        .unyank("yankable", "1.0.0")
-        .assert_rate_limited(LimitedAction::YankUnyank);
-    check_yanked(&app, true);
-
-    // Let the rate limit refill.
-    std::thread::sleep(Duration::from_millis(500));
-
-    // Unyanking now works.
-    token.unyank("yankable", "1.0.0").good();
-    check_yanked(&app, false);
-
-    // Yanking again will trigger the rate limit.
+    // Yank it and wait for the ratelimit to hit.
     token
         .yank("yankable", "1.0.0")
         .assert_rate_limited(LimitedAction::YankUnyank);
     check_yanked(&app, false);
+}
+
+#[test]
+fn yank_ratelimit_expires() {
+    let (app, _, _, token) = TestApp::full()
+        .with_rate_limit(LimitedAction::YankUnyank, Duration::from_millis(500), 1)
+        .with_token();
+
+    // Set up the database so it'll think we've massively ratelimited ourselves
+    app.db(|conn| {
+        // Ratelimit bucket should next refill right now!
+        let just_now = Utc::now().naive_utc() - Duration::from_millis(500);
+        diesel::insert_into(publish_limit_buckets::table)
+            .values((
+                publish_limit_buckets::user_id.eq(token.as_model().user_id),
+                publish_limit_buckets::action.eq(LimitedAction::YankUnyank),
+                publish_limit_buckets::tokens.eq(0),
+                publish_limit_buckets::last_refill.eq(just_now),
+            ))
+            .execute(conn)
+            .expect("Failed to set fake ratelimit")
+    });
+
+    // Upload a new crate
+    let crate_to_publish = PublishBuilder::new("yankable", "1.0.0");
+    token.publish_crate(crate_to_publish).good();
+    check_yanked(&app, false);
+
+    token.yank("yankable", "1.0.0").good();
+    check_yanked(&app, true);
 }
 
 #[test]
