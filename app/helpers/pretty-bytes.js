6ce48bc6f9b12fc90227190c63d5bf3701746856
kana-rus <kanarus786@gmail.com>
2023-10-02 13:59:07 +0900

    Add: doc comment & test

--- app/helpers/pretty-bytes.js
+++ app/helpers/pretty-bytes.js
@@ -2,4 +2,15 @@ import { helper } from '@ember/component/helper';
 
 import prettyBytes from 'pretty-bytes';
 
-export default helper(([bytes], options) => prettyBytes(bytes, { ...options, binary: true}));
+/**
+ * See https://github.com/rust-lang/crates.io/discussions/7177
+ * 
+ * Here set {minimum,maximum}FractionDigits to 1 because
+ * `cargo publish` uses this format
+*/
+export default helper(([bytes], options) => prettyBytes(bytes, {
+    ...options,
+    binary: true,
+    minimumFractionDigits: 1,
+    maximumFractionDigits: 1,
+}));
---
+++ tests/unit/helpers/pretty-bytes-test.js
@@ -0,0 +1,26 @@
+import { render } from '@ember/test-helpers';
+import { module, test } from 'qunit';
+
+import { hbs } from 'ember-cli-htmlbars';
+
+import { setupRenderingTest } from 'cargo/tests/helpers';
+
+module('Unit | Helper | pretty-bytes', function (hooks) {
+    setupRenderingTest(hooks);
+
+    test("it displays as expected", async function (assert) {
+        this.owner.lookup('service:intl').locale = 'en';
+
+        await render(hbs`{{pretty-bytes 42}}`);
+        assert.dom().hasText('42.0 B');
+
+        await render(hbs`{{pretty-bytes 42000}}`);
+        assert.dom().hasText('41.0 KiB');
+
+        await render(hbs`{{pretty-bytes 42420}}`);
+        assert.dom().hasText('41.4 KiB');
+
+        await render(hbs`{{pretty-bytes 42424242}}`);
+        assert.dom().hasText('40.5 MiB');
+    })
+});
