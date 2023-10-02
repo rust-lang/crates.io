f6199a72d200d8500c46171ee06ad0de3d29c873
kana-rus <kanarus786@gmail.com>
2023-10-02 14:28:08 +0900

    Fix: format

--- app/helpers/pretty-bytes.js
+++ app/helpers/pretty-bytes.js
@@ -9,8 +9,8 @@ import prettyBytes from 'pretty-bytes';
  * uses this format (see https://github.com/rust-lang/cargo/blob/master/src/cargo/ops/cargo_package.rs#L168-L171)
 */
 export default helper(([bytes], options) => prettyBytes(bytes, {
-    ...options,
-    binary: true,
-    minimumFractionDigits: 1,
-    maximumFractionDigits: 1,
+  ...options,
+  binary: true,
+  minimumFractionDigits: 1,
+  maximumFractionDigits: 1,
 }));
