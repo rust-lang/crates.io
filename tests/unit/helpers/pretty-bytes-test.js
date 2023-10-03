1cf5862146d93e97a012bc6323ea59b08e54129e
kana-rus <kanarus786@gmail.com>
2023-10-03 19:26:43 +0900

    Fix: `prettyBytes` helper: not set franction digits to 1 & Fix test

--- tests/unit/helpers/pretty-bytes-test.js
+++ tests/unit/helpers/pretty-bytes-test.js
@@ -12,15 +12,29 @@ module('Unit | Helper | pretty-bytes', function (hooks) {
     this.owner.lookup('service:intl').locale = 'en';
 
     await render(hbs`{{pretty-bytes 42}}`);
-    assert.dom().hasText('42.0 B');
+    assert.dom().hasText('42 B');
 
+    await render(hbs`{{pretty-bytes 1024}}`);
+    assert.dom().hasText('1 KiB');
+
+    // 4200 / 1024 = 4.101...
+    await render(hbs`{{pretty-bytes 4200}}`);
+    assert.dom().hasText('4.1 KiB');
+
+    // 4200 / 1024 = 4.142...
+    await render(hbs`{{pretty-bytes 4242}}`);
+    assert.dom().hasText('4.14 KiB');
+
+    // 42000 / 1024 = 41.0156...
     await render(hbs`{{pretty-bytes 42000}}`);
-    assert.dom().hasText('41.0 KiB');
+    assert.dom().hasText('41 KiB');
 
-    await render(hbs`{{pretty-bytes 42420}}`);
-    assert.dom().hasText('41.4 KiB');
+    // 42623 / 1024 = 41.625
+    await render(hbs`{{pretty-bytes 42624}}`);
+    assert.dom().hasText('41.6 KiB');
 
-    await render(hbs`{{pretty-bytes 42424242}}`);
-    assert.dom().hasText('40.5 MiB');
+    // 424242 / 1024 = 414.2988...
+    await render(hbs`{{pretty-bytes 424242}}`);
+    assert.dom().hasText('414 KiB');
   });
 });
