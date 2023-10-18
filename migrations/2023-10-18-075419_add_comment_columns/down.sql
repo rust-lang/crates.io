f219198b61a67c88d1814f311c354f12a32f420d
Tobias Bieniek <tobias@bieniek.cloud>
2023-10-18 10:12:32 +0200

    database: Add comments to a few table columns (#7307)
    see https://github.com/rust-lang/crates.io/discussions/7030

---
+++ migrations/2023-10-18-075419_add_comment_columns/down.sql
@@ -0,0 +1,5 @@
+COMMENT ON COLUMN crate_owners.owner_kind IS NULL;
+COMMENT ON COLUMN crate_owners.owner_id IS NULL;
+COMMENT ON COLUMN teams.login IS NULL;
+COMMENT ON COLUMN teams.github_id IS NULL;
+COMMENT ON COLUMN teams.org_id IS NULL;
