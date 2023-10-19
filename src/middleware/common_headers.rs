6054d2c5bf2a7055d9f87315d1657abe1ac2c323
Tobias Bieniek <tobias@bieniek.cloud>
2023-10-19 14:54:58 +0200

    Construct `Content-Security-Policy` header only once on startup

--- src/middleware/common_headers.rs
+++ src/middleware/common_headers.rs
@@ -21,24 +21,12 @@ pub async fn add_common_headers<B: Send + 'static>(
     headers.insert(header::STRICT_TRANSPORT_SECURITY, v("max-age=31536000"));
 
     if NGINX_SUCCESS_CODES.contains(&response.status().as_u16()) {
-        let cdn_prefix = state.config.storage.cdn_prefix.as_ref();
-        let cdn_domain = cdn_prefix.map(|cdn_prefix| format!("https://{cdn_prefix}"));
-        let cdn_domain = cdn_domain.unwrap_or_default();
-
-        let csp = format!(
-            "default-src 'self'; \
-            connect-src 'self' *.ingest.sentry.io https://docs.rs https://play.rust-lang.org {cdn_domain}; \
-            script-src 'self' 'unsafe-eval' 'sha256-n1+BB7Ckjcal1Pr7QNBh/dKRTtBQsIytFodRiIosXdE='; \
-            style-src 'self' 'unsafe-inline' https://code.cdn.mozilla.net; \
-            font-src https://code.cdn.mozilla.net; \
-            img-src *; \
-            object-src 'none'"
-        );
-
         headers.insert(header::X_CONTENT_TYPE_OPTIONS, v("nosniff"));
         headers.insert(header::X_FRAME_OPTIONS, v("SAMEORIGIN"));
         headers.insert(header::X_XSS_PROTECTION, v("0"));
-        headers.insert(header::CONTENT_SECURITY_POLICY, csp.parse().unwrap());
+        if let Some(ref csp) = state.config.content_security_policy {
+            headers.insert(header::CONTENT_SECURITY_POLICY, csp.clone());
+        }
         headers.insert(header::VARY, v("Accept, Accept-Encoding, Cookie"));
     }
 
