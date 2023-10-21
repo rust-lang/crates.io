e1829a6f2715857986bc0a9f1d18fe9a66a9efbe
Tobias Bieniek <tobias@bieniek.cloud>
2023-10-21 18:05:28 +0200

    tower-http: Use `AddExtensionLayer` instead of custom middleware function (#7333)

--- src/middleware/app.rs
+++ src/middleware/app.rs
@@ -1,20 +1,5 @@
-use axum::middleware::Next;
-use axum::response::Response;
-use http::Request;
-
 use crate::app::AppState;
 use crate::controllers::util::RequestPartsExt;
-
-/// `axum` middleware that injects the `AppState` instance into the `Request` extensions.
-pub async fn add_app_state_extension<B>(
-    app_state: AppState,
-    mut request: Request<B>,
-    next: Next<B>,
-) -> Response {
-    request.extensions_mut().insert(app_state);
-
-    next.run(request).await
-}
 
 /// Adds an `app()` method to the `Request` type returning the global `App` instance
 pub trait RequestApp {
