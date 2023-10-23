7b446fdc884306294774ec4ba00adabc74ae76ac
Tobias Bieniek <tobias@bieniek.cloud>
2023-10-23 10:14:22 +0200

    Precompress static frontend assets with gzip and brotli (#7332)

---
+++ script/precompress-assets.mjs
@@ -0,0 +1,31 @@
+import { stat } from 'node:fs/promises';
+import { pipeline } from 'node:stream/promises';
+import { createReadStream, createWriteStream } from 'node:fs';
+import { createGzip, createBrotliCompress, constants } from 'node:zlib';
+import { globby } from 'globby';
+
+let paths = await globby(['**/*.css', '**/*.html', '**/*.js', '**/*.map', '**/*.svg', '**/*.txt', '**/*.xml'], {
+  cwd: 'dist',
+});
+
+for (let path of paths) {
+  let fullPath = `dist/${path}`;
+  console.log(`Compressing ${fullPath}…`);
+
+  let gzipPath = fullPath + '.gz';
+  await pipeline(createReadStream(fullPath), createGzip({ level: 9 }), createWriteStream(gzipPath));
+
+  let brPath = fullPath + '.br';
+  await pipeline(
+    createReadStream(fullPath),
+    createBrotliCompress({
+      params: {
+        [constants.BROTLI_PARAM_QUALITY]: constants.BROTLI_MAX_QUALITY,
+        [constants.BROTLI_PARAM_SIZE_HINT]: (await stat(fullPath)).size,
+      },
+    }),
+    createWriteStream(brPath),
+  );
+}
+
+console.log('\nCompression completed');
