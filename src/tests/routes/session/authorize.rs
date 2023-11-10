491265dec75ac32bb3e1300b6b87baf1c39f006e
Tobias Bieniek <tobias@bieniek.cloud>
2023-11-10 11:42:22 +0100

    controllers/user/session: Extract `AuthorizeQuery` struct

--- src/tests/routes/session/authorize.rs
+++ src/tests/routes/session/authorize.rs
@@ -7,7 +7,7 @@ fn access_token_needs_data() {
     let response = anon.get::<()>("/api/private/session/authorize");
     assert_eq!(response.status(), StatusCode::BAD_REQUEST);
     assert_eq!(
-        response.into_json(),
-        json!({ "errors": [{ "detail": "invalid state parameter" }] })
+        response.into_text(),
+        "Failed to deserialize query string: missing field `code`"
     );
 }
