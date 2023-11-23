c53358382ae60eba345cd682d541a264270d72c1
Tobias Bieniek <tobias@bieniek.cloud>
2023-11-23 13:33:43 +0100

    controllers/conduit_axum: Inline `spawn_blocking()` fn

--- src/controllers/conduit_axum.rs
+++ src/controllers/conduit_axum.rs
@@ -1,27 +1,22 @@
 use sentry::Hub;
 use std::convert::identity;
-use tokio::task::{JoinError, JoinHandle};
+use tokio::task::JoinError;
 
-/// Just like [tokio::task::spawn_blocking], but automatically runs the passed
-/// in function in the context of the current Sentry hub.
-fn spawn_blocking<F, R>(f: F) -> JoinHandle<R>
-where
-    F: FnOnce() -> R + Send + 'static,
-    R: Send + 'static,
-{
-    let hub = Hub::current();
-    tokio::task::spawn_blocking(move || Hub::run(hub, f))
-}
-
-/// This runs the passed-in function in a synchronous [spawn_blocking] context
-/// and returns a flattened [Result].
+/// Runs the provided closure on a thread where blocking is acceptable.
+///
+/// This is using [tokio::task::spawn_blocking] internally, but automatically
+/// runs the callback function in the context of the current Sentry [Hub].
+///
+/// The function also returns a flattened [Result], which requires the error
+/// variant of the [Result] to implement [From\<JoinError>].
 pub async fn conduit_compat<F, R, E>(f: F) -> Result<R, E>
 where
     F: FnOnce() -> Result<R, E> + Send + 'static,
     R: Send + 'static,
     E: Send + From<JoinError> + 'static,
 {
-    spawn_blocking(f)
+    let hub = Hub::current();
+    tokio::task::spawn_blocking(move || Hub::run(hub, f))
         .await
         // Convert `JoinError` to `E`
         .map_err(Into::into)
