9f9b43e495e90eddc971c5c397e30948a1a624c0
Tobias Bieniek <tobias@bieniek.cloud>
2023-11-27 10:21:19 +0100

    worker: Extract 'JobRegistry` struct

--- crates_io_worker/src/job_registry.rs
+++ crates_io_worker/src/job_registry.rs
@@ -5,12 +5,31 @@ use tokio::runtime::Handle;
 
 type RunTaskFn<Context> = dyn Fn(Context, serde_json::Value) -> anyhow::Result<()> + Send + Sync;
 
-pub type JobRegistry<Context> = HashMap<String, Arc<RunTaskFn<Context>>>;
+#[derive(Clone)]
+pub struct JobRegistry<Context> {
+    entries: HashMap<String, Arc<RunTaskFn<Context>>>,
+}
 
-pub fn runnable<J: BackgroundJob>(
-    ctx: J::Context,
-    payload: serde_json::Value,
-) -> anyhow::Result<()> {
+impl<Context> Default for JobRegistry<Context> {
+    fn default() -> Self {
+        Self {
+            entries: HashMap::new(),
+        }
+    }
+}
+
+impl<Context: Clone + Send + 'static> JobRegistry<Context> {
+    pub fn register<J: BackgroundJob<Context = Context>>(&mut self) {
+        self.entries
+            .insert(J::JOB_NAME.to_string(), Arc::new(runnable::<J>));
+    }
+
+    pub fn get(&self, key: &str) -> Option<&Arc<RunTaskFn<Context>>> {
+        self.entries.get(key)
+    }
+}
+
+fn runnable<J: BackgroundJob>(ctx: J::Context, payload: serde_json::Value) -> anyhow::Result<()> {
     let job: J = serde_json::from_value(payload)?;
     Handle::current().block_on(job.run(ctx))
 }
