aa42f79e84f2dca5591dadc7090583f443d11f93
Tobias Bieniek <tobias@bieniek.cloud>
2023-12-28 14:53:39 +0100

    models/token: Derive `MockApiTokenBuilder`

--- src/models/token.rs
+++ src/models/token.rs
@@ -1,6 +1,7 @@
 mod scopes;
 
 use chrono::NaiveDateTime;
+use derive_builder::Builder;
 use diesel::prelude::*;
 
 pub use self::scopes::{CrateScope, EndpointScope};
@@ -11,24 +12,34 @@ use crate::util::rfc3339;
 use crate::util::token::{HashedToken, PlainToken};
 
 /// The model representing a row in the `api_tokens` database table.
-#[derive(Debug, Identifiable, Queryable, Selectable, Associations, Serialize)]
+#[derive(Debug, Identifiable, Queryable, Selectable, Associations, Serialize, Builder)]
 #[diesel(belongs_to(User))]
+#[builder(name = "MockApiTokenBuilder")]
 pub struct ApiToken {
+    #[builder(default)]
     pub id: i32,
     #[serde(skip)]
+    #[builder(default)]
     pub user_id: i32,
+    #[builder(default, setter(into))]
     pub name: String,
     #[serde(with = "rfc3339")]
+    #[builder(default, setter(strip_option))]
     pub created_at: NaiveDateTime,
     #[serde(with = "rfc3339::option")]
+    #[builder(default, setter(strip_option))]
     pub last_used_at: Option<NaiveDateTime>,
     #[serde(skip)]
+    #[builder(default = "false")]
     pub revoked: bool,
     /// `None` or a list of crate scope patterns (see RFC #2947)
+    #[builder(default, setter(strip_option))]
     pub crate_scopes: Option<Vec<CrateScope>>,
     /// A list of endpoint scopes or `None` for the `legacy` endpoint scope (see RFC #2947)
+    #[builder(default, setter(strip_option))]
     pub endpoint_scopes: Option<Vec<EndpointScope>>,
     #[serde(with = "rfc3339::option")]
+    #[builder(default, setter(strip_option))]
     pub expired_at: Option<NaiveDateTime>,
 }
 
@@ -95,6 +106,10 @@ impl ApiToken {
         .or_else(|_| tokens.select(ApiToken::as_select()).first(conn))
         .map_err(Into::into)
     }
+
+    pub fn mock_builder() -> MockApiTokenBuilder {
+        MockApiTokenBuilder::default()
+    }
 }
 
 #[derive(Debug)]
@@ -110,22 +125,22 @@ mod tests {
 
     #[test]
     fn api_token_serializes_to_rfc3339() {
-        let tok = ApiToken {
-            id: 12345,
-            user_id: 23456,
-            revoked: false,
-            name: "".to_string(),
-            created_at: NaiveDate::from_ymd_opt(2017, 1, 6)
-                .unwrap()
-                .and_hms_opt(14, 23, 11)
-                .unwrap(),
-            last_used_at: NaiveDate::from_ymd_opt(2017, 1, 6)
-                .unwrap()
-                .and_hms_opt(14, 23, 12),
-            crate_scopes: None,
-            endpoint_scopes: None,
-            expired_at: None,
-        };
+        let created_at = NaiveDate::from_ymd_opt(2017, 1, 6)
+            .unwrap()
+            .and_hms_opt(14, 23, 11)
+            .unwrap();
+
+        let last_used_at = NaiveDate::from_ymd_opt(2017, 1, 6)
+            .unwrap()
+            .and_hms_opt(14, 23, 12)
+            .unwrap();
+
+        let tok = ApiToken::mock_builder()
+            .created_at(created_at)
+            .last_used_at(last_used_at)
+            .build()
+            .unwrap();
+
         let json = serde_json::to_string(&tok).unwrap();
         assert_some!(json
             .as_str()
