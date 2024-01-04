2d4832e9553eb8af0feb26483bf07c8a93993f6a
Tobias Bieniek <tobias@bieniek.cloud>
2024-01-04 13:27:44 +0100

    tests/krate/following: Convert constant variables to constants

--- src/tests/krate/following.rs
+++ src/tests/krate/following.rs
@@ -23,44 +23,45 @@ fn unfollow(crate_name: &str, user: &impl RequestHelper) {
 
 #[test]
 fn following() {
+    const CRATE_NAME: &str = "foo_following";
+
     // TODO: Test anon requests as well?
     let (app, _, user) = TestApp::init().with_user();
 
-    let crate_name = "foo_following";
     app.db(|conn| {
-        CrateBuilder::new(crate_name, user.as_model().id).expect_build(conn);
+        CrateBuilder::new(CRATE_NAME, user.as_model().id).expect_build(conn);
     });
 
-    assert_is_following(crate_name, false, &user);
-    follow(crate_name, &user);
-    follow(crate_name, &user);
-    assert_is_following(crate_name, true, &user);
+    assert_is_following(CRATE_NAME, false, &user);
+    follow(CRATE_NAME, &user);
+    follow(CRATE_NAME, &user);
+    assert_is_following(CRATE_NAME, true, &user);
     assert_that!(user.search("following=1").crates, len(eq(1)));
 
-    unfollow(crate_name, &user);
-    unfollow(crate_name, &user);
-    assert_is_following(crate_name, false, &user);
+    unfollow(CRATE_NAME, &user);
+    unfollow(CRATE_NAME, &user);
+    assert_is_following(CRATE_NAME, false, &user);
     assert_that!(user.search("following=1").crates, empty());
 }
 
 #[test]
 fn getting_followed_crates_allows_api_token_auth() {
+    const CRATE_TO_FOLLOW: &str = "some_crate_to_follow";
+    const CRATE_NOT_TO_FOLLOW: &str = "another_crate";
+
     let (app, _, user, token) = TestApp::init().with_token();
     let api_token = token.as_model();
 
-    let crate_to_follow = "some_crate_to_follow";
-    let crate_not_followed = "another_crate";
-
     app.db(|conn| {
-        CrateBuilder::new(crate_to_follow, api_token.user_id).expect_build(conn);
-        CrateBuilder::new(crate_not_followed, api_token.user_id).expect_build(conn);
+        CrateBuilder::new(CRATE_TO_FOLLOW, api_token.user_id).expect_build(conn);
+        CrateBuilder::new(CRATE_NOT_TO_FOLLOW, api_token.user_id).expect_build(conn);
     });
 
-    follow(crate_to_follow, &token);
+    follow(CRATE_TO_FOLLOW, &token);
 
     // Token auth on GET for get following status is disallowed
-    assert_is_following(crate_to_follow, true, &user);
-    assert_is_following(crate_not_followed, false, &user);
+    assert_is_following(CRATE_TO_FOLLOW, true, &user);
+    assert_is_following(CRATE_NOT_TO_FOLLOW, false, &user);
 
     let json = token.search("following=1");
     assert_that!(json.crates, len(eq(1)));
