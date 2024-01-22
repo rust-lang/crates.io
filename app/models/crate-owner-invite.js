548e0b4104d5bb524cac6f1f2ee9869f90548c1b
Tobias Bieniek <tobias@bieniek.cloud>
2024-01-22 17:54:14 +0100

    models/crate-owner-invite: Remove obsolete `invited_by_username` field
    `invited_by_username` isn't actually sent anymore by the v2 (aka. `private`) API and it had been showing an empty column for 14 months now... 😅

--- app/models/crate-owner-invite.js
+++ app/models/crate-owner-invite.js
@@ -1,7 +1,6 @@
 import Model, { attr, belongsTo } from '@ember-data/model';
 
 export default class CrateOwnerInvite extends Model {
-  @attr invited_by_username;
   @attr crate_name;
   @attr crate_id;
   @attr('date') created_at;
