5c1d75358e8142cd68b837732c6667a1a7c38322
Tobias Bieniek <tobias@bieniek.cloud>
2024-01-26 17:13:51 +0100

    cdn_logs: Implement download counting benchmarks

---
+++ crates_io_cdn_logs/benches/count_downloads.rs
@@ -0,0 +1,29 @@
+use crates_io_cdn_logs::{cloudfront, fastly};
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use std::io::Cursor;
+
+fn criterion_benchmark(c: &mut Criterion) {
+    let rt = tokio::runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()
+        .unwrap();
+
+    let bytes = include_bytes!("../test_data/cloudfront/basic.log");
+    c.bench_function("cloudfront", |b| {
+        // Insert a call to `to_async` to convert the bencher to async mode.
+        // The timing loops are the same as with the normal bencher.
+        b.to_async(&rt)
+            .iter(|| cloudfront::count_downloads(black_box(Cursor::new(bytes))));
+    });
+
+    let bytes = include_bytes!("../test_data/fastly/basic.log");
+    c.bench_function("fastly", |b| {
+        // Insert a call to `to_async` to convert the bencher to async mode.
+        // The timing loops are the same as with the normal bencher.
+        b.to_async(&rt)
+            .iter(|| fastly::count_downloads(black_box(Cursor::new(bytes))));
+    });
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
