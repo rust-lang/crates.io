852596489a1bf3961534aa16953a5f0f3cac4198
Tobias Bieniek <tobias@bieniek.cloud>
2024-02-20 12:37:38 +0100

    components/crate-sidebar: Use `=` prefixes if exact versions are shown (#8161)

--- tests/components/crate-sidebar/toml-snippet-test.js
+++ tests/components/crate-sidebar/toml-snippet-test.js
@@ -11,6 +11,23 @@ module('Component | CrateSidebar | toml snippet', function (hooks) {
   setupRenderingTest(hooks);
   setupMirage(hooks);
 
+  test('show version number with `=` prefix', async function (assert) {
+    let crate = this.server.create('crate', { name: 'foo' });
+    this.server.create('version', { crate, num: '1.0.0' });
+
+    let store = this.owner.lookup('service:store');
+    this.crate = await store.findRecord('crate', crate.name);
+    this.version = (await this.crate.versions).firstObject;
+
+    await render(hbs`<CrateSidebar @crate={{this.crate}} @version={{this.version}} />`);
+    assert.dom('[title="Copy command to clipboard"]').exists().hasText('cargo add foo');
+    assert.dom('[title="Copy Cargo.toml snippet to clipboard"]').exists().hasText('foo = "1.0.0"');
+
+    await render(hbs`<CrateSidebar @crate={{this.crate}} @version={{this.version}} @requestedVersion="1.0.0" />`);
+    assert.dom('[title="Copy command to clipboard"]').exists().hasText('cargo add foo@=1.0.0');
+    assert.dom('[title="Copy Cargo.toml snippet to clipboard"]').exists().hasText('foo = "=1.0.0"');
+  });
+
   test('show version number without build metadata', async function (assert) {
     let crate = this.server.create('crate', { name: 'foo' });
     this.server.create('version', { crate, num: '1.0.0+abcdef' });
