d8ef2a83ce78ab47ffba6f6376c0b9d3a0d5e7df
Tobias Bieniek <tobias@bieniek.cloud>
2024-02-23 11:13:24 +0100

    db: Extract `statement_timeout` variable

--- src/db.rs
+++ src/db.rs
@@ -148,11 +148,8 @@ pub struct ConnectionConfig {
 
 impl ConnectionConfig {
     fn apply(&self, conn: &mut PgConnection) -> QueryResult<()> {
-        diesel::sql_query(format!(
-            "SET statement_timeout = {}",
-            self.statement_timeout.as_millis()
-        ))
-            .execute(conn)?;
+        let statement_timeout = self.statement_timeout.as_millis();
+        diesel::sql_query(format!("SET statement_timeout = {statement_timeout}")).execute(conn)?;
 
         if self.read_only {
             diesel::sql_query("SET default_transaction_read_only = 't'").execute(conn)?;
