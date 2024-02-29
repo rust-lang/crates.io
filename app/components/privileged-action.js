1e85bd0469e95e9bf3663ded9200ee1020ad66be
Adam Harvey <adam@adamharvey.name>
2024-02-28 18:13:07 -0800

    components: add a `PrivilegedAction` component
    This component wraps controls that can be used by either an owning user
    or by an admin. The `@userAuthorised` argument is used to indicate that
    the current user is allowed to perform the action wrapped by the
    component, while the admin status is extracted from the current session.
    
    If an admin is logged in, but sudo mode is disabled, then a disabled
    version of the control(s) will be shown by default. This can be
    customised by specifying the `:placeholder` block.

---
+++ app/components/privileged-action.js
@@ -0,0 +1,55 @@
+import { inject as service } from '@ember/service';
+import Component from '@glimmer/component';
+
+/**
+ * A component that wraps elements (probably mostly buttons in practice) that
+ * can be used to perform potentially privileged actions.
+ *
+ * This component requires a `userAuthorised` property, which must be a
+ * `boolean` indicating whether the user is authorised for this action. Admin
+ * rights need not be taken into account.
+ *
+ * If the current user is an admin and they have enabled sudo mode, then they
+ * are always allowed to perform the action, regardless of the return value of
+ * `userAuthorised`.
+ *
+ * There are three content blocks supported by this component:
+ *
+ * - `default`: required; this is displayed when the user is authorised to
+ *              perform the action.
+ * - `placeholder`: this is displayed when the user _could_ be authorised to
+ *                  perform the action (that is, they're an admin but have not
+ *                  enabled sudo mode), but currently cannot perform the action.
+ *                  If omitted, the `default` block is used with all form
+ *                  controls disabled and a tooltip added.
+ * - `unprivileged`: this is displayed when the user is not able to perform this
+ *                   action, and cannot be authorised to do so. If omitted, an
+ *                   empty block will be used.
+ *
+ * Note that all blocks will be output with a wrapping `<div>` for technical
+ * reasons, so be sure to style accordingly if necessary.
+ */
+export default class PrivilegedAction extends Component {
+  /** @type {import("../services/session").default} */
+  @service session;
+
+  /** @return {boolean} */
+  get isPrivileged() {
+    return this.session.isSudoEnabled || this.args.userAuthorised;
+  }
+
+  /** @return {boolean} */
+  get canBePrivileged() {
+    return !this.args.userAuthorised && this.session.currentUser?.is_admin && !this.session.isSudoEnabled;
+  }
+
+  /**
+   * @param {Element} element
+   * @return {void}
+   */
+  disableComponents(element) {
+    [...element.querySelectorAll('input, select, textarea, button')].forEach(control => {
+      control.setAttribute('disabled', 'disabled');
+    });
+  }
+}
---
+++ tests/components/privileged-action-test.js
@@ -0,0 +1,139 @@
+import { render } from '@ember/test-helpers';
+import { module, test } from 'qunit';
+
+import { hbs } from 'ember-cli-htmlbars';
+
+import { setupRenderingTest } from 'crates-io/tests/helpers';
+
+import setupMirage from '../helpers/setup-mirage';
+
+module('Component | PrivilegedAction', hooks => {
+  setupRenderingTest(hooks);
+  setupMirage(hooks);
+
+  hooks.beforeEach(function () {
+    // Adds a utility function that renders a PrivilegedAction with all the
+    // possible content blocks.
+    this.renderComponent = async function (userAuthorised) {
+      this.userAuthorised = userAuthorised;
+      await render(hbs`
+      <PrivilegedAction @userAuthorised={{this.userAuthorised}}>
+        <:default><div data-test-privileged>privileged</div></:default>
+        <:placeholder><div data-test-placeholder>placeholder</div></:placeholder>
+        <:unprivileged><div data-test-unprivileged>unprivileged</div></:unprivileged>
+      </PrivilegedAction>
+    `);
+    };
+  });
+
+  test('unprivileged block is shown to logged out users', async function (assert) {
+    await this.renderComponent(false);
+    assert.dom('[data-test-privileged]').doesNotExist();
+    assert.dom('[data-test-placeholder]').doesNotExist();
+    assert.dom('[data-test-unprivileged]').exists();
+  });
+
+  test('unprivileged block is shown to a logged in user without access', async function (assert) {
+    const user = this.server.create('user');
+    this.authenticateAs(user);
+
+    await this.renderComponent(false);
+    assert.dom('[data-test-privileged]').doesNotExist();
+    assert.dom('[data-test-placeholder]').doesNotExist();
+    assert.dom('[data-test-unprivileged]').exists();
+  });
+
+  test('privileged block is shown to a logged in user with access', async function (assert) {
+    const user = this.server.create('user');
+    this.authenticateAs(user);
+
+    await this.renderComponent(true);
+    assert.dom('[data-test-privileged]').exists();
+    assert.dom('[data-test-placeholder]').doesNotExist();
+    assert.dom('[data-test-unprivileged]').doesNotExist();
+  });
+
+  test('placeholder block is shown to a logged in admin without sudo', async function (assert) {
+    const user = this.server.create('user', { isAdmin: true });
+    this.authenticateAs(user);
+
+    const session = this.owner.lookup('service:session');
+    let { currentUser } = await session.loadUserTask.perform();
+    assert.true(currentUser.is_admin);
+    assert.false(session.isSudoEnabled);
+
+    await this.renderComponent(false);
+    assert.dom('[data-test-privileged]').doesNotExist();
+    assert.dom('[data-test-placeholder]').exists();
+    assert.dom('[data-test-unprivileged]').doesNotExist();
+  });
+
+  test('privileged block is shown to a logged in admin without sudo with access', async function (assert) {
+    const user = this.server.create('user', { isAdmin: true });
+    this.authenticateAs(user);
+
+    const session = this.owner.lookup('service:session');
+    let { currentUser } = await session.loadUserTask.perform();
+    assert.true(currentUser.is_admin);
+    assert.false(session.isSudoEnabled);
+
+    await this.renderComponent(true);
+    assert.dom('[data-test-privileged]').exists();
+    assert.dom('[data-test-placeholder]').doesNotExist();
+    assert.dom('[data-test-unprivileged]').doesNotExist();
+  });
+
+  test('privileged block is shown to a logged in admin with sudo', async function (assert) {
+    const user = this.server.create('user', { isAdmin: true });
+    this.authenticateAs(user);
+
+    const session = this.owner.lookup('service:session');
+    let { currentUser } = await session.loadUserTask.perform();
+    assert.true(currentUser.is_admin);
+    session.setSudo(86_400_000);
+    assert.true(session.isSudoEnabled);
+
+    await this.renderComponent(false);
+    assert.dom('[data-test-privileged]').exists();
+    assert.dom('[data-test-placeholder]').doesNotExist();
+    assert.dom('[data-test-unprivileged]').doesNotExist();
+  });
+
+  test('automatic placeholder block', async function (assert) {
+    const user = this.server.create('user', { isAdmin: true });
+    this.authenticateAs(user);
+
+    const session = this.owner.lookup('service:session');
+    let { currentUser } = await session.loadUserTask.perform();
+    assert.true(currentUser.is_admin);
+    assert.false(session.isSudoEnabled);
+
+    // We're trying to confirm that all the form controls are automatically
+    // disabled.
+    await render(hbs`
+      <PrivilegedAction @userAuthorised={{false}}>
+        <div data-test-content>
+          <button data-test-control type="button">Click me maybe?</button>
+          <label for="input">Input: </label><input data-test-control type="text" id="input" />
+          <label for="select">Select: </label><select data-test-control id="select"><option>foo</option></select>
+          <label for="textarea">Textarea: </label><textarea data-test-control id="textarea" />
+        </div>
+      </PrivilegedAction>
+    `);
+    assert.dom('[data-test-content] [data-test-control]').exists().isDisabled();
+  });
+
+  test('automatic unprivileged block', async function (assert) {
+    // We're testing that the default block content isn't shown, and that the
+    // automatically generated div has no content.
+    await render(hbs`
+      <div data-test-container>
+        <PrivilegedAction @userAuthorised={{false}}>
+          <div data-test-content>should not be shown</div>
+        </PrivilegedAction>
+      </div>
+    `);
+    assert.dom('[data-test-content]').doesNotExist();
+    assert.dom('[data-test-container] > div').exists().hasNoText();
+  });
+});
