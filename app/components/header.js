4455ac82e32523ecd32c3e3c09fa7c94d7fe376d
Adam Harvey <adam@adamharvey.name>
2024-02-28 18:08:17 -0800

    header: add a menu item for admins to enter and leave sudo mode
    If the user is in sudo mode, the expiry time is also displayed in the
    menu.

--- app/components/header.js
+++ app/components/header.js
@@ -1,6 +1,19 @@
+import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 import Component from '@glimmer/component';
 
 export default class Header extends Component {
+  /** @type {import("../services/session").default} */
   @service session;
+
+  @action
+  enableSudo() {
+    // FIXME: hard coded six hour duration.
+    this.session.setSudo(6 * 60 * 60 * 1000);
+  }
+
+  @action
+  disableSudo() {
+    this.session.setSudo(0);
+  }
 }
