662dd315e027808dabab67ceef5a5dfb850738a5
Adam Harvey <adam@adamharvey.name>
2024-02-29 14:28:07 -0800

    header: use a constant for the sudo session duration

--- app/components/header.js
+++ app/components/header.js
@@ -2,14 +2,16 @@ import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 import Component from '@glimmer/component';
 
+// Six hours.
+const SUDO_SESSION_DURATION_MS = 6 * 60 * 60 * 1000;
+
 export default class Header extends Component {
   /** @type {import("../services/session").default} */
   @service session;
 
   @action
   enableSudo() {
-    // FIXME: hard coded six hour duration.
-    this.session.setSudo(6 * 60 * 60 * 1000);
+    this.session.setSudo(SUDO_SESSION_DURATION_MS);
   }
 
   @action
