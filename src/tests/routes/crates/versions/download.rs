83ff39ca8b794ff0f10b1b74a20ecaf05519c8f5
Tobias Bieniek <tobias@bieniek.cloud>
2024-03-13 09:37:37 +0100

    tests/crates/versions/download: Remove obsolete tests

--- src/tests/routes/crates/versions/download.rs
+++ src/tests/routes/crates/versions/download.rs
@@ -1,48 +1,9 @@
 use crate::builders::{CrateBuilder, VersionBuilder};
 use crate::util::{RequestHelper, TestApp};
-use http::StatusCode;
 
 #[test]
-fn download_nonexistent_version_of_existing_crate_404s() {
-    let (app, anon, user) = TestApp::init()
-        .with_config(|config| config.cdn_log_counting_enabled = false)
-        .with_user();
-    let user = user.as_model();
-
-    app.db(|conn| {
-        CrateBuilder::new("foo_bad", user.id).expect_build(conn);
-    });
-
-    anon.get("/api/v1/crates/foo_bad/0.1.0/download")
-        .assert_not_found();
-}
-
-#[test]
-fn rejected_non_canonical_download() {
-    let (app, anon, user) = TestApp::init()
-        .with_config(|config| config.cdn_log_counting_enabled = false)
-        .with_user();
-
-    app.db(|conn| {
-        let user = user.as_model();
-        CrateBuilder::new("foo_download", user.id)
-            .version(VersionBuilder::new("1.0.0"))
-            .expect_build(conn);
-    });
-
-    // Request download for "foo-download" with a dash instead of an underscore,
-    // and assert that the correct download link is returned.
-    let response = anon.get::<()>("/api/v1/crates/foo-download/1.0.0/download");
-    assert_eq!(response.status(), StatusCode::NOT_FOUND);
-}
-
-#[test]
-fn force_unconditional_redirect() {
-    let (app, anon, user) = TestApp::init()
-        .with_config(|config| {
-            config.force_unconditional_redirects = true;
-        })
-        .with_user();
+fn test_redirects() {
+    let (app, anon, user) = TestApp::init().with_user();
 
     app.db(|conn| {
         CrateBuilder::new("foo-download", user.as_model().id)
@@ -65,44 +26,6 @@ fn force_unconditional_redirect() {
     // Redirects to missing crates are performed unconditionally.
     anon.get::<()>("/api/v1/crates/bar-download/1.0.0/download")
         .assert_redirect_ends_with("/crates/bar-download/bar-download-1.0.0.crate");
-}
-
-#[test]
-fn download_caches_version_id() {
-    use super::super::downloads;
-    use crates_io::schema::crates;
-    use diesel::prelude::*;
-
-    let (app, anon, user) = TestApp::init()
-        .with_config(|config| config.cdn_log_counting_enabled = false)
-        .with_user();
-    let user = user.as_model();
-
-    app.db(|conn| {
-        CrateBuilder::new("foo_download", user.id)
-            .version(VersionBuilder::new("1.0.0"))
-            .expect_build(conn);
-    });
-
-    anon.get::<()>("/api/v1/crates/foo_download/1.0.0/download")
-        .assert_redirect_ends_with("/crates/foo_download/foo_download-1.0.0.crate");
-
-    // Rename the crate, so that `foo_download` will not be found if its version_id was not cached
-    app.db(|conn| {
-        diesel::update(crates::table.filter(crates::name.eq("foo_download")))
-            .set(crates::name.eq("other"))
-            .execute(conn)
-            .unwrap();
-    });
-
-    // This would result in a 404 if the endpoint tried to read from the database
-    anon.get::<()>("/api/v1/crates/foo_download/1.0.0/download")
-        .assert_redirect_ends_with("/crates/foo_download/foo_download-1.0.0.crate");
-
-    // Downloads are persisted by version_id, so the rename doesn't matter
-    downloads::persist_downloads_count(&app);
-    // Check download count against the new name, rather than rename it back to the original value
-    downloads::assert_dl_count(&anon, "other/1.0.0", None, 2);
 }
 
 #[test]
