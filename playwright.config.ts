10b5f24fddb290ffb0684a0f7ca87c3ae3783ef1
eth3lbert <eth3lbert+dev@gmail.com>
2024-05-24 18:26:47 +0800

    CI: Adjust playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,9 +18,11 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 2 : undefined,
+  workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.CI ? 'dot' : [['html', { open: 'never' }]],
+  reporter: process.env.CI
+    ? [['github'], ['html', { outputFolder: 'playwright-report' }]]
+    : [['html', { open: 'never' }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -73,5 +75,6 @@ export default defineConfig({
     command: 'pnpm start',
     url: 'http://127.0.0.1:4200',
     reuseExistingServer: !process.env.CI,
+    timeout: 5 * 60 * 1000,
   },
 });
