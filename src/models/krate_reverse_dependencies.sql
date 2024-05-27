5ff41e735e7fb8e42613d941b56e70bdb83ce598
eth3lbert <eth3lbert+dev@gmail.com>
2024-05-28 00:15:23 +0800

    Improve the performance of reverse dependencies using the `default_versions`

--- src/models/krate_reverse_dependencies.sql
+++ src/models/krate_reverse_dependencies.sql
@@ -1,42 +1,32 @@
-SELECT
-    dependencies.*, crate_downloads, crate_name, total
-FROM (
-    -- Apply pagination to the crates
-    SELECT *, COUNT(*) OVER () as total FROM (
-        SELECT
-            crate_downloads.downloads AS crate_downloads,
-            crates.name AS crate_name,
-            versions.id AS version_id
-        FROM
-        -- We only want the crates whose *max* version is dependent, so we join on a
-        -- subselect that includes the versions with their ordinal position
-        (
-            SELECT DISTINCT ON (crate_id)
-               crate_id, semver_no_prerelease, id
+WITH filtered_default_versions as (
+    SELECT default_versions.*
+    FROM default_versions
+    WHERE version_id IN (SELECT version_id FROM dependencies WHERE crate_id = $1)
+        AND NOT EXISTS (
+            SELECT 1
             FROM versions
-            WHERE NOT yanked
-            ORDER BY
-                crate_id,
-                semver_no_prerelease DESC NULLS LAST,
-                id DESC
-        ) versions
-        INNER JOIN crates
-          ON crates.id = versions.crate_id
-        INNER JOIN crate_downloads
-          ON crate_downloads.crate_id = crates.id
-        WHERE versions.id IN (SELECT version_id FROM dependencies WHERE crate_id = $1)
-    ) c
-    ORDER BY
-        crate_downloads DESC,
-        crate_name ASC
-) crates
+            WHERE id = version_id and yanked
+    )
+)
+SELECT
+    dependencies.*,
+    crate_downloads.downloads as crate_downloads,
+    crates.name as crate_name,
+    (SELECT COUNT(*) from filtered_default_versions) as total
+FROM filtered_default_versions
+INNER JOIN crates
+    ON crates.id = filtered_default_versions.crate_id
+INNER JOIN crate_downloads using (crate_id)
 -- Multiple dependencies can exist, we only want first one
 CROSS JOIN LATERAL (
     SELECT dependencies.*
     FROM dependencies
-    WHERE dependencies.crate_id = $1 AND dependencies.version_id = crates.version_id
+    WHERE dependencies.crate_id = $1 AND dependencies.version_id = filtered_default_versions.version_id
     ORDER BY id ASC
     LIMIT 1
 ) dependencies
+ORDER BY
+    crate_downloads DESC,
+    crate_name ASC
 OFFSET $2
 LIMIT $3
