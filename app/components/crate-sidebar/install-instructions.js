ff0405acc6d5e638ac424495d870a4d174e551a5
Tobias Bieniek <tobias@bieniek.cloud>
2024-06-17 09:35:13 +0200

    CrateSidebar: Show "cargo install" snippet for crates with binaries

--- app/components/crate-sidebar/install-instructions.js
+++ app/components/crate-sidebar/install-instructions.js
@@ -1,6 +1,12 @@
 import Component from '@glimmer/component';
 
 export default class InstallInstructions extends Component {
+  get cargoInstallCommand() {
+    return this.args.exactVersion
+      ? `cargo install ${this.args.crate}@${this.args.version}`
+      : `cargo install ${this.args.crate}`;
+  }
+
   get cargoAddCommand() {
     return this.args.exactVersion
       ? `cargo add ${this.args.crate}@=${this.args.version}`
