d1b3f3edeae0751461d368214220e9f78cd552fa
hi-rustin <rustin.liu@gmail.com>
2024-06-28 22:53:01 +0800

    feat: add token info fetching on new token page
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>

--- app/routes/settings/tokens/new.js
+++ app/routes/settings/tokens/new.js
@@ -1,6 +1,45 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+
+import { patternDescription } from '../../../utils/token-scopes';
 
 export default class TokenListRoute extends Route {
+  @service store;
+
+  queryParams = {
+    token_id: {
+      refreshModel: true,
+    },
+  };
+
+  async model(params) {
+    const tokenId = params.token_id;
+    if (tokenId) {
+      return await this.store.findRecord('api-token', tokenId);
+    }
+    return null;
+  }
+
+  setupController(controller, model) {
+    super.setupController(controller, model);
+    if (model) {
+      const { name, endpoint_scopes, crate_scopes } = model;
+      let properties = {
+        name,
+        ...(endpoint_scopes && { scopes: endpoint_scopes }),
+        ...(crate_scopes && {
+          crateScopes: crate_scopes.map(pattern => ({
+            pattern,
+            showAsInvalid: false,
+            description: patternDescription(pattern),
+          })),
+        }),
+      };
+
+      controller.setProperties(properties);
+    }
+  }
+
   resetController(controller) {
     controller.saveTokenTask.cancelAll();
   }
