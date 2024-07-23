0d9147b4bce4003916061d81a78a93ba89b72044
Tobias Bieniek <tobias@bieniek.cloud>
2024-07-23 16:34:16 +0200

    controllers/user/other: Remove `spawn_blocking()` calls

--- src/controllers/user/other.rs
+++ src/controllers/user/other.rs
@@ -1,49 +1,44 @@
 use crate::controllers::frontend_prelude::*;
 use bigdecimal::{BigDecimal, ToPrimitive};
-use diesel_async::async_connection_wrapper::AsyncConnectionWrapper;
 
 use crate::models::{CrateOwner, OwnerKind, User};
 use crate::schema::{crate_downloads, crate_owners, crates, users};
 use crate::sql::lower;
 use crate::views::EncodablePublicUser;
+use diesel_async::RunQueryDsl;
 
 /// Handles the `GET /users/:user_id` route.
 pub async fn show(state: AppState, Path(user_name): Path<String>) -> AppResult<Json<Value>> {
-    let conn = state.db_read_prefer_primary().await?;
-    spawn_blocking(move || {
-        let conn: &mut AsyncConnectionWrapper<_> = &mut conn.into();
+    let mut conn = state.db_read_prefer_primary().await?;
 
-        use self::users::dsl::{gh_login, id, users};
+    use self::users::dsl::{gh_login, id, users};
 
-        let name = lower(&user_name);
-        let user: User = users
-            .filter(lower(gh_login).eq(name))
-            .order(id.desc())
-            .first(conn)?;
+    let name = lower(&user_name);
+    let user: User = users
+        .filter(lower(gh_login).eq(name))
+        .order(id.desc())
+        .first(&mut conn)
+        .await?;
 
-        Ok(Json(json!({ "user": EncodablePublicUser::from(user) })))
-    })
-    .await
+    Ok(Json(json!({ "user": EncodablePublicUser::from(user) })))
 }
 
 /// Handles the `GET /users/:user_id/stats` route.
 pub async fn stats(state: AppState, Path(user_id): Path<i32>) -> AppResult<Json<Value>> {
-    let conn = state.db_read_prefer_primary().await?;
-    spawn_blocking(move || {
-        let conn: &mut AsyncConnectionWrapper<_> = &mut conn.into();
+    let mut conn = state.db_read_prefer_primary().await?;
 
-        use diesel::dsl::sum;
+    use diesel::dsl::sum;
+    use diesel_async::RunQueryDsl;
 
-        let data = CrateOwner::by_owner_kind(OwnerKind::User)
-            .inner_join(crates::table)
-            .inner_join(crate_downloads::table.on(crates::id.eq(crate_downloads::crate_id)))
-            .filter(crate_owners::owner_id.eq(user_id))
-            .select(sum(crate_downloads::downloads))
-            .first::<Option<BigDecimal>>(conn)?
-            .map(|d| d.to_u64().unwrap_or(u64::MAX))
-            .unwrap_or(0);
+    let data = CrateOwner::by_owner_kind(OwnerKind::User)
+        .inner_join(crates::table)
+        .inner_join(crate_downloads::table.on(crates::id.eq(crate_downloads::crate_id)))
+        .filter(crate_owners::owner_id.eq(user_id))
+        .select(sum(crate_downloads::downloads))
+        .first::<Option<BigDecimal>>(&mut conn)
+        .await?
+        .map(|d| d.to_u64().unwrap_or(u64::MAX))
+        .unwrap_or(0);
 
-        Ok(Json(json!({ "total_downloads": data })))
-    })
-    .await
+    Ok(Json(json!({ "total_downloads": data })))
 }
