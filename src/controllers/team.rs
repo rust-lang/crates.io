8c47e69dab18b1a34405d6b95e951b6b6c140d23
Tobias Bieniek <tobias@bieniek.cloud>
2024-07-23 16:30:48 +0200

    controllers/team: Remove `spawn_blocking()` call

--- src/controllers/team.rs
+++ src/controllers/team.rs
@@ -1,19 +1,15 @@
 use crate::controllers::frontend_prelude::*;
-use diesel_async::async_connection_wrapper::AsyncConnectionWrapper;
 
 use crate::models::Team;
 use crate::schema::teams;
 use crate::views::EncodableTeam;
+use diesel_async::RunQueryDsl;
 
 /// Handles the `GET /teams/:team_id` route.
 pub async fn show_team(state: AppState, Path(name): Path<String>) -> AppResult<Json<Value>> {
     use self::teams::dsl::{login, teams};
 
-    let conn = state.db_read().await?;
-    spawn_blocking(move || {
-        let conn: &mut AsyncConnectionWrapper<_> = &mut conn.into();
-        let team: Team = teams.filter(login.eq(&name)).first(conn)?;
-        Ok(Json(json!({ "team": EncodableTeam::from(team) })))
-    })
-    .await
+    let mut conn = state.db_read().await?;
+    let team: Team = teams.filter(login.eq(&name)).first(&mut conn).await?;
+    Ok(Json(json!({ "team": EncodableTeam::from(team) })))
 }
