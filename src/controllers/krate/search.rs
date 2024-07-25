0f02cedf4e84ee01f68129aedcb0acfb56804c13
Tobias Bieniek <tobias@bieniek.cloud>
2024-07-25 19:15:49 +0200

    Replace `once_cell::sync::OnceCell` with `std::cell::OnceCell`

--- src/controllers/krate/search.rs
+++ src/controllers/krate/search.rs
@@ -4,7 +4,7 @@ use crate::auth::AuthCheck;
 use diesel::dsl::*;
 use diesel::sql_types::{Array, Bool, Text};
 use diesel_full_text_search::*;
-use once_cell::sync::OnceCell;
+use std::cell::OnceCell;
 
 use crate::controllers::cargo_prelude::*;
 use crate::controllers::helpers::Paginate;
@@ -282,11 +282,17 @@ impl<'a> FilterParams<'a> {
             .as_deref()
     }
 
-    fn authed_user_id(&self, req: &Parts, conn: &mut PgConnection) -> AppResult<&i32> {
-        self._auth_user_id.get_or_try_init(|| {
-            let user_id = AuthCheck::default().check(req, conn)?.user_id();
-            Ok(user_id)
-        })
+    fn authed_user_id(&self, req: &Parts, conn: &mut PgConnection) -> AppResult<i32> {
+        if let Some(val) = self._auth_user_id.get() {
+            return Ok(*val);
+        }
+
+        let user_id = AuthCheck::default().check(req, conn)?.user_id();
+
+        // This should not fail, because of the `get()` check above
+        let _ = self._auth_user_id.set(user_id);
+
+        Ok(user_id)
     }
 
     fn make_query(
