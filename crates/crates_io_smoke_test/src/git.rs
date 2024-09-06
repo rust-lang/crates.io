0804216ab20f4f4913fe874f961508837b086b5c
Tobias Bieniek <tobias@bieniek.cloud>
2024-09-06 12:00:14 +0200

    smoke_test: Set git username and email before committing (#9406)
    On GitHub Actions the smoke test currently fails with:
    
    ```
    Author identity unknown
    *** Please tell me who you are.
    Run
      git config --global user.email "you@example.com"
      git config --global user.name "Your Name"
    to set your account's default identity.
    Omit --global to set the identity only in this repository.
    ```
    
    This change adds the necessary git commands to our setup to hopefully make the `git commit` call succeed now.

--- crates/crates_io_smoke_test/src/git.rs
+++ crates/crates_io_smoke_test/src/git.rs
@@ -3,6 +3,28 @@ use std::path::Path;
 use tokio::process::Command;
 
 #[allow(unstable_name_collisions)]
+pub async fn set_user_name(project_path: &Path, name: &str) -> anyhow::Result<()> {
+    Command::new("git")
+        .args(["config", "user.name", name])
+        .current_dir(project_path)
+        .status()
+        .await?
+        .exit_ok()
+        .map_err(Into::into)
+}
+
+#[allow(unstable_name_collisions)]
+pub async fn set_user_email(project_path: &Path, email: &str) -> anyhow::Result<()> {
+    Command::new("git")
+        .args(["config", "user.email", email])
+        .current_dir(project_path)
+        .status()
+        .await?
+        .exit_ok()
+        .map_err(Into::into)
+}
+
+#[allow(unstable_name_collisions)]
 pub async fn add_all(project_path: &Path) -> anyhow::Result<()> {
     Command::new("git")
         .args(["add", "--all"])
