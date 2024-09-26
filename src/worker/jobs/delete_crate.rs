e021fe8797265a47e0274c40db0ac481f693716e
Tobias Bieniek <tobias@bieniek.cloud>
2024-09-26 15:18:28 +0200

    jobs/delete_crate: Run deletions concurrently

--- src/worker/jobs/delete_crate.rs
+++ src/worker/jobs/delete_crate.rs
@@ -3,6 +3,7 @@ use crate::worker::Environment;
 use anyhow::Context;
 use crates_io_worker::BackgroundJob;
 use std::sync::Arc;
+use tokio::try_join;
 
 /// A background job that deletes all files associated with a crate from the storage backend.
 #[derive(Serialize, Deserialize)]
@@ -24,18 +25,24 @@ impl BackgroundJob for DeleteCrateFromStorage {
     async fn run(&self, ctx: Self::Context) -> anyhow::Result<()> {
         let name = &self.name;
 
-        info!("{name}: Deleting crate files from S3…");
-        let result = ctx.storage.delete_all_crate_files(name).await;
-        result.context("Failed to delete crate files from S3")?;
-
-        info!("{name}: Deleting readme files from S3…");
-        let result = ctx.storage.delete_all_readmes(name).await;
-        result.context("Failed to delete readme files from S3")?;
-
-        info!("{name}: Deleting RSS feed from S3…");
-        let feed_id = FeedId::Crate { name };
-        let result = ctx.storage.delete_feed(&feed_id).await;
-        result.context("Failed to delete RSS feed from S3")?;
+        try_join!(
+            async {
+                info!("{name}: Deleting crate files from S3…");
+                let result = ctx.storage.delete_all_crate_files(name).await;
+                result.context("Failed to delete crate files from S3")
+            },
+            async {
+                info!("{name}: Deleting readme files from S3…");
+                let result = ctx.storage.delete_all_readmes(name).await;
+                result.context("Failed to delete readme files from S3")
+            },
+            async {
+                info!("{name}: Deleting RSS feed from S3…");
+                let feed_id = FeedId::Crate { name };
+                let result = ctx.storage.delete_feed(&feed_id).await;
+                result.context("Failed to delete RSS feed from S3")
+            }
+        )?;
 
         info!("{name}: Successfully deleted crate from S3");
         Ok(())
