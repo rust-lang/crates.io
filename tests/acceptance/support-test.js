bed11e318cd80ce9cb8df9238998d0c31283ec54
eth3lbert <eth3lbert+dev@gmail.com>
2024-09-27 14:05:57 +0800

    frontend: Add a crate report form to support page
    A report form will be displayed on the support page. The user must input
    a crate name, select reasons, and provide detailed information
    (required when the "other" reason is chosen) before submitting.

--- tests/acceptance/support-test.js
+++ tests/acceptance/support-test.js
@@ -1,8 +1,10 @@
-import { currentURL, findAll } from '@ember/test-helpers';
+import { click, currentURL, fillIn, findAll } from '@ember/test-helpers';
 import { module, test } from 'qunit';
 
 import percySnapshot from '@percy/ember';
 import a11yAudit from 'ember-a11y-testing/test-support/audit';
+import window from 'ember-window-mock';
+import { setupWindowMock } from 'ember-window-mock/test-support';
 
 import { setupApplicationTest } from 'crates-io/tests/helpers';
 
@@ -41,5 +43,151 @@ module('Acceptance | support', function (hooks) {
       listitem.map(item => item.textContent.trim()),
       ['Report a crate that violates policies'],
     );
+  });
+
+  module('reporting a crate from support page', function () {
+    setupWindowMock(hooks);
+
+    async function prepare(context, assert) {
+      let server = context.server;
+      let crate = server.create('crate', { name: 'nanomsg' });
+      server.create('version', { crate, num: '0.6.0' });
+
+      window.open = (url, target, features) => {
+        window.openKwargs = { url, target, features };
+        return { document: { write() {}, close() {} }, close() {} };
+      };
+
+      await visit('/support');
+      await click('[data-test-id="link-crate-violation"]');
+      assert.strictEqual(currentURL(), '/support?inquire=crate-violation');
+    }
+
+    test('show a report form', async function (assert) {
+      await prepare(this, assert);
+
+      assert.dom('[data-test-id="support-main-content"] section').exists({ count: 1 });
+      assert.dom('[data-test-id="crate-violation-section"]').exists();
+      assert.dom('[data-test-id="fieldset-crate"]').exists();
+      assert.dom('[data-test-id="fieldset-reasons"]').exists();
+      assert.dom('[data-test-id="fieldset-detail"]').exists();
+      assert.dom('[data-test-id="report-button"]').hasText('Report');
+
+      await percySnapshot(assert);
+      await a11yAudit(axeConfig);
+    });
+
+    test('empty form should shows errors', async function (assert) {
+      await prepare(this, assert);
+      await click('[data-test-id="report-button"]');
+
+      assert.dom('[data-test-id="crate-invalid"]').exists();
+      assert.dom('[data-test-id="reasons-invalid"]').exists();
+      assert.dom('[data-test-id="detail-invalid"]').doesNotExist();
+
+      assert.strictEqual(window.openKwargs, undefined);
+    });
+
+    test('empty crate should shows errors', async function (assert) {
+      await prepare(this, assert);
+      assert.dom('[data-test-id="crate-input"]').hasValue('');
+      await click('[data-test-id="report-button"]');
+
+      assert.dom('[data-test-id="crate-invalid"]').exists();
+      assert.dom('[data-test-id="reasons-invalid"]').exists();
+      assert.dom('[data-test-id="detail-invalid"]').doesNotExist();
+
+      assert.strictEqual(window.openKwargs, undefined);
+    });
+
+    test('other reason selected without given detail shows an error', async function (assert) {
+      await prepare(this, assert);
+      await fillIn('[data-test-id="crate-input"]', 'nanomsg');
+      assert.dom('[data-test-id="crate-input"]').hasValue('nanomsg');
+
+      await click('[data-test-id="spam-checkbox"]');
+      assert.dom('[data-test-id="spam-checkbox"]').isChecked();
+      await click('[data-test-id="other-checkbox"]');
+      assert.dom('[data-test-id="other-checkbox"]').isChecked();
+      assert.dom('[data-test-id="detail-input"]').hasValue('');
+      await click('[data-test-id="report-button"]');
+
+      assert.dom('[data-test-id="crate-invalid"]').doesNotExist();
+      assert.dom('[data-test-id="reasons-invalid"]').doesNotExist();
+      assert.dom('[data-test-id="detail-invalid"]').exists();
+
+      assert.strictEqual(window.openKwargs, undefined);
+    });
+
+    test('valid form without detail', async function (assert) {
+      await prepare(this, assert);
+      await fillIn('[data-test-id="crate-input"]', 'nanomsg');
+      assert.dom('[data-test-id="crate-input"]').hasValue('nanomsg');
+
+      await click('[data-test-id="spam-checkbox"]');
+      assert.dom('[data-test-id="spam-checkbox"]').isChecked();
+      assert.dom('[data-test-id="detail-input"]').hasValue('');
+      await click('[data-test-id="report-button"]');
+
+      assert.dom('[data-test-id="crate-invalid"]').doesNotExist();
+      assert.dom('[data-test-id="reasons-invalid"]').doesNotExist();
+      assert.dom('[data-test-id="detail-invalid"]').doesNotExist();
+
+      let body = `I'm reporting the https://crates.io/crates/nanomsg crate because:
+
+- [x] it contains spam
+- [ ] it is name-squatting (reserving a crate name without content)
+- [ ] it is abusive or otherwise harmful
+- [ ] it contains a vulnerability (please try to contact the crate author first)
+- [ ] it is violating the usage policy in some other way (please specify below)
+
+Additional details:
+
+
+`;
+      let subject = `The "nanomsg" crate`;
+      let address = 'help@crates.io';
+      let mailto = `mailto:${address}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+      assert.true(!!window.openKwargs);
+      assert.strictEqual(window.openKwargs.url, mailto);
+      assert.strictEqual(window.openKwargs.target, '_self');
+    });
+
+    test('valid form with required detail', async function (assert) {
+      await prepare(this, assert);
+      await fillIn('[data-test-id="crate-input"]', 'nanomsg');
+      assert.dom('[data-test-id="crate-input"]').hasValue('nanomsg');
+
+      await click('[data-test-id="spam-checkbox"]');
+      assert.dom('[data-test-id="spam-checkbox"]').isChecked();
+      await click('[data-test-id="other-checkbox"]');
+      assert.dom('[data-test-id="other-checkbox"]').isChecked();
+      await fillIn('[data-test-id="detail-input"]', 'test detail');
+      assert.dom('[data-test-id="detail-input"]').hasValue('test detail');
+      await click('[data-test-id="report-button"]');
+
+      assert.dom('[data-test-id="crate-invalid"]').doesNotExist();
+      assert.dom('[data-test-id="reasons-invalid"]').doesNotExist();
+      assert.dom('[data-test-id="detail-invalid"]').doesNotExist();
+
+      let body = `I'm reporting the https://crates.io/crates/nanomsg crate because:
+
+- [x] it contains spam
+- [ ] it is name-squatting (reserving a crate name without content)
+- [ ] it is abusive or otherwise harmful
+- [ ] it contains a vulnerability (please try to contact the crate author first)
+- [x] it is violating the usage policy in some other way (please specify below)
+
+Additional details:
+
+test detail
+`;
+      let subject = `The "nanomsg" crate`;
+      let address = 'help@crates.io';
+      let mailto = `mailto:${address}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+      assert.true(!!window.openKwargs);
+      assert.strictEqual(window.openKwargs.url, mailto);
+      assert.strictEqual(window.openKwargs.target, '_self');
+    });
   });
 });
