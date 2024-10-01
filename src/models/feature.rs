22cbdb8f955cf6fbbecda9908bbef441f3306e65
Tobias Bieniek <tobias@bieniek.cloud>
2024-10-01 14:35:58 +0200

    Add real-world test for `split_features()` fn

--- src/models/feature.rs
+++ src/models/feature.rs
@@ -17,7 +17,7 @@ pub fn split_features(features: FeaturesMap) -> (FeaturesMap, FeaturesMap) {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use insta::assert_compact_debug_snapshot;
+    use insta::{assert_compact_debug_snapshot, assert_debug_snapshot};
 
     #[test]
     fn test_split_features_no_deps() {
@@ -95,5 +95,33 @@ mod tests {
 
         assert_compact_debug_snapshot!(features, @r#"{"feature1": ["feature2"], "feature2": ["feature3"]}"#);
         assert_compact_debug_snapshot!(features2, @r#"{"feature3": ["dep:foo"]}"#);
+    }
+
+    #[test]
+    fn test_split_features_clap() {
+        let json = json!({
+            "env": ["clap_builder/env"],
+            "std": ["clap_builder/std"],
+            "help": ["clap_builder/help"],
+            "cargo": ["clap_builder/cargo"],
+            "color": ["clap_builder/color"],
+            "debug": ["clap_builder/debug", "clap_derive?/debug"],
+            "usage": ["clap_builder/usage"],
+            "derive": ["dep:clap_derive"],
+            "string": ["clap_builder/string"],
+            "default": ["std", "color", "help", "usage", "error-context", "suggestions"],
+            "unicode": ["clap_builder/unicode"],
+            "wrap_help": ["clap_builder/wrap_help"],
+            "deprecated": ["clap_builder/deprecated", "clap_derive?/deprecated"],
+            "suggestions": ["clap_builder/suggestions"],
+            "unstable-v5": ["clap_builder/unstable-v5", "clap_derive?/unstable-v5", "deprecated"],
+            "unstable-doc": ["clap_builder/unstable-doc", "derive"],
+            "unstable-ext": ["clap_builder/unstable-ext"],
+            "error-context": ["clap_builder/error-context"],
+            "unstable-styles": ["clap_builder/unstable-styles"]
+        });
+
+        let features = serde_json::from_value::<FeaturesMap>(json).unwrap();
+        assert_debug_snapshot!(split_features(features));
     }
 }
