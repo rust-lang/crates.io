c232a08510cfd8aa31c3c49500408d92ec365175
Tobias Bieniek <tobias@bieniek.cloud>
2024-11-18 00:02:52 +0100

    tests/util/github: Fix test flakyness (#9980)
    `gh_id = 0` has a special meaning within our database. Depending on the order of the tests we could sometimes see tests failing because the initial user was created with `gh_id = 0`. This commit fixes the flakiness by starting the `gh_id` values at 1 instead.

--- src/tests/util/github.rs
+++ src/tests/util/github.rs
@@ -5,7 +5,7 @@ use crates_io_github::{
 };
 use std::sync::atomic::{AtomicUsize, Ordering};
 
-static NEXT_GH_ID: AtomicUsize = AtomicUsize::new(0);
+static NEXT_GH_ID: AtomicUsize = AtomicUsize::new(1);
 
 pub fn next_gh_id() -> i32 {
     NEXT_GH_ID.fetch_add(1, Ordering::SeqCst) as i32
