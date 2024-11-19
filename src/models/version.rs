c7720103756145d5995982b662478556c536c360
Tobias Bieniek <tobias@bieniek.cloud>
2024-11-19 10:26:46 +0100

    publish: Save `description`, `homepage`, `documentation` and `repository` fields in the database

--- src/models/version.rs
+++ src/models/version.rs
@@ -99,6 +99,10 @@ pub struct NewVersion<'a> {
     pub has_lib: Option<bool>,
     pub bin_names: Option<&'a [&'a str]>,
     edition: Option<&'a str>,
+    description: Option<&'a str>,
+    homepage: Option<&'a str>,
+    documentation: Option<&'a str>,
+    repository: Option<&'a str>,
 }
 
 impl NewVersion<'_> {
