24da935525a9238f869038abe2b6bcd4f51d1de5
Tobias Bieniek <tobias@bieniek.cloud>
2024-11-20 15:21:48 +0100

    controllers/keyword: Remove `spawn_blocking()` call

--- src/controllers/keyword.rs
+++ src/controllers/keyword.rs
@@ -2,14 +2,12 @@ use crate::app::AppState;
 use crate::controllers::helpers::pagination::PaginationOptions;
 use crate::controllers::helpers::{pagination::Paginated, Paginate};
 use crate::models::Keyword;
-use crate::tasks::spawn_blocking;
 use crate::util::errors::AppResult;
 use crate::views::EncodableKeyword;
 use axum::extract::{Path, Query};
 use axum_extra::json;
 use axum_extra::response::ErasedJson;
 use diesel::prelude::*;
-use diesel_async::async_connection_wrapper::AsyncConnectionWrapper;
 use http::request::Parts;
 
 #[derive(Deserialize)]
@@ -30,23 +28,18 @@ pub async fn index(state: AppState, qp: Query<IndexQuery>, req: Parts) -> AppRes
 
     let query = query.pages_pagination(PaginationOptions::builder().gather(&req)?);
 
-    let conn = state.db_read().await?;
-    spawn_blocking(move || {
-        let conn: &mut AsyncConnectionWrapper<_> = &mut conn.into();
+    let mut conn = state.db_read().await?;
+    let data: Paginated<Keyword> = query.async_load(&mut conn).await?;
+    let total = data.total();
+    let kws = data
+        .into_iter()
+        .map(Keyword::into)
+        .collect::<Vec<EncodableKeyword>>();
 
-        let data: Paginated<Keyword> = query.load(conn)?;
-        let total = data.total();
-        let kws = data
-            .into_iter()
-            .map(Keyword::into)
-            .collect::<Vec<EncodableKeyword>>();
-
-        Ok(json!({
-            "keywords": kws,
-            "meta": { "total": total },
-        }))
-    })
-    .await?
+    Ok(json!({
+        "keywords": kws,
+        "meta": { "total": total },
+    }))
 }
 
 /// Handles the `GET /keywords/:keyword_id` route.
