329e82ff8bc66078a98d75a961d316f2dc5ab100
eth3lbert <eth3lbert+dev@gmail.com>
2024-11-22 15:22:06 +0800

    tarball: Add `AsyncLimitErrorReader`

--- crates/crates_io_tarball/src/limit_reader.rs
+++ crates/crates_io_tarball/src/limit_reader.rs
@@ -1,5 +1,9 @@
 use std::io;
 use std::io::prelude::*;
+use std::pin::Pin;
+use std::task::{Context, Poll};
+
+use tokio::io::{AsyncRead, AsyncReadExt, ReadBuf};
 
 #[derive(Debug)]
 pub struct LimitErrorReader<R> {
@@ -21,6 +25,36 @@ impl<R: Read> Read for LimitErrorReader<R> {
                 io::ErrorKind::Other,
                 "maximum limit reached when reading",
             )),
+            e => e,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct AsyncLimitErrorReader<R> {
+    inner: tokio::io::Take<R>,
+}
+
+impl<R: AsyncRead + Unpin> AsyncLimitErrorReader<R> {
+    pub fn new(r: R, limit: u64) -> AsyncLimitErrorReader<R> {
+        AsyncLimitErrorReader {
+            inner: r.take(limit),
+        }
+    }
+}
+
+impl<R: AsyncRead + Unpin> AsyncRead for AsyncLimitErrorReader<R> {
+    fn poll_read(
+        mut self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        buf: &mut ReadBuf<'_>,
+    ) -> Poll<io::Result<()>> {
+        let reader = Pin::new(&mut self.inner);
+        match reader.poll_read(cx, buf) {
+            Poll::Ready(Ok(())) if self.inner.limit() == 0 => Poll::Ready(Err(io::Error::new(
+                io::ErrorKind::Other,
+                "maximum limit reached when reading",
+            ))),
             e => e,
         }
     }
