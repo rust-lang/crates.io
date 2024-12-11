2f484ccc45f854a3b864001f0b0a7ab2ef8a44db
Tobias Bieniek <tobias@bieniek.cloud>
2024-12-11 15:46:06 +0100

    openapi: Override derived metadata

--- src/openapi.rs
+++ src/openapi.rs
@@ -2,6 +2,14 @@ use utoipa::OpenApi;
 use utoipa_axum::router::OpenApiRouter;
 
 #[derive(OpenApi)]
+#[openapi(info(
+    title = "crates.io",
+    description = "API documentation for the [crates.io](https://crates.io/) package registry",
+    terms_of_service = "https://crates.io/policies",
+    contact(name = "the crates.io team", email = "help@crates.io"),
+    license(),
+    version = "0.0.0",
+))]
 pub struct BaseOpenApi;
 
 impl BaseOpenApi {
