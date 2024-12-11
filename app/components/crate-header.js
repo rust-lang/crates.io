914964eb8dfad336cd05d9743a88f70aa6a5449c
eth3lbert <eth3lbert+dev@gmail.com>
2024-12-11 16:26:38 +0800

    crate-header: Load `crate.owner_user` explicitly with a task
    This fixes the following deprecation warnings:
    - The findBy method on ember-data's PromiseManyArray is deprecated.
    - The `findBy` method on the class ManyArray is deprecated.

--- app/components/crate-header.js
+++ app/components/crate-header.js
@@ -8,6 +8,7 @@ export default class CrateHeader extends Component {
   @service session;
 
   @alias('loadKeywordsTask.last.value') keywords;
+  @alias('loadOwnerUserTask.last.value') ownerUser;
 
   constructor() {
     super(...arguments);
@@ -15,13 +16,22 @@ export default class CrateHeader extends Component {
     this.loadKeywordsTask.perform().catch(() => {
       // ignore all errors and just don't display keywords if the request fails
     });
+    this.loadOwnerUserTask.perform().catch(() => {
+      // ignore all errors and just don't display settings if the request fails
+    });
   }
 
   get isOwner() {
-    return this.args.crate.owner_user.findBy('id', this.session.currentUser?.id);
+    let ownerUser = this.ownerUser ?? [];
+    let currentUserId = this.session.currentUser?.id;
+    return ownerUser.some(({ id }) => id === currentUserId);
   }
 
   loadKeywordsTask = task(async () => {
     return (await this.args.crate?.keywords) ?? [];
+  });
+
+  loadOwnerUserTask = task(async () => {
+    return (await this.args.crate?.owner_user) ?? [];
   });
 }
