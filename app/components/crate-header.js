07ada01ea9b8f8779178aa2af8049898fb63fb7b
eth3lbert <eth3lbert+dev@gmail.com>
2024-12-11 16:24:48 +0800

    crate-header: Load `crate.keywords` explicitly with a task
    This fixes the following deprecation warning:
    - Do not use A() on an EmberData PromiseManyArray.

--- app/components/crate-header.js
+++ app/components/crate-header.js
@@ -1,10 +1,27 @@
 import { inject as service } from '@ember/service';
 import Component from '@glimmer/component';
 
+import { task } from 'ember-concurrency';
+import { alias } from 'macro-decorators';
+
 export default class CrateHeader extends Component {
   @service session;
+
+  @alias('loadKeywordsTask.last.value') keywords;
+
+  constructor() {
+    super(...arguments);
+
+    this.loadKeywordsTask.perform().catch(() => {
+      // ignore all errors and just don't display keywords if the request fails
+    });
+  }
 
   get isOwner() {
     return this.args.crate.owner_user.findBy('id', this.session.currentUser?.id);
   }
+
+  loadKeywordsTask = task(async () => {
+    return (await this.args.crate?.keywords) ?? [];
+  });
 }
