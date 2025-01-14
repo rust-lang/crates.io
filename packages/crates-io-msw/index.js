5f94902d36f5cf7742eb67e3d78cdb1eefa3655a
Tobias Bieniek <tobias@bieniek.cloud>
2025-01-14 14:44:58 +0100

    msw: Add `@mswjs/data` mock database

--- packages/crates-io-msw/index.js
+++ packages/crates-io-msw/index.js
@@ -1,1 +1,5 @@
+import { factory } from '@mswjs/data';
+
 export const handlers = [];
+
+export const db = factory({});
--- packages/crates-io-msw/vitest.setup.js
+++ packages/crates-io-msw/vitest.setup.js
@@ -1,10 +1,12 @@
+import { drop } from '@mswjs/data';
 import { setupServer } from 'msw/node';
 import { afterAll, afterEach, beforeAll } from 'vitest';
 
-import { handlers } from './index.js';
+import { db, handlers } from './index.js';
 
 const server = setupServer(...handlers);
 
 beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
+afterEach(() => drop(db));
 afterAll(() => server.close());
