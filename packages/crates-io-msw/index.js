61a9a03344c0f15ffde56c3e2ad8bed7f12341d3
Tobias Bieniek <tobias@bieniek.cloud>
2025-01-15 12:33:55 +0100

    msw: Implement `user` model

--- packages/crates-io-msw/index.js
+++ packages/crates-io-msw/index.js
@@ -1,6 +1,7 @@
 import category from './models/category.js';
 import crate from './models/crate.js';
 import keyword from './models/keyword.js';
+import user from './models/user.js';
 import { factory } from './utils/factory.js';
 
 export const handlers = [];
@@ -9,4 +10,5 @@ export const db = factory({
   category,
   crate,
   keyword,
+  user,
 });
