ccb4cef918b85a7a8b069a906a9f57b358a7a596
Tobias Bieniek <tobias@bieniek.cloud>
2025-01-20 14:58:04 +0100

    msw: Implement `keyword` model

--- packages/crates-io-msw/index.js
+++ packages/crates-io-msw/index.js
@@ -1,8 +1,10 @@
 import category from './models/category.js';
+import keyword from './models/keyword.js';
 import { factory } from './utils/factory.js';
 
 export const handlers = [];
 
 export const db = factory({
   category,
+  keyword,
 });
