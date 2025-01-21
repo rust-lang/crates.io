0abb0dd38b7e82226aefd959f18bfff3c9b35cc9
Tobias Bieniek <tobias@bieniek.cloud>
2025-01-21 11:15:01 +0100

    msw: Import `underscore()` fn from `@ember/string`

--- packages/crates-io-msw/utils/strings.js
+++ packages/crates-io-msw/utils/strings.js
@@ -4,3 +4,10 @@ export function dasherize(str) {
     .toLowerCase()
     .replace(/[ _]/g, '-');
 }
+
+export function underscore(str) {
+  return str
+    .replace(/([a-z\d])([A-Z]+)/g, '$1_$2')
+    .replace(/-|\s+/g, '_')
+    .toLowerCase();
+}
--- packages/crates-io-msw/utils/strings.test.js
+++ packages/crates-io-msw/utils/strings.test.js
@@ -1,6 +1,6 @@
 import { describe, test } from 'vitest';
 
-import { dasherize } from './strings.js';
+import { dasherize, underscore } from './strings.js';
 
 describe('dasherize', () => {
   function assert(input, expected) {
@@ -17,4 +17,20 @@ describe('dasherize', () => {
   assert('PrivateDocs/OwnerInvoice', 'private-docs/owner-invoice');
   assert('privateDocs/ownerInvoice', 'private-docs/owner-invoice');
   assert('private_docs/owner_invoice', 'private-docs/owner-invoice');
+});
+
+describe('underscore', () => {
+  function assert(input, expected) {
+    test(input, ({ expect }) => {
+      expect(underscore(input)).toBe(expected);
+    });
+  }
+
+  assert('my favorite items', 'my_favorite_items');
+  assert('css-class-name', 'css_class_name');
+  assert('action_name', 'action_name');
+  assert('innerHTML', 'inner_html');
+  assert('PrivateDocs/OwnerInvoice', 'private_docs/owner_invoice');
+  assert('privateDocs/ownerInvoice', 'private_docs/owner_invoice');
+  assert('private-docs/owner-invoice', 'private_docs/owner_invoice');
 });
