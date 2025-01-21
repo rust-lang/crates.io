0329a7748f3e598c7f3d653cf73037d7eeea293a
Tobias Bieniek <tobias@bieniek.cloud>
2025-01-21 15:53:52 +0100

    msw: Implement `GET /api/v1/me` request handler

--- packages/crates-io-msw/handlers/users.js
+++ packages/crates-io-msw/handlers/users.js
@@ -1,5 +1,6 @@
 import getUser from './users/get.js';
+import me from './users/me.js';
 import resend from './users/resend.js';
 import updateUser from './users/update.js';
 
-export default [getUser, updateUser, resend];
+export default [getUser, updateUser, resend, me];
