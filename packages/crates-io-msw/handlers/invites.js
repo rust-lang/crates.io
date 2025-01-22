48db71427f1cd496676458a30929c30be5d0be66
Tobias Bieniek <tobias@bieniek.cloud>
2025-01-22 12:41:53 +0100

    msw: Implement `PUT /api/v1/me/crate_owner_invitations/:crate_id` request handler

--- packages/crates-io-msw/handlers/invites.js
+++ packages/crates-io-msw/handlers/invites.js
@@ -1,4 +1,5 @@
 import legacyListInvites from './invites/legacy-list.js';
 import listInvites from './invites/list.js';
+import redeemByCrateId from './invites/redeem-by-crate-id.js';
 
-export default [listInvites, legacyListInvites];
+export default [listInvites, legacyListInvites, redeemByCrateId];
