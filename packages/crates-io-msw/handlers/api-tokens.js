20d7f4631fdfa73216b5cac95d26de873146f446
Tobias Bieniek <tobias@bieniek.cloud>
2025-01-22 15:37:18 +0100

    msw: Implement `DELETE /api/v1/me/tokens/:tokenId` request handler

--- packages/crates-io-msw/handlers/api-tokens.js
+++ packages/crates-io-msw/handlers/api-tokens.js
@@ -1,4 +1,5 @@
 import createToken from './api-tokens/create.js';
+import deleteToken from './api-tokens/delete.js';
 import listTokens from './api-tokens/list.js';
 
-export default [createToken, listTokens];
+export default [createToken, listTokens, deleteToken];
