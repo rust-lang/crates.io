81573a1c9dd0be5e2eb0947003a329bb25744d64
Tobias Bieniek <tobias@bieniek.cloud>
2025-01-23 18:54:05 +0100

    msw: Implement `PATCH /api/v1/crates/:name/:version` request handler

--- packages/crates-io-msw/handlers/versions.js
+++ packages/crates-io-msw/handlers/versions.js
@@ -2,8 +2,9 @@ import dependencies from './versions/dependencies.js';
 import downloads from './versions/downloads.js';
 import getVersion from './versions/get.js';
 import listVersions from './versions/list.js';
+import patchVersion from './versions/patch.js';
 import readme from './versions/readme.js';
 import unyankVersion from './versions/unyank.js';
 import yankVersion from './versions/yank.js';
 
-export default [listVersions, getVersion, yankVersion, unyankVersion, dependencies, downloads, readme];
+export default [listVersions, getVersion, patchVersion, yankVersion, unyankVersion, dependencies, downloads, readme];
