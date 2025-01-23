14d7481fb266ad4a3b89b71934c6257f3183ae83
Tobias Bieniek <tobias@bieniek.cloud>
2025-01-23 19:33:02 +0100

    msw: Implement `GET /api/v1/summary` request handler

--- packages/crates-io-msw/index.js
+++ packages/crates-io-msw/index.js
@@ -6,6 +6,7 @@ import inviteHandlers from './handlers/invites.js';
 import keywordHandlers from './handlers/keywords.js';
 import metadataHandlers from './handlers/metadata.js';
 import sessionHandlers from './handlers/sessions.js';
+import summaryHandlers from './handlers/summary.js';
 import teamHandlers from './handlers/teams.js';
 import userHandlers from './handlers/users.js';
 import versionHandlers from './handlers/versions.js';
@@ -32,6 +33,7 @@ export const handlers = [
   ...keywordHandlers,
   ...metadataHandlers,
   ...sessionHandlers,
+  ...summaryHandlers,
   ...teamHandlers,
   ...userHandlers,
   ...versionHandlers,
