e2fbce4b55def2470496c24855f9abf9422466a5
Tobias Bieniek <tobias@bieniek.cloud>
2025-01-23 16:16:22 +0100

    msw: Implement `DELETE /api/v1/crates/:name/follow` request handler

--- packages/crates-io-msw/handlers/crates.js
+++ packages/crates-io-msw/handlers/crates.js
@@ -3,5 +3,6 @@ import followCrate from './crates/follow.js';
 import following from './crates/following.js';
 import getCrate from './crates/get.js';
 import listCrates from './crates/list.js';
+import unfollowCrate from './crates/unfollow.js';
 
-export default [listCrates, getCrate, deleteCrate, following, followCrate];
+export default [listCrates, getCrate, deleteCrate, following, followCrate, unfollowCrate];
---
+++ packages/crates-io-msw/handlers/crates/unfollow.js
@@ -0,0 +1,26 @@
+import { http, HttpResponse } from 'msw';
+
+import { db } from '../../index.js';
+import { notFound } from '../../utils/handlers.js';
+import { getSession } from '../../utils/session.js';
+
+export default http.delete('/api/v1/crates/:name/follow', async ({ params }) => {
+  let { user } = getSession();
+  if (!user) {
+    return HttpResponse.json({ errors: [{ detail: 'must be logged in to perform that action' }] }, { status: 403 });
+  }
+
+  let crate = db.crate.findFirst({ where: { name: { equals: params.name } } });
+  if (!crate) {
+    return notFound();
+  }
+
+  db.user.update({
+    where: { id: { equals: user.id } },
+    data: {
+      followedCrates: user.followedCrates.filter(c => c.id !== crate.id),
+    },
+  });
+
+  return HttpResponse.json({ ok: true });
+});
