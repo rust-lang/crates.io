4ff2177939c0b6e463dd56b2f99fa85256f3b02c
Tobias Bieniek <tobias@bieniek.cloud>
2025-01-23 18:46:12 +0100

    msw: Implement `GET /api/v1/crates/:name/:version/readme` request handler

--- packages/crates-io-msw/handlers/versions.js
+++ packages/crates-io-msw/handlers/versions.js
@@ -2,7 +2,8 @@ import dependencies from './versions/dependencies.js';
 import downloads from './versions/downloads.js';
 import getVersion from './versions/get.js';
 import listVersions from './versions/list.js';
+import readme from './versions/readme.js';
 import unyankVersion from './versions/unyank.js';
 import yankVersion from './versions/yank.js';
 
-export default [listVersions, getVersion, yankVersion, unyankVersion, dependencies, downloads];
+export default [listVersions, getVersion, yankVersion, unyankVersion, dependencies, downloads, readme];
