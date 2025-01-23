b15d71c85fb305af8b31ba028f06261fd627a30c
Tobias Bieniek <tobias@bieniek.cloud>
2025-01-23 18:42:25 +0100

    msw: Implement `PUT /api/v1/crates/:name/:version/unyank` request handler

--- packages/crates-io-msw/handlers/versions.js
+++ packages/crates-io-msw/handlers/versions.js
@@ -2,6 +2,7 @@ import dependencies from './versions/dependencies.js';
 import downloads from './versions/downloads.js';
 import getVersion from './versions/get.js';
 import listVersions from './versions/list.js';
+import unyankVersion from './versions/unyank.js';
 import yankVersion from './versions/yank.js';
 
-export default [listVersions, getVersion, yankVersion, dependencies, downloads];
+export default [listVersions, getVersion, yankVersion, unyankVersion, dependencies, downloads];
