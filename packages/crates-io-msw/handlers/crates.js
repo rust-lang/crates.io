9bbc9b7fbc7cc5d879069af0172342b5c4a36ae7
Tobias Bieniek <tobias@bieniek.cloud>
2025-01-23 17:27:41 +0100

    msw: Implement `GET /api/v1/crates/:name/downloads` request handler

--- packages/crates-io-msw/handlers/crates.js
+++ packages/crates-io-msw/handlers/crates.js
@@ -1,4 +1,5 @@
 import deleteCrate from './crates/delete.js';
+import downloads from './crates/downloads.js';
 import followCrate from './crates/follow.js';
 import following from './crates/following.js';
 import getCrate from './crates/get.js';
@@ -7,4 +8,14 @@ import teamOwners from './crates/team-owners.js';
 import unfollowCrate from './crates/unfollow.js';
 import userOwners from './crates/user-owners.js';
 
-export default [listCrates, getCrate, deleteCrate, following, followCrate, unfollowCrate, userOwners, teamOwners];
+export default [
+  listCrates,
+  getCrate,
+  deleteCrate,
+  following,
+  followCrate,
+  unfollowCrate,
+  userOwners,
+  teamOwners,
+  downloads,
+];
