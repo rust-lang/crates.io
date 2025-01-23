4206f5a59eb5b11b1851cdaf21328756739f9c66
Tobias Bieniek <tobias@bieniek.cloud>
2025-01-23 16:13:43 +0100

    msw: Implement `PUT /api/v1/crates/:name/follow` request handler

--- packages/crates-io-msw/handlers/crates.js
+++ packages/crates-io-msw/handlers/crates.js
@@ -1,6 +1,7 @@
 import deleteCrate from './crates/delete.js';
+import followCrate from './crates/follow.js';
 import following from './crates/following.js';
 import getCrate from './crates/get.js';
 import listCrates from './crates/list.js';
 
-export default [listCrates, getCrate, deleteCrate, following];
+export default [listCrates, getCrate, deleteCrate, following, followCrate];
