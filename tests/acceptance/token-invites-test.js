36422786d09033c458db8608001328e5292079e8
Tobias Bieniek <tobias@bieniek.cloud>
2025-01-24 15:48:30 +0100

    tests/acceptance/token-invites: Migrate from `mirage` to `@crates-io/msw`

--- tests/acceptance/token-invites-test.js
+++ tests/acceptance/token-invites-test.js
@@ -2,13 +2,14 @@ import { currentURL } from '@ember/test-helpers';
 import { module, test } from 'qunit';
 
 import percySnapshot from '@percy/ember';
+import { http, HttpResponse } from 'msw';
 
 import { setupApplicationTest } from 'crates-io/tests/helpers';
 
 import { visit } from '../helpers/visit-ignoring-abort';
 
 module('Acceptance | /accept-invite/:token', function (hooks) {
-  setupApplicationTest(hooks);
+  setupApplicationTest(hooks, { msw: true });
 
   test('visiting to /accept-invite shows 404 page', async function (assert) {
     await visit('/accept-invite');
@@ -25,6 +26,9 @@ module('Acceptance | /accept-invite/:token', function (hooks) {
   });
 
   test('shows error for unknown token', async function (assert) {
+    let error = HttpResponse.json({}, { status: 404 });
+    this.worker.use(http.put('/api/v1/me/crate_owner_invitations/accept/:token', () => error));
+
     await visit('/accept-invite/unknown');
     assert.strictEqual(currentURL(), '/accept-invite/unknown');
     assert.dom('[data-test-error-message]').hasText('You may want to visit crates.io/me/pending-invites to try again.');
@@ -34,7 +38,8 @@ module('Acceptance | /accept-invite/:token', function (hooks) {
     let errorMessage =
       'The invitation to become an owner of the demo_crate crate expired. Please reach out to an owner of the crate to request a new invitation.';
     let payload = { errors: [{ detail: errorMessage }] };
-    this.server.put('/api/v1/me/crate_owner_invitations/accept/:token', payload, 410);
+    let error = HttpResponse.json(payload, { status: 410 });
+    this.worker.use(http.put('/api/v1/me/crate_owner_invitations/accept/:token', () => error));
 
     await visit('/accept-invite/secret123');
     assert.strictEqual(currentURL(), '/accept-invite/secret123');
@@ -42,12 +47,12 @@ module('Acceptance | /accept-invite/:token', function (hooks) {
   });
 
   test('shows success for known token', async function (assert) {
-    let inviter = this.server.create('user');
-    let invitee = this.server.create('user');
+    let inviter = this.db.user.create();
+    let invitee = this.db.user.create();
 
-    let crate = this.server.create('crate', { name: 'nanomsg' });
-    this.server.create('version', { crate });
-    let invite = this.server.create('crate-owner-invitation', { crate, invitee, inviter });
+    let crate = this.db.crate.create({ name: 'nanomsg' });
+    this.db.version.create({ crate });
+    let invite = this.db.crateOwnerInvitation.create({ crate, invitee, inviter });
 
     await visit(`/accept-invite/${invite.token}`);
     assert.strictEqual(currentURL(), `/accept-invite/${invite.token}`);
