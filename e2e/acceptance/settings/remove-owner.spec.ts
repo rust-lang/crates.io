3748f6fd848c9855b946fe928d32060ce50849ea
Tobias Bieniek <tobias@bieniek.cloud>
2025-01-27 12:45:51 +0100

    e2e/acceptance/settings/remove-owner: Migrate from `mirage` to `@crates-io/msw`

--- e2e/acceptance/settings/remove-owner.spec.ts
+++ e2e/acceptance/settings/remove-owner.spec.ts
@@ -1,29 +1,23 @@
-import { test, expect } from '@/e2e/helper';
+import { expect, test } from '@/e2e/helper';
+import { http, HttpResponse } from 'msw';
 
 test.describe('Acceptance | Settings | Remove Owner', { tag: '@acceptance' }, () => {
-  test.beforeEach(async ({ page, mirage }) => {
-    await page.addInitScript(() => {
-      globalThis.crate = { name: 'nanomsg' };
-    });
-    await mirage.addHook(server => {
-      let user1 = server.create('user', { name: 'blabaere' });
-      let user2 = server.create('user', { name: 'thehydroimpulse' });
-      let team1 = server.create('team', { org: 'org', name: 'blabaere' });
-      let team2 = server.create('team', { org: 'org', name: 'thehydroimpulse' });
+  let user1, user2, team1, team2, crate;
 
-      let crate = server.create('crate', { name: 'nanomsg' });
-      server.create('version', { crate, num: '1.0.0' });
-      server.create('crate-ownership', { crate, user: user1 });
-      server.create('crate-ownership', { crate, user: user2 });
-      server.create('crate-ownership', { crate, team: team1 });
-      server.create('crate-ownership', { crate, team: team2 });
+  test.beforeEach(async ({ msw }) => {
+    user1 = msw.db.user.create({ name: 'blabaere' });
+    user2 = msw.db.user.create({ name: 'thehydroimpulse' });
+    team1 = msw.db.team.create({ org: 'org', name: 'blabaere' });
+    team2 = msw.db.team.create({ org: 'org', name: 'thehydroimpulse' });
 
-      authenticateAs(user1);
+    crate = msw.db.crate.create({ name: 'nanomsg' });
+    msw.db.version.create({ crate, num: '1.0.0' });
+    msw.db.crateOwnership.create({ crate, user: user1 });
+    msw.db.crateOwnership.create({ crate, user: user2 });
+    msw.db.crateOwnership.create({ crate, team: team1 });
+    msw.db.crateOwnership.create({ crate, team: team2 });
 
-      globalThis.crate = crate;
-      globalThis.user2 = user2;
-      globalThis.team1 = team1;
-    });
+    await msw.authenticateAs(user1);
   });
 
   test('remove a crate owner when owner is a user', async ({ page }) => {
@@ -36,19 +30,13 @@ test.describe('Acceptance | Settings | Remove Owner', { tag: '@acceptance' }, ()
     await expect(page.locator('[data-test-owner-user]')).toHaveCount(1);
   });
 
-  test('remove a user crate owner (error behavior)', async ({ page, mirage }) => {
-    await mirage.addHook(server => {
-      // we are intentionally returning a 200 response here, because is what
-      // the real backend also returns due to legacy reasons
-      server.delete('/api/v1/crates/nanomsg/owners', { errors: [{ detail: 'nope' }] });
-    });
-
-    await page.goto('about:blank');
-    let crate = await page.evaluate<{ name: string }>('crate');
+  test('remove a user crate owner (error behavior)', async ({ page, msw }) => {
+    // we are intentionally returning a 200 response here, because is what
+    // the real backend also returns due to legacy reasons
+    let error = HttpResponse.json({ errors: [{ detail: 'nope' }] });
+    await msw.worker.use(http.delete('/api/v1/crates/nanomsg/owners', () => error));
 
     await page.goto(`/crates/${crate.name}/settings`);
-
-    const user2 = await page.evaluate(() => JSON.parse(JSON.stringify(user2)));
     await page.click(`[data-test-owner-user="${user2.login}"] [data-test-remove-owner-button]`);
 
     await expect(page.locator('[data-test-notification-message="error"]')).toHaveText(
@@ -67,19 +55,13 @@ test.describe('Acceptance | Settings | Remove Owner', { tag: '@acceptance' }, ()
     await expect(page.locator('[data-test-owner-team]')).toHaveCount(1);
   });
 
-  test('remove a team crate owner (error behavior)', async ({ page, mirage }) => {
-    await mirage.addHook(server => {
-      // we are intentionally returning a 200 response here, because is what
-      // the real backend also returns due to legacy reasons
-      server.delete('/api/v1/crates/nanomsg/owners', { errors: [{ detail: 'nope' }] });
-    });
-
-    await page.goto('about:blank');
-    let crate = await page.evaluate<{ name: string }>('crate');
+  test('remove a team crate owner (error behavior)', async ({ page, msw }) => {
+    // we are intentionally returning a 200 response here, because is what
+    // the real backend also returns due to legacy reasons
+    let error = HttpResponse.json({ errors: [{ detail: 'nope' }] });
+    await msw.worker.use(http.delete('/api/v1/crates/nanomsg/owners', () => error));
 
     await page.goto(`/crates/${crate.name}/settings`);
-
-    let team1 = await page.evaluate(() => JSON.parse(JSON.stringify(team1)));
     await page.click(`[data-test-owner-team="${team1.login}"] [data-test-remove-owner-button]`);
 
     await expect(page.locator('[data-test-notification-message="error"]')).toHaveText(
