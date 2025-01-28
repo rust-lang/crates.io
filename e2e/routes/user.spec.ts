c34c9df6717ed5e6b84b200723781695645d3156
Tobias Bieniek <tobias@bieniek.cloud>
2025-01-28 13:37:23 +0100

    e2e/routes/user: Migrate from `mirage` to `@crates-io/msw`

--- e2e/routes/user.spec.ts
+++ e2e/routes/user.spec.ts
@@ -1,4 +1,5 @@
 import { expect, test } from '@/e2e/helper';
+import { http, HttpResponse } from 'msw';
 
 test.describe('Route | user', { tag: '@routes' }, () => {
   test("shows an error message if the category can't be found", async ({ page }) => {
@@ -10,10 +11,8 @@ test.describe('Route | user', { tag: '@routes' }, () => {
     await expect(page.locator('[data-test-try-again]')).toHaveCount(0);
   });
 
-  test('server error causes the error page to be shown', async ({ page, mirage }) => {
-    await mirage.addHook(server => {
-      server.get('/api/v1/users/:id', {}, 500);
-    });
+  test('server error causes the error page to be shown', async ({ page, msw }) => {
+    msw.worker.use(http.get('/api/v1/users/:id', () => HttpResponse.json({}, { status: 500 })));
 
     await page.goto('/users/foo');
     await expect(page).toHaveURL('/users/foo');
