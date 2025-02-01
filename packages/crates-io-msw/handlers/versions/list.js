46011f0c2bf43b0b6f7dfd2121381dd4463d22da
eth3lbert <eth3lbert+dev@gmail.com>
2025-02-01 14:08:05 +0800

    msw: Add seek pagination support for `GET /api/v1/crates/:name/versions`

--- packages/crates-io-msw/handlers/versions/list.js
+++ packages/crates-io-msw/handlers/versions/list.js
@@ -27,13 +27,35 @@ export default http.get('/api/v1/crates/:name/versions', async ({ request, param
 
   let include = url.searchParams.get('include') ?? '';
   let includes = include ? include.split(',') : [];
-
-  let serializedVersions = versions.map(v => serializeVersion(v, { includePublishedBy: true }));
   let meta = { total, next_page: null };
 
   if (includes.includes('release_tracks')) {
     meta.release_tracks = calculateReleaseTracks(versions);
   }
 
+  // seek pagination
+  // A simplified seek encoding is applied here for testing purposes only. It should be opaque in
+  // real-world scenarios.
+  let next_seek = null;
+  let per_page = url.searchParams.get('per_page');
+  if (per_page != null) {
+    let seek = url.searchParams.get('seek');
+    if (seek != null) {
+      let start = versions.findIndex(it => it.num === seek);
+      versions = versions.slice(start + 1);
+    }
+    versions = versions.slice(0, parseInt(per_page));
+
+    if (versions.length == per_page) {
+      next_seek = versions.at(-1).num;
+    }
+  }
+  if (next_seek) {
+    let next_params = new URLSearchParams(url.searchParams);
+    next_params.set('seek', next_seek);
+    meta.next_page = `?${next_params}`;
+  }
+
+  let serializedVersions = versions.map(v => serializeVersion(v, { includePublishedBy: true }));
   return HttpResponse.json({ versions: serializedVersions, meta });
 });
