387e58c5d173395c650e2c038b573f62d2271fc5
Tobias Bieniek <tobias@bieniek.cloud>
2025-02-27 21:28:36 +0100

    Add OpenAPI documentation for `GET /api/v1/crates/{name}/{version}` response payload (#10709)

--- src/views.rs
+++ src/views.rs
@@ -632,43 +632,129 @@ impl From<User> for EncodablePublicUser {
     }
 }
 
-#[derive(Deserialize, Serialize, Debug)]
+#[derive(Deserialize, Serialize, Debug, utoipa::ToSchema)]
 pub struct EncodableAuditAction {
+    /// The action that was performed.
+    #[schema(example = "publish")]
     pub action: String,
+
+    /// The user who performed the action.
     pub user: EncodablePublicUser,
+
+    /// The date and time the action was performed.
+    #[schema(example = "2019-12-13T13:46:41Z")]
     pub time: DateTime<Utc>,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, utoipa::ToSchema)]
+#[schema(as = Version)]
 pub struct EncodableVersion {
+    /// An opaque identifier for the version.
+    #[schema(example = 42)]
     pub id: i32,
+
+    /// The name of the crate.
     #[serde(rename = "crate")]
+    #[schema(example = "serde")]
     pub krate: String,
+
+    /// The version number.
+    #[schema(example = "1.0.0")]
     pub num: String,
+
+    /// The API path to download the crate.
+    #[schema(example = "/api/v1/crates/serde/1.0.0/download")]
     pub dl_path: String,
+
+    /// The API path to download the crate's README file as HTML code.
+    #[schema(example = "/api/v1/crates/serde/1.0.0/readme")]
     pub readme_path: String,
+
+    /// The date and time this version was last updated (i.e. yanked or unyanked).
+    #[schema(example = "2019-12-13T13:46:41Z")]
     pub updated_at: DateTime<Utc>,
+
+    /// The date and time this version was created.
+    #[schema(example = "2019-12-13T13:46:41Z")]
     pub created_at: DateTime<Utc>,
-    // NOTE: Used by shields.io, altering `downloads` requires a PR with shields.io
+
+    /// The total number of downloads for this version.
+    #[schema(example = 123_456)]
     pub downloads: i32,
+
+    /// The features defined by this version.
+    #[schema(value_type = Object)]
     pub features: serde_json::Value,
+
+    /// Whether this version has been yanked.
+    #[schema(example = false)]
     pub yanked: bool,
+
+    /// The message given when this version was yanked, if any.
+    #[schema(example = "Security vulnerability")]
     pub yank_message: Option<String>,
+
+    /// The name of the native library this version links with, if any.
+    #[schema(example = "git2")]
     pub lib_links: Option<String>,
-    // NOTE: Used by shields.io, altering `license` requires a PR with shields.io
+
+    /// The license of this version of the crate.
+    #[schema(example = "MIT")]
     pub license: Option<String>,
+
+    /// Links to other API endpoints related to this version.
     pub links: EncodableVersionLinks,
+
+    /// The size of the compressed crate file in bytes.
+    #[schema(example = 1_234)]
     pub crate_size: i32,
+
+    /// The user who published this version.
+    ///
+    /// This field may be `null` if the version was published before crates.io
+    /// started recording this information.
     pub published_by: Option<EncodablePublicUser>,
+
+    /// A list of actions performed on this version.
+    #[schema(inline)]
     pub audit_actions: Vec<EncodableAuditAction>,
+
+    /// The SHA256 checksum of the compressed crate file encoded as a
+    /// hexadecimal string.
+    #[schema(example = "e8dfc9d19bdbf6d17e22319da49161d5d0108e4188e8b680aef6299eed22df60")]
     pub checksum: String,
+
+    /// The minimum version of the Rust compiler required to compile
+    /// this version, if set.
+    #[schema(example = "1.31")]
     pub rust_version: Option<String>,
+
+    /// Whether this version can be used as a library.
+    #[schema(example = true)]
     pub has_lib: Option<bool>,
+
+    /// The names of the binaries provided by this version, if any.
+    #[schema(example = json!([]))]
     pub bin_names: Option<Vec<Option<String>>>,
+
+    /// The Rust Edition used to compile this version, if set.
+    #[schema(example = "2021")]
     pub edition: Option<String>,
+
+    /// The description of this version of the crate.
+    #[schema(example = "A generic serialization/deserialization framework")]
     pub description: Option<String>,
+
+    /// The URL to the crate's homepage, if set.
+    #[schema(example = "https://serde.rs")]
     pub homepage: Option<String>,
+
+    /// The URL to the crate's documentation, if set.
+    #[schema(example = "https://docs.rs/serde")]
     pub documentation: Option<String>,
+
+    /// The URL to the crate's repository, if set.
+    #[schema(example = "https://github.com/serde-rs/serde")]
     pub repository: Option<String>,
 }
 
@@ -747,10 +833,19 @@ impl EncodableVersion {
     }
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, utoipa::ToSchema)]
+#[schema(as = VersionLinks)]
 pub struct EncodableVersionLinks {
+    /// The API path to download this version's dependencies.
+    #[schema(example = "/api/v1/crates/serde/1.0.0/dependencies")]
     pub dependencies: String,
+
+    /// The API path to download this version's download numbers.
+    #[schema(example = "/api/v1/crates/serde/1.0.0/downloads")]
     pub version_downloads: String,
+
+    /// The API path to download this version's authors.
+    #[schema(deprecated, example = "/api/v1/crates/serde/1.0.0/authors")]
     pub authors: String,
 }
 
