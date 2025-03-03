5f7a4494225ce0e00856546ca08ada7752d5d328
Tobias Bieniek <tobias@bieniek.cloud>
2025-03-03 15:28:29 +0100

    Improve OpenAPI documentation for `GET /api/v1/teams/{team}` endpoint

--- src/controllers/team.rs
+++ src/controllers/team.rs
@@ -2,11 +2,15 @@ use crate::app::AppState;
 use crate::models::Team;
 use crate::util::errors::AppResult;
 use crate::views::EncodableTeam;
+use axum::Json;
 use axum::extract::Path;
-use axum_extra::json;
-use axum_extra::response::ErasedJson;
 use diesel::prelude::*;
 use diesel_async::RunQueryDsl;
+
+#[derive(Debug, Serialize, utoipa::ToSchema)]
+pub struct GetResponse {
+    team: EncodableTeam,
+}
 
 /// Find team by login.
 #[utoipa::path(
@@ -16,12 +20,13 @@ use diesel_async::RunQueryDsl;
         ("team" = String, Path, description = "Name of the team", example = "github:rust-lang:crates-io"),
     ),
     tag = "teams",
-    responses((status = 200, description = "Successful Response")),
+    responses((status = 200, description = "Successful Response", body = inline(GetResponse))),
 )]
-pub async fn find_team(state: AppState, Path(name): Path<String>) -> AppResult<ErasedJson> {
+pub async fn find_team(state: AppState, Path(name): Path<String>) -> AppResult<Json<GetResponse>> {
     use crate::schema::teams::dsl::{login, teams};
 
     let mut conn = state.db_read().await?;
     let team: Team = teams.filter(login.eq(&name)).first(&mut conn).await?;
-    Ok(json!({ "team": EncodableTeam::from(team) }))
+    let team = EncodableTeam::from(team);
+    Ok(Json(GetResponse { team }))
 }
