2c4fc61cff45eff1bda2c082daecd7da06add91a
eth3lbert <eth3lbert+dev@gmail.com>
2025-03-19 16:26:42 +0800

    controllers/category: Pipeline SQL queries (#10849)

--- src/controllers/category.rs
+++ src/controllers/category.rs
@@ -8,6 +8,7 @@ use axum::Json;
 use axum::extract::{FromRequestParts, Path, Query};
 use diesel::QueryDsl;
 use diesel_async::RunQueryDsl;
+use futures_util::FutureExt;
 use http::request::Parts;
 
 #[derive(Debug, Deserialize, FromRequestParts, utoipa::IntoParams)]
@@ -62,11 +63,13 @@ pub async fn list_categories(
 
     let offset = options.offset().unwrap_or_default();
 
-    let categories = Category::toplevel(&mut conn, sort, options.per_page, offset).await?;
-    let categories = categories.into_iter().map(Category::into).collect();
+    let (categories, total) = tokio::try_join!(
+        Category::toplevel(&mut conn, sort, options.per_page, offset).boxed(),
+        // Query for the total count of categories
+        Category::count_toplevel(&mut conn).boxed(),
+    )?;
 
-    // Query for the total count of categories
-    let total = Category::count_toplevel(&mut conn).await?;
+    let categories = categories.into_iter().map(Category::into).collect();
 
     let meta = ListMeta { total };
     Ok(Json(ListResponse { categories, meta }))
@@ -94,18 +97,13 @@ pub async fn find_category(
     let mut conn = state.db_read().await?;
 
     let cat: Category = Category::by_slug(&slug).first(&mut conn).await?;
-    let subcats = cat
-        .subcategories(&mut conn)
-        .await?
-        .into_iter()
-        .map(Category::into)
-        .collect();
-    let parents = cat
-        .parent_categories(&mut conn)
-        .await?
-        .into_iter()
-        .map(Category::into)
-        .collect();
+    let (subcats, parents) = tokio::try_join!(
+        cat.subcategories(&mut conn),
+        cat.parent_categories(&mut conn).boxed(),
+    )?;
+
+    let subcats = subcats.into_iter().map(Category::into).collect();
+    let parents = parents.into_iter().map(Category::into).collect();
 
     let mut category = EncodableCategory::from(cat);
     category.subcategories = Some(subcats);
