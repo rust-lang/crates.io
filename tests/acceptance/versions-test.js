123d6bb5e03eb83fe6eb87326446a7e7a60433a6
eth3lbert <eth3lbert+dev@gmail.com>
2025-04-11 22:57:28 +0800

    modesl/version: Refresh `releaseTracks` after yanking/unyanking

--- tests/acceptance/versions-test.js
+++ tests/acceptance/versions-test.js
@@ -29,4 +29,54 @@ module('Acceptance | crate versions page', function (hooks) {
     versions = findAll('[data-test-version]').map(it => it.dataset.testVersion);
     assert.deepEqual(versions, ['0.3.0', '0.2.1', '0.2.0', '0.1.0']);
   });
+
+  test('shows correct release tracks label after yanking/unyanking', async function (assert) {
+    let user = this.db.user.create();
+    this.authenticateAs(user);
+
+    let crate = this.db.crate.create({ name: 'nanomsg' });
+    this.db.crateOwnership.create({ crate, user });
+
+    this.db.version.create({ crate, num: '0.1.0', created_at: '2017-01-01' });
+    this.db.version.create({ crate, num: '0.2.0', created_at: '2018-01-01' });
+    this.db.version.create({ crate, num: '0.3.0', created_at: '2019-01-01', rust_version: '1.69' });
+    this.db.version.create({ crate, num: '0.2.1', created_at: '2020-01-01' });
+
+    await visit('/crates/nanomsg/versions');
+    assert.strictEqual(currentURL(), '/crates/nanomsg/versions');
+
+    let versions = findAll('[data-test-version]').map(it => it.dataset.testVersion);
+    assert.deepEqual(versions, ['0.2.1', '0.3.0', '0.2.0', '0.1.0']);
+
+    assert
+      .dom('[data-test-version="0.2.1"]')
+      .hasClass(/.*latest/)
+      .hasNoClass(/.yanked/);
+    assert
+      .dom('[data-test-version="0.2.0"]')
+      .hasNoClass(/.*latest/)
+      .hasNoClass(/.yanked/);
+
+    // yanking
+    await click('[data-test-version-yank-button="0.2.1"]');
+    assert
+      .dom('[data-test-version="0.2.1"]')
+      .hasNoClass(/.*latest/)
+      .hasClass(/.*yanked/);
+    assert
+      .dom('[data-test-version="0.2.0"]')
+      .hasClass(/.*latest/)
+      .hasNoClass(/.*yanked/);
+
+    // unyanking
+    await click('[data-test-version-unyank-button="0.2.1"]');
+    assert
+      .dom('[data-test-version="0.2.1"]')
+      .hasClass(/.*latest/)
+      .hasNoClass(/.yanked/);
+    assert
+      .dom('[data-test-version="0.2.0"]')
+      .hasNoClass(/.*latest/)
+      .hasNoClass(/.yanked/);
+  });
 });
