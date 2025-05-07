1af9085cfa2be65e6bde1327f31f2ad8be07dce0
Tobias Bieniek <tobias@bieniek.cloud>
2025-05-07 15:30:55 +0200

    trustpub: Add `OidcKeyStore` trait

--- crates/crates_io_trustpub/src/keystore/mod.rs
+++ crates/crates_io_trustpub/src/keystore/mod.rs
@@ -1,1 +1,14 @@
 mod load_jwks;
+
+use async_trait::async_trait;
+use jsonwebtoken::DecodingKey;
+
+/// A trait for fetching OIDC keys from a key store.
+#[async_trait]
+pub trait OidcKeyStore: Send + Sync {
+    /// Fetches a [`DecodingKey`] from the key store using the provided `key_id`.
+    ///
+    /// If the key is not found on the server, it will return `None`. If there
+    /// is an error while fetching the key, it will return an error.
+    async fn get_oidc_key(&self, key_id: &str) -> anyhow::Result<Option<DecodingKey>>;
+}
