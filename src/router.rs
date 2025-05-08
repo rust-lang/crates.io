8f25d27e76f5947520abb60c4be995ea5091c545
Tobias Bieniek <tobias@bieniek.cloud>
2025-05-08 07:07:26 +0200

    Implement `PUT /api/v1/trusted_publishing/tokens` API endpoint

--- src/router.rs
+++ src/router.rs
@@ -89,6 +89,7 @@ pub fn build_axum_router(state: AppState) -> Router<()> {
         .routes(routes!(session::authorize_session))
         .routes(routes!(session::end_session))
         // OIDC / Trusted Publishing
+        .routes(routes!(trustpub::tokens::exchange::exchange_trustpub_token))
         .routes(routes!(
             trustpub::github_configs::create::create_trustpub_github_config,
             trustpub::github_configs::delete::delete_trustpub_github_config,
