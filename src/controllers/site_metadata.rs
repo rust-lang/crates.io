e63643773c1cbb6db85288a0ab026b11be24d652
Tobias Bieniek <tobias@bieniek.cloud>
2025-05-21 11:40:34 +0200

    controllers/site_metadata: Avoid unnecessary allocation (#11221)

--- src/controllers/site_metadata.rs
+++ src/controllers/site_metadata.rs
@@ -29,12 +29,12 @@ pub struct MetadataResponse<'a> {
 pub async fn get_site_metadata(state: AppState) -> impl IntoResponse {
     let read_only = state.config.db.are_all_read_only();
 
-    let deployed_sha =
-        dotenvy::var("HEROKU_SLUG_COMMIT").unwrap_or_else(|_| String::from("unknown"));
+    let deployed_sha = dotenvy::var("HEROKU_SLUG_COMMIT");
+    let deployed_sha = deployed_sha.as_deref().unwrap_or("unknown");
 
     Json(MetadataResponse {
-        deployed_sha: &deployed_sha,
-        commit: &deployed_sha,
+        deployed_sha,
+        commit: deployed_sha,
         read_only,
     })
     .into_response()
