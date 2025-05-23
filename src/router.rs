5699949b1853f931b5c0cc7c1cf9aab23fae2e35
Tobias Bieniek <tobias@bieniek.cloud>
2025-05-23 18:08:54 +0200

    Add `DELETE /api/v1/trusted_publishing/tokens` API endpoint

--- src/router.rs
+++ src/router.rs
@@ -89,7 +89,10 @@ pub fn build_axum_router(state: AppState) -> Router<()> {
         .routes(routes!(session::authorize_session))
         .routes(routes!(session::end_session))
         // OIDC / Trusted Publishing
-        .routes(routes!(trustpub::tokens::exchange::exchange_trustpub_token))
+        .routes(routes!(
+            trustpub::tokens::exchange::exchange_trustpub_token,
+            trustpub::tokens::revoke::revoke_trustpub_token
+        ))
         .routes(routes!(
             trustpub::github_configs::create::create_trustpub_github_config,
             trustpub::github_configs::delete::delete_trustpub_github_config,
