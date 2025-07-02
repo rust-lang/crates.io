473039acd77aa0301ff8a161f58d6973cfdd5e2a
Yoh Deadfall <yoh.deadfall@hotmail.com>
2025-07-03 00:37:47 +0300

    Skip running a job if the crate/version deleted

--- src/worker/jobs/update_default_version.rs
+++ src/worker/jobs/update_default_version.rs
@@ -7,7 +7,7 @@ use diesel::prelude::*;
 use diesel_async::RunQueryDsl;
 use serde::{Deserialize, Serialize};
 use std::sync::Arc;
-use tracing::info;
+use tracing::{info, warn};
 
 #[derive(Serialize, Deserialize)]
 pub struct UpdateDefaultVersion {
@@ -32,16 +32,25 @@ impl BackgroundJob for UpdateDefaultVersion {
 
         info!("Updating default version for crate {crate_id}");
         let mut conn = ctx.deadpool.get().await?;
-        update_default_version(crate_id, &mut conn).await?;
-
-        // Get the crate name for OG image generation
-        let crate_name: String = crates::table
-            .filter(crates::id.eq(crate_id))
-            .select(crates::name)
-            .first(&mut conn)
-            .await?;
+        let crate_name = update_default_version(crate_id, &mut conn).await.and(
+            // Get the crate name for OG image generation
+            crates::table
+                .filter(crates::id.eq(crate_id))
+                .select(crates::name)
+                .first::<String>(&mut conn)
+                .await,
+        );
+        if let Err(diesel::result::Error::DatabaseError(
+            diesel::result::DatabaseErrorKind::ForeignKeyViolation,
+            ..,
+        )) = crate_name
+        {
+            warn!("Skipping update default version for crate for {crate_id}: no crate found");
+            return Ok(());
+        }
 
         // Generate OG image after updating default version
+        let crate_name = crate_name?;
         info!("Enqueueing OG image generation for crate {crate_name}");
         GenerateOgImage::new(crate_name).enqueue(&mut conn).await?;
 
