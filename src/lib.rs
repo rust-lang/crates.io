eaba46994a54005951a7a09c3ec0f5524cae4ba8
2ndDerivative <niclas@klugmann.de>
2025-08-12 17:34:26 +0200

    only use jemalloc with MSVC

--- src/lib.rs
+++ src/lib.rs
@@ -10,8 +10,10 @@ use std::sync::Arc;
 
 use crate::app::AppState;
 use crate::router::build_axum_router;
+#[cfg(not(target_env = "msvc"))]
 use tikv_jemallocator::Jemalloc;
 
+#[cfg(not(target_env = "msvc"))]
 #[global_allocator]
 static ALLOC: Jemalloc = Jemalloc;
 
