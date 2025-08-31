d0a9140cb88f5d211fb77e21a21d581a340e1cdb
Chris Manson <chris@manson.ie>
2025-08-31 11:42:36 +0100

    re-enable ember-exam

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,17 +1,17 @@
-// TODO re-enable ember exam
-import emberExamStart from 'ember-exam/test-support/start';
-import { forceModulesToBeLoaded, sendCoverage } from 'ember-cli-code-coverage/test-support';
-import registerMatchJsonAssertion from './helpers/match-json';
+import { setApplication } from '@ember/test-helpers';
+import { start as startEmberExam } from 'ember-exam/test-support';
+import { setupEmberOnerrorValidation } from 'ember-qunit';
+import * as QUnit from 'qunit';
 
+import { forceModulesToBeLoaded, sendCoverage } from 'ember-cli-code-coverage/test-support';
+import { setup } from 'qunit-dom';
 
 import Application from 'crates-io/app';
 import config from 'crates-io/config/environment';
-import * as QUnit from 'qunit';
-import { setApplication } from '@ember/test-helpers';
-import { setup } from 'qunit-dom';
-import { start as qunitStart, setupEmberOnerrorValidation } from 'ember-qunit';
 
-export function start() {
+import registerMatchJsonAssertion from './helpers/match-json';
+
+export async function start({ availableModules }) {
   setApplication(Application.create(config.APP));
 
   setup(QUnit.assert);
@@ -23,5 +23,5 @@ export function start() {
     await sendCoverage();
   });
 
-  qunitStart();
+  await startEmberExam({ availableModules });
 }
