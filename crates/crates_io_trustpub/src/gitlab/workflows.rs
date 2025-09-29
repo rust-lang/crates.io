286956c3ef3e07bb7d464c760022fcd5f33efa96
Tobias Bieniek <tobias@bieniek.cloud>
2025-09-29 17:03:45 +0200

    trustpub/gitlab: Implement `extract_workflow_filepath` fn

---
+++ crates/crates_io_trustpub/src/gitlab/workflows.rs
@@ -0,0 +1,106 @@
+/// Extracts the workflow path from a GitLab `ci_config_ref_uri` claim.
+///
+/// In other words, it turns e.g. `gitlab.com/rust-lang/regex//foo/bar/baz.yml@refs/heads/main`
+/// into `foo/bar/baz.yml`, or `None` if the reference is in an unexpected format.
+///
+/// This was initially using a regular expression (`//(.*[^/]\.(yml|yaml))@.+`),
+/// but was changed to string operations to avoid potential ReDoS attack vectors
+/// (see `test_extract_workflow_filename_redos` test below).
+pub(crate) fn extract_workflow_filepath(workflow_ref: &str) -> Option<&str> {
+    // Find the double slash that separates project path from workflow path
+    let start = workflow_ref.find("//")?;
+    let after_double_slash = &workflow_ref[start + 2..];
+
+    // Find the last @ that separates workflow path from ref
+    let end = after_double_slash.rfind('@')?;
+    let filepath = &after_double_slash[..end];
+
+    // Validate: must end with .yml or .yaml
+    if !filepath.ends_with(".yml") && !filepath.ends_with(".yaml") {
+        return None;
+    }
+
+    // Get the basename (part after last slash, or whole string if no slash)
+    let basename = filepath.rsplit('/').next()?;
+
+    // Basename must not start with a dot (rejects ".yml", ".yaml", "somedir/.yaml")
+    if basename.starts_with('.') {
+        return None;
+    }
+
+    Some(filepath)
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_extract_workflow_filename() {
+        let test_cases = [
+            // Well-formed `ci_config_ref_uri`s, including obnoxious ones.
+            (
+                "gitlab.com/foo/bar//notnested.yml@/some/ref",
+                Some("notnested.yml"),
+            ),
+            (
+                "gitlab.com/foo/bar//notnested.yaml@/some/ref",
+                Some("notnested.yaml"),
+            ),
+            (
+                "gitlab.com/foo/bar//basic/basic.yml@/some/ref",
+                Some("basic/basic.yml"),
+            ),
+            (
+                "gitlab.com/foo/bar//more/nested/example.yml@/some/ref",
+                Some("more/nested/example.yml"),
+            ),
+            (
+                "gitlab.com/foo/bar//too//many//slashes.yml@/some/ref",
+                Some("too//many//slashes.yml"),
+            ),
+            ("gitlab.com/foo/bar//has-@.yml@/some/ref", Some("has-@.yml")),
+            (
+                "gitlab.com/foo/bar//foo.bar.yml@/some/ref",
+                Some("foo.bar.yml"),
+            ),
+            (
+                "gitlab.com/foo/bar//foo.yml.bar.yml@/some/ref",
+                Some("foo.yml.bar.yml"),
+            ),
+            (
+                "gitlab.com/foo/bar//foo.yml@bar.yml@/some/ref",
+                Some("foo.yml@bar.yml"),
+            ),
+            (
+                "gitlab.com/foo/bar//@foo.yml@bar.yml@/some/ref",
+                Some("@foo.yml@bar.yml"),
+            ),
+            (
+                "gitlab.com/foo/bar//@.yml.foo.yml@bar.yml@/some/ref",
+                Some("@.yml.foo.yml@bar.yml"),
+            ),
+            ("gitlab.com/foo/bar//a.yml@refs/heads/main", Some("a.yml")),
+            (
+                "gitlab.com/foo/bar//a/b.yml@refs/heads/main",
+                Some("a/b.yml"),
+            ),
+            // Malformed `ci_config_ref_uri`s.
+            ("gitlab.com/foo/bar//notnested.wrongsuffix@/some/ref", None),
+            ("gitlab.com/foo/bar//@/some/ref", None),
+            ("gitlab.com/foo/bar//.yml@/some/ref", None),
+            ("gitlab.com/foo/bar//.yaml@/some/ref", None),
+            ("gitlab.com/foo/bar//somedir/.yaml@/some/ref", None),
+        ];
+
+        for (input, expected) in test_cases {
+            let result = super::extract_workflow_filepath(input);
+            assert_eq!(result, expected, "Input: {input}");
+        }
+    }
+
+    #[test]
+    fn test_extract_workflow_filename_redos() {
+        let _ = super::extract_workflow_filepath(
+            &(".yml@//".repeat(200_000_000) + ".yml@/\n//\x00.yml@y"),
+        );
+    }
+}
