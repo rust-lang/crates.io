0ba146959fa298e95bf40f07b051c5a037153a84
Tobias Bieniek <tobias@bieniek.cloud>
2025-11-10 13:43:30 +0100

    trustpub: Implement GitLab config creation

--- app/templates/crate/settings/new-trusted-publisher.gjs
+++ app/templates/crate/settings/new-trusted-publisher.gjs
@@ -24,6 +24,7 @@ import LoadingSpinner from 'crates-io/components/loading-spinner';
           disabled={{@controller.saveConfigTask.isRunning}}
           class='publisher-select base-input'
           data-test-publisher
+          {{on 'change' @controller.publisherChanged}}
         >
           {{#each @controller.publishers as |publisher|}}
             <option value={{publisher}} selected={{eq @controller.publisher publisher}}>{{publisher}}</option>
@@ -32,7 +33,7 @@ import LoadingSpinner from 'crates-io/components/loading-spinner';
       {{/let}}
 
       <div class='note'>
-        crates.io currently only supports GitHub, but we are planning to support other platforms in the future.
+        Select the CI/CD platform where your publishing workflow is configured.
       </div>
     </div>
 
@@ -203,6 +204,126 @@ import LoadingSpinner from 'crates-io/components/loading-spinner';
             that the above workflow uses for publishing. This should be configured in the repository settings. A
             dedicated publishing environment is not required, but is
             <strong>strongly recommended</strong>, especially if your repository has maintainers with commit access who
+            should not have crates.io publishing access.
+          </div>
+        {{/let}}
+      </div>
+    {{else if (eq @controller.publisher 'GitLab')}}
+      <div class='form-group' data-test-namespace-group>
+        {{#let (uniqueId) as |id|}}
+          <label for={{id}} class='form-group-name'>Namespace</label>
+
+          <Input
+            id={{id}}
+            @type='text'
+            @value={{@controller.namespace}}
+            disabled={{@controller.saveConfigTask.isRunning}}
+            aria-required='true'
+            aria-invalid={{if @controller.namespaceInvalid 'true' 'false'}}
+            class='input base-input'
+            data-test-namespace
+            {{autoFocus}}
+            {{on 'input' @controller.resetNamespaceValidation}}
+          />
+
+          {{#if @controller.namespaceInvalid}}
+            <div class='form-group-error' data-test-error>
+              Please enter a namespace.
+            </div>
+          {{else}}
+            <div class='note'>
+              The GitLab group name or GitLab username that owns the project.
+            </div>
+          {{/if}}
+        {{/let}}
+      </div>
+
+      <div class='form-group' data-test-project-group>
+        {{#let (uniqueId) as |id|}}
+          <label for={{id}} class='form-group-name'>Project</label>
+
+          <Input
+            id={{id}}
+            @type='text'
+            @value={{@controller.project}}
+            disabled={{@controller.saveConfigTask.isRunning}}
+            aria-required='true'
+            aria-invalid={{if @controller.projectInvalid 'true' 'false'}}
+            class='input base-input'
+            data-test-project
+            {{on 'input' @controller.resetProjectValidation}}
+          />
+
+          {{#if @controller.projectInvalid}}
+            <div class='form-group-error' data-test-error>
+              Please enter a project name.
+            </div>
+          {{else}}
+            <div class='note'>
+              The name of the GitLab project that contains the publishing workflow.
+            </div>
+          {{/if}}
+        {{/let}}
+      </div>
+
+      <div class='form-group' data-test-workflow-group>
+        {{#let (uniqueId) as |id|}}
+          <label for={{id}} class='form-group-name'>Workflow filepath</label>
+
+          <Input
+            id={{id}}
+            @type='text'
+            @value={{@controller.workflow}}
+            disabled={{@controller.saveConfigTask.isRunning}}
+            aria-required='true'
+            aria-invalid={{if @controller.workflowInvalid 'true' 'false'}}
+            class='input base-input'
+            data-test-workflow
+            {{on 'input' @controller.resetWorkflowValidation}}
+          />
+
+          {{#if @controller.workflowInvalid}}
+            <div class='form-group-error' data-test-error>
+              Please enter a workflow filepath.
+            </div>
+          {{else}}
+            <div class='note'>
+              The filepath to the GitLab CI configuration file, relative to the
+              {{#if @controller.repository}}<a
+                  href='https://gitlab.com/{{@controller.repository}}/'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >{{@controller.repository}}</a>
+              {{/if}}
+              repository{{unless @controller.repository ' configured above'}}
+              root. For example:
+              <code>.gitlab-ci.yml</code>
+              or
+              <code>ci/publish.yml</code>.
+            </div>
+          {{/if}}
+        {{/let}}
+      </div>
+
+      <div class='form-group' data-test-environment-group>
+        {{#let (uniqueId) as |id|}}
+          <label for={{id}} class='form-group-name'>Environment name (optional)</label>
+
+          <Input
+            id={{id}}
+            @type='text'
+            @value={{@controller.environment}}
+            disabled={{@controller.saveConfigTask.isRunning}}
+            class='input base-input'
+            data-test-environment
+          />
+
+          <div class='note'>
+            The name of the
+            <a href='https://docs.gitlab.com/ee/ci/environments/'>GitLab environment</a>
+            that the above workflow uses for publishing. This should be configured in the project settings. A dedicated
+            publishing environment is not required, but is
+            <strong>strongly recommended</strong>, especially if your project has maintainers with merge access who
             should not have crates.io publishing access.
           </div>
         {{/let}}
--- e2e/routes/crate/settings/new-trusted-publisher.spec.ts
+++ e2e/routes/crate/settings/new-trusted-publisher.spec.ts
@@ -330,6 +330,149 @@ test.describe('Route | crate.settings.new-trusted-publisher', { tag: '@routes' }
   });
 
   test.describe('GitLab', () => {
-    // Placeholder for GitLab tests when they are implemented
+    test('happy path', async ({ msw, page }) => {
+      let { crate } = await prepare(msw);
+
+      msw.db.trustpubGitlabConfig.create({
+        crate,
+        namespace: 'johndoe',
+        project: 'crates.io',
+        workflow_filepath: '.gitlab-ci.yml',
+      });
+
+      await page.goto(`/crates/${crate.name}/settings`);
+      await page.click('[data-test-add-trusted-publisher-button]');
+      await expect(page).toHaveURL(`/crates/${crate.name}/settings/new-trusted-publisher`);
+
+      // Check that the form is displayed correctly
+      await expect(page.locator('[data-test-publisher]')).toBeVisible();
+      await expect(page.locator('[data-test-namespace]')).toBeVisible();
+      await expect(page.locator('[data-test-project]')).toBeVisible();
+      await expect(page.locator('[data-test-workflow]')).toBeVisible();
+
+      // Select GitLab from the publisher dropdown
+      await page.selectOption('[data-test-publisher]', 'GitLab');
+
+      // Check that GitLab fields are displayed
+      await expect(page.locator('[data-test-namespace]')).toBeVisible();
+      await expect(page.locator('[data-test-project]')).toBeVisible();
+      await expect(page.locator('[data-test-workflow]')).toBeVisible();
+      await expect(page.locator('[data-test-environment]')).toBeVisible();
+      await expect(page.locator('[data-test-add]')).toBeVisible();
+      await expect(page.locator('[data-test-cancel]')).toBeVisible();
+
+      // Fill in the form
+      await page.fill('[data-test-namespace]', 'rust-lang');
+      await page.fill('[data-test-project]', 'crates.io');
+      await page.fill('[data-test-workflow]', '.gitlab-ci.yml');
+      await page.fill('[data-test-environment]', 'production');
+
+      // Submit the form
+      await page.click('[data-test-add]');
+
+      // Check that we're redirected back to the crate settings page
+      await expect(page).toHaveURL(`/crates/${crate.name}/settings`);
+
+      // Check that the config was created
+      let config = msw.db.trustpubGitlabConfig.findFirst({
+        where: {
+          namespace: { equals: 'rust-lang' },
+          project: { equals: 'crates.io' },
+          workflow_filepath: { equals: '.gitlab-ci.yml' },
+          environment: { equals: 'production' },
+        },
+      });
+      expect(config, 'Config was created').toBeDefined();
+
+      // Check that the success notification is displayed
+      await expect(page.locator('[data-test-notification-message]')).toHaveText(
+        'Trusted Publishing configuration added successfully',
+      );
+
+      // Check that the config is displayed on the crate settings page
+      await expect(page.locator('[data-test-gitlab-config]')).toHaveCount(2);
+      await expect(page.locator('[data-test-gitlab-config="2"] td:nth-child(1)')).toHaveText('GitLab');
+      let details = page.locator('[data-test-gitlab-config="2"] td:nth-child(2)');
+      await expect(details).toContainText('Repository: rust-lang/crates.io');
+      await expect(details).toContainText('Workflow: .gitlab-ci.yml');
+      await expect(details).toContainText('Environment: production');
+    });
+
+    test('validation errors', async ({ msw, page }) => {
+      let { crate } = await prepare(msw);
+
+      await page.goto(`/crates/${crate.name}/settings/new-trusted-publisher`);
+      await expect(page).toHaveURL(`/crates/${crate.name}/settings/new-trusted-publisher`);
+
+      // Select GitLab from the publisher dropdown
+      await page.selectOption('[data-test-publisher]', 'GitLab');
+
+      // Submit the form without filling in required fields
+      await page.click('[data-test-add]');
+
+      // Check that validation errors are displayed
+      await expect(page.locator('[data-test-namespace-group] [data-test-error]')).toBeVisible();
+      await expect(page.locator('[data-test-project-group] [data-test-error]')).toBeVisible();
+      await expect(page.locator('[data-test-workflow-group] [data-test-error]')).toBeVisible();
+
+      // Fill in the required fields
+      await page.fill('[data-test-namespace]', 'rust-lang');
+      await page.fill('[data-test-project]', 'crates.io');
+      await page.fill('[data-test-workflow]', '.gitlab-ci.yml');
+
+      // Submit the form
+      await page.click('[data-test-add]');
+
+      // Check that we're redirected back to the crate settings page
+      await expect(page).toHaveURL(`/crates/${crate.name}/settings`);
+    });
+
+    test('loading and error state', async ({ msw, page }) => {
+      let { crate } = await prepare(msw);
+
+      // Mock the server to return an error
+      let deferred = defer();
+      msw.worker.use(http.post('/api/v1/trusted_publishing/gitlab_configs', () => deferred.promise));
+
+      await page.goto(`/crates/${crate.name}/settings/new-trusted-publisher`);
+      await expect(page).toHaveURL(`/crates/${crate.name}/settings/new-trusted-publisher`);
+
+      // Select GitLab from the publisher dropdown
+      await page.selectOption('[data-test-publisher]', 'GitLab');
+
+      // Fill in the form
+      await page.fill('[data-test-namespace]', 'rust-lang');
+      await page.fill('[data-test-project]', 'crates.io');
+      await page.fill('[data-test-workflow]', '.gitlab-ci.yml');
+
+      // Submit the form
+      await page.click('[data-test-add]');
+      await expect(page.locator('[data-test-add] [data-test-spinner]')).toBeVisible();
+      await expect(page.locator('[data-test-publisher]')).toBeDisabled();
+      await expect(page.locator('[data-test-namespace]')).toBeDisabled();
+      await expect(page.locator('[data-test-project]')).toBeDisabled();
+      await expect(page.locator('[data-test-workflow]')).toBeDisabled();
+      await expect(page.locator('[data-test-environment]')).toBeDisabled();
+      await expect(page.locator('[data-test-add]')).toBeDisabled();
+
+      // Resolve the deferred with an error
+      deferred.resolve(HttpResponse.json({ errors: [{ detail: 'Server error' }] }, { status: 500 }));
+
+      // Check that the error notification is displayed
+      await expect(page.locator('[data-test-notification-message]')).toHaveText(
+        'An error has occurred while adding the Trusted Publishing configuration: Server error',
+      );
+
+      await expect(page.locator('[data-test-publisher]')).toBeEnabled();
+      await expect(page.locator('[data-test-namespace]')).toBeEnabled();
+      await expect(page.locator('[data-test-project]')).toBeEnabled();
+      await expect(page.locator('[data-test-workflow]')).toBeEnabled();
+      await expect(page.locator('[data-test-environment]')).toBeEnabled();
+      await expect(page.locator('[data-test-add]')).toBeEnabled();
+
+      await page.click('[data-test-cancel]');
+      await expect(page).toHaveURL(`/crates/${crate.name}/settings`);
+      await expect(page.locator('[data-test-gitlab-config]')).toHaveCount(0);
+    });
   });
 });
