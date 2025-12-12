7bc54ddeb96b6235dd28ec46f6f38bf321fae83a
Tobias Bieniek <tobias@bieniek.cloud>
2025-12-12 19:09:25 +0100

    svelte: Use SPA mode
    This should make the migration a bit easier for now. We can go back to SSR rendering in the future once SPA mode works sufficiently well.

--- svelte/svelte.config.js
+++ svelte/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,10 +8,11 @@ const config = {
   preprocess: vitePreprocess(),
 
   kit: {
-    // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-    // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-    // See https://svelte.dev/docs/kit/adapters for more information about adapters.
-    adapter: adapter(),
+    adapter: adapter({
+      // https://svelte.dev/docs/kit/single-page-apps#Usage recommends to
+      // avoid using `index.html` as a fallback page, so we use `200.html` instead.
+      fallback: '200.html',
+    }),
   },
 };
 
