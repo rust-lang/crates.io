44287df2be5bb68f807b7472369602fec91f3fb0
Tobias Bieniek <tobias@bieniek.cloud>
2025-12-12 18:57:00 +0100

    svelte: Adjust prettier rules to match Ember.js app

--- svelte/src/routes/+layout.svelte
+++ svelte/src/routes/+layout.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
+  import favicon from '$lib/assets/favicon.svg';
 
-	let { children } = $props();
+  let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+  <link rel="icon" href={favicon} />
 </svelte:head>
 
 {@render children()}
