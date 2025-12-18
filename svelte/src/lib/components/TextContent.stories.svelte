ad74adcb230f496411949f922a8d17b066dc1884
Tobias Bieniek <tobias@bieniek.cloud>
2025-12-18 16:17:56 +0100

    svelte: Add Storybook setup

---
+++ svelte/src/lib/components/TextContent.stories.svelte
@@ -0,0 +1,21 @@
+<script module lang="ts">
+  import { defineMeta } from '@storybook/addon-svelte-csf';
+
+  import TextContent from './TextContent.svelte';
+
+  const { Story } = defineMeta({
+    title: 'TextContent',
+    component: TextContent,
+    tags: ['autodocs'],
+  });
+</script>
+
+<Story name="Default">
+  <p>This is some example text content with a <code>code snippet</code> inside.</p>
+  <pre>fn main() {'{'} println!("Hello, world!"); }</pre>
+</Story>
+
+<Story name="Boxed" args={{ boxed: true }}>
+  <h2>Boxed Content</h2>
+  <p>This content appears in a boxed container with padding and background color.</p>
+</Story>
