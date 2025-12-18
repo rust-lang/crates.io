095d71bb0f3fd2881b497dca2e212b1cab7d72bd
Tobias Bieniek <tobias@bieniek.cloud>
2025-12-18 20:04:39 +0100

    svelte: Port `StatsValue` component from Ember.js app

---
+++ svelte/src/lib/components/frontpage/StatsValue.stories.svelte
@@ -0,0 +1,23 @@
+<script module>
+  import { defineMeta } from '@storybook/addon-svelte-csf';
+
+  import SearchIcon from '$lib/assets/search.svg?component';
+  import StatsValue from './StatsValue.svelte';
+
+  const { Story } = defineMeta({
+    title: 'Frontpage/StatsValue',
+    component: StatsValue,
+    tags: ['autodocs'],
+    argTypes: {
+      icon: {
+        table: {
+          disable: true,
+        },
+      },
+    },
+  });
+</script>
+
+<Story name="Default" args={{ value: '1,234', label: 'Downloads', icon: SearchIcon }} />
+
+<Story name="Large Number" args={{ value: '1,234,567', label: 'Total Downloads', icon: SearchIcon }} />
