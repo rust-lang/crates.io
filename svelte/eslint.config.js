b3275a04c5f59b67403b8be593683b025a81598d
Tobias Bieniek <tobias@bieniek.cloud>
2025-12-22 16:16:22 +0100

    ESLint: Exclude `_` prefix from `no-unused-vars` rule

--- svelte/eslint.config.js
+++ svelte/eslint.config.js
@@ -28,6 +28,16 @@ export default defineConfig(
       // typescript-eslint strongly recommend that you do not use the no-undef lint rule on TypeScript projects.
       // see: https://typescript-eslint.io/troubleshooting/faqs/eslint/#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
       'no-undef': 'off',
+
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+        },
+      ],
     },
   },
   {
