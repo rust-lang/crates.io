d2b8068288d696d4ae5c96ba24e449d4fcd04628
Tobias Bieniek <tobias@bieniek.cloud>
2025-12-27 07:58:29 +0100

    tests/routes/support: Remove `pristine-query` mock tests
    AFAICT these tests don't check the actual behavior of our app, but instead override the behavior of the app and then test that mocked behavior.

--- e2e/routes/support.spec.ts
+++ e2e/routes/support.spec.ts
@@ -35,33 +35,4 @@ test.describe('Route | support', { tag: '@routes' }, () => {
     await page.getByTestId('link-crate-violation').click();
     await expect(page).toHaveURL('/support?inquire=crate-violation');
   });
-
-  test('LinkTo support must overwirte query', async ({ page, ember }) => {
-    await ember.addHook(async owner => {
-      const { Service } = owner.lookup('service:testing');
-      // query params of LinkTo support's in footer will not be cleared
-      class MockService extends Service {
-        paramsFor() {
-          return {};
-        }
-      }
-      owner.register('service:pristine-query', MockService);
-    });
-    await page.goto('/support?inquire=crate-violation');
-    await expect(page).toHaveURL('/support?inquire=crate-violation');
-    let section = page.getByTestId('support-main-content').locator('section');
-    await expect(section).toHaveCount(1);
-    await expect(section).toHaveAttribute('data-test-id', 'crate-violation-section');
-    // without overwriting, link in footer will contain the query params in support route
-    let link = page.locator('footer').getByRole('link', { name: 'Support', exact: true });
-    await expect(link).not.toHaveAttribute('href', '/support');
-    await expect(link).toHaveAttribute('href', '/support?inquire=crate-violation');
-
-    // back to index
-    await page.locator('header [href="/"]').click();
-    await expect(page).toHaveURL('/');
-    link = page.locator('footer').getByRole('link', { name: 'Support', exact: true });
-    await expect(link).toBeVisible();
-    await expect(link).toHaveAttribute('href', '/support');
-  });
 });
