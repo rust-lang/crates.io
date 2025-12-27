7bf87f84b47334f7140d0fde3f73c72a8cb96dc2
Tobias Bieniek <tobias@bieniek.cloud>
2025-12-27 16:56:40 +0100

    Replace `pristine-query` service with const import (#12608)

--- app/services/pristine-query.js
+++
@@ -1,9 +0,0 @@
-import { getOwner } from '@ember/owner';
-import Service from '@ember/service';
-
-export default class PristineParamsService extends Service {
-  paramsFor(route) {
-    let params = getOwner(this).lookup(`controller:${route}`)?.queryParams || [];
-    return Object.fromEntries(params.map(k => [k, null]));
-  }
-}
