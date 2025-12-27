b2a2b3355e1d9256a28e4bcf38be87c911f5068c
Tobias Bieniek <tobias@bieniek.cloud>
2025-12-27 15:09:14 +0100

    svelte: Add `ResultsCount` component

---
+++ svelte/src/lib/components/ResultsCount.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import type { HTMLAttributes } from 'svelte/elements';
+
+  interface Props extends HTMLAttributes<HTMLSpanElement> {
+    start: number;
+    end: number;
+    total: number;
+    name?: string;
+  }
+
+  let { start, end, total, name, ...restProps }: Props = $props();
+</script>
+
+<span class="text--small" {...restProps}>
+  Displaying
+  <span class="highlight" data-test-current-rows>{start}-{end}</span>
+  of
+  <span class="highlight" data-test-total-rows>{total}</span>
+  {name ?? 'total results'}
+</span>
+
+<style>
+  .highlight {
+    color: var(--main-color);
+    font-weight: bold;
+  }
+</style>
