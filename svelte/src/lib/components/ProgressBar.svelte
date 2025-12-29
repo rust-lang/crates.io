1932187177bcf9e0a3a497e68abee13e2ec12210
Tobias Bieniek <tobias@bieniek.cloud>
2025-12-29 16:39:10 +0100

    svelte: Implement `ProgressBar` component and corresponding context class
    # Conflicts:
    #	svelte/src/routes/+layout.svelte
    
    # Conflicts:
    #	svelte/src/routes/+layout.svelte

---
+++ svelte/src/lib/components/ProgressBar.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { getProgressContext } from '$lib/progress.svelte';
+
+  let progress = getProgressContext();
+</script>
+
+<div class="progress-bar" style={progress.style}></div>
+
+<style>
+  .progress-bar {
+    position: fixed;
+    left: 0;
+    top: 0;
+    height: 3px;
+    box-shadow: 0 0 10px rgba(0, 13, 41, 0.6);
+    background: var(--yellow500);
+  }
+</style>
