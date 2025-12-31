98834ab62f93ca1eb73982612f406a0714e41430
Tobias Bieniek <tobias@bieniek.cloud>
2025-12-31 04:08:44 +0100

    svelte: Add `support/CrateReportForm` component

---
+++ svelte/src/lib/components/support/CrateReportForm.stories.svelte
@@ -0,0 +1,18 @@
+<script module>
+  import { defineMeta } from '@storybook/addon-svelte-csf';
+
+  import CrateReportForm from './CrateReportForm.svelte';
+
+  const { Story } = defineMeta({
+    title: 'support/CrateReportForm',
+    component: CrateReportForm,
+    tags: ['autodocs'],
+    argTypes: {
+      crate: {
+        control: 'text',
+      },
+    },
+  });
+</script>
+
+<Story name="Default" />
