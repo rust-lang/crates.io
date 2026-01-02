9237ac16ba3317e36e2cd606af9659be244e8dad
Tobias Bieniek <tobias@bieniek.cloud>
2026-01-02 12:42:53 +0100

    svelte: Add `NavTabs` components

---
+++ svelte/src/lib/components/nav-tabs/NavTabs.stories.svelte
@@ -0,0 +1,23 @@
+<script module>
+  import { defineMeta } from '@storybook/addon-svelte-csf';
+
+  import * as NavTabs from './index';
+
+  const { Story } = defineMeta({
+    title: 'NavTabs',
+    component: NavTabs.Root,
+    tags: ['autodocs'],
+    argTypes: {
+      children: { table: { disable: true } },
+    },
+  });
+</script>
+
+<Story name="Default" asChild>
+  <NavTabs.Root>
+    <NavTabs.Tab href="#readme" active={false}>Readme</NavTabs.Tab>
+    <NavTabs.Tab href="#versions" active={true}>Versions</NavTabs.Tab>
+    <NavTabs.Tab href="#dependencies" active={false}>Dependencies</NavTabs.Tab>
+    <NavTabs.Tab href="#dependents" active={false}>Dependents</NavTabs.Tab>
+  </NavTabs.Root>
+</Story>
---
+++ svelte/src/lib/components/nav-tabs/Tab.svelte
@@ -0,0 +1,79 @@
+<script lang="ts">
+  import type { Snippet } from 'svelte';
+  import type { HTMLAttributes } from 'svelte/elements';
+
+  import { page } from '$app/state';
+
+  interface Props extends HTMLAttributes<HTMLLIElement> {
+    href: string;
+    active?: boolean;
+    children: Snippet;
+  }
+
+  let { href, active, children, ...restProps }: Props = $props();
+
+  let isActive = $derived(active ?? page.url.pathname === href);
+</script>
+
+<li {...restProps}>
+  <!-- eslint-disable-next-line svelte/no-navigation-without-resolve -->
+  <a {href} class="link" class:active={isActive} data-test-active={isActive}>
+    {@render children()}
+  </a>
+</li>
+
+<style>
+  .link {
+    display: block;
+    padding: calc(var(--nav-tabs-padding-v) + var(--nav-tabs-border-width)) var(--nav-tabs-padding-h)
+      var(--nav-tabs-padding-v);
+    color: var(--main-color);
+    border-top-left-radius: var(--nav-tabs-radius);
+    border-top-right-radius: var(--nav-tabs-radius);
+    border-bottom: var(--nav-tabs-border-width) solid transparent;
+    margin-bottom: calc(0px - var(--nav-tabs-border-width));
+    transition:
+      color var(--transition-medium),
+      border-bottom-color var(--transition-medium);
+
+    &.active {
+      color: var(--link-hover-color);
+      border-bottom-color: var(--link-hover-color);
+      background: var(--main-bg-dark);
+    }
+
+    &:hover {
+      color: var(--link-hover-color);
+      border-bottom-color: var(--link-hover-color);
+      transition:
+        color var(--transition-instant),
+        border-bottom-color var(--transition-instant);
+    }
+
+    &:focus-visible {
+      outline: none;
+      margin: -3px;
+      border: 3px solid var(--yellow500);
+      position: relative;
+      transition: border-bottom-color var(--transition-instant);
+      z-index: 1;
+    }
+
+    @media only screen and (max-width: 550px) {
+      padding: var(--nav-tabs-padding-v) var(--nav-tabs-padding-h) var(--nav-tabs-padding-v)
+        calc(var(--nav-tabs-padding-h) + var(--nav-tabs-border-width));
+
+      border-top-left-radius: 0;
+      border-bottom-right-radius: var(--nav-tabs-radius);
+      border-bottom: none;
+      border-left: var(--nav-tabs-border-width) solid transparent;
+      margin-bottom: 0;
+      margin-left: calc(0px - var(--nav-tabs-border-width));
+
+      &.active,
+      &:hover {
+        border-left-color: var(--link-hover-color);
+      }
+    }
+  }
+</style>
