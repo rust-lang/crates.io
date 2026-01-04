c22dd5abd0125b6bb02c2d4dd9e233bdd8323e67
Tobias Bieniek <tobias@bieniek.cloud>
2026-01-04 19:12:12 +0100

    svelte: Migrate `SideMenu` components

---
+++ svelte/src/lib/components/side-menu/SideMenu.stories.svelte
@@ -0,0 +1,21 @@
+<script module>
+  import { defineMeta } from '@storybook/addon-svelte-csf';
+
+  import * as SideMenu from './index';
+
+  const { Story } = defineMeta({
+    title: 'SideMenu',
+    component: SideMenu.Root,
+    tags: ['autodocs'],
+    argTypes: {
+      children: { table: { disable: true } },
+    },
+  });
+</script>
+
+<Story name="Default" asChild>
+  <SideMenu.Root>
+    <SideMenu.Item href="#profile" active>Profile</SideMenu.Item>
+    <SideMenu.Item href="#tokens" active={false}>API Tokens</SideMenu.Item>
+  </SideMenu.Root>
+</Story>
