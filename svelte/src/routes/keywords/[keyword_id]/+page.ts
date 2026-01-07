47c6e5d692fb16044eb3a6623cf376cf2855cd8e
Tobias Bieniek <tobias@bieniek.cloud>
2026-01-07 13:51:19 +0100

    svelte: Improve error handling for `/keywords/[keyword_id]` route

--- svelte/src/routes/keywords/[keyword_id]/+page.ts
+++ svelte/src/routes/keywords/[keyword_id]/+page.ts
@@ -1,4 +1,7 @@
+import type { paths } from '@crates-io/api-client';
+
 import { createClient } from '@crates-io/api-client';
+import { error } from '@sveltejs/kit';
 
 const MAX_PAGES = 20;
 
@@ -10,27 +13,32 @@ export async function load({ fetch, url, params }) {
   let perPage = 10;
   let sort = url.searchParams.get('sort') ?? 'recent-downloads';
 
-  let response = await client.GET('/api/v1/crates', {
-    params: {
-      query: {
-        keyword,
-        page,
-        per_page: perPage,
-        sort,
-      },
-    },
-  });
+  let cratesResponse = await loadCrates(client, keyword, { keyword, page, per_page: perPage, sort });
 
-  if (response.error) {
-    throw new Error(`Failed to fetch crates for keyword "${keyword}"`);
+  return { keyword, cratesResponse, page, perPage, sort, maxPages: MAX_PAGES };
+}
+
+function loadCratesError(keyword: string, status: number): never {
+  error(status, { message: `${keyword}: Failed to load crates`, tryAgain: true });
+}
+
+async function loadCrates(
+  client: ReturnType<typeof createClient>,
+  keyword: string,
+  query: paths['/api/v1/crates']['get']['parameters']['query'],
+) {
+  let response;
+  try {
+    response = await client.GET('/api/v1/crates', { params: { query } });
+  } catch (_error) {
+    // Network errors are treated as `504 Gateway Timeout`
+    loadCratesError(keyword, 504);
   }
 
-  return {
-    keyword,
-    crates: response.data,
-    page,
-    perPage,
-    sort,
-    maxPages: MAX_PAGES,
-  };
+  let status = response.response.status;
+  if (response.error) {
+    loadCratesError(keyword, status);
+  }
+
+  return response.data;
 }
