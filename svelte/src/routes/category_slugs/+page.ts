e5dc47bd8c34c771f471bb53d3206102909e11a5
Tobias Bieniek <tobias@bieniek.cloud>
2026-01-07 13:55:16 +0100

    svelte: Improve error handling for `/category_slugs` route

--- svelte/src/routes/category_slugs/+page.ts
+++ svelte/src/routes/category_slugs/+page.ts
@@ -1,15 +1,31 @@
 import { createClient } from '@crates-io/api-client';
+import { error } from '@sveltejs/kit';
 
 export async function load({ fetch }) {
   let client = createClient({ fetch });
 
-  let response = await client.GET('/api/v1/category_slugs');
+  let categorySlugs = await loadCategorySlugs(client);
 
-  if (response.error) {
-    throw new Error('Failed to fetch category slugs');
+  return { categorySlugs };
+}
+
+function loadCategorySlugsError(status: number): never {
+  error(status, { message: 'Failed to load category slugs', tryAgain: true });
+}
+
+async function loadCategorySlugs(client: ReturnType<typeof createClient>) {
+  let response;
+  try {
+    response = await client.GET('/api/v1/category_slugs');
+  } catch (_error) {
+    // Network errors are treated as `504 Gateway Timeout`
+    loadCategorySlugsError(504);
   }
 
-  return {
-    categorySlugs: response.data.category_slugs,
-  };
+  let status = response.response.status;
+  if (response.error) {
+    loadCategorySlugsError(status);
+  }
+
+  return response.data.category_slugs;
 }
