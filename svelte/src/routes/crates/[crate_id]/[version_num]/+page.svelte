ca8b5431f57f067640ea8cc41091b1e1fab699b3
Tobias Bieniek <tobias@bieniek.cloud>
2026-01-07 16:41:46 +0100

    svelte: Add basic data loading for `/crates/[crate_id]/[version_num]` page

--- svelte/src/routes/crates/[crate_id]/[version_num]/+page.svelte
+++ svelte/src/routes/crates/[crate_id]/[version_num]/+page.svelte
@@ -2,5 +2,5 @@
   let { data } = $props();
 </script>
 
-<h1>Crate: {data.crate_id} v{data.version_num}</h1>
+<h1>Crate: {data.crate.name} v{data.version.num}</h1>
 <p>Stub route for /crates/:crate_id/:version_num</p>
--- svelte/src/routes/crates/[crate_id]/[version_num]/+page.ts
+++ svelte/src/routes/crates/[crate_id]/[version_num]/+page.ts
@@ -1,3 +1,38 @@
-export function load({ params }) {
-  return { crate_id: params.crate_id, version_num: params.version_num };
+import { createClient } from '@crates-io/api-client';
+import { error } from '@sveltejs/kit';
+
+export async function load({ fetch, params }) {
+  let client = createClient({ fetch });
+
+  let crateName = params.crate_id;
+  let versionNum = params.version_num;
+
+  let version = await loadVersion(client, crateName, versionNum);
+
+  return { version, requestedVersion: versionNum };
+}
+
+async function loadVersion(client: ReturnType<typeof createClient>, name: string, version: string) {
+  let response;
+  try {
+    response = await client.GET('/api/v1/crates/{name}/{version}', { params: { path: { name, version } } });
+  } catch (_error) {
+    // Network errors are treated as `504 Gateway Timeout`
+    loadVersionError(name, version, 504);
+  }
+
+  let status = response.response.status;
+  if (response.error) {
+    loadVersionError(name, version, status);
+  }
+
+  return response.data.version;
+}
+
+function loadVersionError(name: string, version: string, status: number): never {
+  if (status === 404) {
+    error(404, { message: `${name}: Version ${version} not found` });
+  } else {
+    error(status, { message: `${name}: Failed to load version data`, tryAgain: true });
+  }
 }
