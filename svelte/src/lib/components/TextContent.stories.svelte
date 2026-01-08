6c31ffed77766d42e7c39ba9b7372ad1fd23ec93
Tobias Bieniek <tobias@bieniek.cloud>
2026-01-08 18:43:14 +0100

    svelte: Add `.markdown-alert` styles to `TextContent` component

--- svelte/src/lib/components/TextContent.stories.svelte
+++ svelte/src/lib/components/TextContent.stories.svelte
@@ -19,3 +19,38 @@
   <h2>Boxed Content</h2>
   <p>This content appears in a boxed container with padding and background color.</p>
 </Story>
+
+<Story name="Alerts">
+  <div class="markdown-alert markdown-alert-note">
+    <p class="markdown-alert-title">Note</p>
+    <p>Useful information that users should know, even when skimming content.</p>
+  </div>
+  <div class="markdown-alert markdown-alert-tip">
+    <p class="markdown-alert-title">Tip</p>
+    <p>Helpful advice for doing things better or more easily.</p>
+  </div>
+  <div class="markdown-alert markdown-alert-important">
+    <p class="markdown-alert-title">Important</p>
+    <p>Key information users need to know to achieve their goal.</p>
+  </div>
+  <div class="markdown-alert markdown-alert-warning">
+    <p class="markdown-alert-title">Warning</p>
+    <p>Urgent info that needs immediate user attention to avoid problems.</p>
+  </div>
+  <div class="markdown-alert markdown-alert-caution">
+    <p class="markdown-alert-title">Caution</p>
+    <p>Advises about risks or negative outcomes of certain actions.</p>
+  </div>
+
+  <div class="markdown-alert markdown-alert-note">
+    <p class="markdown-alert-title">Note</p>
+    <div class="markdown-alert markdown-alert-important">
+      <p class="markdown-alert-title">Important</p>
+      <div class="markdown-alert markdown-alert-caution">
+        <p class="markdown-alert-title">Caution</p>
+        <p>Rick roll</p>
+        <p>Never gonna give you up</p>
+      </div>
+    </div>
+  </div>
+</Story>
