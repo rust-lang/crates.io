0509547d4134044cb25d6fe5bda1b97e9773e4d9
Tobias Bieniek <tobias@bieniek.cloud>
2026-01-16 12:27:45 +0100

    svelte: Reduce duplication in `ReadmePlaceholder` component

--- svelte/src/lib/components/ReadmePlaceholder.svelte
+++ svelte/src/lib/components/ReadmePlaceholder.svelte
@@ -1,76 +1,20 @@
 <script lang="ts">
   import Placeholder from '$lib/components/Placeholder.svelte';
+
+  let radius = 'var(--space-3xs)';
+  let text = { width: '100%', height: '16px', opacity: 0.3, style: 'margin-top: var(--space-xs)' };
 </script>
 
 <div>
-  <Placeholder
-    width="30%"
-    height="25px"
-    radius="var(--space-3xs)"
-    opacity={0.6}
-    style="margin: var(--space-s) 0 var(--space-m)"
-  />
-  <Placeholder
-    width="100%"
-    height="16px"
-    radius="var(--space-3xs)"
-    opacity={0.3}
-    style="margin-top: var(--space-xs)"
-  />
-  <Placeholder
-    width="100%"
-    height="16px"
-    radius="var(--space-3xs)"
-    opacity={0.3}
-    style="margin-top: var(--space-xs)"
-  />
-  <Placeholder
-    width="100%"
-    height="16px"
-    radius="var(--space-3xs)"
-    opacity={0.3}
-    style="margin-top: var(--space-xs)"
-  />
-  <Placeholder
-    width="100%"
-    height="16px"
-    radius="var(--space-3xs)"
-    opacity={0.3}
-    style="margin-top: var(--space-xs)"
-  />
-  <Placeholder
-    width="100%"
-    height="16px"
-    radius="var(--space-3xs)"
-    opacity={0.3}
-    style="margin-top: var(--space-xs)"
-  />
-  <Placeholder
-    width="50%"
-    height="20px"
-    radius="var(--space-3xs)"
-    opacity={0.6}
-    style="margin: var(--space-l) 0 var(--space-m);"
-  />
-  <Placeholder
-    width="100%"
-    height="16px"
-    radius="var(--space-3xs)"
-    opacity={0.3}
-    style="margin-top: var(--space-xs)"
-  />
-  <Placeholder
-    width="100%"
-    height="16px"
-    radius="var(--space-3xs)"
-    opacity={0.3}
-    style="margin-top: var(--space-xs)"
-  />
-  <Placeholder
-    width="100%"
-    height="16px"
-    radius="var(--space-3xs)"
-    opacity={0.3}
-    style="margin-top: var(--space-xs)"
-  />
+  <!-- eslint-disable svelte/require-each-key -->
+
+  <Placeholder width="30%" height="25px" {radius} opacity={0.6} style="margin: var(--space-s) 0 var(--space-m)" />
+  {#each { length: 5 } as _}
+    <Placeholder {...text} {radius} />
+  {/each}
+
+  <Placeholder width="50%" height="20px" {radius} opacity={0.6} style="margin: var(--space-l) 0 var(--space-m)" />
+  {#each { length: 3 } as _}
+    <Placeholder {...text} {radius} />
+  {/each}
 </div>
