9221cfb010363c6894b6cc19a78d931ae2f2a47d
Tobias Bieniek <tobias@bieniek.cloud>
2026-01-16 12:22:47 +0100

    svelte: Add `ReadmePlaceholder` story

---
+++ svelte/src/lib/components/ReadmePlaceholder.stories.svelte
@@ -0,0 +1,13 @@
+<script module>
+  import { defineMeta } from '@storybook/addon-svelte-csf';
+
+  import ReadmePlaceholder from './ReadmePlaceholder.svelte';
+
+  const { Story } = defineMeta({
+    title: 'ReadmePlaceholder',
+    component: ReadmePlaceholder,
+    tags: ['autodocs'],
+  });
+</script>
+
+<Story name="Default" />
