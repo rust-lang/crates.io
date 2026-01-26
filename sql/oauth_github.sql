b7dd83101c1249b2f1edb37688417000adf68561
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2026-01-25 21:53:12 -0500

    Add SQL to mass duplicate github info to oauth_github table
    `heroku run` with:
    
    ```
    ./target/release/crates-admin enqueue-job run_sql oauth_github.sql
    ```
    
    To be run at a convenient time (that is, not at startup with the schema
    migrations) via the `run_sql` background job.
    
    There are ~56k user records in the database currently. This SQL will get
    the `oauth_github` table caught up without everyone needing to log in
    (but the code changes will keep the `oauth_github` table in sync with
    the `users` table going forward).

---
+++ sql/oauth_github.sql
@@ -0,0 +1,14 @@
+-- Duplicate GitHub-related user info from the `users` table to the `oauth_github` table. The
+-- `users` table is still the source of truth, but the code should be updating this table as well.
+--
+-- This migration is safe to run multiple times.
+INSERT INTO oauth_github
+(user_id, account_id, encrypted_token, login, avatar)
+SELECT
+  id as user_id,
+  gh_id as account_id,
+  gh_encrypted_token as encrypted_token,
+  gh_login as login,
+  gh_avatar as avatar
+FROM users
+ON CONFLICT DO NOTHING;
