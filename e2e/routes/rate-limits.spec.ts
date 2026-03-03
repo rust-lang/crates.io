efb2b327f2c38a634e41f6e85a67ce6dde583cc7
Tobias Bieniek <tobias@bieniek.cloud>
2026-03-03 11:01:58 +0100

    e2e: Update rate limits test URL

--- e2e/routes/rate-limits.spec.ts
+++ e2e/routes/rate-limits.spec.ts
@@ -2,7 +2,7 @@ import { expect, test } from '@/e2e/helper';
 
 test.describe('Route | rate-limits', { tag: '@routes' }, () => {
   test('has a page title and passes a11y', async ({ page, a11y }) => {
-    await page.goto('/rate-limits');
+    await page.goto('/docs/rate-limits');
     await expect(page.locator('[data-test-page-header] h1')).toHaveText('Publishing Rate Limits');
     await a11y.audit();
   });
