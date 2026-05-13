7642185176e1b891fd4685c69e58d1f36fdcc8b2
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2026-05-13 14:12:50 -0400

    Mark add column with constant default safe
    The diesel-guard message says:
    
    >  Note: For Postgres 11+, this is safe if the default is a constant value.
    
    which it is.
    
    We don't actually need the part where I changed the default after
    adding the column because we set the default explicitly for new rows in
    the code.

--- migrations/2026-05-05-180007-0000_add_last_sync_to_oauth_github/up.sql
+++ migrations/2026-05-05-180007-0000_add_last_sync_to_oauth_github/up.sql
@@ -1,8 +1,8 @@
+-- safety-assured:start
+-- Adding a column with a constant value default is safe because we're using Postgres > 11.
 ALTER TABLE IF EXISTS oauth_github
--- Set existing rows' last sync value to 1970
-ADD COLUMN last_sync timestamptz NOT NULL DEFAULT to_timestamp(0),
--- Set new rows' last sync value to the current time going forward, because creating a new
--- crates.io account fetches the user info from GitHub
-ALTER COLUMN last_sync SET DEFAULT now();
+-- Set existing rows' last sync value to 1970.
+ADD COLUMN last_sync timestamptz NOT NULL DEFAULT to_timestamp(0);
+-- safety-assured:end
 
 comment on column oauth_github.last_sync is 'The last time we verified with GitHub what the GitHub username for this user was, and whether the account was valid';
