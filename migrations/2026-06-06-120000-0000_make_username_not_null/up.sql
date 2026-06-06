addab5b37fbe0db63f3b206f2b23225b98b03b41
Mr Sheerluck <mrsheerluck003@gmail.com>
2026-06-06 23:25:50 +0530

    add saftey assured

--- migrations/2026-06-06-120000-0000_make_username_not_null/up.sql
+++ migrations/2026-06-06-120000-0000_make_username_not_null/up.sql
@@ -1,13 +1,9 @@
+-- safety-assured:start
+-- All rows in the `users` table already have a non-null `username` value after
+-- the admin backfill from `gh_login`. The validation scan that `SET NOT NULL`
+-- performs under ACCESS EXCLUSIVE is expected to be fast.
+alter table users
+    alter column username set not null;
 
-alter table users add constraint username_not_null check (username is not null) not valid;
-
-
-alter table users validate constraint username_not_null;
-
-
-alter table users alter column username set not null;
-
-
-alter table users drop constraint username_not_null;
-
-comment on column users.username is 'Username associated with the user''s crates.io account, independent of linked OAuth usernames. This column is now non-nullable following a data backfill from gh_login.';
+comment on column users.username is 'Username associated with the user''s crates.io account, independent of linked OAuth usernames.';
+-- safety-assured:end
