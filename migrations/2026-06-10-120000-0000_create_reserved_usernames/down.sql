60fe288163d25764d66fed287d8ec8b6322ab61a
Mr Sheerluck <mrsheerluck003@gmail.com>
2026-06-12 08:44:36 +0530

    fix migration and tests

--- migrations/2026-06-10-120000-0000_create_reserved_usernames/down.sql
+++ migrations/2026-06-10-120000-0000_create_reserved_usernames/down.sql
@@ -1,3 +1,6 @@
-DROP TRIGGER trigger_ensure_username_not_reserved ON users;
-DROP FUNCTION ensure_username_not_reserved();
-DROP TABLE reserved_usernames;
+DROP TRIGGER IF EXISTS trigger_ensure_username_not_reserved ON users;
+DROP FUNCTION IF EXISTS ensure_username_not_reserved();
+-- safety-assured:start
+DROP TABLE IF EXISTS reserved_usernames;
+-- safety-assured:end
+DROP FUNCTION IF EXISTS canon_username(text);
--- migrations/2026-06-10-120000-0000_create_reserved_usernames/up.sql
+++ migrations/2026-06-10-120000-0000_create_reserved_usernames/up.sql
@@ -1,12 +1,21 @@
-CREATE TABLE reserved_usernames (
+CREATE FUNCTION canon_username(text) RETURNS text AS $$
+    SELECT replace(lower($1), '-', '_');
+$$ LANGUAGE SQL IMMUTABLE;
+
+-- safety-assured:start
+CREATE TABLE IF NOT EXISTS reserved_usernames (
     username TEXT PRIMARY KEY
 );
-CREATE UNIQUE INDEX ON reserved_usernames (lower(username));
+-- safety-assured:end
+
+-- safety-assured:start
+CREATE UNIQUE INDEX IF NOT EXISTS idx_reserved_usernames_canon_username ON reserved_usernames (canon_username(username));
+-- safety-assured:end
 
 CREATE FUNCTION ensure_username_not_reserved() RETURNS trigger AS $$
 BEGIN
-    IF lower(NEW.username) IN (
-        SELECT lower(username) FROM reserved_usernames
+    IF canon_username(NEW.username) IN (
+        SELECT canon_username(username) FROM reserved_usernames
     ) THEN
         RAISE EXCEPTION 'cannot create user with reserved username';
     END IF;
