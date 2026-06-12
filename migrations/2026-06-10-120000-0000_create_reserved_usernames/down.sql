0f99c36268c9caa524e4904df69c40e91ddbb602
Mr Sheerluck <mrsheerluck003@gmail.com>
2026-06-12 17:47:59 +0530

    update comments

--- migrations/2026-06-10-120000-0000_create_reserved_usernames/down.sql
+++ migrations/2026-06-10-120000-0000_create_reserved_usernames/down.sql
@@ -1,6 +1,9 @@
 DROP TRIGGER IF EXISTS trigger_ensure_username_not_reserved ON users;
 DROP FUNCTION IF EXISTS ensure_username_not_reserved();
 -- safety-assured:start
+-- Suppresses the "DROP TABLE" check. This is a down migration that is only run
+-- manually during development; the table will be dropped before it contains any
+-- significant data.
 DROP TABLE IF EXISTS reserved_usernames;
 -- safety-assured:end
 DROP FUNCTION IF EXISTS canon_username(text);
