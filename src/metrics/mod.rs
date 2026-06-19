d6573eeeae0b48bf3153d3e86b820556a8005c4d
Tobias Bieniek <tobias@bieniek.cloud>
2026-06-19 14:33:47 +0200

    Add `DatadogEncoder` for service metrics

--- src/metrics/mod.rs
+++ src/metrics/mod.rs
@@ -2,6 +2,7 @@ pub use self::instance::InstanceMetrics;
 pub use self::log_encoder::LogEncoder;
 pub use self::service::ServiceMetrics;
 
+pub mod datadog;
 mod instance;
 mod log_encoder;
 mod macros;
