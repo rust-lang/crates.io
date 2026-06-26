f4d2a978839187c988883c70189b268e5d91211f
moskirathe <tech@alphajiri.co>
2026-06-25 23:26:54 -0400

    fix test - no longer need to assert that username is updated when user updates or deletes gh account

--- src/tests/worker/update_user_from_github.rs
+++ src/tests/worker/update_user_from_github.rs
@@ -82,7 +82,6 @@ impl UpdateTest {
         // For now, we want to update the `User` record too
         let user_after_update = User::find(&conn, user_id).await?;
         assert_eq!(user_after_update.gh_login, expected_username);
-        assert_eq!(user_after_update.username, expected_username);
 
         if job_result.is_err() {
             // The worker leaves failed rows in `background_jobs` so they can
