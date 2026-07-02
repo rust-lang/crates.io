60462b1648119fb450250122894d4922c3aa3555
Tobias Bieniek <tobias@bieniek.cloud>
2026-07-02 22:42:56 +0200

    msw: Convert `handlers/teams/` to TypeScript
    This types the `:team_id` path parameter via the
    `http.get<{ ... }>()` generic.

--- packages/crates-io-msw/handlers/teams/get.ts
+++ packages/crates-io-msw/handlers/teams/get.ts
@@ -4,7 +4,7 @@ import { db } from '../../index.js';
 import { serializeTeam } from '../../serializers/team.js';
 import { notFound } from '../../utils/handlers.js';
 
-export default http.get('/api/v1/teams/:team_id', ({ params }) => {
+export default http.get<{ team_id: string }>('/api/v1/teams/:team_id', ({ params }) => {
   let login = params.team_id;
   let team = db.team.findFirst(q => q.where({ login }));
   if (!team) {
