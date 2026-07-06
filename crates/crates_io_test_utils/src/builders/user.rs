fda9676581b621f6198f69f0ae9b33853a68ec04
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2026-07-06 13:02:50 -0400

    Add a with_username method to UserBuilder

--- crates/crates_io_test_utils/src/builders/user.rs
+++ crates/crates_io_test_utils/src/builders/user.rs
@@ -4,19 +4,27 @@ use crates_io_database::models::User;
 /// directly into the database.
 ///
 /// If you want to test logic that happens as part of signing up or logging in,
-pub struct UserBuilder {}
+pub struct UserBuilder<'a> {
+    username: &'a str,
+}
 
-impl UserBuilder {
+impl<'a> UserBuilder<'a> {
     /// Create a new instance of the builder. To get a `User`, call `build` (creates an instance in
     /// memory only) or `insert` (creates a record in the database).
     pub fn new() -> Self {
-        Self {}
+        Self {
+            username: "octocat",
+        }
+    }
+
+    pub fn with_username(self, username: &'a str) -> Self {
+        Self { username }
     }
 
     pub fn build(self) -> User {
         User {
             id: 1,
-            gh_login: "octocat".into(),
+            gh_login: self.username.into(),
             name: Some("The Octocat".into()),
             gh_id: 123,
             gh_avatar: None,
@@ -25,13 +33,13 @@ impl UserBuilder {
             account_lock_until: None,
             is_admin: false,
             publish_notifications: true,
-            username: "octocat".into(),
+            username: self.username.into(),
             created_at: None,
         }
     }
 }
 
-impl Default for UserBuilder {
+impl Default for UserBuilder<'_> {
     fn default() -> Self {
         Self::new()
     }
