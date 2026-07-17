769724c0e139283de35aff04d43dc6bc23f41458
Fabian Bernhart <135550293+fabianbernhart@users.noreply.github.com>
2026-07-17 04:56:34 -0700

    Fix README image distortion by preserving aspect ratio (#14261)

--- e2e/acceptance/readme-rendering.spec.ts
+++ e2e/acceptance/readme-rendering.spec.ts
@@ -115,6 +115,12 @@ impl Stack {
 </li>
 </ol>
 </section>
+
+<h3 align="center">
+  <a>
+    <img width="1000" height="200" alt="Banner with Logo" src="https://static.rerun.io/d0f5443d4803cac65c73fcc064936c09f5e7f208_rerun_banner.png" />
+  </a>
+</h3>
 `;
 
 test.describe('Acceptance | README rendering', { tag: '@acceptance' }, () => {
