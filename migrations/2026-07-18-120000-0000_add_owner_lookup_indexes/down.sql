eb3855922f2e60845e6ca825729100733a6ecb63
moskirathe <tech@alphajiri.co>
2026-07-21 01:31:40 -0400

    fix migration

--- migrations/2026-07-18-120000-0000_add_owner_lookup_indexes/down.sql
+++ migrations/2026-07-18-120000-0000_add_owner_lookup_indexes/down.sql
@@ -1,2 +1,2 @@
-DROP INDEX CONCURRENTLY IF EXISTS index_users_username;
-DROP INDEX CONCURRENTLY IF EXISTS index_oauth_github_login;
+DROP INDEX IF EXISTS index_users_username;
+DROP INDEX IF EXISTS index_oauth_github_login;
--- migrations/2026-07-18-120000-0000_add_owner_lookup_indexes/up.sql
+++ migrations/2026-07-18-120000-0000_add_owner_lookup_indexes/up.sql
@@ -1,2 +1,2 @@
-CREATE INDEX CONCURRENTLY IF NOT EXISTS index_users_username ON users (canon_username(username));
-CREATE INDEX CONCURRENTLY IF NOT EXISTS index_oauth_github_login ON oauth_github (lower(login));
+CREATE INDEX IF NOT EXISTS index_users_username ON users (canon_username(username));
+CREATE INDEX IF NOT EXISTS index_oauth_github_login ON oauth_github (lower(login));
