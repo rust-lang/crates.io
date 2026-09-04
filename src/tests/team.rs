005cec0ac8c2e09dc6311c8a33288142e78529ba
Tobias Bieniek <tobias@bieniek.cloud>
2026-09-04 15:31:14 +0200

    controllers/owners: Support `crates.io:` user invitations
    Owner invitations now accept `crates.io:username` to select an account by
    its canonical crates.io username, independently of its GitHub login.
    
    Co-authored-by: moskirathe <tech@alphajiri.co>

--- src/tests/team.rs
+++ src/tests/team.rs
@@ -59,21 +59,6 @@ async fn weird_name() {
     assert_snapshot!(response.text(), @r#"{"errors":[{"detail":"organization cannot contain special characters like /"}]}"#);
 }
 
-/// Tests adding team without second `:`
-#[tokio::test(flavor = "multi_thread")]
-async fn one_colon() {
-    let (app, _, user, token) = TestApp::full().with_token().await;
-    let mut conn = app.db_conn().await;
-
-    CrateBuilder::new("foo_one_colon", user.as_model().id)
-        .expect_build(&mut conn)
-        .await;
-
-    let response = token.add_named_owner("foo_one_colon", "github:foo").await;
-    assert_snapshot!(response.status(), @"400 Bad Request");
-    assert_snapshot!(response.text(), @r#"{"errors":[{"detail":"prefixed usernames are not supported yet"}]}"#);
-}
-
 #[tokio::test(flavor = "multi_thread")]
 async fn add_nonexistent_team() {
     let (app, _, user, token) = TestApp::full().with_token().await;
