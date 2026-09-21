8ef6cd05a18562f6a6803df68911391fe11d4210
Tobias Bieniek <tobias@bieniek.cloud>
2026-09-21 17:17:17 +0200

    trustpub: Include causes in configuration notification failures

--- src/controllers/trustpub/github_configs/create.rs
+++ src/controllers/trustpub/github_configs/create.rs
@@ -145,7 +145,7 @@ pub async fn create_trustpub_github_config(
         };
 
         if let Err(err) = send_notification_email(&ctx, email_address, context).await {
-            warn!("Failed to send trusted publishing notification to {email_address}: {err}");
+            warn!("Failed to send trusted publishing notification to {email_address}: {err:#}");
         }
     }
 
