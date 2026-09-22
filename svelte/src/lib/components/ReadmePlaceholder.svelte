b2c94c35a541e5e141e257968bdeee1331178cb1
Tobias Bieniek <tobias@bieniek.cloud>
2026-09-23 00:42:42 +0200

    svelte: Use spacing utilities for README placeholders

--- svelte/src/lib/components/ReadmePlaceholder.svelte
+++ svelte/src/lib/components/ReadmePlaceholder.svelte
@@ -2,18 +2,18 @@
   import Placeholder from '$lib/components/Placeholder.svelte';
 
   let radius = 'var(--space-3xs)';
-  let text = { width: '100%', height: '16px', opacity: 0.3, style: 'margin-top: var(--space-xs)' };
+  let text = { width: '100%', height: '16px', opacity: 0.3, class: 'mt-xs' };
 </script>
 
 <div>
   <!-- eslint-disable svelte/require-each-key -->
 
-  <Placeholder width="30%" height="25px" {radius} opacity={0.6} style="margin: var(--space-s) 0 var(--space-m)" />
+  <Placeholder width="30%" height="25px" {radius} opacity={0.6} class="mt-s mb-m" />
   {#each { length: 5 }}
     <Placeholder {...text} {radius} />
   {/each}
 
-  <Placeholder width="50%" height="20px" {radius} opacity={0.6} style="margin: var(--space-l) 0 var(--space-m)" />
+  <Placeholder width="50%" height="20px" {radius} opacity={0.6} class="mt-l mb-m" />
   {#each { length: 3 }}
     <Placeholder {...text} {radius} />
   {/each}
