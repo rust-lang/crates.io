384f35f948d5000df3a5d445f4d72c9fad8f97b3
Tobias Bieniek <tobias@bieniek.cloud>
2026-09-22 09:28:02 +0200

    metrics: Use OTLP endpoint constants
    We might as well use them to avoid typos :)

--- src/config/metrics.rs
+++ src/config/metrics.rs
@@ -1,4 +1,5 @@
 use crates_io_env_vars::var;
+use opentelemetry_otlp::{OTEL_EXPORTER_OTLP_ENDPOINT, OTEL_EXPORTER_OTLP_METRICS_ENDPOINT};
 
 #[derive(Debug, Default)]
 pub struct MetricsConfig {
@@ -11,8 +12,8 @@ pub struct MetricsConfig {
 
 impl MetricsConfig {
     pub fn from_env() -> anyhow::Result<Self> {
-        let otlp_endpoint = var("OTEL_EXPORTER_OTLP_ENDPOINT")?;
-        let metrics_endpoint = var("OTEL_EXPORTER_OTLP_METRICS_ENDPOINT")?;
+        let otlp_endpoint = var(OTEL_EXPORTER_OTLP_ENDPOINT)?;
+        let metrics_endpoint = var(OTEL_EXPORTER_OTLP_METRICS_ENDPOINT)?;
         let otlp_enabled = otlp_endpoint.is_some() || metrics_endpoint.is_some();
 
         Ok(Self { otlp_enabled })
